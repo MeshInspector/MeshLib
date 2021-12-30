22c23881d053d21d26ab43533bb885babc412a20
Grant Karapetyan <GKarapetyan@adalisk.com>
2021-12-30 19:27:54 +0300

    distribution_update (#1)
    * distribution_update
    
    * remove tokens
    
    * fix distribution scripts paths
    
    * return needed tokens
    
    * add install thirdparty in make deb steps
    
    * fixes
    
    * restore debug lines

--- scripts/distribution.sh
+++ scripts/distribution.sh
@@ -7,11 +7,6 @@
 # exit if any command failed
 set -eo pipefail
 
-# copy prebuilded libs if they exist
-if [ -d "/usr/local/lib/meshrus-thirdparty-lib/lib" ]; then
- cp -r /usr/local/lib/meshrus-thirdparty-lib/lib .
-fi
-
 if [ ! -f "./lib/libcpr.so" ]; then
  printf "Thirdparty build was not found. Building...\n"
  ./scripts/build_thirdparty.sh
@@ -37,11 +32,11 @@ MR_INSTALL_THIRDPARTY_INCLUDE_DIR="/usr/local/include/"
 MR_INSTALL_INCLUDE_DIR="/usr/local/include/MeshLib/"
 PYTHON_DIR="/usr/lib/python3"
 
-mkdir -p distr/meshrus-dev/DEBIAN
-mkdir -p "distr/meshrus-dev${MR_INSTALL_LIB_DIR}"
-mkdir -p "distr/meshrus-dev${MR_INSTALL_RES_DIR}"
-mkdir -p "distr/meshrus-dev${MR_INSTALL_INCLUDE_DIR}"
-mkdir -p "distr/meshrus-dev${MR_INSTALL_THIRDPARTY_INCLUDE_DIR}"
+mkdir -p distr/meshlib-dev/DEBIAN
+mkdir -p "distr/meshlib-dev${MR_INSTALL_LIB_DIR}"
+mkdir -p "distr/meshlib-dev${MR_INSTALL_RES_DIR}"
+mkdir -p "distr/meshlib-dev${MR_INSTALL_INCLUDE_DIR}"
+mkdir -p "distr/meshlib-dev${MR_INSTALL_THIRDPARTY_INCLUDE_DIR}"
 
 if [ ${1} ]; then
   echo ${1:1} > build/Release/bin/mr.version
@@ -50,8 +45,8 @@ else
 fi
 
 #create control file
-CONTROL_FILE="./distr/meshrus-dev/DEBIAN/control"
-echo "Package: meshrus-dev" > "$CONTROL_FILE"
+CONTROL_FILE="./distr/meshlib-dev/DEBIAN/control"
+echo "Package: meshlib-dev" > "$CONTROL_FILE"
 echo "Essential: no" >> "$CONTROL_FILE"
 echo "Priority: optional" >> "$CONTROL_FILE"
 echo "Section: model" >> "$CONTROL_FILE"
@@ -74,48 +69,45 @@ do
 done
 echo "${DEPENDS_LINE}" >> "$CONTROL_FILE"
 
-cp "./scripts/preinstall_trick.sh" ./distr/meshrus-dev/DEBIAN/preinst
-chmod +x ./distr/meshrus-dev/DEBIAN/preinst
+cp "./scripts/preinstall_trick.sh" ./distr/meshlib-dev/DEBIAN/preinst
+chmod +x ./distr/meshlib-dev/DEBIAN/preinst
 
-cp "./scripts/postinstall.sh" ./distr/meshrus-dev/DEBIAN/postinst
-chmod +x ./distr/meshrus-dev/DEBIAN/postinst
+cp "./scripts/postinstall.sh" ./distr/meshlib-dev/DEBIAN/postinst
+chmod +x ./distr/meshlib-dev/DEBIAN/postinst
 
 #copy lib dir
 CURRENT_DIR="`pwd`"
 cd "${MR_LIB_DIR}"
-find . -name '*.so*' -type f,l -exec cp -fP \{\} "${CURRENT_DIR}/distr/meshrus-dev${MR_INSTALL_LIB_DIR}" \;
+find . -name '*.so*' -type f,l -exec cp -fP \{\} "${CURRENT_DIR}/distr/meshlib-dev${MR_INSTALL_LIB_DIR}" \;
 cd -
 printf "Thirdparty libs copy done\n"
 
 #copy libs
-cp -r build/Release/bin/*.so "distr/meshrus-dev${MR_INSTALL_LIB_DIR}"
+cp -r build/Release/bin/*.so "distr/meshlib-dev${MR_INSTALL_LIB_DIR}"
 printf "MR libs copy done\n"
 
 #copy verison file
-cp build/Release/bin/mr.version "distr/meshrus-dev${MR_INSTALL_RES_DIR}"
+cp build/Release/bin/mr.version "distr/meshlib-dev${MR_INSTALL_RES_DIR}"
 printf "MR version copy done\n"
 
 #copy headers
 cd "${MR_LIB_DIR}"
-find . -name '*.h' -type f -exec cp -f --recursive --parents \{\} "${CURRENT_DIR}/distr/meshrus-dev${MR_INSTALL_THIRDPARTY_INCLUDE_DIR}" \;
+find . -name '*.h' -type f -exec cp -f --recursive --parents \{\} "${CURRENT_DIR}/distr/meshlib-dev${MR_INSTALL_THIRDPARTY_INCLUDE_DIR}" \;
 cd -
 cd thirdparty/eigen
-find . -name '*.h' -type f -exec cp -f --recursive --parents \{\} "${CURRENT_DIR}/distr/meshrus-dev${MR_INSTALL_THIRDPARTY_INCLUDE_DIR}" \;
+find . -name '*.h' -type f -exec cp -f --recursive --parents \{\} "${CURRENT_DIR}/distr/meshlib-dev${MR_INSTALL_THIRDPARTY_INCLUDE_DIR}" \;
 cd -
 cd source
-find . -name '*.h' -type f -exec cp -f --recursive --parents \{\} "${CURRENT_DIR}/distr/meshrus-dev${MR_INSTALL_INCLUDE_DIR}" \;
+find . -name '*.h' -type f -exec cp -f --recursive --parents \{\} "${CURRENT_DIR}/distr/meshlib-dev${MR_INSTALL_INCLUDE_DIR}" \;
 cd -
 printf "Headers copy done\n"
 
 #call dpkg
 cd distr
-dpkg-deb --build ./meshrus-dev
+dpkg-deb --build ./meshlib-dev
 
-if [ -f "./meshrus-dev.deb" ]; then
+if [ -f "./meshlib-dev.deb" ]; then
  printf "Dev deb package has been built.\n"
- if [ ${1} ]; then
-  cp ./meshrus-dev.deb "${1}"
- fi
 else
  printf "Failed to build dev.deb package!"
  exit 8

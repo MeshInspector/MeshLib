079b024e7497a2d1970dd6543fd833e5cbe0b87c
Maxim Rayskiy <mraiskii@adalisk.com>
2022-01-12 18:10:51 +0300

    fix relative path

--- thirdparty/install.bat
+++ thirdparty/install.bat
@@ -1,2 +1,2 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows-meshrus
-vcpkg install "@../requirements/windows.txt" --recurse --binarysource=clear
+vcpkg install "@%~dp0\..\requirements\windows.txt" --recurse --binarysource=clear

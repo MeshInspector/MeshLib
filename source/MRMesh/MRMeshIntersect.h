7a117c9e5901bbab3a43b3896607e518f7091c7b
Fedr <fchelnokov@adalisk.com>
2022-01-13 13:10:42 +0300

    rayMeshIntersect can find any intersection for better performance; rayMultiMeshAnyIntersect added (#4)

--- source/MRMesh/MRMeshIntersect.h
+++ source/MRMesh/MRMeshIntersect.h
@@ -20,12 +20,37 @@ struct MeshIntersectionResult
     float distanceAlongLine = 0;
 };
 
-// Intersects ray with mesh. Finds the closest to origin intersection
+// Intersects ray with mesh. Finds the closest to origin intersection (or any intersection for better performance if !closestIntersect)
 MRMESH_API std::optional<MeshIntersectionResult> rayMeshIntersect( const MeshPart& meshPart, const Line3f& line,
-    float rayStart = 0.0f, float rayEnd = FLT_MAX, const IntersectionPrecomputes<float>* prec = nullptr );
+    float rayStart = 0.0f, float rayEnd = FLT_MAX, const IntersectionPrecomputes<float>* prec = nullptr, bool closestIntersect = true );
 // Use double precision
 MRMESH_API std::optional<MeshIntersectionResult> rayMeshIntersect( const MeshPart& meshPart, const Line3d& line,
-    double rayStart = 0.0, double rayEnd = DBL_MAX, const IntersectionPrecomputes<double>* prec = nullptr );
+    double rayStart = 0.0, double rayEnd = DBL_MAX, const IntersectionPrecomputes<double>* prec = nullptr, bool closestIntersect = true );
+
+struct MultiMeshIntersectionResult : MeshIntersectionResult
+{
+    // the intersection found in this mesh
+    const Mesh * mesh = nullptr;
+};
+
+template<typename T>
+struct Line3Mesh
+{
+    Line3<T> line; // in the reference frame of mesh
+    IntersectionPrecomputes<T> prec;
+    const Mesh * mesh = nullptr; // must be set a valid pointer before use
+    const AABBTree * tree = nullptr; // must be set a valid pointer before use
+    const FaceBitSet * region = nullptr; // may remain nullptr, meaning all mesh
+};
+using Line3fMesh = Line3Mesh<float>;
+using Line3dMesh = Line3Mesh<double>;
+
+// Intersects ray with many meshes. Finds any intersection (not the closest)
+MRMESH_API std::optional<MultiMeshIntersectionResult> rayMultiMeshAnyIntersect( const std::vector<Line3fMesh> & lineMeshes,
+    float rayStart = 0.0f, float rayEnd = FLT_MAX );
+// Use double precision
+MRMESH_API std::optional<MultiMeshIntersectionResult> rayMultiMeshAnyIntersect( const std::vector<Line3dMesh> & lineMeshes,
+    double rayStart = 0.0, double rayEnd = DBL_MAX );
 
 // this callback is envoked for each encountered ray-mesh intersection;
 // if it returns false, then the search immediately terminates

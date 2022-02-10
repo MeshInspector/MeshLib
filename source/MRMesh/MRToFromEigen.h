b79b0b8c85f3e8e2df23032679a9985e850ca2e8
Fedr <fchelnokov@adalisk.com>
2022-02-10 15:40:32 +0300

    Eigen <-> Matrix2/3

--- source/MRMesh/MRToFromEigen.h
+++ source/MRMesh/MRToFromEigen.h
@@ -6,6 +6,8 @@
 #include "MRVector4.h"
 #include "MRSymMatrix2.h"
 #include "MRSymMatrix3.h"
+#include "MRMatrix2.h"
+#include "MRMatrix3.h"
 
 // unknown pragmas
 #pragma warning(disable:4068)
@@ -30,12 +32,30 @@ template <typename T>
 }
 
 template <typename T>
-static Eigen::Matrix<T, 2, 2> toEigen( const SymMatrix2<T> & m )
+[[nodiscard]] inline Eigen::Matrix<T, 2, 2> toEigen( const SymMatrix2<T> & m )
 {
     Eigen::Matrix<T, 2, 2> res;
     res << m.xx, m.xy,
            m.xy, m.yy;
     return res;
+}
+
+template <typename T>
+[[nodiscard]] inline Eigen::Matrix<T, 2, 2> toEigen( const Matrix2<T> & m )
+{
+    Eigen::Matrix<T, 2, 2> res;
+    res << m.x.x, m.x.y,
+           m.y.x, m.y.y;
+    return res;
+}
+
+template <typename T>
+[[nodiscard]] inline Matrix2<T> fromEigen( const Eigen::Matrix<T, 2, 2> & m )
+{
+    return Matrix2<T> {
+        { m( 0, 0 ), m( 0, 1 ) },
+        { m( 1, 0 ), m( 1, 1 ) }
+    };
 }
 
 template <typename T>
@@ -58,6 +78,26 @@ template <typename T>
            m.xy, m.yy, m.yz,
            m.xz, m.yz, m.zz;
     return res;
+}
+
+template <typename T>
+[[nodiscard]] inline Eigen::Matrix<T, 3, 3> toEigen( const Matrix3<T> & m )
+{
+    Eigen::Matrix<T, 3, 3> res;
+    res << m.x.x, m.x.y, m.x.z,
+           m.y.x, m.y.y, m.y.z,
+           m.z.x, m.z.y, m.z.z;
+    return res;
+}
+
+template <typename T>
+[[nodiscard]] inline Matrix3<T> fromEigen( const Eigen::Matrix<T, 3, 3> & m )
+{
+    return Matrix3<T> {
+        { m( 0, 0 ), m( 0, 1 ), m( 0, 2 ) },
+        { m( 1, 0 ), m( 1, 1 ), m( 1, 2 ) },
+        { m( 2, 0 ), m( 2, 1 ), m( 2, 2 ) }
+    };
 }
 
 template <typename T>

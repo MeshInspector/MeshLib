f9c3ec1479d7ec3743c594ced502f1cc1e968ec7
Grant Karapetyan <gkarapetyan@adalisk.com>
2022-02-21 18:56:16 +0300

    Introduce SceneSettings

---
+++ source/MRMesh/MRSceneSettings.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "MRMeshFwd.h"
+#include <array>
+
+namespace MR
+{
+
+// This singleton struct contain default settings for scene objects
+class SceneSettings
+{
+public:
+
+    enum Type
+    {
+        MeshFlatShading,
+        Count
+    };
+
+    MRMESH_API static bool get( Type type );
+    MRMESH_API static void set( Type type, bool value );
+private:
+    SceneSettings() = default;
+    ~SceneSettings() = default;
+
+    static SceneSettings& instance_();
+
+    std::array<bool, size_t( Type::Count ) > settings_{ false };
+};
+
+}

056187e235bb79221d499670d328b96bf4199b83
Fedr <fchelnokov@adalisk.com>
2022-02-21 19:01:11 +0300

    python bindings

--- source/mrmeshpy/MRPythonIO.cpp
+++ source/mrmeshpy/MRPythonIO.cpp
@@ -3,10 +3,13 @@
 #include "MRMesh/MRSceneRoot.h"
 #include "MRMesh/MRObjectMesh.h"
 #include "MRMesh/MRObjectVoxels.h"
+#include "MRMesh/MRPolyline.h"
 #include "MRMesh/MRObjectPoints.h"
 #include "MRMesh/MRMesh.h"
 #include "MRMesh/MRMeshSave.h"
 #include "MRMesh/MRVoxelsSave.h"
+#include "MRMesh/MRLinesSave.h"
+#include "MRMesh/MRLinesLoad.h"
 #include "MRMesh/MRPointsSave.h"
 #include "MRMesh/MRPointsLoad.h"
 #include "MRMesh/MRObjectLoad.h"
@@ -30,6 +33,20 @@ Mesh pythonLoadMeshFromAnyFormat( const std::string& path )
     return {};
 }
 
+bool pythonSaveLinesToAnyFormat( const Polyline& lines, const std::string& path )
+{
+    auto res = MR::LinesSave::toAnySupportedFormat( lines, path );
+    return res.has_value();
+}
+
+MR::Polyline pythonLoadLinesFromAnyFormat( const std::string& path )
+{
+    auto res = MR::LinesLoad::fromAnySupportedFormat( path );
+    if ( res.has_value() )
+        return *res;
+    return {};
+}
+
 bool pythonSavePointCloudToAnyFormat( const PointCloud& points, const std::string& path )
 {
     auto res = MR::PointsSave::toAnySupportedFormat( points, path );
@@ -46,5 +63,7 @@ PointCloud pythonLoadPointCloudFromAnyFormat( const std::string& path )
 
 MR_ADD_PYTHON_FUNCTION( mrmeshpy, save_mesh, pythonSaveMeshToAnyFormat, "saves mesh in file of known format/extention" )
 MR_ADD_PYTHON_FUNCTION( mrmeshpy, load_mesh, pythonLoadMeshFromAnyFormat, "load mesh of known format" )
+MR_ADD_PYTHON_FUNCTION( mrmeshpy, save_lines, pythonSaveLinesToAnyFormat, "saves lines in file of known format/extention" )
+MR_ADD_PYTHON_FUNCTION( mrmeshpy, load_lines, pythonLoadLinesFromAnyFormat, "load lines of known format" )
 MR_ADD_PYTHON_FUNCTION( mrmeshpy, save_points, pythonSavePointCloudToAnyFormat, "saves point cloud in file of known format/extention" )
 MR_ADD_PYTHON_FUNCTION( mrmeshpy, load_points, pythonLoadPointCloudFromAnyFormat, "load point cloud of known format" )

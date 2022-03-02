d6902bbf896ca7884dd2e451fdd8874866313f7a
Fedr <fchelnokov@adalisk.com>
2022-03-02 22:45:37 +0300

    project initial cube on sphere

--- source/MRMesh/MRSphere.cpp
+++ source/MRMesh/MRSphere.cpp
@@ -3,7 +3,7 @@
 #include "MRMesh.h"
 #include "MRMeshSubdivide.h"
 #include "MRTimer.h"
-#include <climits>
+#include <cmath>
 
 namespace MR
 {
@@ -11,15 +11,28 @@ namespace MR
 Mesh makeSphere( const SphereParams & params )
 {
     MR_TIMER
-    auto mesh = makeCube( Vector3f::diagonal( params.radius ), Vector3f::diagonal( -0.5f * params.radius ) );
+    auto mesh = makeCube();
+
+    auto projectOnSphere = [radius = params.radius]( const Vector3f & v )
+    { 
+        return radius * v.normalized();
+    };
+    for ( auto & p : mesh.points )
+        p = projectOnSphere( p );
 
     SubdivideSettings ss;
     ss.maxEdgeLen = params.maxEdgeLen;
-    ss.maxEdgeSplits = INT_MAX;
+
+    // sphere area is 4*pi*radius^2
+    // min triangle area 1/2*(1/4*maxEdgeLen^2)
+    // every split create 2 triangles
+    // maxSplits = 4*pi*radius^2 / (1/4*maxEdgeLen^2), but we put a larger limit
+    ss.maxEdgeSplits = (int)std::lround( sqr( 15 * params.radius / params.maxEdgeLen ) );
+
     ss.maxDeviationAfterFlip = params.radius;
     ss.onVertCreated = [&]( VertId vid )
     { 
-        mesh.points[vid] = params.radius * mesh.points[vid].normalized();
+        mesh.points[vid] = projectOnSphere( mesh.points[vid] );
     };
 
     subdivideMesh( mesh, ss );

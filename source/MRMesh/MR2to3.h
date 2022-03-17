0f4e95be968677706706930ded26769e308c02c2
Fedr <fchelnokov@adalisk.com>
2022-03-17 11:45:02 +0300

    Conversion functions to2dim and to3dim (#80)

---
+++ source/MRMesh/MR2to3.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "MRVector2.h"
+#include "MRVector3.h"
+#include "MRMatrix2.h"
+#include "MRMatrix3.h"
+#include "MRAffineXf.h"
+
+namespace MR
+{
+
+// Conversion functions from lower-dimensional data to higher-dimensional put 0 in the additional dimensions
+// Conversion functions from higher-dimensional data to lower-dimensional just omit additional dimensions
+
+template <typename T>
+[[nodiscard]] inline Vector3<T> to3dim( const Vector2<T> & v )
+{
+    return { v.x, v.y, T{0} };
+}
+
+template <typename T>
+[[nodiscard]] inline Vector2<T> to2dim( const Vector3<T> & v )
+{
+    return { v.x, v.y };
+}
+
+template <typename T>
+[[nodiscard]] inline Matrix3<T> to3dim( const Matrix2<T> & m )
+{
+    return { to3dim( m.x ), to3dim( m.y ), Vector3<T>{} };
+}
+
+template <typename T>
+[[nodiscard]] inline Matrix2<T> to2dim( const Matrix3<T> & m )
+{
+    return { to2dim( m.x ), to2dim( m.y ) };
+}
+
+template <typename T>
+[[nodiscard]] inline AffineXf3<T> to3dim( const AffineXf2<T> & xf )
+{
+    return { to3dim( xf.A ), to3dim( xf.b ) };
+}
+
+template <typename T>
+[[nodiscard]] inline AffineXf2<T> to2dim( const AffineXf3<T> & xf )
+{
+    return { to2dim( xf.A ), to2dim( xf.b ) };
+}
+
+} //namespace MR

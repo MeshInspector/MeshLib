d63923ea087a5a4c7a0d2224e96c9cab6aceb1db
Fedr <fchelnokov@adalisk.com>
2022-03-21 15:43:46 +0300

    Polyline template with Polyline2 and Polyline3 instances (#85)

--- source/MRMesh/MRComputeBoundingBox.h
+++ source/MRMesh/MRComputeBoundingBox.h
@@ -7,6 +7,7 @@ namespace MR
 
 // passes through all region points and finds the minimal bounding box containing all of them;
 // if toWorld transformation is given then returns minimal bounding box in world space
-MRMESH_API Box3f computeBoundingBox( const VertCoords & points, const VertBitSet & region, const AffineXf3f * toWorld = nullptr );
+template<typename V>
+MRMESH_API Box<V> computeBoundingBox( const Vector<V, VertId> & points, const VertBitSet & region, const AffineXf<V> * toWorld = nullptr );
 
 } //namespace MR
--- source/MRMesh/MRPolyline.h
+++ source/MRMesh/MRPolyline.h
@@ -6,39 +6,40 @@
 namespace MR
 {
 
-// 3 dimensional polyline
-struct Polyline3
+// polyline that stores points of type V
+template<typename V>
+struct Polyline
 {
 public:
     PolylineTopology topology;
-    VertCoords points;
+    Vector<V, VertId> points;
 
-    Polyline3() = default;
+    Polyline() = default;
 
-    // creates polyline from 2d contours (each point Z coord = 0)
-    MRMESH_API Polyline3( const Contours2f& contours );
+    // creates polyline from 2d contours
+    MRMESH_API Polyline( const Contours2f& contours );
 
     // adds connected line in this, passing progressively via points *[vs, vs+num);
     // if closed argument is true then the last and the first points will be additionally connected;
     // return the edge from first new to second new vertex    
-    MRMESH_API EdgeId addFromPoints( const Vector3f * vs, size_t num, bool closed );
-    [[deprecated]] EdgeId makePolyline( const Vector3f * vs, size_t num, bool closed ) { return addFromPoints( vs, num, closed ); }
+    MRMESH_API EdgeId addFromPoints( const V * vs, size_t num, bool closed );
+    [[deprecated]] EdgeId makePolyline( const V * vs, size_t num, bool closed ) { return addFromPoints( vs, num, closed ); }
 
     // adds connected line in this, passing progressively via points *[vs, vs+num);
     // if vs[0] == vs[num-1] then a closed line is created;
     // return the edge from first new to second new vertex
-    MRMESH_API EdgeId addFromPoints( const Vector3f * vs, size_t num );
-    [[deprecated]] EdgeId makePolyline( const Vector3f * vs, size_t num ) { return addFromPoints( vs, num ); }
+    MRMESH_API EdgeId addFromPoints( const V * vs, size_t num );
+    [[deprecated]] EdgeId makePolyline( const V * vs, size_t num ) { return addFromPoints( vs, num ); }
 
     // returns coordinates of the edge origin
-    Vector3f orgPnt( EdgeId e ) const { return points[ topology.org( e ) ]; }
+    V orgPnt( EdgeId e ) const { return points[ topology.org( e ) ]; }
     // returns coordinates of the edge destination
-    Vector3f destPnt( EdgeId e ) const { return points[ topology.dest( e ) ]; }
+    V destPnt( EdgeId e ) const { return points[ topology.dest( e ) ]; }
     // returns a point on the edge: origin point for f=0 and destination point for f=1
-    Vector3f edgePoint( EdgeId e, float f ) const { return f * destPnt( e ) + ( 1 - f ) * orgPnt( e ); }
+    V edgePoint( EdgeId e, float f ) const { return f * destPnt( e ) + ( 1 - f ) * orgPnt( e ); }
 
     // returns vector equal to edge destination point minus edge origin point
-    Vector3f edgeVector( EdgeId e ) const { return destPnt( e ) - orgPnt( e ); }
+    V edgeVector( EdgeId e ) const { return destPnt( e ) - orgPnt( e ); }
     // returns Euclidean length of the edge
     float edgeLength( EdgeId e ) const { return edgeVector( e ).length(); }
     // returns squared Euclidean length of the edge (faster to compute than length)
@@ -47,25 +48,26 @@ public:
     MRMESH_API float totalLength() const;
 
     // returns cached aabb-tree for this polyline, creating it if it did not exist in a thread-safe manner
-    MRMESH_API const AABBTreePolyline3& getAABBTree() const;
+    MRMESH_API const AABBTreePolyline<V>& getAABBTree() const;
     // returns the minimal bounding box containing all valid vertices (implemented via getAABBTree())
-    MRMESH_API Box3f getBoundingBox() const;
+    MRMESH_API Box<V> getBoundingBox() const;
     // passes through all valid points and finds the minimal bounding box containing all of them;
     // if toWorld transformation is given then returns minimal bounding box in world space
-    MRMESH_API Box3f computeBoundingBox( const AffineXf3f * toWorld = nullptr ) const;
+    MRMESH_API Box<V> computeBoundingBox( const AffineXf<V> * toWorld = nullptr ) const;
 
     // applies given transformation to all valid polyline vertices
-    MRMESH_API void transform( const AffineXf3f & xf );
+    MRMESH_API void transform( const AffineXf<V> & xf );
 
     // Invalidates caches (e.g. aabb-tree) after a change in polyline
     void invalidateCaches() { AABBTreeOwner_.reset(); };
 
-    // convert Polyline3 to simple contour structures with vector of points inside
+    // convert Polyline to simple contour structures with vector of points inside
     // if all even edges are consistently oriented, then the output contours will be oriented the same
     MRMESH_API Contours2f contours() const;
 
-    // convert Polyline3 to Polyline2 with removed Z component
-    MRMESH_API Polyline2 toPolyline2() const;
+    // convert Polyline3 to Polyline2 or vice versa
+    template<typename U>
+    Polyline<U> toPolyline() const;
 
     // adds path to this polyline
     // returns the edge from first new to second new vertex
@@ -75,7 +77,21 @@ public:
     // returns the edge from first new to second new vertex
     MRMESH_API EdgeId addFromSurfacePath( const Mesh& mesh, const SurfacePath& path );
 private:
-    mutable UniqueThreadSafeOwner<AABBTreePolyline3> AABBTreeOwner_;
+    mutable UniqueThreadSafeOwner<AABBTreePolyline<V>> AABBTreeOwner_;
 };
+
+template<typename V>
+template<typename U>
+Polyline<U> Polyline<V>::toPolyline() const
+{
+    Polyline<U> res;
+    res.topology = topology;
+    res.points.reserve( points.size() );
+    for ( size_t i = 0; i < points.size(); i++ )
+    {
+        res.points.push_back( U{ points[VertId( i )] } );
+    }
+    return res;
+}
 
 } //namespace MR

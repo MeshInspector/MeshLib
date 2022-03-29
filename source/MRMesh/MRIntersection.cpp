69026c9d434786478640252f33216b4312d55682
Alexandr Burakov <aburakov@adalisk.com>
2022-03-29 19:21:32 +0300

    reworks and fixes

--- source/MRMesh/MRIntersection.cpp
+++ source/MRMesh/MRIntersection.cpp
@@ -21,13 +21,12 @@ TEST( MRMesh, IntersectPlanePlane )
     ASSERT_FALSE( is1.has_value() );
 
 
-    auto cl0 = closestPoints( plane1, plane2 );
-    ASSERT_FALSE( cl0.has_value() );
+    auto dist0 = distance( plane1, plane2 );
+    ASSERT_FALSE( dist0.has_value() );
 
-    auto cl1 = closestPoints( plane1, plane4 );
-    ASSERT_TRUE( cl1.has_value() );
-    ASSERT_NEAR( ( cl1->a - Vector3d( 1, 0, 0 ) ).length(), 0., 1e-15 );
-    ASSERT_NEAR( ( cl1->b - Vector3d( -1, 0, 0 ) ).length(), 0., 1e-15 );
+    auto dist1 = distance( plane1, plane4 );
+    ASSERT_TRUE( dist1.has_value() );
+    ASSERT_NEAR( *dist1, 2., 1e-15 );
 
 }
 
@@ -39,26 +38,26 @@ TEST(MRMesh, IntersectPlaneLine)
 
     auto is0 = intersection( plane, line );
     ASSERT_TRUE( is0.has_value() );
-    ASSERT_NEAR( ( *is0 - Vector3d{ 1., 1., 1. } ).length(), 0., 1e-15 );
+    ASSERT_NEAR( ( *is0 - Vector3d( 1, 1, 1 ) ).length(), 0., 1e-15 );
 
     auto is1 = intersection( plane, line1 );
     ASSERT_FALSE( is1.has_value() );
 
 
-    auto cl0 = closestPoints( plane, line );
-    ASSERT_FALSE( cl0.has_value() );
+    auto dist0 = distance( plane, line );
+    ASSERT_FALSE( dist0.has_value() );
 
-    auto cl1 = closestPoints( plane, line1 );
-    ASSERT_TRUE( cl1.has_value() );
-    ASSERT_NEAR( ( cl1->a - Vector3d( 1, 0, 0 ) ).length(), 0., 1e-15 );
-    ASSERT_NEAR( ( cl1->b - Vector3d( 0, 0, 0 ) ).length(), 0., 1e-15 );
+    auto dist1 = distance( plane, line1 );
+    ASSERT_TRUE( dist1.has_value() );
+    ASSERT_NEAR( *dist1, 1., 1e-15 );
 }
 
 TEST( MRMesh, IntersectLineLine )
 {
-    Line3d line1( Vector3d( 1, 0, 0. ), Vector3d( 0, 1, 0 ) );
-    Line3d line2( Vector3d( 0, 1, 0. ), Vector3d( 1, 0, 0 ) );
+    Line3d line1( Vector3d( 1, 0, 0 ), Vector3d( 0, 1, 0 ) );
+    Line3d line2( Vector3d( 0, 1, 0 ), Vector3d( 1, 0, 0 ) );
     Line3d line3( Vector3d( 0, 0, 0 ), Vector3d( 0, 0, 1 ) );
+    Line3d line4( Vector3d( 0, 1, 0 ), Vector3d( 0, -1, 0 ) );
 
 
     auto is0 = intersection( line1, line2 );
@@ -68,6 +67,21 @@ TEST( MRMesh, IntersectLineLine )
     auto is1 = intersection( line1, line3 );
     ASSERT_FALSE( is1.has_value() );
 
+    auto is2 = intersection( line1, line4 );
+    ASSERT_FALSE( is2.has_value() );
+
+
+    auto dist0 = distance( line1, line2 );
+    ASSERT_FALSE( dist0.has_value() );
+
+    auto dist1 = distance( line1, line3 );
+    ASSERT_TRUE( dist1.has_value() );
+    ASSERT_NEAR( *dist1, 1., 1e-15 );
+
+    auto dist2 = distance( line1, line4 );
+    ASSERT_TRUE( dist2.has_value() );
+    ASSERT_NEAR( *dist2, 1., 1e-15 );
+
 
     auto cl0 = closestPoints( line1, line2 );
     ASSERT_FALSE( cl0.has_value() );
@@ -76,6 +90,9 @@ TEST( MRMesh, IntersectLineLine )
     ASSERT_TRUE( cl1.has_value() );
     ASSERT_NEAR( ( cl1->a - Vector3d( 1, 0, 0 ) ).length(), 0., 1e-15 );
     ASSERT_NEAR( ( cl1->b - Vector3d( 0, 0, 0 ) ).length(), 0., 1e-15 );
+
+    auto cl2 = closestPoints( line1, line4 );
+    ASSERT_FALSE( cl2.has_value() );
 }
 
 } //namespace MR
--- source/MRMesh/MRIntersection.h
+++ source/MRMesh/MRIntersection.h
@@ -43,7 +43,7 @@ std::optional<Vector3<T>> intersection( const Plane3<T>& plane, const Line3<T>& 
 }
 
 // finds an intersection between a line1 and a line2
-// plane and line should be normalized for check parallelism
+// line1 and line2 should be normalized for check parallelism
 // returns nullopt if they are not intersect (even if they match)
 template<typename T>
 std::optional<Vector3<T>> intersection( const Line3<T>& line1, const Line3<T>& line2,
@@ -55,20 +55,23 @@ std::optional<Vector3<T>> intersection( const Line3<T>& line1, const Line3<T>& l
 
     const auto p1 = dot( crossDir, line1.p );
     const auto p2 = dot( crossDir, line2.p );
-    if ( ( p1 - p2 ) >= errorLimit )
+    if ( std::abs( p1 - p2 ) >= errorLimit )
         return {};
 
     const auto n2 = cross( line2.d, crossDir );
-    return line1.p + dot( ( line2.p - line1.p ), n2 ) / dot( line1.d, n2 ) * line1.d;
+    const float den = dot( line1.d, n2 );
+    if ( den == 0 ) // check for calculation
+        return {};
+    return line1.p + dot( ( line2.p - line1.p ), n2 ) / den * line1.d;
 }
 
 
 
 
-// finds closest point on a plane1 and a plane2;
+// finds distance between a plane1 and a plane2
 // returns nullopt if they intersect
 template<typename T>
-std::optional<LineSegm3<T>> closestPoints( const Plane3<T>& plane1, const Plane3<T>& plane2,
+std::optional<T> distance( const Plane3<T>& plane1, const Plane3<T>& plane2,
     T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
 {
     const auto crossDir = cross( plane1.n, plane2.n );
@@ -76,41 +79,60 @@ std::optional<LineSegm3<T>> closestPoints( const Plane3<T>& plane1, const Plane3
     if ( crossDir.lengthSq() >= errorLimit * errorLimit )
         return {};
 
-    const auto plane1Point = plane1.n * plane1.d;
-    const auto plane2Point = plane2.n * plane2.d;
-    return LineSegm3<T>( plane1Point, plane2Point );
+    return ( plane2.n * plane2.d - plane1.n * plane1.d ).length();
 }
 
-// finds closest point on a plane and a line;
+// finds distance between a plane and a line;
 // returns nullopt if they intersect
 template<typename T>
-std::optional<LineSegm3<T>> closestPoints( const Plane3<T>& plane, const Line3<T>& line,
+std::optional<T> distance( const Plane3<T>& plane, const Line3<T>& line,
     T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
 {
     const auto den = dot( plane.n, line.d );
     if ( std::abs( den ) >= errorLimit )
         return {};
 
-    return LineSegm3<T>( plane.project( line.p ), line.p );
+    return std::abs( dot( line.p, plane.n ) - plane.d );
 }
 
-// finds closest point on a line1 and a line2;
+// finds distance between parallel or skew lines ( a line1 and a line2 )
 // returns nullopt if they intersect
+template<typename T>
+std::optional<T> distance( const Line3<T>& line1, const Line3<T>& line2,
+    T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
+{
+    const auto crossDir = cross( line1.d, line2.d );
+    if ( crossDir.lengthSq() < errorLimit * errorLimit )
+        return (line1.project( line2.p ) - line2.p ).length();
+    
+    const auto p1 = dot( crossDir, line1.p );
+    const auto p2 = dot( crossDir, line2.p );
+    if ( std::abs( p1 - p2 ) < errorLimit )
+        return {};
+
+    return std::abs( p1 - p2 );
+}
+
+// finds closest point between skew lines ( a line1 and a line2 )
+// returns nullopt if they intersect or parallel
 template<typename T>
 std::optional<LineSegm3<T>> closestPoints( const Line3<T>& line1, const Line3<T>& line2,
     T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
 {
     const auto crossDir = cross( line1.d, line2.d );
     if ( crossDir.lengthSq() < errorLimit * errorLimit )
-        return LineSegm3<T>( line1.project( line2.p ), line2.p );
+        return {};
 
     const auto p1 = dot( crossDir, line1.p );
     const auto p2 = dot( crossDir, line2.p );
-    if ( ( p1 - p2 ) < errorLimit )
+    if ( std::abs( p1 - p2 ) < errorLimit )
         return {};
 
     const auto n2 = cross( line2.d, crossDir );
-    const auto closest1 = line1.p + dot( ( line2.p - line1.p ), n2 ) / dot( line1.d, n2 ) * line1.d;
+    const float den = dot( line1.d, n2 );
+    if ( den == 0 ) // check for calculation
+        return {};
+    const auto closest1 = line1.p + dot( ( line2.p - line1.p ), n2 ) / den * line1.d;
     const auto closest2 = closest1 + ( p2 - p1 ) * crossDir;
     return LineSegm3<T>( closest1, closest2 );
 }

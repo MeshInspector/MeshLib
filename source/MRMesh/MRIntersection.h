6a07e780e8363bd4b796985d10c95684401fd271
Alexandr Burakov <aburakov@adalisk.com>
2022-03-29 16:44:10 +0300

    reworks and fixes

--- source/MRMesh/MRIntersection.h
+++ source/MRMesh/MRIntersection.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "MRMeshFwd.h"
 #include "MRPlane3.h"
 #include "MRLine3.h"
 #include "MRLineSegm.h"
@@ -10,130 +9,108 @@ namespace MR
 {
 
 template<typename T>
-const T errorLimit = std::numeric_limits<T>::epsilon() * 20.f;
+const T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 );
 
 // finds an intersection between a plane1 and a plane2
-// returns nullopt if they are parallel
-// returns line from plane1 if plane1 equal plane2
+// plane1 and plane2 should be normalized for check parallelism
+// returns nullopt if they are parallel (even if they match)
 template<typename T>
-std::optional<Line3<T>> intersection( const Plane3<T>& plane1, const Plane3<T>& plane2 )
+std::optional<Line3<T>> intersection( const Plane3<T>& plane1, const Plane3<T>& plane2,
+    T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
 {
-    const auto plane1N = plane1.normalized();
-    const auto plane2N = plane2.normalized();
-    const auto crossDir = cross( plane1N.n, plane2N.n );
+    const auto crossDir = cross( plane1.n, plane2.n );
 
-    const auto planeNpoint = plane1N.n * plane1N.d;
-    const auto plane2Npoint = plane2N.n * plane2N.d;
+    if ( crossDir.lengthSq() < errorLimit * errorLimit )
+        return {};
 
-    if ( crossDir.length() < errorLimit<T> )
-    {
-        if ( ( planeNpoint - plane2Npoint ).length() < errorLimit<T> )
-        {
-            auto bv = plane1N.n.furthestBasisVector();
-            return Line3<T>( planeNpoint, cross( plane1N.n, bv ).normalized() );
-        }
-        else
-            return {};
-    }
+    Matrix3<T> matrix( plane1.n, plane2.n, crossDir );
+    const auto point = matrix.inverse() * Vector3<T>( plane1.d, plane2.d, 0 );
 
-    const auto p1 = plane1N.project( plane2Npoint );
-    const auto p2 = plane2N.project( planeNpoint );
-    const auto p3 = Line3<T>(plane2Npoint, ( p1 - plane2Npoint ) ).project(p2);
-    return Line3<T>( plane2Npoint + ( p2 - plane2Npoint ) * ( p1 - plane2Npoint ).length() / ( p3 - plane2Npoint ).length(), crossDir.normalized() );
+    return Line3<T>( point, crossDir.normalized() );
 }
 
-// finds an intersection between a plane and a line;
-// returns nullopt if they are parallel
+// finds an intersection between a plane and a line
+// plane and line should be normalized for check parallelism
+// returns nullopt if they are parallel (even line belongs to plane)
 template<typename T>
-std::optional<Vector3<T>> intersection( const Plane3<T> & plane, const Line3<T> & line )
+std::optional<Vector3<T>> intersection( const Plane3<T>& plane, const Line3<T>& line,
+    T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
 {
-    const auto planeN = plane.normalized();
-    const auto lineN = line.normalized();
-
-    const auto den = dot( planeN.n, lineN.d );
-    if ( den == 0 )
+    const auto den = dot( plane.n, line.d );
+    if ( std::abs(den) < errorLimit )
         return {};
     return line.p + ( plane.d - dot( plane.n, line.p ) ) / den * line.d;
 }
 
 // finds an intersection between a line1 and a line2
-// returns nullopt if they are not intersect
+// plane and line should be normalized for check parallelism
+// returns nullopt if they are not intersect (even if they match)
 template<typename T>
-std::optional<Vector3<T>> intersection( const Line3<T>& line1, const Line3<T>& line2 )
+std::optional<Vector3<T>> intersection( const Line3<T>& line1, const Line3<T>& line2,
+    T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
 {
-    const auto line1N = line1.normalized();
-    const auto line2N = line2.normalized();
-    const auto crossDir = cross( line1N.d, line2N.d );
-    if ( crossDir.length() < errorLimit<T> )
-    {
-        if ( ( line1N.project( line2N.p ) - line2N.p ).length() < errorLimit<T> )
-            return line1N.p;
-        else
-            return {};
-    }
-    
-    const auto p1 = dot( crossDir, line1N.p );
-    const auto p2 = dot( crossDir, line2N.p );
-    if ( ( p1 - p2 ) >= errorLimit<T> )
+    const auto crossDir = cross( line1.d, line2.d );
+    if ( crossDir.lengthSq() < errorLimit * errorLimit )
         return {};
 
-    const auto n2 = cross( line2N.d, crossDir );
-    return line1N.p + dot( ( line2N.p - line1N.p ), n2 ) / dot( line1N.d, n2 ) * line1N.d;
+    const auto p1 = dot( crossDir, line1.p );
+    const auto p2 = dot( crossDir, line2.p );
+    if ( ( p1 - p2 ) >= errorLimit )
+        return {};
+
+    const auto n2 = cross( line2.d, crossDir );
+    return line1.p + dot( ( line2.p - line1.p ), n2 ) / dot( line1.d, n2 ) * line1.d;
 }
 
 
 
 
 // finds closest point on a plane1 and a plane2;
-// returns nullopt if they are intersect
+// returns nullopt if they intersect
 template<typename T>
-std::optional<LineSegm3<T>> closestPoints( const Plane3<T>& plane1, const Plane3<T>& plane2 )
+std::optional<LineSegm3<T>> closestPoints( const Plane3<T>& plane1, const Plane3<T>& plane2,
+    T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
 {
-    const auto plane1N = plane1.normalized();
-    const auto plane2N = plane2.normalized();
-    const auto crossDir = cross( plane1N.n, plane2N.n );
+    const auto crossDir = cross( plane1.n, plane2.n );
 
-    if ( crossDir.length() >= errorLimit<T> )
+    if ( crossDir.lengthSq() >= errorLimit * errorLimit )
         return {};
 
-    const auto planeNpoint = plane1N.n * plane1N.d;
-    const auto plane2Npoint = plane2N.n * plane2N.d;
-    return LineSegm3<T>( planeNpoint, plane2Npoint );
+    const auto plane1Point = plane1.n * plane1.d;
+    const auto plane2Point = plane2.n * plane2.d;
+    return LineSegm3<T>( plane1Point, plane2Point );
 }
 
 // finds closest point on a plane and a line;
-// returns nullopt if they are intersect
+// returns nullopt if they intersect
 template<typename T>
-std::optional<LineSegm3<T>> closestPoints( const Plane3<T>& plane, const Line3<T>& line )
+std::optional<LineSegm3<T>> closestPoints( const Plane3<T>& plane, const Line3<T>& line,
+    T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
 {
-    const auto planeN = plane.normalized();
-    const auto lineN = line.normalized();
-
-    const auto den = dot( planeN.n, lineN.d );
-    if ( std::abs(den) >= errorLimit<T> )
+    const auto den = dot( plane.n, line.d );
+    if ( std::abs( den ) >= errorLimit )
         return {};
 
-    return LineSegm3<T>( planeN.project( lineN.p ), lineN.p );
+    return LineSegm3<T>( plane.project( line.p ), line.p );
 }
 
 // finds closest point on a line1 and a line2;
-// returns nullopt if they are intersect
+// returns nullopt if they intersect
 template<typename T>
-std::optional<LineSegm3<T>> closestPoints( const Line3<T>& line1, const Line3<T>& line2 )
+std::optional<LineSegm3<T>> closestPoints( const Line3<T>& line1, const Line3<T>& line2,
+    T errorLimit = std::numeric_limits<T>::epsilon() * T( 20 ) )
 {
-    const auto line1N = line1.normalized();
-    const auto line2N = line2.normalized();
-    const auto crossDir = cross( line1N.d, line2N.d );
-    if ( crossDir.length() < errorLimit<T> )
-        return LineSegm3<T>( line1N.p, line2N.project( line1N.p ) );
+    const auto crossDir = cross( line1.d, line2.d );
+    if ( crossDir.lengthSq() < errorLimit * errorLimit )
+        return LineSegm3<T>( line1.project( line2.p ), line2.p );
 
-    const auto p1 = dot( crossDir, line1N.p );
-    const auto p2 = dot( crossDir, line2N.p );
-    if ( ( p1 - p2 ) < errorLimit<T> )
+    const auto p1 = dot( crossDir, line1.p );
+    const auto p2 = dot( crossDir, line2.p );
+    if ( ( p1 - p2 ) < errorLimit )
         return {};
 
-    const auto n2 = cross( line2N.d, crossDir );
-    const auto closest1 = line1N.p + dot( ( line2N.p - line1N.p ), n2 ) / dot( line1N.d, n2 ) * line1N.d;
+    const auto n2 = cross( line2.d, crossDir );
+    const auto closest1 = line1.p + dot( ( line2.p - line1.p ), n2 ) / dot( line1.d, n2 ) * line1.d;
     const auto closest2 = closest1 + ( p2 - p1 ) * crossDir;
     return LineSegm3<T>( closest1, closest2 );
 }

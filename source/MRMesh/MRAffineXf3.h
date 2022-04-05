a9590077ffa5c157a157fbce3fa942480e26d0f8
Fedr <fchelnokov@adalisk.com>
2022-04-05 17:17:25 +0300

    AffineXf3<T> lookAt( const Vector3<T>& center, const Vector3<T>& eye, const Vector3<T>& up )

--- source/MRMesh/MRAffineXf3.h
+++ source/MRMesh/MRAffineXf3.h
@@ -2,3 +2,26 @@
 
 #include "MRMatrix3.h"
 #include "MRAffineXf.h"
+
+namespace MR
+{
+
+// computes rigid transformation xf:
+// xf.z - directed from center to eye
+// xf.x - directed orthogonal to up and xf.z
+// xf.y - directed orthogonal to xf.z and xf.x
+// xf(eye) = 0
+template<typename T>
+AffineXf3<T> lookAt( const Vector3<T>& center, const Vector3<T>& eye, const Vector3<T>& up )
+{
+    const auto f = (center - eye).normalized();
+    const auto s = cross( f, up ).normalized();
+    const auto u = cross( s, f );
+
+    return AffineXf3f{ 
+        Matrix3f{ s, u, -f },
+        Vector3f{ -dot( s, eye ), -dot( u, eye ), dot( f, eye ) }
+    };
+}
+
+} //namespace MR

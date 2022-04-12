ac2af3e497ae6060e676ea9b2f59cfdce8e72862
Maxim Rayskiy <mraiskii@adalisk.com>
2022-04-12 15:37:21 +0300

    Update install.bat

--- thirdparty/install.bat
+++ thirdparty/install.bat
@@ -1,6 +1,7 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows-meshrus
 
 for /f "delims=" %%i in ('where vcpkg') do set vcpkg_path=%%i
+if not exist %vcpkg_path%\..\downloads mkdir %vcpkg_path%\..\downloads
 copy "%~dp0vcpkg\downloads" %vcpkg_path%\..\downloads
 
 vcpkg install "@%~dp0\..\requirements\windows.txt" --recurse --binarysource=clear

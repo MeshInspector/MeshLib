57a3b962f833ad776557e81e96e7b8a34f701b0d
Maxim Rayskiy <mraiskii@adalisk.com>
2022-04-12 14:02:14 +0300

    cp missed vcpkg packages

--- thirdparty/install.bat
+++ thirdparty/install.bat
@@ -1,2 +1,6 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows-meshrus
+
+for /f "delims=" %%i in ('where vcpkg') do set vcpkg_path=%%i
+copy "%~dp0vcpkg\downloads" %vcpkg_path%\..\downloads
+
 vcpkg install "@%~dp0\..\requirements\windows.txt" --recurse --binarysource=clear

f931a5d8049ffc2918f94e8f5e5cdf5ccf4af75f
Fedr <fchelnokov@adalisk.com>
2022-04-21 12:20:28 +0300

    CR fix

--- source/MRMesh/MRHeapBytes.h
+++ source/MRMesh/MRHeapBytes.h
@@ -11,7 +11,7 @@ namespace MR
 template<typename T>
 [[nodiscard]] inline size_t heapBytes( const std::vector<T> & vec )
 {
-    return vec.capacity() * sizeof( typename std::vector<T>::value_type );
+    return vec.capacity() * sizeof( T );
 }
 
 // returns the amount of memory this smart pointer and its pointed object own together on heap

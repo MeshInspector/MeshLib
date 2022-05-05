859187d3f1e85b0162ee6317b66bd5ab9e3f2220
Grant Karapetyan <GKarapetyan@adalisk.com>
2022-05-05 19:33:26 +0400

    Add progress bar to triangulation (#156)
    * Add progress bar to triangulation
    
    * fix python exposing

--- source/mrealgorithmspy/MREPythonPointCloudTriangulation.cpp
+++ source/mrealgorithmspy/MREPythonPointCloudTriangulation.cpp
@@ -1,6 +1,7 @@
 #include "MRMesh/MRPython.h"
 #include "MRMesh/MRPointCloud.h"
 #include "MREAlgorithms/MREPointCloudTriangulation.h"
+#include <pybind11/functional.h>
 
 MR_ADD_PYTHON_CUSTOM_DEF( mrealgorithmspy, TriangulationParameters, []( pybind11::module_& m )
 {
@@ -9,10 +10,8 @@ MR_ADD_PYTHON_CUSTOM_DEF( mrealgorithmspy, TriangulationParameters, []( pybind11
         def_readwrite( "avgNumNeighbours", &MRE::TriangulationParameters::avgNumNeighbours ).
         def_readwrite( "critAngle", &MRE::TriangulationParameters::critAngle ).
         def_readwrite( "critHoleLength", &MRE::TriangulationParameters::critHoleLength );
+
+    m.def( "triangulate_point_cloud", MRE::triangulatePointCloud,
+        pybind11::arg( "pointCloud" ), pybind11::arg( "params" ), pybind11::arg( "progressCallback" ) = nullptr,
+        "Creates mesh from given point cloud" );
 } )
-
-MR_ADD_PYTHON_FUNCTION( mrealgorithmspy, triangulate_point_cloud, MRE::triangulatePointCloud, "Creates mesh from given point cloud" )
-
-
-
-

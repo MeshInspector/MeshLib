ca532c86daaa38be04e03ea48b9c74842e76119b
Fedr <fchelnokov@adalisk.com>
2022-05-29 11:48:02 +0300

    disable warning 4189 in pybind, which VS2022 detects

--- source/MRPch/MRPch.h
+++ source/MRPch/MRPch.h
@@ -68,6 +68,7 @@
 #include "OpenCTM/openctm.h"
 
 #pragma warning(push)
+#pragma warning(disable:4189) //'has_args': local variable is initialized but not referenced
 #pragma warning(disable:4191) //'reinterpret_cast': unsafe conversion from 'PyObject *(__cdecl *)(PyObject *,PyObject *,PyObject *)' to 'void (__cdecl *)(void)'
 #pragma warning(disable:4355) //'this': used in base member initializer list
 #pragma warning(disable:4464) //relative include path contains '..'

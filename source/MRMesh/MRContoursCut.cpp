e81688f32a2633f4512da461cef70ae1829d8e01
Grant Karapetyan <GKarapetyan@adalisk.com>
2022-06-03 23:23:47 +0400

    Subdivide lone contours update (#193)
    * Subdivide lone contours update
    
    * update
    
    * CR Fix

--- source/MRMesh/MRContoursCut.cpp
+++ source/MRMesh/MRContoursCut.cpp
@@ -240,15 +240,16 @@ void subdivideLoneContours( Mesh& mesh, const OneMeshContours& contours, FaceMap
     }
     for ( auto& [faceId, conts] : face2contoursMap )
     {
+        assert( !conts.empty() );
         Vector3f massCenter;
         int counter = 0;
-        for ( const auto& cont : conts )
+        // here we find centroid of first lone contour of this face because
+        // splitting with average centroid does not guarantee that any lone contour will be subdivided
+        // and splitting with first contour centroid guarantee that at least this contour will be subdivided
+        for ( const auto& p : contours[conts.front()].intersections )
         {
-            for ( const auto& p : contours[cont].intersections )
-            {
-                ++counter;
-                massCenter += p.coordinate;
-            }
+            ++counter;
+            massCenter += p.coordinate;
         }
         massCenter /= float( counter );
 

73df564f853ef54808bebdd4e550bc535b239b84
Max <mraiskii@adalisk.com>
2022-06-14 19:45:04 +0300

    fix tests

--- scripts/run_python_test_script.py
+++ scripts/run_python_test_script.py
@@ -4,13 +4,13 @@ import platform
 
 python_cmd = "py -3.10 "
 platformSystem = platform.system()
-platformRelease = platform.freedesktop_os_release()
-print("{} {}".format(platformSystem, platformRelease))
 
 if platformSystem == 'Linux':
 	python_cmd = "python3.9 "
-	if platformRelease['VERSION_ID'].split(".")[0] == "22":
-		python_cmd = "python3.10 "
+	if str(platform.python_version_tuple()[1]) == "10":
+		platformRelease = platform.freedesktop_os_release()
+		if platformRelease['VERSION_ID'].split(".")[0] == "22":
+			python_cmd = "python3.10 "
 elif platformSystem == 'Darwin':
 	python_cmd = "python3 "
 

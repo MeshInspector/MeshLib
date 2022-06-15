05d4746760c7b0c1f9655c0d2ad1f6ee2edaaff5
Grant Karapetyan <GKarapetyan@adalisk.com>
2022-06-15 11:03:56 +0400

    Add ObjectLabel (#207)
    * Add ObjectLabel
    
    * CR Fixes

---
+++ source/MRMesh/MRPositionedText.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "MRVector3.h"
+#include <string>
+
+namespace MR
+{
+
+struct PositionedText
+{
+    PositionedText() = default;
+    PositionedText( const std::string& text, const Vector3f& position ) : text{text}, position{position}{}
+
+    std::string text;
+    Vector3f position;
+
+    bool operator==( const PositionedText& ) const = default;
+};
+
+using MeshLabel [[deprecated]] = PositionedText;
+
+}

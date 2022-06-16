c90cc6aaf3d329eb4bdc1917b9ed5bb003e350dd
MaxRayskiy <mraiskii@adalisk.com>
2022-06-16 11:58:49 +0300

    undo tests, get ready to merge

--- thirdparty/install.bat
+++ thirdparty/install.bat
@@ -1,6 +1,5 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows-meshrus
 
-
 for /f "delims=" %%i in ('where vcpkg') do set vcpkg_path=%%i
 if not exist %vcpkg_path%\..\downloads mkdir %vcpkg_path%\..\downloads
 copy "%~dp0vcpkg\downloads" %vcpkg_path%\..\downloads

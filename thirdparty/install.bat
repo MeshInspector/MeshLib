2d1af7829380370fb3bc722e2b7de95754c143e8
MaxRayskiy <mraiskii@adalisk.com>
2022-06-16 11:54:24 +0300

    test ./thirdparty/

--- thirdparty/install.bat
+++ thirdparty/install.bat
@@ -1,5 +1,6 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows-meshrus
 
+
 for /f "delims=" %%i in ('where vcpkg') do set vcpkg_path=%%i
 if not exist %vcpkg_path%\..\downloads mkdir %vcpkg_path%\..\downloads
 copy "%~dp0vcpkg\downloads" %vcpkg_path%\..\downloads

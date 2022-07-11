c21e338fec2bda7a26e86c53fbf4ec8feac0eea6
ADALISK\akoksin <akoksin@adalisk.com>
2022-07-11 19:02:54 +0400

    refactor pop-ups in MRMenu

--- source/MRViewer/MRMenu.cpp
+++ source/MRViewer/MRMenu.cpp
@@ -792,16 +792,18 @@ void Menu::draw_helpers()
         ImGui::End();
     }
 
-    if ( show_rename_modal_ )
-    {
+    if ( show_rename_modal_  && !ImGui::IsPopupOpen( "Rename object") )
         ImGui::OpenPopup( "Rename object" );
-        ImGui::BeginModalNoAnimation( "Rename object", nullptr,
-                                ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize );
 
+    if ( ImGui::BeginModalNoAnimation( "Rename object", nullptr,
+        ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize ) )
+    {
         auto obj = getAllObjectsInTree( &SceneRoot::get(), ObjectSelectivityType::Selected ).front();
         if ( !obj )
+        {
             show_rename_modal_ = false;
-
+            ImGui::CloseCurrentPopup();
+        }
         if ( ImGui::IsWindowAppearing() )
             ImGui::SetKeyboardFocusHere();
         ImGui::InputText( "Name", renameBuffer, ImGuiInputTextFlags_AutoSelectAll );
@@ -813,37 +815,48 @@ void Menu::draw_helpers()
             AppendHistory( std::make_shared<ChangeNameAction>( "Rename object", obj ) );
             obj->setName( renameBuffer );
             show_rename_modal_ = false;
+            ImGui::CloseCurrentPopup();
         }
         ImGui::SameLine( 0, p );
         if ( ImGui::Button( "Cancel", ImVec2( ( w - p ) / 2.f, 0 ) ) || esc_pressed_ )
+        {
             show_rename_modal_ = false;
+            ImGui::CloseCurrentPopup();
+        }
 
-        if ( ImGui::IsMouseClicked( 0 ) && !( ImGui::IsAnyItemHovered() || ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow) ) )
+        if ( ImGui::IsMouseClicked( 0 ) && !( ImGui::IsAnyItemHovered() || ImGui::IsWindowHovered( ImGuiHoveredFlags_AnyWindow ) ) )
+        {
             show_rename_modal_ = false;
+            ImGui::CloseCurrentPopup();
+        }
 
         ImGui::EndPopup();
     }
 
-    if ( !storedError_.empty() )
-    {
-        auto bgBackUp = ImGui::GetStyle().Colors[ImGuiCol_ModalWindowDimBg];
+    auto bgBackUp = ImGui::GetStyle().Colors[ImGuiCol_ModalWindowDimBg];
+
+    if ( !storedError_.empty() && !ImGui::IsPopupOpen( " Error##modal" ) )
+    {        
         ImGui::GetStyle().Colors[ImGuiCol_ModalWindowDimBg] = ImVec4( 1, 0.125f, 0.125f, bgBackUp.w );
         ImGui::OpenPopup( " Error##modal" );
-        ImGui::BeginModalNoAnimation( " Error##modal", nullptr,
-                                ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize );
+    }
 
+    if ( ImGui::BeginModalNoAnimation( " Error##modal", nullptr,
+                                ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize ) )
+    {
         ImGui::Text( "%s", storedError_.c_str() );
         
         ImGui::Spacing();
         ImGui::SameLine( ImGui::GetContentRegionAvail().x * 0.5f - 40.0f, ImGui::GetStyle().FramePadding.x );
-        if ( ImGui::Button( "Okay", ImVec2( 80.0f, 0 ) ) || enter_pressed_ )
+        if ( ImGui::Button( "Okay", ImVec2( 80.0f, 0 ) ) || enter_pressed_ ||
+           ( ImGui::IsMouseClicked( 0 ) && !( ImGui::IsAnyItemHovered() || ImGui::IsWindowHovered( ImGuiHoveredFlags_AnyWindow ) ) ) )
+        {
             storedError_.clear();
+            ImGui::GetStyle().Colors[ImGuiCol_ModalWindowDimBg] = bgBackUp;
+            ImGui::CloseCurrentPopup();
+        }        
 
-        if ( ImGui::IsMouseClicked( 0 ) && !( ImGui::IsAnyItemHovered() || ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow) ) )
-            storedError_.clear();
-
-        ImGui::EndPopup();
-        ImGui::GetStyle().Colors[ImGuiCol_ModalWindowDimBg] = bgBackUp;
+        ImGui::EndPopup();        
     }
 
     enter_pressed_ = false;

73de5774f3087a8878404e1fa75f8d12efaaf5a0
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2022-07-12 13:57:04 +0500

    Keep Area algorithm

--- source/MRMesh/MRPolylineRelax.cpp
+++ source/MRMesh/MRPolylineRelax.cpp
@@ -2,6 +2,7 @@
 #include "MRPolyline.h"
 #include "MRPolylineRelax.h"
 #include "MRTimer.h"
+#include "MRWriter.h"
 
 namespace MR
 {
@@ -12,6 +13,7 @@ bool relax( Polyline3 &polyline, const PolylineRelaxParams &params, ProgressCall
         return true;
 
     MR_TIMER
+    MR_WRITER(polyline)
 
     VertCoords newPoints;
     const auto& zone = polyline.topology.getValidVerts();
@@ -51,14 +53,70 @@ bool relax( Polyline3 &polyline, const PolylineRelaxParams &params, ProgressCall
     return keepGoing;
 }
 
-bool relaxKeepVolume( Polyline3 &polyline, const PolylineRelaxParams &params, ProgressCallback cb )
+bool relaxKeepArea( Polyline3 &polyline, const PolylineRelaxParams &params, ProgressCallback cb )
 {
     if ( params.iterations <= 0 )
         return true;
 
     MR_TIMER
+    MR_WRITER(polyline)
 
-    return false;
+    VertCoords newPoints;
+    const auto& zone = polyline.topology.getValidVerts();
+    std::vector<Vector3f> vertPushForces( zone.size() );
+
+    bool keepGoing = true;
+    for ( int i = 0; i < params.iterations; ++i )
+    {
+        ProgressCallback internalCb1, internalCb2;
+        if ( cb )
+        {
+            internalCb1 = [&] ( float p )
+            {
+                return cb( ( float( i ) + p * 0.5f ) / float( params.iterations ) );
+            };
+            internalCb2 = [&] ( float p )
+            {
+                return cb( ( float( i ) + p * 0.5f + 0.5f ) / float( params.iterations ) );
+            };
+        }
+
+        keepGoing = BitSetParallelFor( zone, [&]( VertId v )
+        {
+            auto e0 = polyline.topology.edgeWithOrg( v );
+            auto e1 = polyline.topology.next( e0 );
+            if ( !e0.valid() || !e1.valid() )
+                return;
+
+            Vector3d sum;
+            sum += Vector3d( polyline.points[polyline.topology.dest( e0 )] );
+            sum += Vector3d( polyline.points[polyline.topology.dest( e1 )] );
+
+            vertPushForces[v] = params.force * ( Vector3f{sum / 2.} - polyline.points[v] );
+        }, internalCb1 );
+        if ( !keepGoing )
+            break;
+
+        newPoints = polyline.points;
+        keepGoing = BitSetParallelFor( zone, [&]( VertId v )
+        {
+            auto e0 = polyline.topology.edgeWithOrg( v );
+            auto e1 = polyline.topology.next( e0 );
+            if ( !e0.valid() || !e1.valid() )
+                return;
+
+            auto& np = newPoints[v];
+            np += vertPushForces[v];
+            auto modifier = 1.0f / 2.0f;
+            np -= ( vertPushForces[polyline.topology.dest( e0 )] * modifier );
+            np -= ( vertPushForces[polyline.topology.dest( e1 )] * modifier );
+        }, internalCb2 );
+        polyline.points.swap( newPoints );
+        if ( !keepGoing )
+            break;
+    }
+
+    return keepGoing;
 }
 
 } // namespace MR
--- source/MRMesh/MRPolylineRelax.h
+++ source/MRMesh/MRPolylineRelax.h
@@ -13,14 +13,14 @@ struct PolylineRelaxParams : RelaxParams
     //
 };
 
-/// applies given number of relaxation iterations to the whole pointCloud ( or some region if it is specified )
+/// applies given number of relaxation iterations to the whole polyline ( or some region if it is specified )
 /// \return true if was finished successfully, false if was interrupted by progress callback
 MRMESH_API bool relax( Polyline3& polyline, const PolylineRelaxParams& params = {}, ProgressCallback cb = {} );
 
-/// applies given number of relaxation iterations to the whole pointCloud ( or some region if it is specified )
-/// do not really keeps volume but tries hard
+/// applies given number of relaxation iterations to the whole polyline ( or some region if it is specified )
+/// do not really keeps area but tries hard
 /// \return true if was finished successfully, false if was interrupted by progress callback
-MRMESH_API bool relaxKeepVolume( Polyline3& polyline, const PolylineRelaxParams& params = {}, ProgressCallback cb = {} );
+MRMESH_API bool relaxKeepArea( Polyline3& polyline, const PolylineRelaxParams& params = {}, ProgressCallback cb = {} );
 
 /// \}
 

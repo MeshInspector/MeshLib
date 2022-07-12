6ccc243cb8f5e72906c49880d75f0112862eb6e3
Grant Karapetyan <gkarapetyan@adalisk.com>
2022-07-12 19:55:25 +0400

    Update relax polylines exporting

--- source/MRMesh/MRPolylineRelax.h
+++ source/MRMesh/MRPolylineRelax.h
@@ -12,13 +12,13 @@ namespace MR
 /// applies given number of relaxation iterations to the whole polyline ( or some region if it is specified )
 /// \return true if was finished successfully, false if was interrupted by progress callback
 template<typename V>
-MRMESH_API bool relax( Polyline<V>& polyline, const RelaxParams& params = {}, ProgressCallback cb = {} );
+bool relax( Polyline<V>& polyline, const RelaxParams& params = {}, ProgressCallback cb = {} );
 
 /// applies given number of relaxation iterations to the whole polyline ( or some region if it is specified )
 /// do not really keeps area but tries hard
 /// \return true if was finished successfully, false if was interrupted by progress callback
 template<typename V>
-MRMESH_API bool relaxKeepArea( Polyline<V>& polyline, const RelaxParams& params = {}, ProgressCallback cb = {} );
+bool relaxKeepArea( Polyline<V>& polyline, const RelaxParams& params = {}, ProgressCallback cb = {} );
 
 /// \}
 

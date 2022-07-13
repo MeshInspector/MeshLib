73f586706497353312244593ceae99e5de0c7bfa
Grant Karapetyan <GKarapetyan@adalisk.com>
2022-07-13 15:08:27 +0300

    Improve transfrom context (#267)
    * Improve transfrom context
    
    * fix spelling
    
    * Fix typo

--- source/MRViewer/MRMenu.cpp
+++ source/MRViewer/MRMenu.cpp
@@ -934,8 +934,10 @@ float Menu::drawTransform_()
     if ( selected.size() == 1 && !selected[0]->isLocked() )
     {
         resultHeight_ = ImGui::GetTextLineHeight() + style.FramePadding.y * 2 + style.ItemSpacing.y;
+        bool openedContext = false;
         if ( ImGui::CollapsingHeader( "Transform", ImGuiTreeNodeFlags_DefaultOpen ) )
         {
+            openedContext = drawTransformContextMenu_( selected[0] );
             const float transformHeight = ( ImGui::GetTextLineHeight() + style.FramePadding.y * 2 ) * 3 + style.ItemSpacing.y * 2;
             resultHeight_ += transformHeight + style.ItemSpacing.y;
             ImGui::BeginChild( "SceneTransform", ImVec2( 0, transformHeight ) );
@@ -1015,8 +1017,11 @@ float Menu::drawTransform_()
             }
             data.setXf( xf );
             ImGui::EndChild();
-            drawTransformContextMenu_( selected[0] );
+            if ( !openedContext )
+                openedContext = drawTransformContextMenu_( selected[0] );
         }
+        if ( !openedContext )
+            drawTransformContextMenu_( selected[0] );
     }
 
     return resultHeight_;

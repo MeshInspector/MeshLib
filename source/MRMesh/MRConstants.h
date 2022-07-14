7da434e6de286d882e1449d0f5aed8e6b51e1b7c
ADALISK\akoksin <akoksin@adalisk.com>
2022-07-14 19:14:03 +0400

    delete constant

--- source/MRMesh/MRConstants.h
+++ source/MRMesh/MRConstants.h
@@ -11,7 +11,6 @@ constexpr static double PI   = 3.141592653589793238463;
 constexpr static double PI2  = PI / 2.;
 constexpr static float  PI_F = 3.14159265358979f;
 constexpr static float  PI2_F = PI_F / 2.f;
-constexpr static float sqrt3_F = 1.73205080757f;
 /// \}
 
 } // namespace MR

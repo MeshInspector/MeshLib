c191f0f9ffefb22a97284e9b0401c4903a0b4a2d
ADALISK\akoksin <akoksin@adalisk.com>
2022-07-14 19:14:36 +0400

    add line

--- source/MRMesh/MRConstants.h
+++ source/MRMesh/MRConstants.h
@@ -11,6 +11,7 @@ constexpr static double PI   = 3.141592653589793238463;
 constexpr static double PI2  = PI / 2.;
 constexpr static float  PI_F = 3.14159265358979f;
 constexpr static float  PI2_F = PI_F / 2.f;
+
 /// \}
 
 } // namespace MR

86da9b2b2c467c6fa0ac5d2aa1fbd79e0b536f72
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2022-07-15 17:21:24 +0500

    Generic suppress warning macro

--- source/MRPch/MRSuppressWarning.h
+++ source/MRPch/MRSuppressWarning.h
@@ -1,23 +1,32 @@
 #pragma once
 
+#define STR(...) #__VA_ARGS__
+
 #if defined( __clang__ )
-#define SUPPRESS_DEPRECATION_WARNING( x ) \
-_Pragma( "clang diagnostic push" )      \
-_Pragma( "clang diagnostic ignored \"-Wdeprecated-declarations\"" ) \
-{ x }                                   \
+#define SUPPRESS_WARNING_PUSH( x, y ) \
+_Pragma( "clang diagnostic push" )    \
+_Pragma( STR(clang diagnostic ignored x) )
+#elif defined( __GNUC__ )
+#define SUPPRESS_WARNING_PUSH( x, y ) \
+_Pragma( "GCC diagnostic push ")       \
+_Pragma( STR(GCC diagnostic ignored x) )
+#elif defined( _MSC_VER )
+#define SUPPRESS_WARNING_PUSH( x, y ) \
+_pragma( warning( push ) )            \
+_pragma( warning( disable: y ) )
+#else
+#define SUPPRESS_WARNING_PUSH( x, y )
+#endif
+
+#if defined( __clang__ )
+#define SUPPRESS_WARNING_POP \
 _Pragma( "clang diagnostic pop" )
 #elif defined( __GNUC__ )
-#define SUPPRESS_DEPRECATION_WARNING( x ) \
-_Pragma( "GCC diagnostic push" )        \
-_Pragma( "GCC diagnostic ignored \"-Wdeprecated-declarations\"" ) \
-{ x }                                   \
+#define SUPPRESS_WARNING_POP \
 _Pragma( "GCC diagnostic pop" )
 #elif defined( _MSC_VER )
-#define SUPPRESS_DEPRECATION_WARNING( x ) \
-_pragma( warning( push ) )              \
-_pragma( warning( disable: 4996 ) )     \
-{ x }                                   \
+#define SUPPRESS_WARNING_POP \
 _pragma( warning( pop ) )
 #else
-#define SUPPRESS_DEPRECATION_WARNING( x ) { x }
+#define SUPPRESS_WARNING_POP
 #endif

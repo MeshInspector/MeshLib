3e50a3e6010b62d9e82e2143c992cc026d4d5d7f
Alexandr Burakov <aburakov@adalisk.com>
2022-07-22 19:57:55 +0300

    move some code

--- source/MRViewer/MRViewerIO.h
+++ source/MRViewer/MRViewerIO.h
@@ -8,4 +8,18 @@ class VisualObject;
 
 MRVIEWER_API std::string saveObjectToFile( const std::shared_ptr<VisualObject>& obj, const std::filesystem::path& filename,
                                            ProgressCallback callback = emptyProgressCallback );
+
+#ifdef __EMSCRIPTEN__
+
+extern "C" {
+
+MRVIEWER_API EMSCRIPTEN_KEEPALIVE int load_files( int count, const char** filenames );
+
+MRVIEWER_API EMSCRIPTEN_KEEPALIVE int save_file( const char* filename );
+
+MRVIEWER_API EMSCRIPTEN_KEEPALIVE int save_scene( const char* filename );
+
+}
+#endif
+
 }

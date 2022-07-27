bec5a602b320b1ef0f399cc3e71d9ab8987c58bb
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2022-07-27 12:48:59 +0500

    Draw label's source point and leader line (#308)
    * Render source point
    
    * Render leader line
    
    * Cache label geometry
    
    * Revert "Cache label geometry"
    
    This reverts commit d3abf83a5171c86961dca0c3cdb707ec43a30ce8.
    
    * Apply padding for leader line
    
    * Monitor pivot point
    
    * Monitor text position
    
    * Fix

--- source/MRViewer/MRRenderLabelObject.cpp
+++ source/MRViewer/MRRenderLabelObject.cpp
@@ -12,6 +12,21 @@
 #include "MRViewer.h"
 #include "MRGladGlfw.h"
 
+namespace
+{
+    constexpr int cBackgroundPaddingPx = 8;
+
+    void applyPadding( MR::Box3f& box, float padding )
+    {
+        box.min.x -= padding;
+        box.min.y -= padding;
+        box.min.z -= padding;
+        box.max.x += padding;
+        box.max.y += padding;
+        box.max.z += padding;
+    }
+}
+
 namespace MR
 {
 
@@ -104,9 +119,47 @@ void RenderLabelObject::render( const RenderParams& renderParams ) const
     GL_EXEC( glDepthFunc( GL_LESS ) );
 }
 
-void RenderLabelObject::renderSourcePoint_( const RenderParams& ) const
+void RenderLabelObject::renderSourcePoint_( const RenderParams& renderParams ) const
 {
-    //
+    GL_EXEC( glBindVertexArray( srcArrayObjId_ ) );
+
+    auto shader = ShadersHolder::getShaderId( ShadersHolder::DrawPoints );
+    GL_EXEC( glUseProgram( shader ) );
+
+    const std::vector<Vector3f> point { objLabel_->getLabel().position };
+    bindVertexAttribArray( shader, "position", srcVertPosBufferObjId_, point, 3, dirtySrc_ );
+
+    constexpr std::array<VertId, 1> pointIndices{ VertId( 0 ) };
+    GL_EXEC( glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, srcIndicesBufferObjId_ ) );
+    if ( dirtySrc_ )
+    {
+        GL_EXEC( glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( VertId ) * pointIndices.size(), pointIndices.data(), GL_DYNAMIC_DRAW ) );
+    }
+
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "model" ), 1, GL_TRUE, renderParams.modelMatrixPtr ) );
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "view" ), 1, GL_TRUE, renderParams.viewMatrixPtr ) );
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "proj" ), 1, GL_TRUE, renderParams.projMatrixPtr ) );
+
+    const auto& backColor = Vector4f( objLabel_->getBackColor() );
+    GL_EXEC( glUniform4f( glGetUniformLocation( shader, "backColor" ), backColor[0], backColor[1], backColor[2], backColor[3] ) );
+
+    const auto& mainColor = Vector4f( objLabel_->getFrontColor( objLabel_->isSelected() ) );
+    GL_EXEC( glUniform4f( glGetUniformLocation( shader, "mainColor" ), mainColor[0], mainColor[1], mainColor[2], mainColor[3] ) );
+
+    GL_EXEC( glUniform1ui( glGetUniformLocation( shader, "primBucketSize" ), 1 ) );
+
+    getViewerInstance().incrementThisFrameGLPrimitivesCount( Viewer::GLPrimitivesType::PointElementsNum, pointIndices.size() );
+
+    // FIXME: parametrize it
+    const float pointSize = 5.f;
+#ifdef __EMSCRIPTEN__
+    GL_EXEC( glUniform1f( glGetUniformLocation( shader, "pointSize" ), pointSize ) );
+#else
+    GL_EXEC( glPointSize( pointSize ) );
+#endif
+    GL_EXEC( glDrawElements( GL_POINTS, ( GLsizei )pointIndices.size(), GL_UNSIGNED_INT, 0 ) );
+
+    dirtySrc_ = false;
 }
 
 void RenderLabelObject::renderBackground_( const RenderParams& renderParams ) const
@@ -137,14 +190,13 @@ void RenderLabelObject::renderBackground_( const RenderParams& renderParams ) co
     const auto mainColor = Vector4f( objLabel_->getBackColor() );
     GL_EXEC( glUniform4f( glGetUniformLocation( shader, "mainColor" ), mainColor[0], mainColor[1], mainColor[2], mainColor[3] ) );
 
-    const auto box = objLabel_->labelRepresentingMesh()->getBoundingBox();
-    constexpr int cBackgroundPaddingPx = 8;
-    const auto padding = cBackgroundPaddingPx * ( box.max.y - box.min.y ) / height;
+    auto box = objLabel_->labelRepresentingMesh()->getBoundingBox();
+    applyPadding( box, cBackgroundPaddingPx * ( box.max.y - box.min.y ) / height );
     const std::vector<Vector3f> corners {
-        { box.min.x - padding, box.min.y - padding, 0.f },
-        { box.max.x + padding, box.min.y - padding, 0.f },
-        { box.min.x - padding, box.max.y + padding, 0.f },
-        { box.max.x + padding, box.max.y + padding, 0.f },
+        { box.min.x, box.min.y, 0.f },
+        { box.max.x, box.min.y, 0.f },
+        { box.min.x, box.max.y, 0.f },
+        { box.max.x, box.max.y, 0.f },
     };
     bindVertexAttribArray( shader, "position", bgVertPosBufferObjId_, corners, 3, dirtyBg_ );
 
@@ -166,9 +218,61 @@ void RenderLabelObject::renderBackground_( const RenderParams& renderParams ) co
     dirtyBg_ = false;
 }
 
-void RenderLabelObject::renderLeaderLine_( const RenderParams& ) const
+void RenderLabelObject::renderLeaderLine_( const RenderParams& renderParams ) const
 {
-    //
+    GL_EXEC( glBindVertexArray( llineArrayObjId_ ) );
+
+    auto shader = ShadersHolder::getShaderId( ShadersHolder::Labels );
+    GL_EXEC( glUseProgram( shader ) );
+
+    const auto shift = objLabel_->getPivotShift();
+    auto box = objLabel_->labelRepresentingMesh()->getBoundingBox();
+    applyPadding( box, cBackgroundPaddingPx * ( box.max.y - box.min.y ) / objLabel_->getFontHeight() );
+    const std::vector<Vector3f> leaderLineVertices {
+        { shift.x, shift.y, 0.f },
+        { box.min.x, box.min.y, 0.f },
+        { box.max.x, box.min.y, 0.f },
+    };
+    bindVertexAttribArray( shader, "position", llineVertPosBufferObjId_, leaderLineVertices, 3, dirtyLLine_ );
+
+    constexpr std::array<Vector2i, 2> llineEdgesIndices{
+        Vector2i{ 0, 1 },
+        Vector2i{ 1, 2 },
+    };
+    GL_EXEC( glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, llineEdgesIndicesBufferObjId_ ) );
+    if ( dirtyLLine_ )
+    {
+        GL_EXEC( glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( Vector2i ) * llineEdgesIndices.size(), llineEdgesIndices.data(), GL_DYNAMIC_DRAW ) );
+    }
+
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "model" ), 1, GL_TRUE, renderParams.modelMatrixPtr ) );
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "view" ), 1, GL_TRUE, renderParams.viewMatrixPtr ) );
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "proj" ), 1, GL_TRUE, renderParams.projMatrixPtr ) );
+
+    auto height = objLabel_->getFontHeight();
+
+    Vector2f modifier;
+    modifier.y = height / ( SymbolMeshParams::MaxGeneratedFontHeight * renderParams.viewport.w );
+    modifier.x = modifier.y * renderParams.viewport.w / renderParams.viewport.z;
+
+    GL_EXEC( glUniform2f( glGetUniformLocation( shader, "modifier" ), modifier.x, modifier.y ) );
+
+    GL_EXEC( glUniform2f( glGetUniformLocation( shader, "shift" ), shift.x, shift.y ) );
+
+    const auto& pos = objLabel_->getLabel().position;
+    GL_EXEC( glUniform3f( glGetUniformLocation( shader, "basePos" ), pos.x, pos.y, pos.z ) );
+
+    const auto mainColor = Vector4f( objLabel_->getFrontColor( objLabel_->isSelected() ) );
+    GL_EXEC( glUniform4f( glGetUniformLocation( shader, "mainColor" ), mainColor[0], mainColor[1], mainColor[2], mainColor[3] ) );
+
+    getViewerInstance().incrementThisFrameGLPrimitivesCount( Viewer::GLPrimitivesType::LineElementsNum, llineEdgesIndices.size() );
+
+    // FIXME: parametrize it
+    const float lineWidth = 1.f;
+    GL_EXEC( glLineWidth( lineWidth ) );
+    GL_EXEC( glDrawElements( GL_LINES, 2 * int( llineEdgesIndices.size() ), GL_UNSIGNED_INT, 0 ) );
+
+    dirtyLLine_ = false;
 }
 
 void RenderLabelObject::renderPicker( const BaseRenderParams&, unsigned ) const
@@ -199,28 +303,47 @@ void RenderLabelObject::initBuffers_()
     GL_EXEC( glGenBuffers( 1, &vertPosBufferObjId_ ) );
     GL_EXEC( glGenBuffers( 1, &facesIndicesBufferObjId_ ) );
 
+    GL_EXEC( glGenVertexArrays( 1, &srcArrayObjId_ ) );
+    GL_EXEC( glBindVertexArray( srcArrayObjId_ ) );
+    GL_EXEC( glGenBuffers( 1, &srcVertPosBufferObjId_ ) );
+    GL_EXEC( glGenBuffers( 1, &srcIndicesBufferObjId_ ) );
+
     GL_EXEC( glGenVertexArrays( 1, &bgArrayObjId_ ) );
     GL_EXEC( glBindVertexArray( bgArrayObjId_ ) );
     GL_EXEC( glGenBuffers( 1, &bgVertPosBufferObjId_ ) );
     GL_EXEC( glGenBuffers( 1, &bgFacesIndicesBufferObjId_ ) );
 
+    GL_EXEC( glGenVertexArrays( 1, &llineArrayObjId_ ) );
+    GL_EXEC( glBindVertexArray( llineArrayObjId_ ) );
+    GL_EXEC( glGenBuffers( 1, &llineVertPosBufferObjId_ ) );
+    GL_EXEC( glGenBuffers( 1, &llineEdgesIndicesBufferObjId_ ) );
+
     dirty_ = DIRTY_ALL;
+    dirtySrc_ = true;
     dirtyBg_ = true;
+    dirtyLLine_ = true;
 }
 
 void RenderLabelObject::freeBuffers_()
 {
     if ( !Viewer::constInstance()->isGLInitialized() || !loadGL() )
         return;
-    GL_EXEC( glDeleteVertexArrays( 1, &labelArrayObjId_ ) );
 
+    GL_EXEC( glDeleteVertexArrays( 1, &labelArrayObjId_ ) );
     GL_EXEC( glDeleteBuffers( 1, &vertPosBufferObjId_ ) );
     GL_EXEC( glDeleteBuffers( 1, &facesIndicesBufferObjId_ ) );
 
-    GL_EXEC( glDeleteVertexArrays( 1, &bgArrayObjId_ ) );
+    GL_EXEC( glDeleteVertexArrays( 1, &srcArrayObjId_ ) );
+    GL_EXEC( glDeleteBuffers( 1, &srcVertPosBufferObjId_ ) );
+    GL_EXEC( glDeleteBuffers( 1, &srcIndicesBufferObjId_ ) );
 
+    GL_EXEC( glDeleteVertexArrays( 1, &bgArrayObjId_ ) );
     GL_EXEC( glDeleteBuffers( 1, &bgVertPosBufferObjId_ ) );
     GL_EXEC( glDeleteBuffers( 1, &bgFacesIndicesBufferObjId_ ) );
+
+    GL_EXEC( glDeleteVertexArrays( 1, &llineArrayObjId_ ) );
+    GL_EXEC( glDeleteBuffers( 1, &llineVertPosBufferObjId_ ) );
+    GL_EXEC( glDeleteBuffers( 1, &llineEdgesIndicesBufferObjId_ ) );
 }
 
 void RenderLabelObject::update_() const
@@ -244,6 +367,23 @@ void RenderLabelObject::update_() const
         } );
 
         dirtyBg_ = true;
+        dirtyLLine_ = true;
+    }
+
+    const auto position = objLabel_->getLabel().position;
+    if ( position != positionState_ )
+    {
+        positionState_ = position;
+
+        dirtySrc_ = true;
+    }
+
+    const auto pivotPoint = objLabel_->getPivotPoint();
+    if ( pivotPoint != pivotPointState_ || dirty_ & DIRTY_POSITION )
+    {
+        pivotPointState_ = pivotPoint;
+
+        dirtyLLine_ = true;
     }
 
     objLabel_->resetDirty();
--- source/MRViewer/MRRenderLabelObject.h
+++ source/MRViewer/MRRenderLabelObject.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "MRMesh/MRIRenderObject.h"
+#include "MRMesh/MRVector2.h"
 
 namespace MR
 {
@@ -22,9 +23,17 @@ private:
     GLuint vertPosBufferObjId_{ 0 };
     GLuint facesIndicesBufferObjId_{ 0 };
 
+    GLuint srcArrayObjId_{ 0 };
+    GLuint srcVertPosBufferObjId_{ 0 };
+    GLuint srcIndicesBufferObjId_{ 0 };
+
     GLuint bgArrayObjId_{ 0 };
     GLuint bgVertPosBufferObjId_{ 0 };
     GLuint bgFacesIndicesBufferObjId_{ 0 };
+
+    GLuint llineArrayObjId_{ 0 };
+    GLuint llineVertPosBufferObjId_{ 0 };
+    GLuint llineEdgesIndicesBufferObjId_{ 0 };
 
     void renderSourcePoint_( const RenderParams& renderParams ) const;
     void renderBackground_( const RenderParams& renderParams ) const;
@@ -42,7 +51,11 @@ private:
 
     // Marks dirty buffers that need to be uploaded to OpenGL
     mutable uint32_t dirty_;
+    mutable bool dirtySrc_;
     mutable bool dirtyBg_;
+    mutable bool dirtyLLine_;
+    mutable Vector3f positionState_;
+    mutable Vector2f pivotPointState_;
 };
 
 }

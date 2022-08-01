ff24d7e6c514ececd2e653292e2441725ea3d87d
A.Burakov <42890373+ABSitf@users.noreply.github.com>
2022-08-01 20:20:20 +0300

    progress bar in loading files (general function + load obj) (#324)
    * added function to load file with progress callback
    * added progress callback in loading obj file

--- source/MRViewer/MRViewerIO.cpp
+++ source/MRViewer/MRViewerIO.cpp
@@ -13,14 +13,15 @@
 #include "MRPch/MRSpdlog.h"
 #include "MRPch/MRWasm.h"
 #include "MRMesh/MRStringConvert.h"
-#include "MRMenu.h"
-#include "MRAppendHistory.h"
 #include "MRMesh/MRSerializer.h"
+#include "MRMesh/MRMeshLoadObj.h"
+#include "MRViewerInstance.h"
+#include "MRViewer/MRViewer.h"
+#include "MRMesh/MRObjectLoad.h"
+#include "MRViewer/MRAppendHistory.h"
 
 namespace MR
 {
-
-
 
 tl::expected<void, std::string> saveObjectToFile( const Object& obj, const std::filesystem::path& filename, ProgressCallback callback )
 {
@@ -76,9 +77,97 @@ tl::expected<void, std::string> saveObjectToFile( const Object& obj, const std::
     }
 #endif
 
-    if ( result.has_value() )
-        getViewerInstance().recentFilesStore.storeFile( filename );
+    if ( !result.has_value() )
+        spdlog::error( result.error() );
+
+    return result;
+}
+
+tl::expected<std::vector<std::shared_ptr<MR::Object>>, std::string> loadObjectFromFile( const std::filesystem::path& filename,
+                                                                                        ProgressCallback callback )
+{
+    if ( callback && !callback( 0.f ) )
+        return tl::make_unexpected( std::string( "Saving canceled" ) );
+
+    tl::expected<std::vector<std::shared_ptr<Object>>, std::string> result;
+
+    auto ext = filename.extension().u8string();
+    for ( auto& c : ext )
+        c = ( char )tolower( c );
+
+
+    if ( ext == u8".obj" )
+    {
+        auto res = MeshLoad::fromSceneObjFile( filename, false, callback );
+        if ( res.has_value() )
+        {
+            std::vector<std::shared_ptr<Object>> objects( res.value().size() );
+            auto& resValue = *res;
+            for ( int i = 0; i < objects.size(); ++i )
+            {
+                std::shared_ptr<ObjectMesh> objectMesh = std::make_shared<ObjectMesh>();
+                if ( resValue[i].name.empty() )
+                    objectMesh->setName( utf8string( filename.stem() ) );
+                else
+                    objectMesh->setName( std::move( resValue[i].name ) );
+                objectMesh->setMesh( std::make_shared<Mesh>( std::move( resValue[i].mesh ) ) );
+                objects[i] = std::dynamic_pointer_cast< Object >( objectMesh );
+            }
+            result = objects;
+        }
+        else
+            result = tl::make_unexpected( res.error() );
+    }
+    else if ( !SceneFileFilters.empty() && filename.extension() == SceneFileFilters.front().extension.substr( 1 ) )
+    {
+        auto res = deserializeObjectTree( filename );
+        if ( res.has_value() )
+        {
+            result = std::vector( { *res } );
+        }
+        else
+            result = tl::make_unexpected( res.error() );
+    }
     else
+    {
+        auto objectMesh = makeObjectMeshFromFile( filename );
+        if ( objectMesh.has_value() )
+        {
+            objectMesh->select( true );
+            auto obj = std::make_shared<ObjectMesh>( std::move( *objectMesh ) );
+            result = { obj };
+        }
+        else
+        {
+            result = tl::make_unexpected( objectMesh.error() );
+
+            auto objectPoints = makeObjectPointsFromFile( filename );
+            if ( objectPoints.has_value() )
+            {
+                objectPoints->select( true );
+                auto obj = std::make_shared<ObjectPoints>( std::move( objectPoints.value() ) );
+                result = { obj };
+            }
+            else if ( result.error() == "unsupported file extension" )
+            {
+                result = tl::make_unexpected( objectPoints.error() );
+
+                auto objectLines = makeObjectLinesFromFile( filename );
+                if ( objectLines.has_value() )
+                {
+                    objectLines->select( true );
+                    auto obj = std::make_shared<ObjectLines>( std::move( objectLines.value() ) );
+                    result = { obj };
+                }
+                else if ( result.error() == "unsupported file extension" )
+                {
+                    result = tl::make_unexpected( objectLines.error() );
+                }
+            }
+        }
+    }
+
+    if ( !result.has_value() )
         spdlog::error( result.error() );
 
     return result;

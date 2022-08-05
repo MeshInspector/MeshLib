14588bd3d1ce056feee43d1d5e5ed307c1c1c12f
theastrowander <astrowander@gmail.com>
2022-08-05 16:01:36 +0400

    add constants

--- source/MRViewer/MRRibbonConstants.h
+++ source/MRViewer/MRRibbonConstants.h
@@ -22,6 +22,9 @@ constexpr float cRibbonButtonWindowPaddingY = 4.0f;
 constexpr float cCheckboxPadding = 3.0f;
 constexpr float cButtonPadding = 8.0f;
 constexpr float cDefaultItemSpacing = 8.0f;
+constexpr float cDefaultInnerSpacing = 8.0f;
+constexpr float cDefaultWindowPaddingX = 8.0f;
+constexpr float cDefaultWindowPaddingY = 16.0f;
 
 constexpr float cSmallItemDropSizeModifier = 0.5f;
 

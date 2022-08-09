92fb39275854608fa30e76ea125fccc3d8164b59
Grant Karapetyan <gkarapetyan@adalisk.com>
2022-08-09 15:49:02 +0300

    fixes

--- scripts/run_python_test_script.py
+++ scripts/run_python_test_script.py
@@ -14,7 +14,17 @@ if platformSystem == 'Linux':
 elif platformSystem == 'Darwin':
 	python_cmd = "python3 "
 
-res = os.system(python_cmd + "-m pytest -s -v" )
+directory = os.path.dirname(os.path.abspath(__file__))
+if len(sys.argv) == 1:
+    directory = os.path.join(directory, "..")
+    directory = os.path.join(directory, "test_python")
+else:
+    directory = os.path.join(directory, sys.argv[1])
+
+os.environ["MeshLibPyModulesPath"] = os.getcwd()
+os.chdir(directory)
+
+res = os.system(python_cmd + "-m pytest -s -v"  )
 
 if (res != 0):
 	sys.exit(1)

b58b51ad7832a002c92c7e50940c28dfc15ada8f
Alexandr Burakov <aburakov@adalisk.com>
2022-08-10 19:49:59 +0300

    deprecate old plugins for save and load files

--- source/MRViewer/MROpen.h
+++ source/MRViewer/MROpen.h
@@ -7,7 +7,7 @@
 namespace MR
 {
 
-class Open : public MR::ViewerPlugin, public MultiListener<LoadListener, DragDropListener>
+class Open [[deprecated]] : public MR::ViewerPlugin, public MultiListener<LoadListener, DragDropListener>
 {
 public:
     MRVIEWER_API virtual void init( Viewer* _viewer ) override;
@@ -17,7 +17,5 @@ protected:
     MRVIEWER_API virtual bool load_( const std::filesystem::path& filename ) override;
     MRVIEWER_API virtual bool dragDrop_( const std::vector<std::filesystem::path>& paths ) override;
 };
-
-MRVIEWER_API extern Open OpenInstance;
 
 }

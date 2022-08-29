6e4d102e43eb1272aadc504383db5ffc0027fe7a
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2022-08-29 20:07:55 +0400

    Add comments

--- source/MRViewer/MRRenderHelpers.h
+++ source/MRViewer/MRRenderHelpers.h
@@ -9,7 +9,7 @@ namespace MR
 // calc texture resolution, to fit MAX_TEXTURE_SIZE, and have minimal empty pixels
 Vector2i calcTextureRes( int bufferSize, int maxTextWidth );
 
-/// ...
+// provides access to shared buffer with type casting
 template <typename T>
 class RenderBufferRef
 {
@@ -23,28 +23,28 @@ public:
             assert( !dirty_ );
     }
 
-    /// ...
+    // returns reference to i-th element
     T& operator []( std::size_t i ) const noexcept
     {
         assert( dirty_ );
         return data_[i];
     }
-    /// ...
+    // returns pointer to buffer data
     T* data() const noexcept
     {
         return data_;
     }
-    /// returns actual buffer size
+    // returns actual buffer size
     [[nodiscard]] std::size_t size() const noexcept
     {
         return dirty_ ? glSize_ : 0;
     }
-    /// returns number of elements that are about to be loaded or already loaded to GL memory
+    // returns number of elements that are about to be loaded or already loaded to GL memory
     [[nodiscard]] std::size_t glSize() const noexcept
     {
         return glSize_;
     }
-    /// ...
+    // returns true if associated data were updated
     [[nodiscard]] bool dirty() const noexcept
     {
         return dirty_;
@@ -56,7 +56,7 @@ private:
     bool dirty_;
 };
 
-/// ...
+// provides shared buffer for loading different types of data to GL memory
 class RenderObjectBuffer
 {
 public:

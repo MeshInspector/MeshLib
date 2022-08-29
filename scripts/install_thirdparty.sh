4086e72168d6753e1f5e8e3e5cceb8397eb147f3
Maxim Rayskiy <mraiskii@adalisk.com>
2022-08-29 12:15:48 +0100

    fix empty variable (#422)

--- scripts/install_thirdparty.sh
+++ scripts/install_thirdparty.sh
@@ -6,8 +6,8 @@
 # exit if any command failed
 set -eo pipefail
 
-if [ "$MR_STATE" -ne "DOCKER_BUILD" ]; then
- read -t 5 -p "It is strongly recomended to use 'apt install ./distr/meshrus*.deb' instead! Press (y) in 10 seconds to continue (y/N)" -rsn 1
+if [ "$MR_STATE" != "DOCKER_BUILD" ]; then
+ read -t 10 -p "It is strongly recommended to use 'apt install ./meshlib*.deb' instead! Press (y) in 10 seconds to continue (y/N)" -rsn 1
  echo;
  if ! [[ $REPLY =~ ^[Yy]$ ]]; then
   exit 0
@@ -19,6 +19,10 @@ MR_LIB_DIR="./lib/"
 MR_INSTALL_THIRDPARTY_DIR="/usr/local/lib/"
 MR_INSTALL_FONTS_DIR="/usr/local/share/fonts/"
 MR_INSTALL_INCLUDE_DIR="/usr/local/include/"
+
+if [ -z "$MR_THIRDPARTY_DIR" ]; then
+  MR_THIRDPARTY_DIR="./thirdparty"
+fi
 
 if [ "$EUID" -ne 0 ]; then
  printf "Root access required!\n"

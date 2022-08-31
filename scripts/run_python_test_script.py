afd6713dc14c4e8dff4d26bbe65f0e3812dfecae
MaxRayskiy <mraiskii@adalisk.com>
2022-08-31 15:40:36 +0300

    update test init script

--- scripts/run_python_test_script.py
+++ scripts/run_python_test_script.py
@@ -6,13 +6,27 @@ python_cmd = "py -3.10 "
 platformSystem = platform.system()
 
 if platformSystem == 'Linux':
-	python_cmd = "python3.9 "
-	if str(platform.python_version_tuple()[1]) == "10":
-		platformRelease = platform.freedesktop_os_release()
-		if platformRelease['VERSION_ID'].split(".")[0] == "22":
-			python_cmd = "python3.10 "
-		if platformRelease['VERSION_ID'].split(".")[0] == "20":
+	python_cmd = "python3 "
+
+	os_name=""
+	os_version=""
+	if os.path.exists('/etc/os-release'):
+		lines = open('/etc/os-release').read().split('\n')
+		for line in lines:
+			if line.startswith('NAME='):
+				os_name=line.split('=')[-1].replace('"', '')
+			if line.startswith('VERSION_ID='):
+				os_version=line.split('=')[-1].replace('"', '')
+
+	if "ubuntu" in os_name.lower():
+		if os_version.startswith("20"):
 			python_cmd = "python3.8 "
+		elif os_version.startswith("22"):
+			python_cmd = "python3.10 "
+	elif "fedora" in os_name.lower():
+		if os_version.startswith("35"):
+			python_cmd = "python3.9 "
+
 elif platformSystem == 'Darwin':
 	python_cmd = "python3 "
 

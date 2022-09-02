5e4b4a6ff5e8c4d5fd4eb0d4016e1c33a58de603
Fedr <fchelnokov@adalisk.com>
2022-09-02 17:22:25 +0300

    All textures in RenderMeshObject switched to GlTexture2

--- source/MRViewer/MRRenderMeshObject.h
+++ source/MRViewer/MRRenderMeshObject.h
@@ -61,13 +61,11 @@ private:
 
     GlBuffer facesIndicesBuffer_;
     GlBuffer edgesIndicesBuffer_;
-    GLuint texture_{ 0 };
 
+    GlTexture2 texture_;
     GlTexture2 faceSelectionTex_;
-
-    GLuint faceColorsTex_{ 0 };
-
-    GLuint facesNormalsTex_{ 0 };
+    GlTexture2 faceColorsTex_;
+    GlTexture2 facesNormalsTex_;
 
     int maxTexSize_{ 0 };
 

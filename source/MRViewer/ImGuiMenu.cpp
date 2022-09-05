854bef5167cf8f10f8ce334b1ccfa551191ebf4f
Egor Mikhaylov <egor.mikhaylov@smiledirectclub.com>
2022-09-05 16:32:47 +0300

    Fix ImGui backend header paths. (#459)

--- source/MRViewer/ImGuiMenu.cpp
+++ source/MRViewer/ImGuiMenu.cpp
@@ -8,8 +8,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include "ImGuiMenu.h"
 #include "MRMeshViewer.h"
-#include <imgui/backends/imgui_impl_glfw.h>
-#include <imgui/backends/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 #include "imgui_fonts_droid_sans.h"
 #include "MRMesh/MRObjectsAccess.h"
 #include "MRMesh/MRVisualObject.h"

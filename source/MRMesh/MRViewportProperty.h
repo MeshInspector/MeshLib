0f81ce2a95bdfb225a33b70f0930915906137cd3
Fedr <fchelnokov@adalisk.com>
2022-09-05 18:44:35 +0300

    more operations

--- source/MRMesh/MRViewportProperty.h
+++ source/MRMesh/MRViewportProperty.h
@@ -16,26 +16,38 @@ template<typename T>
 class ViewportProperty
 {
 public:
-    // sets default property value
-    void set( T def ) { def_ = std::move( def ); return * this; }
-    // gets default property value
+    /// sets default property value
+    void set( T def ) { def_ = std::move( def ); }
+    /// gets default property value
     const T & get() const { return def_; }
           T & get()       { return def_; }
 
-    // sets specific property value for given viewport
+    /// sets specific property value for given viewport
     void set( ViewportId id, T v ) { assert( id ); map_[id] = std::move( v ); }
-    // gets property value for given viewport: specific if available otherwise default one
-    const T & get( ViewportId id ) const
+    /// gets property value for given viewport: specific if available otherwise default one;
+    /// \param isDef receives true if this viewport does not have specific value and default one is returned
+    const T & get( ViewportId id, bool * isDef = nullptr ) const
     { 
         assert( id );
         auto it = map_.find( id );
         if ( it != map_.end() )
+        {
+            if ( isDef )
+                *isDef = false;
             return it->second;
+        }
+        if ( isDef )
+            *isDef = true;
         return def_;
     }
-    T & get( ViewportId id ) { return const_cast<T&>( const_cast<const ViewportProperty<T>*>( this )->get( id ) ); }
-    // forgets specific property value for given viewport
+    T & get( ViewportId id, bool * isDef = nullptr )
+    { 
+        return const_cast<T&>( const_cast<const ViewportProperty<T>*>( this )->get( id, isDef ) );
+    }
+    /// forgets specific property value for given viewport
     void reset( ViewportId id ) { assert( id ); map_.erase( id ); }
+    /// forgets specific property value for all viewports
+    void reset() { map_.clear(); }
 
 private:
     T def_{};

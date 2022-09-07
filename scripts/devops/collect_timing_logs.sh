7765869725f34c44749b48156f61fe41e62b5edc
Alexandr Burakov <aburakov@adalisk.com>
2022-09-07 21:48:10 +0300

    update

---
+++ scripts/devops/collect_timing_logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $# != 4 ]]
+then
+    echo "invalid argument count"
+    exit 1
+fi
+
+# param 1 root build folder (i.e. ./build/Debug)
+buildFolder=$1
+# param 2 system name (i.e ubuntu20)
+system=$2
+# param 3 config name (i.e Debug)
+config=$3
+# param 4 compiler (i.e GCC 10)
+compiler=`echo $4 | sed "s/ /_/"`
+
+mkdir -p time_log
+for file in `find $buildFolder/*/ -name compile_timings.txt`
+do
+    echo "collect $file"
+    cat $file >> time_log/compile_timings_${system}_${config}_${compiler}.csv
+done
+for file in `find $buildFolder/*/ -name link_timings.txt`
+do
+    echo "collect $file"
+    cat $file >> time_log/link_timings_${system}_${config}_${compiler}.csv
+done

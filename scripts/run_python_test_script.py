11f19f4e6d5cd28ee631b40849460e72b35ed7d3
MaxRayskiy <mraiskii@adalisk.com>
2022-09-08 20:06:32 +0300

    typo fix and more comments

--- scripts/run_python_test_script.py
+++ scripts/run_python_test_script.py
@@ -6,45 +6,46 @@ python_cmd = "py -3.10 "
 platformSystem = platform.system()
 
 if platformSystem == 'Linux':
-	python_cmd = "python3 "
+    python_cmd = "python3 "
 
-	os_name=""
-	os_version=""
-	if os.path.exists('/etc/os-release'):
-		lines = open('/etc/os-release').read().split('\n')
-		for line in lines:
-			if line.startswith('NAME='):
-				os_name=line.split('=')[-1].replace('"', '')
-			if line.startswith('VERSION_ID='):
-				os_version=line.split('=')[-1].replace('"', '')
+    os_name = ""
+    os_version = ""
+    if os.path.exists('/etc/os-release'):
+        lines = open('/etc/os-release').read().split('\n')
+        for line in lines:
+            if line.startswith('NAME='):
+                os_name = line.split('=')[-1].replace('"', '')
+            if line.startswith('VERSION_ID='):
+                os_version = line.split('=')[-1].replace('"', '')
 
-	if "ubuntu" in os_name.lower():
-		if os_version.startswith("20"):
-			python_cmd = "python3.8 "
-		elif os_version.startswith("22"):
-			python_cmd = "python3.10 "
-	elif "fedora" in os_name.lower():
-		if os_version.startswith("35"):
-			python_cmd = "python3.9 "
+    if "ubuntu" in os_name.lower():
+        if os_version.startswith("20"):
+            python_cmd = "python3.8 "
+        elif os_version.startswith("22"):
+            python_cmd = "python3.10 "
+    elif "fedora" in os_name.lower():
+        if os_version.startswith("35"):
+            python_cmd = "python3.9 "
 
 elif platformSystem == 'Darwin':
-	python_cmd = "python3 "
+    python_cmd = "python3 "
 
+# emulate py libs installation - collect all the necessary .pyd and .dll in meshlib folder
 if platformSystem == "Windows":
-	import glob
-	import shutil
+    import glob
+    import shutil
 
-	dest_dir = os.path.join(os.getcwd(), "meshlib")
-	if not os.path.exists(dest_dir):
-		print("Creating " + str(dest_dir))
-		os.makedirs(dest_dir)
-	print("Copying files...")
-	for file in glob.glob(r'*.dll'):
-		print(file)
-		shutil.copy(file, dest_dir)
-	for file in glob.glob(r'*py*'):
-		print(file)
-		shutil.copy(file, dest_dir)
+    dest_dir = os.path.join(os.getcwd(), "meshlib")
+    if not os.path.exists(dest_dir):
+        print("Creating " + str(dest_dir))
+        os.makedirs(dest_dir)
+    print("Copying files...")
+    for file in glob.glob(r'*.dll'):
+        print(file)
+        shutil.copy(file, dest_dir)
+    for file in glob.glob(r'*py*'):
+        print(file)
+        shutil.copy(file, dest_dir)
 
 directory = os.path.dirname(os.path.abspath(__file__))
 if len(sys.argv) == 1:
@@ -56,7 +57,7 @@ else:
 os.environ["MeshLibPyModulesPath"] = os.getcwd()
 os.chdir(directory)
 
-res = os.system(python_cmd + "-m pytest -s -v"  )
+res = os.system(python_cmd + "-m pytest -s -v")
 
-if (res != 0):
-	sys.exit(1)
+if res != 0:
+    sys.exit(1)

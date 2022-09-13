767de51c6b2dbf80599e17457813989f0afe989d
theastrowander <astrowander@gmail.com>
2022-09-13 13:35:39 +0400

    add mouse events

--- source/MRViewer/MRPlaneWidget.cpp
+++ source/MRViewer/MRPlaneWidget.cpp
@@ -23,7 +23,7 @@ onPlaneUpdate_( onPlaneUpdate )
     planeObj_->setBackColor( Color( Vector4f::diagonal( 0.3f ) ) );
     SceneRoot::get().addChild( planeObj_ );
 
-    updateWidget();
+    updateWidget_();
 }
 
 PlaneWidget::~PlaneWidget()
@@ -34,7 +34,7 @@ PlaneWidget::~PlaneWidget()
 void PlaneWidget::updatePlane( const Plane3f& plane, bool updateCameraRotation )
 {
     plane_ = plane;
-    updateWidget( updateCameraRotation );
+    updateWidget_( updateCameraRotation );
     if ( onPlaneUpdate_ )
         onPlaneUpdate_( plane );
 }
@@ -42,10 +42,10 @@ void PlaneWidget::updatePlane( const Plane3f& plane, bool updateCameraRotation )
 void PlaneWidget::updateBox( const Box3f& box, bool updateCameraRotation )
 {
     box_ = box;
-    updateWidget( updateCameraRotation );
+    updateWidget_( updateCameraRotation );
 }
 
-void PlaneWidget::updateWidget( bool updateCameraRotation )
+void PlaneWidget::updateWidget_( bool updateCameraRotation )
 {
     auto viewer = Viewer::instance();
     plane_ = plane_.normalized();
@@ -63,6 +63,56 @@ void PlaneWidget::updateWidget( bool updateCameraRotation )
     auto lastPlaneTransform = trans1 * AffineXf3f::linear( rot2 ) * rot1;
     transform = lastPlaneTransform * scale1;
     planeObj_->setXf( transform );
+}
+
+bool PlaneWidget::onMouseDown_( Viewer::MouseButton button, int mod )
+{
+    if ( button != Viewer::MouseButton::Left || mod != 0 )
+        return false;
+
+    const auto& mousePos = Viewer::instance()->mouseController.getMousePos();
+    startMousePos_ = endMousePos_ = ImVec2( float( mousePos.x ), float( mousePos.y ) );
+    pressed_ = true;
+    return true;
+}
+
+bool PlaneWidget::onMouseUp_( Viewer::MouseButton, int )
+{
+    if ( !pressed_ )
+        return false;
+
+    pressed_ = false;
+
+    ImVec2 dir;
+    dir.x = endMousePos_.x - startMousePos_.x;
+    dir.y = endMousePos_.y - startMousePos_.y;
+    if ( sqr( dir.x ) + sqr( dir.y ) < sqr( 50 ) )
+        return false; // if less then 50 pixes were in line
+
+    auto viewer = Viewer::instance();
+    auto viewportStart = viewer->screenToViewport( Vector3f( float( startMousePos_.x ), float( startMousePos_.y ), 0.f ), viewer->viewport().id );
+    auto start = viewer->viewport().unprojectFromViewportSpace( { viewportStart.x, viewportStart.y, 0.0f } );
+
+    auto viewportStop = viewer->screenToViewport( Vector3f( float( endMousePos_.x ), float( endMousePos_.y ), 0.f ), viewer->viewport().id );
+    auto stop = viewer->viewport().unprojectFromViewportSpace( { viewportStop.x, viewportStop.y, 0.0f } );
+    auto stopFar = viewer->viewport().unprojectFromViewportSpace( { viewportStop.x, viewportStop.y, 1.0f } );
+
+    auto prevNorm = plane_.n;
+    plane_ = Plane3f::fromDirAndPt( cross( stopFar - stop, stop - start ).normalized(), start );
+    if ( angle( -plane_.n, prevNorm ) < angle( plane_.n, prevNorm ) )
+        plane_ = -plane_;
+    updatePlane( plane_ );
+    return true;
+}
+
+bool PlaneWidget::onMouseMove_( int mouse_x, int mouse_y )
+{
+    if ( !pressed_ )
+        return false;
+
+    endMousePos_ = ImVec2( ( float )mouse_x, ( float )mouse_y );
+
+    return true;
 }
 
 }
--- source/MRViewer/MRPlaneWidget.h
+++ source/MRViewer/MRPlaneWidget.h
@@ -5,7 +5,7 @@
 
 namespace MR
 {
-class PlaneWidget
+class PlaneWidget : public MultiListener<MouseDownListener, MouseMoveListener, MouseUpListener>
 {
     std::shared_ptr<ObjectMesh> planeObj_;
     Plane3f plane_;
@@ -13,6 +13,10 @@ class PlaneWidget
     Vector3f cameraUp3Old_;
     
     std::function<void( const Plane3f& )> onPlaneUpdate_;
+
+    bool pressed_ = false;
+    ImVec2 startMousePos_;
+    ImVec2 endMousePos_;
 
 public:
     MRVIEWER_API PlaneWidget( const Plane3f& plane, const Box3f& box, std::function<void( const Plane3f& )> = nullptr );
@@ -22,6 +26,10 @@ public:
     MRVIEWER_API void updateBox( const Box3f& box, bool updateCameraRotation = true );
 
 private:
-    MRVIEWER_API void updateWidget( bool updateCameraRotation = true );
+    MRVIEWER_API void updateWidget_( bool updateCameraRotation = true );
+
+    MRVIEWER_API virtual bool onMouseDown_( Viewer::MouseButton button, int modifier ) override;
+    MRVIEWER_API virtual bool onMouseUp_( Viewer::MouseButton button, int modifier ) override;
+    MRVIEWER_API virtual bool onMouseMove_( int mouse_x, int mouse_y ) override;
 };
 }

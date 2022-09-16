fe939729c89a4017fdc04856d113d102352601b6
Alexey Koksin <astrowander@gmail.com>
2022-09-16 15:40:54 +0400

    add ChangePointCloudNormalsAction (#513)
    * add ChangePointCloudNormalsAction
    
    * code review

---
+++ source/MRMesh/MRChangePointCloudNormalsAction.h
@@ -0,0 +1,63 @@
+#pragma once
+#include "MRHistoryAction.h"
+#include "MRObjectPoints.h"
+#include "MRPointCloud.h"
+#include "MRHeapBytes.h"
+#include <memory>
+
+
+namespace MR
+{
+
+/// Undo action for changing normals in PointCloud
+/// \ingroup HistoryGroup
+class ChangePointCloudNormalsAction : public HistoryAction
+{
+public:
+    using Obj = ObjectPoints;
+
+    /// use this constructor to remember point cloud's normals before making any changes in it
+    ChangePointCloudNormalsAction( std::string name, const std::shared_ptr<ObjectPoints>& obj ) :
+        objPoints_{ obj },
+        name_{ std::move( name ) }
+    {
+        if ( obj )
+        {
+            if ( auto pc = obj->pointCloud() )
+                backupNormals_ = pc->normals;
+        }
+    }
+
+    virtual std::string name() const override
+    {
+        return name_;
+    }
+
+    virtual void action( HistoryAction::Type ) override
+    {
+        if ( !objPoints_ || !objPoints_->varPointCloud() )
+            return;
+        
+        std::swap( objPoints_->varPointCloud()->normals, backupNormals_ );
+        setObjectDirty( objPoints_ );
+    }
+
+    static void setObjectDirty( const std::shared_ptr<ObjectPoints>& obj )
+    {
+        if ( obj )
+            obj->setDirtyFlags( DIRTY_RENDER_NORMALS );
+    }
+
+    [[nodiscard]] virtual size_t heapBytes() const override
+    {
+        return name_.capacity() + backupNormals_.heapBytes();
+    }
+
+private:
+    std::shared_ptr<ObjectPoints> objPoints_;
+    VertCoords backupNormals_;
+
+    std::string name_;
+};
+
+}

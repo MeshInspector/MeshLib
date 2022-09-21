0b74224d69c154c7406ba7061a0d2f94369829ba
Maxim Rayskiy <mraiskii@adalisk.com>
2022-09-21 11:56:12 +0100

    Pip: add win10 (#497)
    * Pip: add win10/py3.10
    
    * py script fix
    
    * update workflow, add win and mac wheels
    
    * test new workflow
    
    * fix: add version
    
    * fix
    
    * test versioning
    
    * update win wheel
    
    * general fix
    
    * fix
    
    * debug win
    
    * debug win
    
    * debug win
    
    * debug win
    
    * debug win
    
    * debug win
    
    * fix win
    
    * fix win
    
    * workflow fix
    
    * macos dylib fix
    
    * save progress before merging
    
    * code review
    
    * rm python tag
    
    * fix extension, do not upload mac
    
    * use python-tag
    
    * update setup.py
    
    * undo debug before merge
    
    * do not wait for macos

--- scripts/wheel/setup.py
+++ scripts/wheel/setup.py
@@ -1,15 +1,38 @@
-# used in pair with create_wheel.sh
 import setuptools
 import pathlib
+import argparse
+import os
+import platform
+import sys
+
+
+platform_system = platform.system()
+print(platform_system)
+
+VERSION = ""
+
+if '--version' in sys.argv:
+    index = sys.argv.index('--version')
+    sys.argv.pop(index)  # Removes the '--foo'
+    VERSION = sys.argv.pop(index)  # Returns the element after the '--foo'
+
+PY_VERSION=str(sys.version_info[0]) + "." + str(sys.version_info[1])
+LIBS_EXTENSION = ""
+
+if platform_system == "Windows":
+    LIBS_EXTENSION = "pyd"
+elif platform_system == "Linux":
+    LIBS_EXTENSION = "so"
+elif platform_system == "Darwin":
+    LIBS_EXTENSION = "so"
 
 here = pathlib.Path(__file__).parent.resolve()
-
 # Get the long description from the readme file
 long_description = (here / "readme.md").read_text(encoding="utf-8")
 
 setuptools.setup(
     name="meshlib",
-    version='',
+    version=VERSION,
     author="MeshLib Team",
     author_email="support@meshinspector.com",
     description="3d processing library",
@@ -18,15 +41,18 @@ setuptools.setup(
     url="https://github.com/MeshInspector/MeshLib",
     license_files=('LICENSE',),
     packages=['meshlib'],
-    package_data={'meshlib': ['mrmeshnumpy.so', 'mrmeshpy.so', 'mrviewerpy.so']},
+    package_data={'meshlib': ['mrmeshnumpy.{ext}', 'mrmeshpy.{ext}', 'mrviewerpy.{ext}'.format(ext=LIBS_EXTENSION)]},
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 3.8",
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
         "License :: Free for non-commercial use",
         "License :: Free For Educational Use",
+        "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
     ],
-    python_requires='',
+    python_requires='>=3.8',
     install_requires=[
         'numpy>=1.21.0',
         'pytest>=7.1.0',

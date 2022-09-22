a07c117782212fb689f55438646ffd4887b65bbd
Alexey Koksin <akoksin@adalisk.com>
2022-09-22 13:33:27 +0400

    forbid double click in palette (#533)

--- source/MRViewer/MRPalette.cpp
+++ source/MRViewer/MRPalette.cpp
@@ -5,6 +5,7 @@
 #include "MRMesh/MRSystem.h"
 #include "MRMesh/MRStringConvert.h"
 #include "MRViewer.h"
+#include "imgui_internal.h"
 #include <string>
 #include <fstream>
 
@@ -328,6 +329,9 @@ void Palette::draw( const ImVec2& pose, const ImVec2& size )
     ImGui::SetNextWindowPos( pose, ImGuiCond_FirstUseEver );
     ImGui::SetNextWindowSize( size, ImGuiCond_Appearing );
     ImGui::SetNextWindowSizeConstraints( { maxTextSize + style.WindowPadding.x + style.FramePadding.x, labels_.size() * ImGui::GetTextLineHeightWithSpacing() }, { width( windowSize ), height( windowSize ) } );
+    const auto ctx = ImGui::GetCurrentContext();
+    if ( ctx && ctx->IO.MouseClickedCount[0] == 2 )
+        ctx->IO.MouseClickedCount[0] = 1; // prevent double-click on the corner to change window size
 
     ImGui::Begin( "Gradient palette", &isWindowOpen_,
         ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoBackground );

e9855bb5cb77d16e34ae904b1d726c53028c8c72
MaxRayskiy <mraiskii@adalisk.com>
2022-09-26 14:51:53 +0300

    fix path import

--- test_python/helper/__init__.py
+++ test_python/helper/__init__.py
@@ -3,7 +3,7 @@ import sys
 import pytest
 
 # insert 0 to find mrpy.so in working directory and not in system
-if os.environ["MeshLibPyModulesPath"] and not os.environ["MeshLibPyModulesPath"] in sys.path:
+if "MeshLibPyModulesPath" in os.environ and not os.environ["MeshLibPyModulesPath"] in sys.path:
     sys.path.insert(0, os.environ["MeshLibPyModulesPath"])
 
 import meshlib.mrmeshpy as mrmesh

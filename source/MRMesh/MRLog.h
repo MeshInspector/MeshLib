5116f150dd6450efe44a8b217c2f1f7621b8f21b
Grant Karapetyan <GKarapetyan@adalisk.com>
2022-09-28 01:21:30 +0400

    Log crash info (#549)
    * Log crash info
    
    * fix win container rebuild
    
    * upd workflow tag
    
    * another attempt
    
    * fix build
    
    * split win and linux images
    
    * fix
    
    * fix macos build
    
    * fix macos build
    
    * Fix builds
    
    * no stacktrace in wasm
    
    * Update build-test-distribute.yml
    
    Co-authored-by: Maxim Rayskiy <mraiskii@adalisk.com>

--- source/MRMesh/MRLog.h
+++ source/MRMesh/MRLog.h
@@ -37,12 +37,18 @@ private:
 /// 1) makes stdout sink
 /// 2) makes file sink (MRLog.txt)
 /// 3) redirect std streams to logger
+/// 4) print stacktrace on crash (not in wasm)
 /// log level - trace
 MRMESH_API void setupLoggerByDefault();
 
 /// Redirects stdcout stdcerr stdclog to default logger
 /// \note do not call this function directly if you use MR::setupLoggerByDefault()
 MRMESH_API void redirectSTDStreamsToLogger();
+
+#ifndef __EMSCRIPTEN__
+/// Print stacktrace on application crash
+MRMESH_API void printStacktraceOnCrash();
+#endif
 
 /// \}
 

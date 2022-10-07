65662a1c70734c66690b34c23638ac830c2184b4
Fedr <fchelnokov@adalisk.com>
2022-10-07 12:54:19 +0300

    Delone: an option to optimize by triangle aspect ratio

--- source/MRMesh/MRMeshDelone.h
+++ source/MRMesh/MRMeshDelone.h
@@ -15,6 +15,9 @@ struct DeloneSettings
     float maxDeviationAfterFlip = FLT_MAX;
     /// Maximal allowed dihedral angle change over the flipped edge
     float maxAngleChange = NoAngleChangeLimit;
+    /// if this value is less than FLT_MAX then the algorithm will try to minimize maximal triangle aspect ratio,
+    /// and ignore dihedral angle check if one of triangles had aspect ratio equal or more than this value
+    float criticalTriAspectRatio = FLT_MAX;
     /// Region on mesh to be processed, it is constant and not updated
     const FaceBitSet* region = nullptr;
     /// Edges specified by this bit-set will never be flipped
@@ -33,6 +36,13 @@ struct DeloneSettings
 /// differ more than on maxAngleChange then also returns true to prevent flipping from 1) to 2)
 MRMESH_API bool checkDeloneQuadrangle( const Vector3d& a, const Vector3d& b, const Vector3d& c, const Vector3d& d, double maxAngleChange = NoAngleChangeLimit );
 MRMESH_API bool checkDeloneQuadrangle( const Vector3f& a, const Vector3f& b, const Vector3f& c, const Vector3f& d, float maxAngleChange = NoAngleChangeLimit );
+
+/// given quadrangle ABCD, checks whether its subdivision on 
+/// (triangles ABD and DBC) is better than on (triangles ABC and ACD),
+/// where "better" means smaller maximum triangle aspect ratio and the flip of edge from BD to AC will
+/// not change dihedral angle more than on given threshold
+MRMESH_API bool checkAspectRatiosInQuadrangle( const Vector3d& a, const Vector3d& b, const Vector3d& c, const Vector3d& d, double maxAngleChange, double criticalTriAspectRatio );
+MRMESH_API bool checkAspectRatiosInQuadrangle( const Vector3f& a, const Vector3f& b, const Vector3f& c, const Vector3f& d, float maxAngleChange, float criticalTriAspectRatio );
 
 /// consider quadrangle formed by left and right triangles of given edge, and
 /// checks whether this edge satisfies Delone's condition in the quadrangle;

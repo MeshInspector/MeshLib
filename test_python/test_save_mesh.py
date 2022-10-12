31a474705f0ab9cd2dd14b249a9bb3f202515174
Daniil <dantipov@adalisk.com>
2022-10-12 11:49:20 +0300

    add test save_load_mesh

--- test_python/test_save_mesh.py
+++ test_python/test_save_mesh.py
@@ -2,8 +2,15 @@ from helper import *
 import pytest
 
 
-def test_save_mesh():
+def test_save_load_mesh():
     torus = mrmesh.makeTorus(2, 1, 10, 10, None)
-    smesh = mrmesh.saveMesh(torus, mrmesh.Path("testTorus_dm.stl"))
+    fs = open("test_save_load_mesh.mrmesh", "wb")
+    smesh = mrmesh.saveMesh(torus, "*.mrmesh", fs)
+    fs.close()
     assert (smesh.has_value())
-    os.remove("testTorus_dm.stl")
+    fr = open("test_save_load_mesh.mrmesh", "rb")
+    torus2 = mrmesh.loadMesh(fr, "*.mrmesh")
+    fr.close()
+    assert(torus2.has_value())
+    assert(torus2.value() == torus)
+    os.remove("test_save_load_mesh.mrmesh")

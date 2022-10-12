b1d1f4322ea113ae2523d54ff27f93b02c633bff
Daniil <dantipov@adalisk.com>
2022-10-12 08:58:36 +0300

    fis test seva_mesh

--- test_python/test_save_mesh.py
+++ test_python/test_save_mesh.py
@@ -4,6 +4,6 @@ import pytest
 
 def test_save_mesh():
     torus = mrmesh.makeTorus(2, 1, 10, 10, None)
-    smesh = mrmesh.saveMesh(torus, mrmesh.Path("c:/temp/testTorus_dm.stl"))
+    smesh = mrmesh.saveMesh(torus, mrmesh.Path("testTorus_dm.stl"))
     assert (smesh.has_value())
-    os.remove("c:/temp/testTorus_dm.stl")
+    os.remove("testTorus_dm.stl")

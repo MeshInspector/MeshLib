04608bc68959910fa4d140c984b9f789f730cdb7
Grant Karapetyan <GKarapetyan@adalisk.com>
2022-10-27 13:36:05 +0400

    Update test_saveSliceToImage.py

--- test_python/test_saveSliceToImage.py
+++ test_python/test_saveSliceToImage.py
@@ -9,4 +9,4 @@ def test_save_slice_to_image():
     params.voxelSize = mrmesh.Vector3f(0.1, 0.1, 0.1)
     expvdbvolume = mrmesh.meshToVolume(mesh, params)
     assert (expvdbvolume.has_value())
-    assert (mrmesh.saveSliceToImage("D:\\slice.png", expvdbvolume.value(), mrmesh.SlicePlain.XY, 26).has_value())
+    assert (mrmesh.saveSliceToImage("slice.png", expvdbvolume.value(), mrmesh.SlicePlain.XY, 26).has_value())

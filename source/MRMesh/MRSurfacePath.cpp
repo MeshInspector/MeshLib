9434e231fb31127975dc2d452a446cc414bbb6cb
Fedr <fchelnokov@adalisk.com>
2022-11-12 11:55:41 +0300

    Shortest edge path construction for start/end points not only in vertices (#688)
    * Shortest edge path construction for start/end points not only in vertices
    
    * start working on buildShortestPathAStar
    
    * Redesign of EdgePathsBuilder
    
    * buildShortestPathAStar works now

--- source/MRMesh/MRSurfacePath.cpp
+++ source/MRMesh/MRSurfacePath.cpp
@@ -314,11 +314,12 @@ tl::expected<SurfacePath, PathError> computeGeodesicPathApprox( const Mesh & mes
     SurfacePath res;
     if ( !fromSameTriangle( mesh.topology, MeshTriPoint{ start }, MeshTriPoint{ end } ) )
     {
-        VertId v1 = mesh.getClosestVertex( start );
-        VertId v2 = mesh.getClosestVertex( end );
-        auto edgePath = atype == GeodesicPathApprox::DijkstraBiDir
-            ? buildShortestPathBiDir( mesh, v1, v2 ) :
-              buildShortestPathAStar( mesh, v1, v2 );
+        VertId v1, v2;
+        EdgePath edgePath = ( atype == GeodesicPathApprox::DijkstraBiDir ) ?
+            buildShortestPathBiDir( mesh, start, end, &v1, &v2 ) :
+            buildShortestPathAStar( mesh, start, end, &v1, &v2 );
+        if ( !v1 || !v2 )
+            return tl::make_unexpected( PathError::StartEndNotConnected );
 
         // remove last segment from the path if end-point and the origin of last segment belong to one triangle
         while( !edgePath.empty()
@@ -338,8 +339,7 @@ tl::expected<SurfacePath, PathError> computeGeodesicPathApprox( const Mesh & mes
 
         if ( edgePath.empty() )
         {
-            if ( v1 != v2 )
-                return tl::make_unexpected( PathError::StartEndNotConnected );
+            assert ( v1 == v2 );
             res = { MeshEdgePoint( mesh.topology.edgeWithOrg( v1 ), 0.0f ) };
         }
         else

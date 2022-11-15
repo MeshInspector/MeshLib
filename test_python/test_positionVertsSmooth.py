a0178fd04ea2770bc052c5f27d8d7a6ebff32911
Fedr <fchelnokov@adalisk.com>
2022-11-15 22:10:44 +0300

    fix 2

--- test_python/test_positionVertsSmooth.py
+++ test_python/test_positionVertsSmooth.py
@@ -15,6 +15,6 @@ def test_position_vers_smooth():
     verts.set(mrmesh.VertId(0), False)
     mrmesh.positionVertsSmoothly(torus, verts, params)
 
-    # now all points are in that range from the center
+    p = torus.points.vec[0]
     for i in torus.points.vec:
-        assert (i.x * i.x + i.y * i.y + i.z * i.z == 0.)
+        assert (i.x * i.x + i.y * i.y + i.z * i.z == p.x * p.x + p.y * p.y + p.z * p.z)

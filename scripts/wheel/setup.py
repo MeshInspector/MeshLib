13a597eac680092e5d2ca0f7d4935ee7ef142d58
MaxRayskiy <mraiskii@adalisk.com>
2022-11-17 19:41:18 +0300

    add python3.11 wheel

--- scripts/wheel/setup.py
+++ scripts/wheel/setup.py
@@ -47,6 +47,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         "License :: Free for non-commercial use",
         "License :: Free For Educational Use",
         "Operating System :: Microsoft :: Windows",

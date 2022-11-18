f45e35bc718d52bb3563da11bcae4e324eb72899
Fedr <fchelnokov@adalisk.com>
2022-11-18 19:18:16 +0300

    2d ray - polyline intersection

---
+++ source/MRMesh/MRRayBoxIntersection2.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "MRBox.h"
+#include "MRIntersectionPrecomputes2.h"
+
+namespace MR
+{
+
+template<typename T>
+bool rayBoxIntersect( const Box2f& box, const Vector2<T> & rayOrigin, T & t0, T & t1, const IntersectionPrecomputes2<T>& prec )
+{
+    const Vector3i& sign = prec.sign;
+
+    // compare and update x-dimension with t0-t1
+    t1 = std::min( (box[sign.x].x - rayOrigin.p.x) * prec.invDir.x, t1 );
+    t0 = std::max( (box[1 - sign.x].x - rayOrigin.p.x) * prec.invDir.x, t0 );
+
+    // compare and update y-dimension with t0-t1
+    t1 = std::min( (box[sign.y].y - rayOrigin.p.y) * prec.invDir.y, t1 );
+    t0 = std::max( (box[1 - sign.y].y - rayOrigin.p.y) * prec.invDir.y, t0 );
+
+    return t0 <= t1;
+}
+
+template <typename T = float>
+bool rayBoxIntersect( const Box2<T>& box, const Line2<T>& line, T t0, T t1 )
+{
+    IntersectionPrecomputes2<T> prec( line.d );
+    return rayBoxIntersect( box, line, t0, t1, prec );
+}
+
+/// \}
+
+}

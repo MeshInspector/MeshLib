c160c5c5c21b31e89c151fe8bd17145d8d460c6a
Fedr <fchelnokov@adalisk.com>
2022-11-18 10:36:12 +0300

    findEdgesInBall (#708)
    * findEdgesInBall
    
    * #include <functional>

--- source/MRMesh/MRPolylineProject.h
+++ source/MRMesh/MRPolylineProject.h
@@ -2,6 +2,7 @@
 #include "MRId.h"
 #include "MRVector3.h"
 #include <cfloat>
+#include <functional>
 
 namespace MR
 {
@@ -64,6 +65,17 @@ MRMESH_API Polyline2ProjectionWithOffsetResult findProjectionOnPolyline2WithOffs
  */
 MRMESH_API PolylineProjectionWithOffsetResult3 findProjectionOnPolylineWithOffset( const Vector3f& pt, const Polyline3& polyline,
     const Vector<float, UndirectedEdgeId>& offsetPerEdge, float upDistLimit = FLT_MAX, AffineXf3f* xf = nullptr );
+
+template<typename V>
+using FoundEdgeCallback = std::function<void( UndirectedEdgeId, const V& closestPt, float distSq )>;
+using FoundEdgeCallback2 = FoundEdgeCallback<Vector2f>;
+using FoundEdgeCallback3 = FoundEdgeCallback<Vector3f>;
+
+/// Finds all edges of given polyline that cross or touch given ball (center, radius)
+MRMESH_API void findEdgesInBall( const Polyline2& polyline, const Vector2f& center, float radius, const FoundEdgeCallback2& foundCallback, AffineXf2f* xf = nullptr );
+
+/// Finds all edges of given polyline that cross or touch given ball (center, radius)
+MRMESH_API void findEdgesInBall( const Polyline3& polyline, const Vector3f& center, float radius, const FoundEdgeCallback3& foundCallback, AffineXf3f* xf = nullptr );
 
 /**
  * \brief computes the closest point on the mesh edges (specified by the tree) to given point

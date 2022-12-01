b83b5fa19735e4394cc9fa45f0c98baefe3b4a82
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2022-12-01 16:03:52 +0400

    Preload file to memory and operate with its lines

--- source/MRMesh/MRMeshLoadObj.cpp
+++ source/MRMesh/MRMeshLoadObj.cpp
@@ -2,6 +2,7 @@
 #include "MRStringConvert.h"
 #include "MRMeshBuilder.h"
 #include "MRTimer.h"
+#include "MRBuffer.h"
 
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
@@ -11,7 +12,7 @@ namespace
 {
     using namespace MR;
 
-    tl::expected<Vector3f, std::string> parse_obj_vertex( const std::string& str )
+    tl::expected<Vector3f, std::string> parse_obj_vertex( const std::string_view& str )
     {
         namespace qi = boost::spirit::qi;
         namespace ascii = boost::spirit::ascii;
@@ -22,7 +23,7 @@ namespace
         bool r = qi::phrase_parse(
             str.begin(),
             str.end(),
-            ( qi::float_[ref( v.x ) = qi::_1] >> qi::float_[ref( v.y ) = qi::_1] >> qi::float_[ref( v.z ) = qi::_1] ),
+            ( 'v' >> qi::float_[ref( v.x ) = qi::_1] >> qi::float_[ref( v.y ) = qi::_1] >> qi::float_[ref( v.z ) = qi::_1] ),
             ascii::space
         );
         if ( !r )
@@ -48,7 +49,7 @@ namespace
         index_accumulator normals;
     };
 
-    tl::expected<obj_face_indices, std::string> parse_obj_face( const std::string& str )
+    tl::expected<obj_face_indices, std::string> parse_obj_face( const std::string_view& str )
     {
         namespace qi = boost::spirit::qi;
         namespace ascii = boost::spirit::ascii;
@@ -62,6 +63,7 @@ namespace
             str.begin(),
             str.end(),
             (
+                'f' >>
                 *( qi::int_[ref( vs.vertices ) += qi::_1]
                     >> -(
                         ( '/' >> qi::int_[ref( vs.textures ) += qi::_1] )
@@ -109,6 +111,7 @@ tl::expected<std::vector<NamedMesh>, std::string> fromSceneObjFile( std::istream
 
     auto finishObject = [&]() 
     {
+        MR_NAMED_TIMER( "finish object" )
         if ( !t.empty() )
         {
             res.emplace_back();
@@ -138,30 +141,42 @@ tl::expected<std::vector<NamedMesh>, std::string> fromSceneObjFile( std::istream
     in.seekg( 0, std::ios_base::end );
     const auto posEnd = in.tellg();
     in.seekg( posStart );
-    const float streamSize = float( posEnd - posStart );
+    const auto streamSize = posEnd - posStart;
+
+    Buffer<char> data;
+    {
+        MR_NAMED_TIMER( "resize buffer" )
+        data.resize( streamSize + 1 );
+    }
+    {
+        MR_NAMED_TIMER( "read data" )
+        in.read( data.data(), streamSize );
+    }
+    if ( !in )
+        return tl::make_unexpected( std::string( "OBJ-format read error" ) );
+    if ( data[streamSize - 1] != '\n' )
+        data[streamSize] = '\n';
+    else
+        data.resize( streamSize );
+
+    std::vector<size_t> newlines{ 0 };
+    for ( size_t i = 0; i < data.size(); i++ )
+        if ( data[i] == '\n' )
+            newlines.emplace_back( i + 1 );
 
     std::vector<int> vs;
-    for ( int i = 0;; ++i )
+    for ( int i = 0; i + 1 < newlines.size(); ++i )
     {
-        if ( !in )
-            return tl::make_unexpected( std::string( "OBJ-format read error" ) );
-        char ch = 0;
-        in >> ch;
-        if ( in.eof() )
-            break;
-        if ( ch == 'v' && in.peek() != 'n' /*normals*/ && in.peek() != 't' /*texture coordinates*/ )
+        std::string_view line( data.data() + newlines[i], newlines[i + 1] - newlines[i + 0] );
+        if ( line[0] == 'v' && line[1] != 'n' /*normals*/ && line[1] != 't' /*texture coordinates*/ )
         {
-            std::string line;
-            std::getline( in, line );
             auto v = parse_obj_vertex( line );
             if ( !v.has_value() )
                 return tl::make_unexpected( v.error() );
             points.emplace_back( *v );
         }
-        else if ( ch == 'f' )
+        else if ( line[0] == 'f' )
         {
-            std::string line;
-            std::getline( in, line );
             auto is = parse_obj_face( line );
             if ( !is.has_value() )
                 return tl::make_unexpected( is.error() );
@@ -183,12 +198,12 @@ tl::expected<std::vector<NamedMesh>, std::string> fromSceneObjFile( std::istream
             for ( int j = 1; j + 1 < vs.size(); ++j )
                 t.push_back( { VertId( vs[0]-1 ), VertId( vs[j]-1 ), VertId( vs[j+1]-1 ) } );
         }
-        else if ( ch == 'o' )
+        else if ( line[0] == 'o' )
         {
             if ( !combineAllObjects )
                 finishObject();
             // next object
-            getline( in, currentObjName );
+            currentObjName = line.substr( 1, std::string::npos );
             while ( !currentObjName.empty() && currentObjName[0] == ' ' )
                 currentObjName.erase( currentObjName.begin() );
             {
@@ -198,17 +213,13 @@ tl::expected<std::vector<NamedMesh>, std::string> fromSceneObjFile( std::istream
         else
         {
             // skip unknown line
-            std::string str;
-            std::getline( in, str );
         }
         if ( callback && !(i & 0x3FF) )
         {
-            const float progress = int( in.tellg() - posStart ) / streamSize;
+            const float progress = float( i + 1 ) / float( newlines.size() );
             if ( !callback( progress ) )
                 return tl::make_unexpected( std::string( "Loading canceled" ));
         }
-        if ( in.eof() )
-            break;
     }
 
     finishObject();

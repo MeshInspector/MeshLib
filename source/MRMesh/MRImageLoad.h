2b61c343cd3fb0ec296cb97121f513bfcfc451a0
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2022-12-02 17:30:33 +0400

    Add read support for JPEG images

--- source/MRMesh/MRImageLoad.h
+++ source/MRMesh/MRImageLoad.h
@@ -23,6 +23,15 @@ MRMESH_API extern const IOFilters Filters;
 MRMESH_API tl::expected<Image, std::string> fromPng( const std::filesystem::path& path );
 #endif
 
+#ifndef __EMSCRIPTEN__
+
+#ifndef MRMESH_NO_JPEG
+/// loads from .jpg format
+MRMESH_API tl::expected<Image, std::string> fromJpeg( const std::filesystem::path& path );
+#endif
+
+#endif
+
 /// detects the format from file extension and loads image from it
 MRMESH_API tl::expected<Image, std::string> fromAnySupportedFormat( const std::filesystem::path& path );
 

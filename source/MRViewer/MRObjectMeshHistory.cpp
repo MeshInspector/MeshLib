28407256391bcab11d71ff85fcaad20651e36e45
Fedr <fchelnokov@adalisk.com>
2022-12-06 13:52:22 +0300

    mapEdgesWithHistory with WholeEdgeHashMap argument

--- source/MRViewer/MRObjectMeshHistory.cpp
+++ source/MRViewer/MRObjectMeshHistory.cpp
@@ -52,7 +52,33 @@ void excludeAllEdgesWithHistory( const std::shared_ptr<ObjectMesh>& objMesh )
     return res;
 }
 
+[[nodiscard]] static UndirectedEdgeBitSet getMapping( const UndirectedEdgeBitSet & src, const WholeEdgeHashMap & map )
+{
+    UndirectedEdgeBitSet res;
+    for ( auto b : src )
+        if ( auto mapped = getAt( map, b ) )
+            res.autoResizeSet( mapped.undirected() );
+    return res;
+}
+
 void mapEdgesWithHistory( const std::shared_ptr<ObjectMesh>& objMesh, const WholeEdgeMap & emap )
+{
+    MR_TIMER
+    if ( !objMesh )
+        return;
+
+    // update edges in the selection
+    auto selEdges = getMapping( objMesh->getSelectedEdges(), emap );
+    Historian<ChangeMeshEdgeSelectionAction> hes( "edge selection", objMesh );
+    objMesh->selectEdges( std::move( selEdges ) );
+
+    // update edges in the creases
+    auto creases = getMapping( objMesh->creases(), emap );
+    Historian<ChangeMeshCreasesAction> hcr( "creases", objMesh );
+    objMesh->setCreases( std::move( creases ) );
+}
+
+void mapEdgesWithHistory( const std::shared_ptr<ObjectMesh>& objMesh, const WholeEdgeHashMap & emap )
 {
     MR_TIMER
     if ( !objMesh )

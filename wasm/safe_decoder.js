eb6189360ee91c0e86499b4c8eaed141ac809f62
Grant Karapetyan <GKarapetyan@adalisk.com>
2022-12-06 16:29:16 +0400

    Safe decoder for multithreaded wasm (#778)

---
+++ wasm/safe_decoder.js
@@ -0,0 +1,35 @@
+
+var registerSafeDecoder = function () {
+    UTF8ArrayToString = function (heapOrArray, idx, maxBytesToRead) {
+        var endIdx = idx + maxBytesToRead;
+        var endPtr = idx;
+        while (heapOrArray[endPtr] && !(endPtr >= endIdx))
+            ++endPtr;
+        var str = "";
+        while (idx < endPtr) {
+            var u0 = heapOrArray[idx++];
+            if (!(u0 & 128)) {
+                str += String.fromCharCode(u0);
+                continue
+            }
+            var u1 = heapOrArray[idx++] & 63;
+            if ((u0 & 224) == 192) {
+                str += String.fromCharCode((u0 & 31) << 6 | u1);
+                continue
+            }
+            var u2 = heapOrArray[idx++] & 63;
+            if ((u0 & 240) == 224) {
+                u0 = (u0 & 15) << 12 | u1 << 6 | u2
+            } else {
+                u0 = (u0 & 7) << 18 | u1 << 12 | u2 << 6 | heapOrArray[idx++] & 63
+            }
+            if (u0 < 65536) {
+                str += String.fromCharCode(u0)
+            } else {
+                var ch = u0 - 65536;
+                str += String.fromCharCode(55296 | ch >> 10, 56320 | ch & 1023)
+            }
+        }
+        return str
+    }
+}

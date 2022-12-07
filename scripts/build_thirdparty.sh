1cbe505ac4882c60b9bcd937234a4a659a088360
MaxRayskiy <mraiskii@adalisk.com>
2022-12-07 14:49:32 +0300

    fedora+mac fix

--- scripts/build_thirdparty.sh
+++ scripts/build_thirdparty.sh
@@ -95,6 +95,10 @@ fi
 
 # copy .so libs (some of them are handled by their cmake --install, but some are not)
 echo "Copying shared libs.."
-cp "${MR_THIRDPARTY_BUILD_DIR}"/*.so "${MR_THIRDPARTY_LIB_DIR}"/
+if [[ $OSTYPE == 'darwin'* ]]; then
+  cp "${MR_THIRDPARTY_BUILD_DIR}"/*.dylib "${MR_THIRDPARTY_LIB_DIR}"/
+else
+  cp "${MR_THIRDPARTY_BUILD_DIR}"/*.so "${MR_THIRDPARTY_LIB_DIR}"/
+fi
 
 printf "\rThirdparty build script successfully finished. Required libs located in ./lib folder. You could run ./scripts/build_source.sh\n\n"

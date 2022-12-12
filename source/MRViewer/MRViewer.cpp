43fbc35954e874488eb3d9cb64d54271c23f1cda
Grant Karapetyan <gkarapetyan@adalisk.com>
2022-12-12 14:37:53 +0400

    CR Fix

--- source/MRViewer/MRViewer.cpp
+++ source/MRViewer/MRViewer.cpp
@@ -1278,15 +1278,14 @@ void Viewer::draw_( bool force )
     setupScene();
     preDrawSignal();
 
-    bool swapped = forceRedrawFramesWithoutSwap_ <= 1;
-    if ( swapped )
+    if ( forceRedrawFramesWithoutSwap_ <= 1 ) // if swapped
         drawScene();
 
     postDrawSignal();
 
     if ( forceRedrawFramesWithoutSwap_ > 0 )
         forceRedrawFramesWithoutSwap_--;
-    swapped = forceRedrawFramesWithoutSwap_ == 0;
+    auto swapped = forceRedrawFramesWithoutSwap_ == 0;
 
     if ( forceRedrawFrames_ > 0 )
     {

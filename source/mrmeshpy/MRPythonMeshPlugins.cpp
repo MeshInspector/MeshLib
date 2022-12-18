32b0887da85f21b162f2e05a743acc352aa9afd3
Fedr <fchelnokov@adalisk.com>
2022-12-18 23:33:15 +0300

    expose surfacePathLength in python

--- source/mrmeshpy/MRPythonMeshPlugins.cpp
+++ source/mrmeshpy/MRPythonMeshPlugins.cpp
@@ -383,7 +383,12 @@ MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, GeodesicPath, [] ( pybind11::module_& m )
 
     m.def( "computeGeodesicPath", &MR::computeGeodesicPath,
         pybind11::arg( "mesh" ), pybind11::arg( "start" ), pybind11::arg( "end" ), pybind11::arg( "atype" ), pybind11::arg( "maxGeodesicIters") = 100,
-            "Returns intermediate points of the geodesic path from start to end, where it crosses mesh edges"
+        "Returns intermediate points of the geodesic path from start to end, where it crosses mesh edges"
+    );
+
+    m.def( "surfacePathLength", &MR::surfacePathLength,
+        pybind11::arg( "mesh" ), pybind11::arg( "surfacePath" ),
+        "Computes the length of surface path"
     );
 
     m.def( "computeSurfaceDistances", (MR::Vector<float, MR::VertId>(*)(const MR::Mesh&, const MeshTriPoint&, float maxDist, const VertBitSet*, int ) )&MR::computeSurfaceDistances,

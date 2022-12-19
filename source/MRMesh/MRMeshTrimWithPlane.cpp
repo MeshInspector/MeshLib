0f0d634abdf0a47e33640dd1d8e44fbe73dbe6c9
Fedr <fchelnokov@adalisk.com>
2022-12-19 13:00:31 +0300

    subdivideWithPlane and trimWithPlane

---
+++ source/MRMesh/MRMeshTrimWithPlane.cpp
@@ -0,0 +1,85 @@
+#include "MRMeshTrimWithPlane.h"
+#include "MRMesh.h"
+#include "MRPlane3.h"
+#include "MRBitSetParallelFor.h"
+#include "MRRingIterator.h"
+#include "MRTimer.h"
+
+namespace MR
+{
+
+FaceBitSet subdivideWithPlane( Mesh & mesh, const Plane3f & plane )
+{
+    MR_TIMER
+    VertBitSet positiveVerts( mesh.topology.lastValidVert() + 1 );
+    VertBitSet negativeVerts( positiveVerts.size() );
+
+    BitSetParallelForAll( positiveVerts, [&]( VertId v )
+    {
+        const auto d = plane.distance( mesh.points[v] );
+        if ( d > 0 )
+            positiveVerts.set( v );
+        else if ( d < 0 )
+            negativeVerts.set( v );
+    } );
+
+    UndirectedEdgeBitSet edgesToCut( mesh.topology.undirectedEdgeSize() );
+    BitSetParallelForAll( edgesToCut, [&]( UndirectedEdgeId ue )
+    {
+        const VertId o = mesh.topology.org( ue );
+        const VertId d = mesh.topology.dest( ue );
+        if ( ( positiveVerts.test( o ) && negativeVerts.test( d ) )
+          || ( positiveVerts.test( d ) && negativeVerts.test( o ) ) )
+            edgesToCut.set( ue );
+    } );
+
+    FaceBitSet positiveFaces( mesh.topology.lastValidFace() + 1 );
+    BitSetParallelForAll( positiveFaces, [&]( FaceId f )
+    {
+        VertId vs[3];
+        mesh.topology.getTriVerts( f, vs );
+        bool pos = false;
+        bool neg = false;
+        for ( VertId v : vs )
+        {
+            if ( positiveVerts.test( v ) )
+                pos = true;
+            else if ( negativeVerts.test( v ) )
+                neg = true;
+        }
+        if ( pos && !neg )
+            positiveFaces.set( f );
+    } );
+
+    MR_WRITER( mesh );
+    for ( EdgeId e : edgesToCut )
+    {
+        const VertId vo = mesh.topology.org( e );
+        const VertId vd = mesh.topology.dest( e );
+        const auto po = mesh.points[vo];
+        const auto pd = mesh.points[vd];
+        const auto o = plane.distance( po );
+        const auto d = plane.distance( pd );
+        assert( o * d < 0 );
+        const auto p = ( o * pd - d * po ) / ( o - d );
+        mesh.splitEdge( e, p );
+        for ( EdgeId ei : orgRing( mesh.topology, e ) )
+        {
+            const auto l = mesh.topology.left( ei );
+            if ( l && positiveVerts.test( mesh.topology.dest( ei ) )
+                   && positiveVerts.test( mesh.topology.dest( mesh.topology.next( ei ) ) ) )
+                positiveFaces.autoResizeSet( l );
+        }
+    }
+
+    return positiveFaces;
+}
+
+void trimWithPlane( Mesh& mesh, const Plane3f & plane )
+{
+    MR_TIMER
+    const auto posFaces = subdivideWithPlane( mesh, plane );
+    mesh.topology.deleteFaces( mesh.topology.getValidFaces() - posFaces );
+}
+
+} //namespace MR

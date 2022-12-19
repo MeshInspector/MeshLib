4f47ba3b2ffc87cd4365beda550f3e7bc60d73b6
ABSitf <absitf.sun@gmail.com>
2022-12-19 20:36:04 +0300

    fix

--- source/MRMesh/MRProgressCallback.h
+++ source/MRMesh/MRProgressCallback.h
@@ -1,8 +1,5 @@
 #pragma once
-#include "MRStringConvert.h"
 #include <functional>
-#include <string>
-#include <filesystem>
 
 namespace MR
 {
@@ -11,11 +8,5 @@ namespace MR
 /// returns true to continue the operation and returns false to stop the operation
 /// \ingroup BasicStructuresGroup
 typedef std::function<bool( float )> ProgressCallback;
-
-// Returns message showed when loading is canceled
-inline std::string getCancelMessage( const std::filesystem::path& path )
-{
-	return "Loading canceled: " + utf8string( path );
-}
 
 }

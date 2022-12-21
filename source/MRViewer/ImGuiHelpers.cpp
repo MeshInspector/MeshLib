5f2b2c320127213fe18f90167bbd2da65d46f6ba
ADALISK\akoksin <akoksin@adalisk.com>
2022-12-21 23:47:23 +0400

    left-align long text in InputTextCenteredReadOnly

--- source/MRViewer/ImGuiHelpers.cpp
+++ source/MRViewer/ImGuiHelpers.cpp
@@ -263,7 +263,8 @@ void InputTextCenteredReadOnly( const char* label, const std::string& str, float
     const auto actualWidth = ( width == 0.0f ) ? estimatedSize.x + padding.x : width;
 
     SetNextItemWidth( actualWidth );
-    PushStyleVar( ImGuiStyleVar_FramePadding, { ( actualWidth - estimatedSize.x ) * 0.5f, style.FramePadding.y } );
+    if ( actualWidth > estimatedSize.x )
+        PushStyleVar( ImGuiStyleVar_FramePadding, { ( actualWidth - estimatedSize.x ) * 0.5f, style.FramePadding.y } );
 
     if ( textColor )
     {
@@ -282,7 +283,8 @@ void InputTextCenteredReadOnly( const char* label, const std::string& str, float
     if ( label && label[0] != '#' && label[0] != '\0' && label[1] != '#' )
         ImGui::Text( "%s", label );
 
-    PopStyleVar();
+    if ( actualWidth > estimatedSize.x )
+        PopStyleVar();
 }
 
 MRVIEWER_API void TransparentText( const char* fmt, ... )

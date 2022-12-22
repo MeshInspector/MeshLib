961774e86da8c3d384f6940f229a058a671968e9
ADALISK\akoksin <akoksin@adalisk.com>
2022-12-22 18:11:38 +0400

    add callback to PointsSampler

--- source/mrmeshpy/MRPythonSampling.cpp
+++ source/mrmeshpy/MRPythonSampling.cpp
@@ -3,12 +3,14 @@
 #include "MRMesh/MRGridSampling.h"
 #include "MRMesh/MRUniformSampling.h"
 
+using namespace MR;
+
 MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, PointsSampling, [] ( pybind11::module_& m )
 {
-    m.def( "pointGridSampling", &MR::pointGridSampling, pybind11::arg( "cloud" ), pybind11::arg( "voxelSize " ),
+    m.def( "pointGridSampling", ( VertBitSet( * )( const PointCloud&, float, ProgressCallback ) )&MR::pointGridSampling, pybind11::arg( "cloud" ), pybind11::arg( "voxelSize" ), pybind11::arg( "cb" ) = ProgressCallback{},
         "performs sampling of point cloud vertices;\n"
         "subdivides point cloud bounding box on voxels of approximately given size and returns at most one vertex per voxel" );
 
-    m.def( "pointUniformSampling", &MR::pointUniformSampling, pybind11::arg( "pointCloud" ), pybind11::arg( "distance " ),
+    m.def( "pointUniformSampling", ( VertBitSet( * )( const PointCloud&, float, ProgressCallback ) )&MR::pointUniformSampling, pybind11::arg( "pointCloud" ), pybind11::arg( "distance" ), pybind11::arg( "cb" ) = ProgressCallback{},
         "Sample vertices, removing ones that are too close" );
 } )

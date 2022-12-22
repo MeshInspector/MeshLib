ac9f8b90e366cf5ac5dc52296fd8edeedb2b6072
Alexey Koksin <akoksin@adalisk.com>
2022-12-22 23:08:11 +0400

    add callback to PointsSampler (#828)
    * add callback to PointsSampler
    
    * add include

--- source/MRMesh/MRGridSampling.h
+++ source/MRMesh/MRGridSampling.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include "MRMeshFwd.h"
+#include "MRProgressCallback.h"
 
 namespace MR
 {
 
 // performs sampling of mesh vertices;
 // subdivides mesh bounding box on voxels of approximately given size and returns at most one vertex per voxel
-MRMESH_API VertBitSet verticesGridSampling( const MeshPart & mp, float voxelSize );
+MRMESH_API VertBitSet verticesGridSampling( const MeshPart& mp, float voxelSize, ProgressCallback cb = {} );
 // the same for point cloud
-MRMESH_API VertBitSet pointGridSampling( const PointCloud & cloud, float voxelSize );
+MRMESH_API VertBitSet pointGridSampling( const PointCloud & cloud, float voxelSize, ProgressCallback cb = {} );
 
 } //namespace MR

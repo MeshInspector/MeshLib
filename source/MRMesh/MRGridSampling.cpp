7a2b1f4cde608d09409847207257f9e06f7c27e8
Grant Karapetyan <gkarapetyan@adalisk.com>
2022-12-26 17:12:00 +0400

    More precise progress callback in sampling

--- source/MRMesh/MRGridSampling.cpp
+++ source/MRMesh/MRGridSampling.cpp
@@ -120,19 +120,17 @@ VertBitSet verticesGridSampling( const MeshPart & mp, float voxelSize, ProgressC
     };
 
     Grid grid( bbox, dims );
-    if ( cb && !cb( 0.5f ) )
+    if ( cb && !cb( 0.1f ) )
         return {};
 
-    if ( mp.region )
+    const auto& regionVerts = mp.region ? getIncidentVerts( mp.mesh.topology, *mp.region ) : mp.mesh.topology.getValidVerts();
+    int counter = 0;
+    int size = int( regionVerts.count() );
+    for ( auto v : regionVerts )
     {
-        VertBitSet regionVerts = getIncidentVerts( mp.mesh.topology, *mp.region );
-        for ( auto v : regionVerts )
-            grid.addVertex( mp.mesh.points[v], v );
-    }
-    else
-    {
-        for ( auto v : mp.mesh.topology.getValidVerts() )
-            grid.addVertex( mp.mesh.points[v], v );
+        grid.addVertex( mp.mesh.points[v], v );
+        if ( cb && counter++ % 100 == 0 && !cb( 0.1f + 0.8f * float( counter ) / float( size ) ) )
+            return {};
     }
 
     const auto res =  grid.getSamples();
@@ -159,11 +157,17 @@ VertBitSet pointGridSampling( const PointCloud & cloud, float voxelSize, Progres
     };
 
     Grid grid( bbox, dims );
-    if ( cb && !cb( 0.5f ) )
+    if ( cb && !cb( 0.1f ) )
         return {};
 
+    int counter = 0;
+    int size = int( cloud.validPoints.count() );
     for ( auto v : cloud.validPoints )
+    {
         grid.addVertex( cloud.points[v], v );
+        if ( cb && counter++ % 100 == 0 && !cb( 0.1f + 0.8f * float( counter ) / float( size ) ) )
+            return {};
+    }
 
     const auto res = grid.getSamples();
     if ( cb && !cb( 1.0f ) )
--- source/MRMesh/MRUniformSampling.h
+++ source/MRMesh/MRUniformSampling.h
@@ -2,12 +2,13 @@
 #include "MRMeshFwd.h"
 #include "MRVector3.h"
 #include "MRBitSet.h"
+#include "MRProgressCallback.h"
 
 namespace MR
 {
 
 /// Sample vertices, removing ones that are too close
 /// \ingroup PointCloudGroup
-MRMESH_API VertBitSet pointUniformSampling( const PointCloud& pointCloud, float distance );
+MRMESH_API VertBitSet pointUniformSampling( const PointCloud& pointCloud, float distance, ProgressCallback cb = {} );
 
 }

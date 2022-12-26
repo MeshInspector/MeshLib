22d890d9135dc4177dfd3cf05dba3a870f74d534
Grant Karapetyan <GKarapetyan@adalisk.com>
2022-12-26 17:21:50 +0300

    More precise progress callback in sampling (#846)
    * More precise progress callback in sampling
    
    * CR Fix

--- source/MRMesh/MRGridSampling.h
+++ source/MRMesh/MRGridSampling.h
@@ -10,6 +10,6 @@ namespace MR
 // subdivides mesh bounding box on voxels of approximately given size and returns at most one vertex per voxel
 MRMESH_API VertBitSet verticesGridSampling( const MeshPart& mp, float voxelSize, ProgressCallback cb = {} );
 // the same for point cloud
-MRMESH_API VertBitSet pointGridSampling( const PointCloud & cloud, float voxelSize, ProgressCallback cb = {} );
+MRMESH_API VertBitSet pointGridSampling( const PointCloud& cloud, float voxelSize, ProgressCallback cb = {} );
 
 } //namespace MR
--- source/MRMesh/MRUniformSampling.cpp
+++ source/MRMesh/MRUniformSampling.cpp
@@ -7,7 +7,7 @@
 namespace MR
 {
 
-VertBitSet pointUniformSampling( const PointCloud& pointCloud, float distance )
+VertBitSet pointUniformSampling( const PointCloud& pointCloud, float distance, ProgressCallback cb )
 {
     auto box = pointCloud.getBoundingBox();
     if ( !box.valid() )
@@ -28,9 +28,15 @@ VertBitSet pointUniformSampling( const PointCloud& pointCloud, float distance )
     std::vector<VertProj> projes( size );
     int n = 0;
     for ( auto v : pointCloud.validPoints )
-        projes[n++] = {dot( pointCloud.points[v],axis ),v};
+    {
+        projes[n++] = { dot( pointCloud.points[v],axis ),v };
+        // n & 0x7f == 0 ~ n % 128 == 0
+        if ( ( ( n & 0x7f ) == 0 ) && cb && !cb( 0.5f * float( n ) / float( size ) ) )
+            return {};
+    }
 
     VertBitSet res( pointCloud.validPoints.size() );
+    n = 0;
     for ( const auto& proj : projes )
     {
         bool ballHasPrevVert = false;
@@ -41,6 +47,9 @@ VertBitSet pointUniformSampling( const PointCloud& pointCloud, float distance )
         } );
         if ( !ballHasPrevVert )
             res.set( proj.id );
+        // n & 0x7f == 0 ~ n % 128 == 0
+        if ( ( ( ( n++ ) & 0x7f ) == 0 ) && cb && !cb( 0.5f + 0.5f * float( n ) / float( size ) ) )
+            return {};
     }
 
     return res;

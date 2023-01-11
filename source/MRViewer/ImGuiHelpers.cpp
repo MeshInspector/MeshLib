e4fb015d89f2453e76516765d8cce0f039440557
A.Burakov <42890373+ABSitf@users.noreply.github.com>
2023-01-11 13:08:41 +0300

    updated setting dialogs design similar to Hotkeys dialog (#875)
    * updated setting dialogs design similar to Hotkeys dialog

--- source/MRViewer/ImGuiHelpers.cpp
+++ source/MRViewer/ImGuiHelpers.cpp
@@ -1353,4 +1353,35 @@ void Spinner( float radius, float scaling )
     MR::getViewerInstance().incrementForceRedrawFrames();
 }
 
+bool ModalBigTitle( const char* title, float scaling )
+{
+    auto font = MR::RibbonFontManager::getFontByTypeStatic( MR::RibbonFontManager::FontType::Headline );
+    if ( font )
+        ImGui::PushFont( font );
+    ImGui::Text( "%s", title);
+    if ( font )
+        ImGui::PopFont();
+
+    const float exitButtonSize = 30.0f * scaling;
+    ImGui::SameLine( ImGui::GetWindowContentRegionMax().x - exitButtonSize );
+
+    std::string closeBtnTxt = "x";
+    ImGui::SetCursorPosY( 2 * MR::cDefaultWindowPaddingY * scaling );
+    ImGui::PushStyleColor( ImGuiCol_Button, MR::ColorTheme::getRibbonColor( MR::ColorTheme::RibbonColorsType::Background ).getUInt32() );
+    ImGui::PushStyleColor( ImGuiCol_Border, MR::ColorTheme::getRibbonColor( MR::ColorTheme::RibbonColorsType::Background ).getUInt32() );
+    font = MR::RibbonFontManager::getFontByTypeStatic( MR::RibbonFontManager::FontType::Icons );
+    if ( font )
+    {
+        ImGui::PushFont( font );
+        closeBtnTxt = "\xef\x80\x8d";
+    }
+    const bool shoudClose = ImGui::Button( closeBtnTxt.c_str(), ImVec2( 30.0f * scaling, 30.0f * scaling ) ) || ImGui::IsKeyPressed( ImGuiKey_Escape );
+    if ( font )
+        ImGui::PopFont();
+    ImGui::PopStyleColor( 2 );
+    ImGui::NewLine();
+
+    return shoudClose;
+}
+
 } // namespace ImGui

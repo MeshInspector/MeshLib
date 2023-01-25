cb93f8c0828439b2670b2f1499c472c9fef1a3ee
Grant Karapetyan <GKarapetyan@adalisk.com>
2023-01-25 18:32:28 +0300

    Internal voxel conversions (#913)
    * Internal voxel conversions
    
    * Update
    
    * Add callback
    
    * fix builds
    
    * fix headers

---
+++ source/MRMesh/MRVoxelsConversions.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "MRMeshFwd.h"
+#include "MRAffineXf3.h"
+#include "MRSimpleVolume.h"
+#include "MRProgressCallback.h"
+#include <optional>
+
+namespace MR
+{
+
+struct BaseVolumeConversionParams
+{
+    AffineXf3f basis; // position of lowest left voxel, and axes vectors (A.transposed().x == x axis of volume)
+    ProgressCallback cb{}; // progress callback
+};
+
+struct MeshToSimpleVolumeParams : BaseVolumeConversionParams
+{
+    Vector3i dimensions{ 100,100,100 }; // num voxels along each axis
+    float maxDistSq{ FLT_MAX }; // maximum value in voxels squared, not used in TopologyOrientation mode
+    enum SignDetectionMode
+    {
+        Unsigned, // unsigned voxels, useful for `Shell` offset
+        TopologyOrientation, // projection sign
+        WindingRule // ray intersection counter, useful to fix self-intersections
+    } signMode{ TopologyOrientation };
+};
+
+struct SimpleVolumeToMeshParams : BaseVolumeConversionParams
+{
+    float iso{ 0.0f };
+    bool lessInside{ false }; // should be false for dense volumes, and true for distance volume
+};
+
+// makes SimpleVolume from Mesh with given params
+// returns nullopt if operation was canceled
+MRMESH_API std::optional<SimpleVolume> meshToSimpleVolume( const Mesh& mesh, const MeshToSimpleVolumeParams& params = {} );
+
+// makes Mesh from SimpleVolume with given params
+// returns nullopt if operation was canceled
+MRMESH_API std::optional<Mesh> simpleVolumeToMesh( const SimpleVolume& volume, const SimpleVolumeToMeshParams& params = {} );
+
+}

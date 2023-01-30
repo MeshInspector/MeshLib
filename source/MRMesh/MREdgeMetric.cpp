d270bddfdd9d8e40727b0153c027d7cbf392d587
Fedr <fchelnokov@adalisk.com>
2023-01-30 16:08:54 +0300

    separate file for MREdgeMetric (#924)

---
+++ source/MRMesh/MREdgeMetric.cpp
@@ -0,0 +1,50 @@
+#include "MREdgeMetric.h"
+#include "MRMesh.h"
+#include "MREdgeIterator.h"
+#include "MRTimer.h"
+
+namespace MR
+{
+
+EdgeMetric identityMetric() 
+{ 
+    return []( EdgeId ) { return 1.0f; };
+}
+
+EdgeMetric edgeLengthMetric( const Mesh & mesh )
+{
+    return [&mesh]( EdgeId e )
+    {
+        return mesh.edgeLength( e );
+    };
+}
+
+EdgeMetric edgeCurvMetric( const Mesh & mesh, float angleSinFactor, float angleSinForBoundary )
+{
+    const float bdFactor = exp( angleSinFactor * angleSinForBoundary );
+
+    return [&mesh, angleSinFactor, bdFactor ]( EdgeId e ) -> float
+    {
+        auto edgeLen = mesh.edgeLength( e );
+        if ( mesh.topology.isBdEdge( e, nullptr ) )
+            return edgeLen * bdFactor;
+
+        return edgeLen * exp( angleSinFactor * mesh.dihedralAngleSin( e ) );
+    };
+}
+
+EdgeMetric edgeTableMetric( const MeshTopology & topology, const EdgeMetric & metric )
+{
+    MR_TIMER
+
+    Vector<float, UndirectedEdgeId> table( topology.undirectedEdgeSize() );
+    for ( auto e : undirectedEdges( topology ) )
+        table[e] = metric( e );
+
+    return [table = std::move( table )]( EdgeId e )
+    {
+        return table[e.undirected()];
+    };
+}
+
+} //namespace MR

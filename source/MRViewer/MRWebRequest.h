ce181506de0af8738635df3b1677a09a9f9303df
Grant Karapetyan <GKarapetyan@adalisk.com>
2023-02-06 23:32:47 +0800

    Unified requests (#948)
    * Unified requests
    
    * Update
    
    * CR Fix

---
+++ source/MRViewer/MRWebRequest.h
@@ -0,0 +1,63 @@
+#pragma once
+#ifndef MRMESH_NO_CPR
+#include "MRViewerFwd.h"
+#include "MRPch/MRJson.h"
+#include <unordered_map>
+#include <string>
+#include <functional>
+
+namespace MR
+{
+
+// this class is needed to unify cpp and wasm requests
+// can perform only one request at a time
+// should be called from GUI thread
+class MRVIEWER_CLASS WebRequest
+{
+public:
+    enum class Method 
+    {
+        Get,
+        Post
+    };
+
+    // returns true if no other request is executing right now
+    MRVIEWER_API static bool readyForNextRequest();
+
+    // clears all request data
+    MRVIEWER_API static void clear();
+
+    MRVIEWER_API static void setMethod( Method method );
+
+    // sets timeout in milliseconds
+    MRVIEWER_API static void setTimeout( int timeoutMs );
+
+    // sets parameters
+    MRVIEWER_API static void setParameters( std::unordered_map<std::string, std::string> parameters );
+
+    MRVIEWER_API static void setHeaders( std::unordered_map<std::string, std::string> headers );
+
+    MRVIEWER_API static void setBody( std::string body );
+
+    using ResponseCallback = std::function<void( const Json::Value& response )>;
+
+    // sends request, calling callback on answer, 
+    // if async then callback is called in next frame after getting response
+    // return true if request was sent, false if other request is processing now
+    // note: check `readyForNextRequest` before sending
+    MRVIEWER_API static bool send( std::string url, ResponseCallback callback, bool async = true );
+private:
+    WebRequest() = default;
+    ~WebRequest() = default;
+
+    Method method_{ Method::Get };
+    int timeout_{ 10000 };
+    std::unordered_map<std::string, std::string> params_;
+    std::unordered_map<std::string, std::string> headers_;
+    std::string body_;
+
+    static WebRequest& instance_();
+};
+
+}
+#endif

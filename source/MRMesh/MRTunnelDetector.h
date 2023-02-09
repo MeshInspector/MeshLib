e9f10ffd2fbd7ee14cec86770b8784051939cec2
Fedr <fchelnokov@adalisk.com>
2023-02-09 16:01:38 +0300

    Tunnel detection can be configured with arbitrary edge metric

--- source/MRMesh/MRTunnelDetector.h
+++ source/MRMesh/MRTunnelDetector.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "MRMeshFwd.h"
+#include "MREdgeMetric.h"
 
 namespace MR
 {
@@ -9,12 +9,14 @@ namespace MR
 /// \ingroup SurfacePathGroup
 /// \{
 
-/// detects all not-contractible-in-point and not-equivalent tunnel loops on the mesh
-MRMESH_API std::vector<EdgeLoop> detectBasisTunnels( const MeshPart & mp );
+/// detects all not-contractible-in-point and not-equivalent tunnel loops on the mesh;
+/// trying to include in the loops the edges with the smallest metric;
+/// if no metric is given then discreteMinusAbsMeanCurvatureMetric will be used
+MRMESH_API std::vector<EdgeLoop> detectBasisTunnels( const MeshPart & mp, EdgeMetric metric = {} );
 
 /// returns tunnels as a number of faces;
 /// if you remove these faces and patch every boundary with disk, then the surface will be topology equivalent to sphere
-MRMESH_API FaceBitSet detectTunnelFaces( const MeshPart & mp, float maxTunnelLength );
+MRMESH_API FaceBitSet detectTunnelFaces( const MeshPart & mp, float maxTunnelLength, const EdgeMetric & metric = {} );
 
 /// \}
 

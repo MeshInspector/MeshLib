174fa562cbe38aaeba200fe9289997d42cd48ec5
Grant Karapetyan <GKarapetyan@adalisk.com>
2023-02-10 22:01:40 +0800

    Add is rigid check for Matrix3 (#963)

--- source/MRMesh/MRMatrix3Decompose.h
+++ source/MRMesh/MRMatrix3Decompose.h
@@ -25,4 +25,20 @@ void decomposeMatrix3( const Matrix3<T>& m, Matrix3<T>& rotation, Matrix3<T>& sc
     rotation = q * sign;
 }
 
+/// Returns true if matrix scale is identity
+template <typename T>
+bool isRigid( const Matrix3<T>& m )
+{
+    Matrix3<T> rot, scale;
+    decomposeMatrix3( m, rot, scale );
+    auto eps = T( 10 ) * std::numeric_limits<T>::epsilon();
+    if ( std::abs( scale.x.x - T( 1 ) ) > eps )
+        return false;
+    if ( std::abs( scale.y.y - T( 1 ) ) > eps )
+        return false;
+    if ( std::abs( scale.z.z - T( 1 ) ) > eps )
+        return false;
+    return true;
+}
+
 } // namespace MR

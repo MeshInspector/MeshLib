b10d7ac0c0b744a74995edf959c376588d63112f
Grant Karapetyan <gkarapetyan@adalisk.com>
2023-02-20 17:44:04 +0400

    fix python test

--- test_python/test_save_voxels.py
+++ test_python/test_save_voxels.py
@@ -3,18 +3,18 @@ import pytest
 import shutil
 
 def test_save_voxels():
-    mesh = mm.makeTorus()
-    mtvParams = mm.MeshToVolumeParams()
-    mtvParams.type = mm.MeshToVolumeParamsType.Signed
+    mesh = mrmesh.makeTorus()
+    mtvParams = mrmesh.MeshToVolumeParams()
+    mtvParams.type = mrmesh.MeshToVolumeParamsType.Signed
     mtvParams.surfaceOffset = 3
     meshBox = mesh.computeBoundingBox()
     boxSize = meshBox.max-meshBox.min
     mtvParams.voxelSize = boxSize / 27.0
-    voxels = mm.meshToVolume(mesh,mtvParams)
+    voxels = mrmesh.meshToVolume(mesh,mtvParams)
 
-    vsParams = mm.VoxelsSaveSavingSettings()
+    vsParams = mrmesh.VoxelsSaveSavingSettings()
     os.mkdir("save_voxels_dir_test")
     vsParams.path = "save_voxels_dir_test"
-    vsParams.slicePlane = mm.SlicePlane.XY
-    mm.saveAllSlicesToImage(voxels,vsParams)
+    vsParams.slicePlane = mrmesh.SlicePlane.XY
+    mrmesh.saveAllSlicesToImage(voxels,vsParams)
     shutil.rmtree("save_voxels_dir_test")

49e163d84870fc908ad3ec165d24fee4c2f1c663
Fedr <fchelnokov@adalisk.com>
2023-02-21 16:08:55 +0300

    comment updated

--- source/MRViewer/MRStatePluginAutoClose.h
+++ source/MRViewer/MRStatePluginAutoClose.h
@@ -41,7 +41,7 @@ protected:
     MRVIEWER_API virtual void onPluginEnable_() override;
     MRVIEWER_API virtual void onPluginDisable_() override;
     MRVIEWER_API virtual bool shouldClose_() const override;
-    // plugin can return the value to false if it changed the mesh by itself and do not want to close
+    // plugin can return the value to false after mesh change if it changed the mesh by itself and does not want to close
     bool meshChanged_{ false };
 
 private:

95734dcd60f6b021ce0eddbb1dfa36ee4a548227
Alexey Koksin <akoksin@adalisk.com>
2023-03-07 17:33:49 +0400

    load textures from OBJ (#1033)
    * Parse texture information for OBJ files
    
    * Apply texture from MTL file to mesh
    
    * load textures from OBJ
    
    * rempve extra lines
    
    * add include
    
    * fix texture init
    
    * code review
    
    * code review
    
    ---------
    
    Co-authored-by: Artur Sharafutdinov <asharafutdinov@adalisk.com>

--- source/MRMesh/MRMeshLoadObj.h
+++ source/MRMesh/MRMeshLoadObj.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "MRMesh.h"
+#include "MRMeshTexture.h"
 #include "MRProgressCallback.h"
 #include <tl/expected.hpp>
 #include <filesystem>
@@ -22,17 +23,18 @@ struct NamedMesh
 {
     std::string name;
     Mesh mesh;
+    Vector<UVCoord, VertId> uvCoords;
+    std::filesystem::path pathToTexture;
 };
 MRMESH_API tl::expected<std::vector<NamedMesh>, std::string> fromSceneObjFile( const std::filesystem::path& file, bool combineAllObjects,
                                                                                ProgressCallback callback = {} );
 /// important on Windows: in stream must be open in binary mode
-MRMESH_API tl::expected<std::vector<NamedMesh>, std::string> fromSceneObjFile( std::istream& in, bool combineAllObjects,
+/// \param dir working directory where materials and textures are located
+MRMESH_API tl::expected<std::vector<NamedMesh>, std::string> fromSceneObjFile( std::istream& in, bool combineAllObjects, const std::filesystem::path& dir,
                                                                                ProgressCallback callback = {} );
-MRMESH_API tl::expected<std::vector<NamedMesh>, std::string> fromSceneObjFile( const char* data, size_t size, bool combineAllObjects,
+/// \param dir working directory where materials and textures are located
+MRMESH_API tl::expected<std::vector<NamedMesh>, std::string> fromSceneObjFile( const char* data, size_t size, bool combineAllObjects, const std::filesystem::path& dir,
                                                                                ProgressCallback callback = {} );
-
-/// \}
-
 } // namespace MeshLoad
 
 } // namespace MR
--- source/MRMesh/MRObjectLoad.cpp
+++ source/MRMesh/MRObjectLoad.cpp
@@ -5,6 +5,7 @@
 #include "MRMesh.h"
 #include "MRTimer.h"
 #include "MRDistanceMapLoad.h"
+#include "MRImageLoad.h"
 #include "MRPointsLoad.h"
 #include "MRVoxelsLoad.h"
 #include "MRObjectVoxels.h"
@@ -185,7 +186,16 @@ tl::expected<std::vector<std::shared_ptr<MR::Object>>, std::string> loadObjectFr
                 else
                     objectMesh->setName( std::move( resValue[i].name ) );
                 objectMesh->select( true );
-                objectMesh->setMesh( std::make_shared<Mesh>( std::move( resValue[i].mesh ) ) );
+                objectMesh->setMesh( std::make_shared<Mesh>( std::move( resValue[i].mesh ) ) );                
+                
+                auto image = ImageLoad::fromAnySupportedFormat( resValue[i].pathToTexture );
+                if ( image.has_value() )
+                {
+                    objectMesh->setUVCoords( std::move( resValue[i].uvCoords ) );
+                    objectMesh->setTexture( { image.value(), FilterType::Linear } );
+                    objectMesh->setVisualizeProperty( true, MeshVisualizePropertyType::Texture, ViewportMask::all() );
+                }
+
                 objects[i] = std::dynamic_pointer_cast< Object >( objectMesh );
             }
             result = objects;

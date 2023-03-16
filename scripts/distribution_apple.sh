db36895ac48a8ce883600186d23235dbf607f019
Maxim Rayskiy <mraiskii@adalisk.com>
2023-03-16 13:52:30 +0300

    Macos: fix gtk bundle (#1062)
    * set env var
    
    * fix
    
    * separate from external project

--- scripts/distribution_apple.sh
+++ scripts/distribution_apple.sh
@@ -19,6 +19,9 @@ cp ./LICENSE ./macos/Resources
 mkdir "${MR_PREFIX}"/requirements/
 cp ./requirements/macos.txt "${MR_PREFIX}"/requirements/
 
+mkdir "${MR_PREFIX}"/share/
+cp -r "$(brew --prefix)"/share/glib-2.0 "${MR_PREFIX}"/share
+
 ln -s "/Library/Frameworks/MeshLib.framework/Versions/${MR_VERSION}" "./Library/Frameworks/MeshLib.framework/Versions/Current"
 ln -s "/Library/Frameworks/MeshLib.framework/Resources" "./Library/Frameworks/MeshLib.framework/Versions/${MR_VERSION}/Resources"
 

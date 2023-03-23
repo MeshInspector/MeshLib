7ff52d7893afa7f2f2dd133fba31b263cbdbb0c6
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2023-03-23 19:05:38 +0400

    Silent warning for MSVC

--- source/MRMesh/MROrder.cpp
+++ source/MRMesh/MROrder.cpp
@@ -6,7 +6,10 @@
 #include "MRTimer.h"
 #include "MRPch/MRTBB.h"
 
+#pragma warning(push)
+#pragma warning(disable: 4464)
 #include <tbb/parallel_sort.h>
+#pragma warning(pop)
 
 #include <algorithm>
 #include <span>

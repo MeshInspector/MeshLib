da451098a153f2a3105f4c168e12afd187ecfa9a
Grant Karapetyan <GKarapetyan@adalisk.com>
2023-04-03 15:38:27 +0400

    Add FaceSelectionChangeSignal (#1101)

--- source/MRMesh/MRObjectMeshHolder.cpp
+++ source/MRMesh/MRObjectMeshHolder.cpp
@@ -383,6 +383,7 @@ void ObjectMeshHolder::selectFaces( FaceBitSet newSelection )
 {
     selectedTriangles_ = std::move( newSelection );
     numSelectedFaces_.reset();
+    faceSelectionChangedSignal();
     dirty_ |= DIRTY_SELECTION;
 }
 
@@ -520,6 +521,15 @@ void ObjectMeshHolder::swapBase_( Object& other )
 {
     if ( auto otherMesh = other.asType<ObjectMeshHolder>() )
         std::swap( *this, *otherMesh );
+    else
+        assert( false );
+}
+
+void ObjectMeshHolder::swapSignals_( Object& other )
+{
+    VisualObject::swapSignals_( other );
+    if ( auto otherMesh = other.asType<ObjectMeshHolder>() )
+        std::swap( faceSelectionChangedSignal, otherMesh->faceSelectionChangedSignal );
     else
         assert( false );
 }

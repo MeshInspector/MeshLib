7bc2cfaed6167f4d809a50064c4e3593bd1542a9
Grant Karapetyan <gkarapetyan@adalisk.com>
2023-04-19 18:29:16 +0400

    Add some cancel checks on partial offset

--- source/MRMesh/MRPartialOffset.cpp
+++ source/MRMesh/MRPartialOffset.cpp
@@ -11,10 +11,15 @@ tl::expected<Mesh, std::string> partialOffsetMesh( const MeshPart& mp, float off
 {
     auto realParams = params;
     realParams.type = OffsetParameters::Type::Shell; // for now only shell can be in partial offset
+    realParams.callBack = subprogress( params.callBack, 0.0f, 0.8f );
     auto offsetPart = offsetMesh( mp, offset, realParams );
+    if ( params.callBack && !params.callBack( 0.8f ) )
+        return tl::make_unexpected( "Operation was canceled." );
     if ( !offsetPart.has_value() )
         return offsetPart;
     auto res = boolean( mp.mesh, *offsetPart, BooleanOperation::Union );
+    if ( params.callBack && !params.callBack( 1.0f ) )
+        return tl::make_unexpected( "Operation was canceled." );
     if ( !res.valid() )
         return tl::make_unexpected("Partial offset failed: " + res.errorString );
     return std::move( res.mesh );

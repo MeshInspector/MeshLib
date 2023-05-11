737d716cbe52422a2812925a8ef33c43a43f9c78
Alexandr Burakov <aburakov@adalisk.com>
2023-05-11 13:41:24 +0300

    fix comment

--- source/MRViewer/MRPickHoleBorderElement.h
+++ source/MRViewer/MRPickHoleBorderElement.h
@@ -22,11 +22,11 @@ MRVIEWER_API HoleEdgePoint findClosestToMouseHoleEdge( const Vector2i& mousePos,
                                                        const std::vector<EdgeId>& holeRepresentativeEdges,
                                                        float accuracy = 5.5f, bool attractToVert = false, float cornerAccuracy = 10.5f );
 
-/// find closest to mouse edge from polyline
+/// find closest to mouse edge from polylines
 /// \mousePos mouse position in screen
 /// \accuracy square of accuracy in pixels
 /// \param accuracy maximum distances from mouse position to line in viewport (screen) space
-/// \return pair closest edge and according index of hole in holeRepresentativeEdges
+/// \return pair closest edge and according index of polyline
 MRVIEWER_API HoleEdgePoint findClosestToMouseEdge( const Vector2i& mousePos, const std::vector<std::shared_ptr<ObjectLines>>& objsLines,
                                                float accuracy = 5.5f );
 

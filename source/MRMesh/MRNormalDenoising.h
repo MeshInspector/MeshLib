04fe1174106ed360b83e3bc14b6222e12b9b04f9
Fedr <fchelnokov@adalisk.com>
2023-05-16 16:59:50 +0300

    comments

--- source/MRMesh/MRNormalDenoising.h
+++ source/MRMesh/MRNormalDenoising.h
@@ -6,10 +6,10 @@ namespace MR
 {
 
 /// Smooth face normals, given
-/// \param mesh topology and edge lengths
+/// \param mesh contains topology information and edge lengths for weights
 /// \param normals input noisy normals and output smooth normals
 /// \param v edge indicator: 1 - smooth edge, 0 - crease edge
-/// \param gamma amount of smoothing (e.g. in [1,30])
+/// \param gamma the more the value, the larger smoothing is (approximately in the range [1,30])
 /// see the article "Mesh Denoising via a Novel Mumford-Shah Framework"
 MRMESH_API void denoiseNormals( const Mesh & mesh, FaceNormals & normals, const Vector<float, UndirectedEdgeId> & v, float gamma );
 

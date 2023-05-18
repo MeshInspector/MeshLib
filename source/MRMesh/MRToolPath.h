9a73dad7dbe1fb1815c14db697c3ff3ad7954a09
Alex Koksin <akoksin@adalisk.com>
2023-05-18 15:33:35 +0400

    mend comments

--- source/MRMesh/MRToolPath.h
+++ source/MRMesh/MRToolPath.h
@@ -46,8 +46,8 @@ struct ToolPathResult
     std::vector<GCommand> commands;
 };
 
-// compute path of the milling tool for the given mesh with parameters. Direction of milling is from up to down. 
-// Mesh can be transformed using xf parameter
+// compute path of the milling tool for the given mesh with parameters ( direction of milling is from up to down )
+// mesh can be transformed using xf parameter
 MRMESH_API ToolPathResult constantZToolPath( const Mesh& mesh, const AffineXf3f& xf, const ToolPathParams& params );
 
 // generates G-Code for milling tool

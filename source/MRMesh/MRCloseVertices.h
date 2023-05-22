a931920a8f9df03027e0607f8abf92fb922974cd
Fedr <fchelnokov@adalisk.com>
2023-05-22 17:52:48 +0300

    findTwinEdges

--- source/MRMesh/MRCloseVertices.h
+++ source/MRMesh/MRCloseVertices.h
@@ -5,16 +5,23 @@
 namespace MR
 {
 
-/// finds all close vertices, where for each vertex there is another one located within given distance
-[[nodiscard]] MRMESH_API VertBitSet findCloseVertices( const Mesh & mesh, float closeDist );
-/// finds all close vertices, where for each vertex there is another one located within given distance
-[[nodiscard]] MRMESH_API VertBitSet findCloseVertices( const VertCoords & points, float closeDist, const VertBitSet * valid = nullptr );
-
 /// returns a map where each valid vertex is mapped to the smallest valid vertex Id located within given distance (including itself), and this smallest vertex is mapped to itself,
 /// each vertex not from valid set is mapped to itself
 [[nodiscard]] MRMESH_API VertMap findSmallestCloseVertices( const Mesh & mesh, float closeDist );
 /// returns a map where each valid vertex is mapped to the smallest valid vertex Id located within given distance (including itself), and this smallest vertex is mapped to itself,
 /// each vertex not from valid set is mapped to itself
 [[nodiscard]] MRMESH_API VertMap findSmallestCloseVertices( const VertCoords & points, float closeDist, const VertBitSet * valid = nullptr );
+
+/// finds all close vertices, where for each vertex there is another one located within given distance
+[[nodiscard]] MRMESH_API VertBitSet findCloseVertices( const Mesh & mesh, float closeDist );
+/// finds all close vertices, where for each vertex there is another one located within given distance
+[[nodiscard]] MRMESH_API VertBitSet findCloseVertices( const VertCoords & points, float closeDist, const VertBitSet * valid = nullptr );
+/// finds all close vertices, where for each vertex there is another one located within given distance; smallestMap is the result of findSmallestCloseVertices function call
+[[nodiscard]] MRMESH_API VertBitSet findCloseVertices( const VertMap & smallestMap );
+
+/// finds all twin edges, which connect vertices different by id but sharing same coordinates
+[[nodiscard]] MRMESH_API EdgeBitSet findTwinEdges( const Mesh & mesh, float closeDist );
+/// finds all twin edges, which connect vertices different by id but sharing same coordinates
+[[nodiscard]] MRMESH_API UndirectedEdgeBitSet findTwinUndirectedEdges( const Mesh & mesh, float closeDist );
 
 } //namespace MR

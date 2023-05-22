86586b35159f4877d52fab1cb9c33d510edf30dd
Fedr <fchelnokov@adalisk.com>
2023-05-22 15:59:56 +0300

    findCloseVertices

--- source/MRMesh/MRCloseVertices.h
+++ source/MRMesh/MRCloseVertices.h
@@ -5,10 +5,14 @@
 namespace MR
 {
 
+/// finds all close vertices, where for each vertex there is another one located within given distance
+[[nodiscard]] MRMESH_API VertBitSet findCloseVertices( const Mesh & mesh, float closeDist );
+/// finds all close vertices, where for each vertex there is another one located within given distance
+[[nodiscard]] MRMESH_API VertBitSet findCloseVertices( const VertCoords & points, float closeDist, const VertBitSet * valid = nullptr );
+
 /// returns a map where each valid vertex is mapped to the smallest valid vertex Id located within given distance (including itself), and this smallest vertex is mapped to itself,
 /// each vertex not from valid set is mapped to itself
 [[nodiscard]] MRMESH_API VertMap findSmallestCloseVertices( const Mesh & mesh, float closeDist );
-
 /// returns a map where each valid vertex is mapped to the smallest valid vertex Id located within given distance (including itself), and this smallest vertex is mapped to itself,
 /// each vertex not from valid set is mapped to itself
 [[nodiscard]] MRMESH_API VertMap findSmallestCloseVertices( const VertCoords & points, float closeDist, const VertBitSet * valid = nullptr );

cc6682a38a192aea6ccc6eae9fb496df0db1ec9b
Alexey Koksin <akoksin@adalisk.com>
2023-06-05 15:02:29 +0400

    Toolpath lacing (#1284)
    * lacing toolpath
    
    * little refactoring
    
    * delete unused variable
    
    * code review
    
    * code review

--- source/MRMesh/MRToolPath.h
+++ source/MRMesh/MRToolPath.h
@@ -1,6 +1,7 @@
 #pragma once
 #if !defined( __EMSCRIPTEN__) && !defined( MRMESH_NO_VOXEL )
 #include "MRMeshFwd.h"
+#include "MRAxis.h"
 #include "MRMesh.h"
 #include "MRPolyline.h"
 
@@ -30,6 +31,14 @@ struct ToolPathParams
     float baseFeed = {};
 };
 
+struct LineInterpolationParams
+{
+    // maximal deviation from given line
+    float eps = {};
+    // maximal length of the line
+    float maxLength = {};
+};
+
 struct ArcInterpolationParams
 {
     // maximal deviation of arc from given path
@@ -43,8 +52,13 @@ enum class MoveType
     FastLinear = 0,
     Linear = 1,
     ArcCW = 2,
-    ArcCCW = 3
+    ArcCCW = 3,
+    PlaneSelectionXY = 17,
+    PlaneSelectionXZ = 18,
+    PlaneSelectionYZ = 19
 };
+
+
 
 struct GCommand
 {
@@ -60,6 +74,11 @@ struct GCommand
     float i = std::numeric_limits<float>::quiet_NaN();
     float j = std::numeric_limits<float>::quiet_NaN();
     float k = std::numeric_limits<float>::quiet_NaN();
+
+    // get coordinate along specified axis
+    float coord( Axis axis ) const;
+    // get projection to the plane orthogonal to the specified axis
+    Vector2f project( Axis axis ) const;
 };
 
 struct ToolPathResult
@@ -72,14 +91,21 @@ struct ToolPathResult
     std::vector<GCommand> commands;
 };
 
-// compute path of the milling tool for the given mesh with parameters ( direction of milling is from up to down )
+// compute path of the milling tool for the given mesh with parameters ( direction of milling is from up to down along Z-direction )
 // mesh can be transformed using xf parameter
 MRMESH_API ToolPathResult constantZToolPath( const Mesh& mesh, const ToolPathParams& params, const AffineXf3f* xf );
 
+// compute path of the milling tool for the given mesh with parameters ( direction of milling is from up to down along Z-direction )
+// mesh can be transformed using xf parameter
+MRMESH_API ToolPathResult lacingToolPath( const Mesh& mesh, const ToolPathParams& params, const AffineXf3f* xf );
+
 // generates G-Code for milling tool
 MRMESH_API std::string exportToolPathToGCode( const std::vector<GCommand>& commands );
+
+// interpolates several points lying on the same straight line with one move
+MRMESH_API void interpolateLines( std::vector<GCommand>& commands, const LineInterpolationParams& params, Axis axis );
 // interpolates given path with arcs
-MRMESH_API void interpolateArcs( std::vector<GCommand>& commands, const ArcInterpolationParams& params );
+MRMESH_API void interpolateArcs( std::vector<GCommand>& commands, const ArcInterpolationParams& params, Axis axis );
 
 }
 #endif

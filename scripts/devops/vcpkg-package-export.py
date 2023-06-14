e2a0858107502c5446f33fa7dbee4db7930f304e
MaxRayskiy <mraiskii@adalisk.com>
2023-06-14 19:04:35 +0300

    test vcpkg export

--- scripts/devops/vcpkg-package-export.py
+++ scripts/devops/vcpkg-package-export.py
@@ -1,16 +1,28 @@
 import os.path
+import argparse
 from subprocess import check_output
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", help="Output filename", default=None)
+args = parser.parse_args()
 
 VCPKG_CMD = "vcpkg export "
 REQUIREMTS_PATH = "./requirements/windows.txt"
 
 if not os.path.exists(REQUIREMTS_PATH):
     print("Cant find {}. Please, run this script from the repo root".format(REQUIREMTS_PATH))
+    exit(1)
 
 with open(REQUIREMTS_PATH) as fin:
     for line in fin.readlines():
-        VCPKG_CMD += line.strip() + ":x64-windows-meshlib "
+        line = line.strip()
+        if line:
+            VCPKG_CMD += line + ":x64-windows-meshlib "
 
 VCPKG_CMD += " --nuget"
+
+# If the output filename is specified
+if args.output:
+    VCPKG_CMD += " --output " + args.output
 
 check_output(VCPKG_CMD, shell=True)

62f879943f8550e2a780e289812c6039db6281d3
Fedr <fchelnokov@adalisk.com>
2023-06-16 14:39:12 +0300

    cooperatively construct AABB tree

--- source/MRMesh/MRUniqueThreadSafeOwner.cpp
+++ source/MRMesh/MRUniqueThreadSafeOwner.cpp
@@ -9,6 +9,9 @@
 namespace MR
 {
 
+struct TaskGroup : tbb::task_group
+{};
+
 template<typename T>
 UniqueThreadSafeOwner<T>::UniqueThreadSafeOwner() = default;
 
@@ -62,26 +65,56 @@ template<typename T>
 void UniqueThreadSafeOwner<T>::reset()
 {
     std::unique_lock lock( mutex_ );
+    assert( !construction_ ); // one thread constructs the object, and this thread resets it
     obj_.reset();
 }
 
 template<typename T>
 T & UniqueThreadSafeOwner<T>::getOrCreate( const std::function<T()> & creator )
 {
-    if ( obj_ ) // fast path to avoid locking when everything is ready
-        return *obj_;
-    assert( creator );
-    std::unique_lock lock( mutex_ );
-    if ( !obj_ )
+    /// if many parallel threads call this function simultaneously, they will join one task_group
+    /// and will cooperatively construct owned object
+    for (;;)
     {
-        // we do not want this thread while inside creator steal outside piece of work 
-        // and call UniqueThreadSafeOwner<T>::getOrCreate recursively
-        tbb::this_task_arena::isolate( [&]
+        if ( obj_ ) // fast path to avoid locking when everything is ready
+            return *obj_;
+        assert( creator );
+        bool firstConstructor = false;
+        std::shared_ptr<TaskGroup> construction;
         {
-            obj_ = std::make_unique<T>( creator() );
-        } );
+            std::unique_lock lock( mutex_ );
+            if ( obj_ ) // already constructed while we waited for lock
+            {
+                assert( !construction_ );
+                return *obj_;
+            }
+            if ( !construction_ )
+            {
+                construction_ = std::make_unique<TaskGroup>();
+                firstConstructor = true;
+            }
+            construction = construction_;
+        }
+        assert( construction );
+        if ( firstConstructor )
+        {
+            // we do not want this thread while inside creator steal outside piece of work 
+            // and call this function recursively
+            tbb::this_task_arena::isolate( [&]
+            {
+               construction->run( [&]
+               {
+                   auto newObj = std::make_unique<T>( creator() );
+                   std::unique_lock lock( mutex_ );
+                   assert( construction == construction_ );
+                   construction_.reset();
+                   obj_ = std::move( newObj );
+               } );
+            } );
+        }
+        construction->wait();
+        // several iterations are necessary only if the object was reset immediately after construction
     }
-    return *obj_;
 }
 
 template<typename T>

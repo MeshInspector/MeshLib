52df858243bc4e5bdb6274018989fbc9d170afcd
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2023-06-29 11:25:43 +0400

    Add documentation

--- source/MRMesh/MRMeshExtrude.h
+++ source/MRMesh/MRMeshExtrude.h
@@ -5,7 +5,15 @@
 namespace MR
 {
 
-/// ...
+/**
+ * \brief Create a band of degenerate faces along the border of the specified region and the rest of the mesh
+ * \details The function is useful for extruding the region without changing the existing faces and creating holes
+ *
+ * @param mesh - the target mesh
+ * @param region - the region required to be separated by a band of degenerate faces
+ * @param outNewFaces - (optional) output newly generated faces
+ * @param old2newMap - (optional) mapping for the region's faces
+ */
 MRMESH_API void makeDegenerateBandAroundRegion( Mesh& mesh, const FaceBitSet& region, FaceBitSet* outNewFaces = nullptr,
                                                 FaceMap* old2newMap = nullptr );
 

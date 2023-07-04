c02cddee36c62ba0cfa6e816b0b1aa432a26a45e
Max <mraiskii@adalisk.com>
2023-07-05 01:40:45 +0300

    debug

--- thirdparty/install_runner.bat
+++ thirdparty/install_runner.bat
@@ -7,4 +7,4 @@ copy "%~dp0vcpkg\downloads\*" "%vcpkg_path%downloads"
 REM Set the binary sources to use the S3 bucket
 set VCPKG_BINARY_SOURCES=clear;x-aws,s3://vcpkg-export/2023.04.15/test/,readwrite;
 
-vcpkg install --host-triplet x64-windows-meshlib --overlay-triplets "%~dp0vcpkg\triplets" --debug
+vcpkg install --debug

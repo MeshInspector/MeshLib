7a6032dcb4b3d21f3e061ea5ba3dc15617714bbf
Max <mraiskii@adalisk.com>
2023-07-05 00:57:20 +0300

    test manifest

--- thirdparty/install.bat
+++ thirdparty/install.bat
@@ -4,10 +4,9 @@ for /f "delims=" %%i in ('where vcpkg') do set vcpkg_path=%%~dpi
 if not exist "%vcpkg_path%downloads" mkdir "%vcpkg_path%downloads"
 copy "%~dp0vcpkg\downloads\*" "%vcpkg_path%downloads"
 
-setlocal enabledelayedexpansion
-set packages=
-for /f "delims=" %%i in ('type "%~dp0..\requirements\windows.txt"') do (
-  set packages=!packages! %%i
-)
-vcpkg install !packages! --recurse --binarysource=clear --overlay-triplets "%~dp0vcpkg\triplets"
+REM Set the binary sources to use the S3 bucket
+set VCPKG_BINARY_SOURCES=clear;x-aws,s3://vcpkg-export/2023.04.15/test/,write;
+
+vcpkg install --overlay-triplets "%~dp0vcpkg\triplets" --x-abi-tools-use-exact-versions --debug
+
 endlocal

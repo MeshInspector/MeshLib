5ba62eac9caa06e21c3aa97387e49c94347e8569
Max <mraiskii@adalisk.com>
2023-07-05 01:28:08 +0300

    test manifest

--- thirdparty/install_runner.bat
+++ thirdparty/install_runner.bat
@@ -4,14 +4,7 @@ for /f "delims=" %%i in ('where vcpkg') do set vcpkg_path=%%~dpi
 if not exist "%vcpkg_path%downloads" mkdir "%vcpkg_path%downloads"
 copy "%~dp0vcpkg\downloads\*" "%vcpkg_path%downloads"
 
-setlocal enabledelayedexpansion
-set packages=
-for /f "delims=" %%i in ('type "%~dp0..\requirements\windows.txt"') do (
-  set packages=!packages! %%i
-)
-
 REM Set the binary sources to use the S3 bucket
 set VCPKG_BINARY_SOURCES=clear;x-aws,s3://vcpkg-export/2023.04.15/test/,readwrite;
 
-vcpkg install eigen3 --recurse --overlay-triplets "%~dp0vcpkg\triplets" --debug --x-abi-tools-use-exact-versions
-endlocal
+vcpkg install --host-triplet x64-windows-meshlib --overlay-triplets "%~dp0vcpkg\triplets"

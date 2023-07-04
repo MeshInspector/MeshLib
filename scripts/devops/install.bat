65bff8306deadc617fe651e82e2052146548eb7a
MaxRayskiy <mraiskii@adalisk.com>
2023-07-04 21:17:16 +0300

    fix path

---
+++ scripts/devops/install.bat
@@ -0,0 +1,17 @@
+set VCPKG_DEFAULT_TRIPLET=x64-windows-meshlib
+
+for /f "delims=" %%i in ('where vcpkg') do set vcpkg_path=%%~dpi
+if not exist "%vcpkg_path%downloads" mkdir "%vcpkg_path%downloads"
+copy "%~dp0vcpkg\downloads\*" "%vcpkg_path%downloads"
+
+setlocal enabledelayedexpansion
+set packages=
+for /f "delims=" %%i in ('type "%~dp0..\requirements\windows.txt"') do (
+  set packages=!packages! %%i
+)
+
+REM Set the binary sources to use the S3 bucket
+set VCPKG_BINARY_SOURCES=clear;x-aws,s3://vcpkg-export/2023.04.15/x64-windows-meshlib/,readwrite;
+
+vcpkg install !packages! --recurse --overlay-triplets "%~dp0vcpkg\triplets"
+endlocal

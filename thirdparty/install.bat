8a6b10e9daf8ab7a0690442b719d0103b9d5b861
Max <mraiskii@adalisk.com>
2023-07-05 18:45:03 +0300

    test

--- thirdparty/install.bat
+++ thirdparty/install.bat
@@ -7,4 +7,5 @@ copy "%~dp0vcpkg\downloads\*" "%vcpkg_path%downloads"
 REM Set the binary sources to use the S3 bucket
 set VCPKG_BINARY_SOURCES=clear;x-aws-config,no-sign-request;x-aws,s3://vcpkg-export/2023.04.15/x64-windows-meshlib/,read;
 
-vcpkg install --host-triplet x64-windows-meshlib --overlay-triplets "%~dp0vcpkg\triplets"
+vcpkg install vcpkg-cmake vcpkg-cmake-config --host-triplet x64-windows-meshlib --overlay-triplets "%~dp0vcpkg\triplets"  --debug --x-abi-tools-use-exact-versions
+vcpkg install --host-triplet x64-windows-meshlib --overlay-triplets "%~dp0vcpkg\triplets" --debug --x-abi-tools-use-exact-versions
--- thirdparty/install_runner.bat
+++ thirdparty/install_runner.bat
@@ -5,7 +5,7 @@ if not exist "%vcpkg_path%downloads" mkdir "%vcpkg_path%downloads"
 copy "%~dp0vcpkg\downloads\*" "%vcpkg_path%downloads"
 
 REM Set the binary sources to use the S3 bucket
-set VCPKG_BINARY_SOURCES=clear;x-aws,s3://vcpkg-export/2023.04.15/test4/,readwrite;
+set VCPKG_BINARY_SOURCES=clear;x-aws,s3://vcpkg-export/2023.04.15/test5/,readwrite;
 
-vcpkg install vcpkg-cmake:x64-windows-meshlib vcpkg-cmake-config:x64-windows-meshlib --debug --x-abi-tools-use-exact-versions
-vcpkg install --debug --x-abi-tools-use-exact-versions
+vcpkg install vcpkg-cmake vcpkg-cmake-config --host-triplet x64-windows-meshlib --overlay-triplets "%~dp0vcpkg\triplets"  --debug --x-abi-tools-use-exact-versions
+vcpkg install --host-triplet x64-windows-meshlib --overlay-triplets "%~dp0vcpkg\triplets" --debug --x-abi-tools-use-exact-versions

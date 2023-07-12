1c1995f9c86bd1a45eedf6c8fcb6da27c069ed89
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2023-07-12 11:15:08 +0400

    Extend unit tests

--- source/MRMesh/MRTriangleIntersection.cpp
+++ source/MRMesh/MRTriangleIntersection.cpp
@@ -3,29 +3,32 @@
 
 namespace
 {
-    constexpr MR::Triangle3f triangle1 {
+    // triangle lying on XY plane
+    constexpr MR::Triangle3f T1 {
         MR::Vector3f { -30.f, -52.f, 0.f },
         MR::Vector3f { +30.f, -52.f, 0.f },
         MR::Vector3f { 0.f, 0.f, 0.f },
     };
-    constexpr MR::Triangle3f triangle2( float x, float y, float z )
+    // triangle lying on XZ plane
+    constexpr MR::Triangle3f T2 {
+        MR::Vector3f { -30.f, 0.f, -52.f },
+        MR::Vector3f { +30.f, 0.f, -52.f },
+        MR::Vector3f { 0.f, 0.f, 0.f },
+    };
+    // triangle lying on YZ plane
+    constexpr MR::Triangle3f T3 {
+        MR::Vector3f { 0.f, 0.f, -30.f },
+        MR::Vector3f { 0.f, 0.f, +30.f },
+        MR::Vector3f { 0.f, -52.f, 0.f },
+    };
+    constexpr MR::Triangle3f shift( const MR::Triangle3f& origin, float x, float y, float z )
     {
-        constexpr MR::Triangle3f origin {
-            MR::Vector3f { -30.f, 0.f, -52.f },
-            MR::Vector3f { +30.f, 0.f, -52.f },
-            MR::Vector3f { 0.f, 0.f, 0.f },
-        };
         return {
             origin[0] + MR::Vector3f { x, y, z },
             origin[1] + MR::Vector3f { x, y, z },
             origin[2] + MR::Vector3f { x, y, z },
         };
     }
-    constexpr MR::Triangle3f triangle3 {
-        MR::Vector3f { 0.f, 0.f, -30.f },
-        MR::Vector3f { 0.f, 0.f, +30.f },
-        MR::Vector3f { 0.f, -52.f, 0.f },
-    };
 }
 
 namespace MR
@@ -47,6 +50,7 @@ TEST( MRMesh, TriangleSegmentIntersectFloat )
 using TrianglesIntersectParameters = std::tuple<bool, Triangle3f, Triangle3f>;
 class TrianglesIntersectTestFixture : public testing::TestWithParam<TrianglesIntersectParameters> { };
 
+/// check all vertex triplet configurations
 TEST_P( TrianglesIntersectTestFixture, TrianglesIntersect )
 {
     using VertexOrder = std::array<size_t, 3>;
@@ -76,19 +80,32 @@ TEST_P( TrianglesIntersectTestFixture, TrianglesIntersect )
 }
 
 INSTANTIATE_TEST_SUITE_P( MRMesh, TrianglesIntersectTestFixture, testing::Values(
-    // shared vertex
-      TrianglesIntersectParameters { false, triangle1, triangle2( 0.f, 0.f, 0.f ) }
-    // one triangle doesn't intersect the other triangle's plane
-    , TrianglesIntersectParameters { false, triangle1, triangle2( 0.f, -26.f, -13.f ) }
-    // one triangle doesn't intersect the other triangle
-    , TrianglesIntersectParameters { false, triangle1, triangle2( +26.f, -26.f, +13.f ) }
-    // edge-tri intersections
-    , TrianglesIntersectParameters { true, triangle1, triangle2( 0.f, -26.f, +13.f ) }
-    // mutual edge-tri intersections
-    , TrianglesIntersectParameters { true, triangle1, triangle2( +13.f, -26.f, +13.f ) }
-    // two edge-edge intersections
-    , TrianglesIntersectParameters { true, triangle1, triangle2( 0.f, -26.f, +26.f ) }
-    // two edge-vertex intersections
-    , TrianglesIntersectParameters { true, triangle1, triangle3 }
+    /*
+     * no intersection
+     */
+    // T1 and T2 share a vertex
+      TrianglesIntersectParameters { false, T1, T2 }
+    // T1 doesn't intersect T2 plane
+    , TrianglesIntersectParameters { false, T1, shift( T2, 0.f, -26.f, -13.f ) }
+    // T1 vertex lies on T2 side
+    , TrianglesIntersectParameters { false, T1, shift( T2, 0.f, -52.f, 0.f ) }
+    // T1 and T2 share a side
+    , TrianglesIntersectParameters { false, T1, shift( T2, 0.f, -52.f, +52.f ) }
+    // T1 intersects T2 plane but doesn't intersect T2
+    , TrianglesIntersectParameters { false, T1, shift( T2, +26.f, -26.f, +13.f ) }
+    // T1 side intersect T3 side
+    , TrianglesIntersectParameters { false, T1, shift( T3, 0.f, -52.f, 0.f ) }
+
+    /*
+     * intersection
+     */
+    // two T1 sides pass through T2
+    , TrianglesIntersectParameters { true, T1, shift( T2, 0.f, -26.f, +13.f ) }
+    // T1 side passes through T2, T2 side passes through T1
+    , TrianglesIntersectParameters { true, T1, shift( T2, +13.f, -26.f, +13.f ) }
+    // two T1 sides intersect corresponding T2 sides
+    , TrianglesIntersectParameters { true, T1, shift( T2, 0.f, -26.f, +26.f ) }
+    // T1 vertex lies on T3 side, T3 vertex lies on T1 side
+    , TrianglesIntersectParameters { true, T1, T3 }
 ) );
 }

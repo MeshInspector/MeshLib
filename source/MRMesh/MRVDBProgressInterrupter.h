90d98f4ad0b0902204634ac9fa8cf7b76b2cffac
Fedr <fchelnokov@adalisk.com>
2023-07-14 09:24:33 +0300

    Move ProgressInterrupter in a separate header to reduce openvdb inclusions in popular headers (#1423)

---
+++ source/MRMesh/MRVDBProgressInterrupter.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#if !defined( __EMSCRIPTEN__) && !defined( MRMESH_NO_VOXEL )
+
+#include "MRProgressCallback.h"
+#include <openvdb/util/NullInterrupter.h>
+#include <algorithm>
+#include <thread>
+
+namespace MR
+{
+
+// This class implements OpenVdb interrupter interface and provides ability to use MR::ProgressCallback in some OpenVdb operations
+struct ProgressInterrupter : openvdb::util::NullInterrupter
+{
+    ProgressInterrupter( ProgressCallback cb )  : cb_{ cb }
+        , progressThreadId_{ std::this_thread::get_id() } {}
+    virtual bool wasInterrupted( int percent = -1 ) override
+    {
+        wasInterrupted_ = false;
+        if ( cb_ && progressThreadId_ == std::this_thread::get_id() )
+            wasInterrupted_ = !cb_( float( std::clamp( percent, 0, 100 ) ) / 100.0f );
+        return wasInterrupted_;
+    }
+    bool getWasInterrupted() const { return wasInterrupted_; }
+
+private:
+    bool wasInterrupted_{ false };
+    ProgressCallback cb_;
+    std::thread::id progressThreadId_;
+};
+
+} //namespace MR
+
+#endif

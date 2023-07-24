e734333bb4217176beeb8ee93281e4eab2f5da27
Fedr <fchelnokov@adalisk.com>
2023-07-24 10:54:40 +0300

    computeFlow

--- source/MRMesh/MRAggregateFlow.cpp
+++ source/MRMesh/MRAggregateFlow.cpp
@@ -1,16 +1,26 @@
 #include "MRSurfacePath.h"
 #include "MRMesh.h"
 #include "MRBitSetParallelFor.h"
+#include "MRParallelFor.h"
 #include "MRTimer.h"
 #include "MRPch/MRTBB.h"
 
 namespace MR
 {
 
+struct FlowOrigin
+{
+    /// point on the mesh, where this flow starts
+    MeshTriPoint point;
+    /// amount of flow, e.g. can be proportional to the horizontal area associated with the start point
+    float amount = 1;
+};
+
 class FlowAggregator
 {
 public:
     FlowAggregator( const Mesh & mesh, const VertScalars & field );
+    VertScalars computeFlow( const std::vector<FlowOrigin> & starts );
 
 private:
     const Mesh & mesh_;
@@ -49,6 +59,41 @@ FlowAggregator::FlowAggregator( const Mesh & mesh, const VertScalars & field ) :
     vertsSortedDesc_.reserve( minusHeightVerts.size() );
     for ( size_t i = 0; i < minusHeightVerts.size(); ++i )
         vertsSortedDesc_.push_back( minusHeightVerts[i].second );
+}
+
+VertScalars FlowAggregator::computeFlow( const std::vector<FlowOrigin> & starts )
+{
+    MR_TIMER
+    VertScalars flowInVert( mesh_.topology.vertSize() );
+    std::vector<VertId> start2downVert( starts.size() ); // for each start point stores what next vertex is on flow path (can be invalid)
+    std::vector<SurfacePath> start2downPath( starts.size() ); // till next vertex
+
+    ParallelFor( starts, [&]( size_t i )
+    {
+        VertId nextVert;
+        SurfacePath path = computeSteepestDescentPath( mesh_, field_, starts[i].point, {}, &nextVert );
+        start2downVert[i] = nextVert;
+        if ( nextVert )
+        {
+            path.push_back( { mesh_.topology, nextVert } );
+            start2downPath[i] = std::move( path );
+        }
+    } );
+
+    for ( size_t i = 0; i < starts.size(); ++i )
+    {
+        if ( auto v = start2downVert[i] )
+            flowInVert[v] += starts[i].amount;
+    }
+
+    for ( size_t i = 0; i < vertsSortedDesc_.size(); ++i )
+    {
+        auto vUp = vertsSortedDesc_[i];
+        if ( auto vDn = downFlowVert_[vUp] )
+            flowInVert[vDn] += flowInVert[vUp];
+    }
+
+    return flowInVert;
 }
 
 } //namespace MR

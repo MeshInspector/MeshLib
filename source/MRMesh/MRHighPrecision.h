017f37da819ed50ee38ff3ae59779c85ff797828
Grant Karapetyan <gkarapetyan@adalisk.com>
2023-07-25 17:42:42 +0300

    New Terrain triangulation

--- source/MRMesh/MRHighPrecision.h
+++ source/MRMesh/MRHighPrecision.h
@@ -14,6 +14,10 @@ using HighPrecisionInt = boost::multiprecision::checked_int128_t;
 
 using Vector2hp = Vector2<HighPrecisionInt>;
 using Vector3hp = Vector3<HighPrecisionInt>;
+using Vector4hp = Vector4<HighPrecisionInt>;
+using Matrix3hp = Matrix3<HighPrecisionInt>;
+using Matrix4hp = Matrix4<HighPrecisionInt>;
+
 
 /// \}
 
---
+++ source/MRMesh/MRTerrainTriangulation.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "MRMesh/MRMeshFwd.h"
+#include "MRMesh.h"
+#include "MRProgressCallback.h"
+#include "MRExpected.h"
+
+namespace MR
+{
+
+// Creates Delaunay triangulation using only XY components of points 
+// points will be changed inside this function take argument by value
+[[nodiscard]] MRMESH_API Expected<Mesh, std::string> terrainTriangulation( std::vector<Vector3f> points, ProgressCallback cb = {} );
+
+}

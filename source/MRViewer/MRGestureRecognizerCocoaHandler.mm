0c7c4621edeb487ea9f5a4d89cee792a76b25fb1
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2023-08-01 17:18:52 +0400

    Fix

--- source/MRViewer/MRGestureRecognizerCocoaHandler.mm
+++ source/MRViewer/MRGestureRecognizerCocoaHandler.mm
@@ -2,6 +2,7 @@
 
 #include "MRGestureRecognizerCocoaHandler.h"
 
+#include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_COCOA
 #include <GLFW/glfw3native.h>
 
--- source/MRViewer/MRGestureRecognizerHandler.h
+++ source/MRViewer/MRGestureRecognizerHandler.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <GLFW/glfw3.h>
-
 #include <functional>
+
+struct GLFWwindow;
 
 namespace MR
 {

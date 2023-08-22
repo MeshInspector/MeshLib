372b7e1bc9af9244c251db120e99480c68d7f08a
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2023-08-22 16:50:21 +0400

    Remove deprecated parameter

--- source/MRViewer/MRTouchpadController.h
+++ source/MRViewer/MRTouchpadController.h
@@ -36,7 +36,7 @@ public:
     {
         /// most touchpads implement kinetic (or inertial) scrolling, this option disables handling of these events
         bool ignoreKineticMoves = false;
-        /// scale coefficient for swipe movements
+        /// deprecated: scale coefficient for swipe movements
         [[deprecated]] float swipeScale = 10.f;
         /// enable gesture's cancellability, i.e. revert its changes in case of external interruption
         bool cancellable = false;

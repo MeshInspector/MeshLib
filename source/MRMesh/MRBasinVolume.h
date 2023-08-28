783a14b3919e25ae1ee950539db6c0c59c0f78fc
Fedr <fchelnokov@adalisk.com>
2023-08-28 14:29:47 +0300

    BasinVolumeCalculator (#1571)
    * BasinVolumeCalculator
    
    * #include <cassert>

---
+++ source/MRMesh/MRBasinVolume.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "MRMeshFwd.h"
+
+namespace MR
+{
+
+/// the class to compute the volume of water some basin can accumulate,
+/// considering that water upper surface has constant z-level
+class BasinVolumeCalculator
+{
+public:
+    /// pass every triangle of the basin here, and the water level;
+    /// \return true if the triangle is at least partially below the water level and influences on the volume
+    MRMESH_API bool addTerrainTri( Triangle3f t, float level );
+
+    /// call it after all addTerrainTri to get the volume
+    [[nodiscard]] double getVolume() const { return sum_ / 6; }
+
+private:
+    double sum_ = 0;
+};
+
+} //namespace MR

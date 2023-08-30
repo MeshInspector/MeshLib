a982ee9ed5bda466bbd66beadcf58a717171abd3
Fedr <fchelnokov@adalisk.com>
2023-08-30 22:08:49 +0300

    packPointsWithHistory

---
+++ source/MRViewer/MRObjectPointsHistory.cpp
@@ -0,0 +1,59 @@
+#include "MRObjectPointsHistory.h"
+#include "MRAppendHistory.h"
+#include <MRMesh/MRChangePointCloudAction.h>
+#include <MRMesh/MRChangeVertsColorMapAction.h>
+#include <MRMesh/MRChangeSelectionAction.h>
+#include <MRMesh/MRObjectPoints.h>
+#include <MRMesh/MRPointCloud.h>
+#include <MRMesh/MRTimer.h>
+#include <MRMesh/MRBitSetParallelFor.h>
+
+namespace MR
+{
+
+bool packPointsWithHistory( const std::shared_ptr<ObjectPoints>& objPoints )
+{
+    MR_TIMER
+
+    Historian<ChangePointCloudAction> h( "set cloud", objPoints );
+        
+    VertMap new2Old;
+    if ( !objPoints->varPointCloud()->pack( &new2Old ) )
+    {
+        h.cancelAction();
+        return false;
+    }
+
+    if ( !objPoints->getVertsColorMap().empty() )
+    {
+        Historian<ChangeVertsColorMapAction> hCM( "color map update", objPoints );
+        VertColors colors;
+        objPoints->updateVertsColorMap( colors );
+        for ( VertId n = 0_v; n < new2Old.size(); ++n )
+        {
+            VertId o = new2Old[n];
+            assert( n <= o );
+            colors[n] = colors[o];
+        }
+        colors.resize( new2Old.size() );
+        objPoints->updateVertsColorMap( colors );
+    }
+
+    // update faces in the selection
+    const auto & oldSel = objPoints->getSelectedPoints();
+    if ( oldSel.any() )
+    {
+        Historian<ChangePointPointSelectionAction> hs( "selection", objPoints );
+        VertBitSet newSel( new2Old.size() );
+        BitSetParallelForAll( newSel, [&]( VertId n )
+        {
+            if ( oldSel.test( new2Old[n] ) )
+                newSel.set( n );
+        } );
+        objPoints->selectPoints( std::move( newSel ) );
+    }
+
+    return true;
+}
+
+} //namespace MR
---
+++ source/MRViewer/MRObjectPointsHistory.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "exports.h"
+#include <MRMesh/MRMeshFwd.h>
+#include <memory>
+
+namespace MR
+{
+
+/// packs ObjectPoints updating colors and adds history records for that;
+/// \returns false if the cloud was packed before the call and nothing has been changed (and no history records added);
+/// it is a good idea to make SCOPED_HISTORY before calling this function
+MRVIEWER_API bool packPointsWithHistory( const std::shared_ptr<ObjectPoints>& objPoints );
+
+} //namespace MR

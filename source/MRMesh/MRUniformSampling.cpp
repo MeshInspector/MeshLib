6a3cc59c68a5946b9735820ee2d9292e7cd5ee90
Fedr <fchelnokov@adalisk.com>
2023-08-30 17:03:18 +0300

    optimize pointUniformSampling for large balls

--- source/MRMesh/MRUniformSampling.cpp
+++ source/MRMesh/MRUniformSampling.cpp
@@ -1,9 +1,9 @@
 #include "MRUniformSampling.h"
 #include "MRPointCloud.h"
-#include "MRCloseVertices.h"
 #include "MRBitSetParallelFor.h"
 #include "MRVector.h"
 #include "MRTimer.h"
+#include "MRPointsInBall.h"
 
 namespace MR
 {
@@ -11,16 +11,27 @@ namespace MR
 std::optional<VertBitSet> pointUniformSampling( const PointCloud& pointCloud, float distance, const ProgressCallback & cb )
 {
     MR_TIMER
-    const auto c = findSmallestCloseVertices( pointCloud, distance, cb );
-    if ( !c )
-        return {};
-    const auto & vmap = *c;
-    VertBitSet res( vmap.size() );
-    BitSetParallelForAll( pointCloud.validPoints, [&]( VertId v )
+
+    const auto sz = pointCloud.points.size();
+    const int reportStep = std::min( int( sz / 64 ), 1024 );
+    VertId reportNext = 0_v;
+
+    VertBitSet res = pointCloud.validPoints;
+    for ( auto v : res )
     {
-        if ( vmap[v] == v )
-            res.set( v );
-    } );
+        if ( cb && v >= reportNext )
+        {
+            if ( !cb( float( v ) / sz ) )
+                return {};
+            reportNext = v + reportStep;
+        }
+
+        findPointsInBall( pointCloud, pointCloud.points[v], distance, [&]( VertId cv, const Vector3f& )
+        {
+            if ( cv > v )
+                res.reset( cv );
+        } );
+    }
     return res;
 }
 

15d82b9de8881336b13c226d891f830b4e08fea9
Fedr <fchelnokov@adalisk.com>
2023-09-03 14:58:06 +0300

    add includes

--- source/MRMesh/Aaaa.cpp
+++ source/MRMesh/Aaaa.cpp
@@ -1,6 +1,7 @@
 #ifndef __EMSCRIPTEN__
 
 #include <fmt/format.h>
+#include "MRGTest.h"
 
 const auto aaa = fmt::format( "{}", FMT_VERSION );
 
--- source/MRMesh/Aaab.cpp
+++ source/MRMesh/Aaab.cpp
@@ -1,6 +1,7 @@
 #ifndef __EMSCRIPTEN__
 
 #include <spdlog/spdlog.h>
+#include "MRStreamOperators.h"
 //#include "MRGTest.h"
 
 void aab()

8dcdfb5d47221b22880184b1901f9078e1e2ba30
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2023-09-18 17:14:12 +0400

    Add function for searching overhangs (#1611)
    * Add function for searching overhangs
    
    * Add timer
    
    * Update comment
    
    * Add parallel computation
    
    * Make region simplification optional
    
    * Change terminology

---
+++ source/MRMesh/MRMeshOverhangs.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "MRMeshFwd.h"
+#include "MRVector3.h"
+
+namespace MR
+{
+
+/// parameters for MR::findOverhangs
+struct FindOverhangsSettings
+{
+    /// base axis marking the up direction
+    Vector3f axis;
+    /// height of a layer
+    float layerHeight { 1.f };
+    /// maximum overhang distance within a layer
+    float maxOverhangDistance { 1.f };
+    /// number of hops used to smooth out the overhang regions (0 - disable smoothing)
+    int hops = 0;
+};
+
+/// \brief Find face regions that might create overhangs
+/// \param mesh - source mesh
+/// \param settings - parameters
+/// \return face regions
+MRMESH_API std::vector<FaceBitSet> findOverhangs( const Mesh& mesh, const FindOverhangsSettings& settings );
+
+} // namespace MR

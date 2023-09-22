ed6ba7d1af99a8adbcb1eceed329edb35405f90c
Fedr <fchelnokov@adalisk.com>
2023-09-22 18:18:44 +0300

    MeshOrPoints class for floating object in ICP (#1633)
    * MeshOrPoints class for floating object in ICP
    
    * Clang fix
    
    * #include <optional>
    
    * MeshOrPoints floating in ICP

---
+++ source/MRMesh/MRMeshOrPoints.cpp
@@ -0,0 +1,66 @@
+#include "MRMeshOrPoints.h"
+#include "MRMesh.h"
+#include "MRPointCloud.h"
+#include "MRBox.h"
+#include "MRGridSampling.h"
+
+namespace MR
+{
+
+template<class... Ts>
+struct overloaded : Ts... { using Ts::operator()...; };
+
+// explicit deduction guide (not needed as of C++20, but still needed in Clang)
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
+
+Box3f MeshOrPoints::computeBoundingBox( const AffineXf3f * toWorld ) const
+{
+    return std::visit( overloaded{
+        [toWorld]( const MeshPart & mp ) { return mp.mesh.computeBoundingBox( mp.region, toWorld ); },
+        [toWorld]( const PointCloud * pc ) { return pc->computeBoundingBox( toWorld ); }
+    }, var_ );
+}
+
+std::optional<VertBitSet> MeshOrPoints::pointsGridSampling( float voxelSize, const ProgressCallback & cb )
+{
+    return std::visit( overloaded{
+        [voxelSize, cb]( const MeshPart & mp ) { return verticesGridSampling( mp, voxelSize, cb ); },
+        [voxelSize, cb]( const PointCloud * pc ) { return pointGridSampling( *pc, voxelSize, cb ); }
+    }, var_ );
+}
+
+const VertCoords & MeshOrPoints::points() const
+{
+    return std::visit( overloaded{
+        []( const MeshPart & mp ) -> const VertCoords & { return mp.mesh.points; },
+        []( const PointCloud * pc ) -> const VertCoords & { return pc->points; }
+    }, var_ );
+}
+
+std::function<Vector3f(VertId)> MeshOrPoints::normals() const
+{
+    return std::visit( overloaded{
+        []( const MeshPart & mp ) -> std::function<Vector3f(VertId)>
+        {
+            return [&mesh = mp.mesh]( VertId v ) { return mesh.normal( v ); };
+        },
+        []( const PointCloud * pc ) -> std::function<Vector3f(VertId)>
+        { 
+            return pc->normals.empty() ? std::function<Vector3f(VertId)>{} : [pc]( VertId v ) { return pc->normals[v]; };
+        }
+    }, var_ );
+}
+
+std::function<float(VertId)> MeshOrPoints::weights() const
+{
+    return std::visit( overloaded{
+        []( const MeshPart & mp ) -> std::function<float(VertId)>
+        {
+            return [&mesh = mp.mesh]( VertId v ) { return mesh.dblArea( v ); };
+        },
+        []( const PointCloud * ) { return std::function<float(VertId)>{}; }
+    }, var_ );
+}
+
+} // namespace MR

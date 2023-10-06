bd0b2432594b6b86fb8190b6576e4015b67ff2bb
Fedr <fchelnokov@adalisk.com>
2023-10-06 16:33:17 +0300

    Move zip-functions in separate files (#1673)
    * Move zip-function in separate files
    
    * #include <fstream>

---
+++ source/MRMesh/MRZip.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "MRMeshFwd.h"
+#include "MRProgressCallback.h"
+#include "MRExpected.h"
+#include <filesystem>
+#include <vector>
+
+namespace MR
+{
+
+/// \defgroup SerializerGroup Serializer
+/// \ingroup IOGroup
+/// \{
+
+/**
+ * \brief decompresses given zip-file into given folder
+ * \param password if password is given then it will be used to decipher encrypted archive
+ */
+MRMESH_API VoidOrErrStr decompressZip( const std::filesystem::path& zipFile, const std::filesystem::path& targetFolder,
+    const char * password = nullptr );
+/**
+ * \brief compresses given folder in given zip-file
+ * \param excludeFiles files that should not be included to result zip 
+ * \param password if password is given then the archive will be encrypted
+ * \param cb an option to get progress notifications and cancel the operation
+ */
+MRMESH_API VoidOrErrStr compressZip( const std::filesystem::path& zipFile, const std::filesystem::path& sourceFolder, 
+    const std::vector<std::filesystem::path>& excludeFiles = {}, const char * password = nullptr, ProgressCallback cb = {} );
+
+/// \}
+
+} // namespace MR

cdcff17406902e712f7b37cfea8efc8c8959cbc2
Grant Karapetyan <gkarapetyan@adalisk.com>
2023-10-12 18:12:35 +0300

    update comments

--- source/MRMesh/MROffsetContours.h
+++ source/MRMesh/MROffsetContours.h
@@ -10,34 +10,34 @@ using ContoursIndicesMap = std::vector<ContourIndicesMap>;
 
 struct OffsetContoursParams
 {
-    // type of offset
+    /// type of offset
     enum class Type
     {
-        Offset, // One-side signed offset, requires closed contours
-        Shell // Two-side offset
+        Offset, ///< One-side signed offset, requires closed contours
+        Shell ///< Two-side offset
     } type{ Type::Offset };
 
-    // type of offsetting on ends of non-closed contours
+    /// type of offsetting on ends of non-closed contours
     enum class EndType
     {
-        Round, // creates round ends (use `minAnglePrecision`)
-        Cut // creates sharp end (same as Round with `minAnglePrecision` < 180 deg)
+        Round, ///< creates round ends (use `minAnglePrecision`)
+        Cut ///< creates sharp end (same as Round with `minAnglePrecision` < 180 deg)
     } endType{ EndType::Round };
 
-    // type of positive offset curve in corners
+    /// type of positive offset curve in corners
     enum class CornerType
     {
-        Round, // creates round corners (use `minAnglePrecision`)
-        Sharp // creates sharp connected corner (use `maxSharpAngle` as limit)
+        Round, ///< creates round corners (use `minAnglePrecision`)
+        Sharp ///< creates sharp connected corner (use `maxSharpAngle` as limit)
     } cornerType{ CornerType::Round };
 
-    // precision of round corners and ends
+    /// precision of round corners and ends
     float minAnglePrecision = PI_F / 9.0f; // 20 deg
-    // limit for sharp corners connection
+    /// limit for sharp corners connection
     float maxSharpAngle = PI_F * 2.0f / 3.0f; // 120 deg
 };
 
-// offsets 2d contours in plane
+/// offsets 2d contours in plane
 [[nodiscard]] MRMESH_API Contours2f offsetContours( const Contours2f& contours, float offset,
     const OffsetContoursParams& params = {} );
 

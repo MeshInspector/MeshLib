052555f9738c3e49a0ef511578bd7ebb3376e2d8
Fedr <fchelnokov@adalisk.com>
2023-10-19 11:51:41 +0300

    findSkyRays

--- source/MRMesh/MRSolarRadiation.h
+++ source/MRMesh/MRSolarRadiation.h
@@ -18,10 +18,20 @@ struct SkyPatch
     float radiation = 0;
 };
 
-/// computes relative radiation in each sample point by emitting rays from that point in the sky:
-/// the radiation is 1.0f if all rays reach the sky not meeting the terrain;
-/// the radiation is 0.0f if all rays do not reach the sky because they are intercepted by the terrain
+/// computes relative radiation in each valid sample point by emitting rays from that point in the sky:
+/// the radiation is 1.0f if all rays reach the sky not hitting the terrain;
+/// the radiation is 0.0f if all rays do not reach the sky because they are intercepted by the terrain;
+/// \param outSkyRays - optional output bitset where for every valid sample #i its rays are stored at indices [i*numPatches; (i+1)*numPatches),
+///                     0s for occluded rays (hitting the terrain) and 1s for the ones which don't hit anything and reach the sky
 [[nodiscard]] MRMESH_API VertScalars computeSkyViewFactor( const Mesh & terrain,
+    const VertCoords & samples, const VertBitSet & validSamples,
+    const std::vector<SkyPatch> & skyPatches,
+    BitSet * outSkyRays = nullptr );
+
+/// In each valid sample point tests the rays from that point in the sky;
+/// \return bitset where for every valid sample #i its rays are stored at indices [i*numPatches; (i+1)*numPatches),
+///         0s for occluded rays (hitting the terrain) and 1s for the ones which don't hit anything and reach the sky
+[[nodiscard]] MRMESH_API BitSet findSkyRays( const Mesh & terrain,
     const VertCoords & samples, const VertBitSet & validSamples,
     const std::vector<SkyPatch> & skyPatches );
 

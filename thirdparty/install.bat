becf95c99078fd2d6eef2b2397214d15e80e866d
Maxim Rayskiy <mraiskii@adalisk.com>
2023-10-19 19:50:56 +0300

    Test vcpkg build

--- thirdparty/install.bat
+++ thirdparty/install.bat
@@ -4,26 +4,7 @@ set VCPKG_DEFAULT_TRIPLET=x64-windows-meshlib
 setlocal enabledelayedexpansion
 REM check if we could use aws s3 binary caching
 aws --version >nul 2>&1
-if errorlevel 1 (
-    echo AWS CLI v2 is not installed.
-) else (
-    echo AWS CLI v2 is installed.
 
-    REM Check if --write-s3 option is provided
-    set "write_s3_option=false"
-    for %%i in (%*) do (
-        if "%%i"=="--write-s3" set "write_s3_option=true"
-    )
-
-    REM Set VCPKG_BINARY_SOURCES based on the option
-    if !write_s3_option! equ true (
-        set "VCPKG_BINARY_SOURCES=clear;x-aws,s3://vcpkg-export/2023.08.09/x64-windows-meshlib/,readwrite;"
-        echo "using aws auth"
-    ) else (
-        set "VCPKG_BINARY_SOURCES=clear;x-aws-config,no-sign-request;x-aws,s3://vcpkg-export/2023.08.09/x64-windows-meshlib/,readwrite;"
-        echo "using no auth"
-    )
-)
 
 for /f "delims=" %%i in ('where vcpkg') do set vcpkg_path=%%~dpi
 if not exist "%vcpkg_path%downloads" mkdir "%vcpkg_path%downloads"

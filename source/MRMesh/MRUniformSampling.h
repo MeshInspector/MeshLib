50e847f3dfe86b06be51dbe12edb10875eacd4ab
Grant Karapetyan <gkarapetyan@adalisk.com>
2023-10-19 19:27:39 +0300

    Add more regular uniform pointcloud sampling

--- source/MRMesh/MRUniformSampling.h
+++ source/MRMesh/MRUniformSampling.h
@@ -13,6 +13,12 @@ namespace MR
 /// \ingroup PointCloudGroup
 [[nodiscard]] MRMESH_API std::optional<VertBitSet> pointUniformSampling( const PointCloud& pointCloud, float distance, const ProgressCallback & cb = {} );
 
+/// Sample vertices, removing ones that are too close;
+/// may take longer than MR::pointUniformSampling but return more regular distribution
+/// returns std::nullopt if it was terminated by the callback
+/// \ingroup PointCloudGroup
+[[nodiscard]] MRMESH_API std::optional<VertBitSet> pointRegularUniformSampling( const PointCloud& pointCloud, float distance, const ProgressCallback& cb = {} );
+
 /// Composes new point cloud consisting of uniform samples of original point cloud;
 /// \param extNormals if given then they will be copied in new point cloud
 /// returns std::nullopt if it was terminated by the callback

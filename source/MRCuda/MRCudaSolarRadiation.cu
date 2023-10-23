ec44c56c53d30d8d3e016b6e0f3260f03c88347d
ASTROCOMP\astro <akoksin@adalisk.com>
2023-10-23 17:33:39 +0400

    add kernel function

--- source/MRCuda/MRCudaSolarRadiation.cu
+++ source/MRCuda/MRCudaSolarRadiation.cu
@@ -1,6 +1,30 @@
-#include "MRCudaContoursDistanceMap.cuh"
+#include "MRCudaSolarRadiation.cuh"
 #include "MRCudaBasic.h"
 #include "device_launch_parameters.h"
 #include "MRCudaFloat.cuh"
 #include <float.h>
 
+namespace MR
+{
+namespace Cuda
+{
+
+__global__ void kernel( const Node3* nodes, const float3* samples, const uint64_t* validSamples, const IntersectionPrecomputes* precs, uint64_t* res, const size_t nodeCount, const size_t sampleCount, const size_t precCount )
+{
+    if ( size == 0 )
+    {
+        assert( false );
+        return;
+    }
+
+}
+
+void findSkyRaysKernel( const Node3* nodes, const float3* samples, const uint64_t* validSamples, const IntersectionPrecomputes* precs, uint64_t* res, const size_t nodeCount, const size_t sampleCount, const size_t precCount )
+{
+    const size_t resSize = sampleCount * precCount;
+    constexpr int maxThreadsPerBlock = 640;
+    int numBlocks = ( int( resSize ) + maxThreadsPerBlock - 1 ) / maxThreadsPerBlock;
+}
+
+}
+}

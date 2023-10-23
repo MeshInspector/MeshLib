59f7c6816a37129e051cf858badfe87abd43c10b
Rodion Orlov <rorlov@adalisk.com>
2023-10-23 13:19:32 +0400

    Post-review fixes

--- source/MRMesh/MRChangeColoringActions.h
+++ source/MRMesh/MRChangeColoringActions.h
@@ -8,50 +8,6 @@
 namespace MR
 {
 
-/// History action for texture change
-/// \ingroup HistoryGroup
-class ChangeTextureAction : public HistoryAction
-{
-public:
-    using Obj = ObjectMeshHolder;
-    /// Constructed from original obj
-    ChangeTextureAction( const std::string& name, const std::shared_ptr<ObjectMeshHolder>& obj ) :
-        obj_{ obj },
-        name_{ name }
-    {
-        if ( obj )
-            texture_ = obj->getTexture();
-    }
-
-    virtual std::string name() const override
-    {
-        return name_;
-    }
-
-    virtual void action( HistoryAction::Type ) override
-    {
-        if ( !obj_ )
-            return;
-        obj_->updateTexture( texture_ );
-    }
-
-    static void setObjectDirty( const std::shared_ptr<ObjectMeshHolder>& obj )
-    {
-        if ( obj )
-            obj->setDirtyFlags( DIRTY_TEXTURE );
-    }
-
-    [[nodiscard]] virtual size_t heapBytes() const override
-    {
-        return name_.capacity() + texture_.heapBytes();
-    }
-
-private:
-    std::shared_ptr<ObjectMeshHolder> obj_;
-    MeshTexture texture_;
-    std::string name_;
-};
-
 /// History action for faces color map change
 /// \ingroup HistoryGroup
 class ChangeFacesColorMapAction : public HistoryAction

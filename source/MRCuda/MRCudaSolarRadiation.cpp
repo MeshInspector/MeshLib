c0ea32436089273f224239bc35a2779f3ec8c98b
ASTROCOMP\astro <akoksin@adalisk.com>
2023-10-27 16:55:09 +0400

    code review

--- source/MRCuda/MRCudaSolarRadiation.cpp
+++ source/MRCuda/MRCudaSolarRadiation.cpp
@@ -1,103 +1,47 @@
 #include "MRCudaSolarRadiation.h"
 #include "MRMesh/MRAABBTree.h"
 #include "MRCudaSolarRadiation.cuh"
+#include "MRMesh/MRTimer.h"
+#include "MRMesh/MRIntersectionPrecomputes.h"
 
 namespace MR
 {
 namespace Cuda
 {
 
-static void findMaxVectorDim( int& dimX, int& dimY, int& dimZ, const Vector3f& dir )
+static std::vector<IntersectionPrecomputes> calcPrecs( const std::vector<MR::SkyPatch>& skyPatches )
 {
-    if ( dir.x > dir.y )
+    std::vector<IntersectionPrecomputes> precs;
+    precs.reserve( skyPatches.size() );
+
+    for ( const auto& skyPatch : skyPatches )
     {
-        if ( dir.x > dir.z )
-        {
-            if ( dir.y > dir.z )
-            {
-                // x>y>z
-                if ( -dir.z > dir.x )
-                {
-                    dimZ = 2; dimX = 1; dimY = 0;
-                }
-                else
-                {
-                    dimZ = 0; dimX = 1; dimY = 2;
-                }
-            }
-            else
-            {
-                // x>z>y
-                if ( -dir.y > dir.x )
-                {
-                    dimZ = 1; dimX = 0; dimY = 2;
-                }
-                else
-                {
-                    dimZ = 0; dimX = 1; dimY = 2;
-                }
-            }
-        }
-        else
-        {
-            // z>x>y
-            if ( -dir.y > dir.z )
-            {
-                dimZ = 1; dimX = 0; dimY = 2;
-            }
-            else
-            {
-                dimZ = 2; dimX = 0; dimY = 1;
-            }
-        }
+        const auto& dir = skyPatch.dir;
+        precs.emplace_back();
+        auto& prec = precs.back();
+        findMaxVectorDim( prec.idxX, prec.idxY, prec.maxDimIdxZ, dir );
+
+        prec.sign.x = dir.x >= 0.0f ? 1 : 0;
+        prec.sign.y = dir.y >= 0.0f ? 1 : 0;
+        prec.sign.z = dir.z >= 0.0f ? 1 : 0;
+
+        prec.Sx = dir[prec.idxX] / dir[prec.maxDimIdxZ];
+        prec.Sy = dir[prec.idxY] / dir[prec.maxDimIdxZ];
+        prec.Sz = 1.0f / dir[prec.maxDimIdxZ];
+
+        prec.invDir.x = ( dir.x == 0 ) ? std::numeric_limits<float>::max() : 1.0f / dir.x;
+        prec.invDir.y = ( dir.y == 0 ) ? std::numeric_limits<float>::max() : 1.0f / dir.y;
+        prec.invDir.z = ( dir.z == 0 ) ? std::numeric_limits<float>::max() : 1.0f / dir.z;
     }
-    else
-    {
-        if ( dir.y > dir.z )
-        {
-            if ( dir.x < dir.z )
-            {
-                // y>z>x
-                if ( -dir.x > dir.y )
-                {
-                    dimZ = 0; dimX = 2; dimY = 1;
-                }
-                else
-                {
-                    dimZ = 1; dimX = 2; dimY = 0;
-                }
-            }
-            else
-            {
-                // y>x>z
-                if ( -dir.z > dir.y )
-                {
-                    dimZ = 2; dimX = 1; dimY = 0;
-                }
-                else
-                {
-                    dimZ = 1; dimX = 2; dimY = 0;
-                }
-            }
-        }
-        else
-        {
-            // z>y>x
-            if ( -dir.x > dir.z )
-            {
-                dimZ = 0; dimX = 2; dimY = 1;
-            }
-            else
-            {
-                dimZ = 2; dimX = 0; dimY = 1;
-            }
-        }
-    }
+
+    return precs;
 }
 
 
 BitSet findSkyRays( const Mesh& terrain, const VertCoords& samples, const VertBitSet& validSamples, const std::vector<MR::SkyPatch>& skyPatches )
 {
+    MR_TIMER
+
     const auto& tree = terrain.getAABBTree();
     const auto& nodes = tree.nodes();
 
@@ -114,26 +58,7 @@ BitSet findSkyRays( const Mesh& terrain, const VertCoords& samples, const VertBi
     boost::to_block_range( validSamples, std::back_inserter( blocks ) );
     DynamicArray<uint64_t> cudaValidSamples( blocks );
 
-    std::vector<IntersectionPrecomputes> precs( skyPatches.size() );
-    for ( size_t i = 0; i < skyPatches.size(); ++i )
-    {
-        const auto& dir = skyPatches[i].dir;
-        auto& prec = precs[i];
-        findMaxVectorDim( prec.idxX, prec.idxY, prec.maxDimIdxZ, dir );
-
-        prec.sign.x = dir.x >= 0.0f ? 1 : 0;
-        prec.sign.y = dir.y >= 0.0f ? 1 : 0;
-        prec.sign.z = dir.z >= 0.0f ? 1 : 0;
-
-        prec.Sx = dir[prec.idxX] / dir[prec.maxDimIdxZ];
-        prec.Sy = dir[prec.idxY] / dir[prec.maxDimIdxZ];
-        prec.Sz = 1.0f / dir[prec.maxDimIdxZ];
-
-        prec.invDir.x = ( dir.x == 0 ) ? std::numeric_limits<float>::max() : 1.0f / dir.x;
-        prec.invDir.y = ( dir.y == 0 ) ? std::numeric_limits<float>::max() : 1.0f / dir.y;
-        prec.invDir.z = ( dir.z == 0 ) ? std::numeric_limits<float>::max() : 1.0f / dir.z;
-    }
-
+    const auto precs = calcPrecs( skyPatches );
     DynamicArray<IntersectionPrecomputes> cudaPrecs( precs );
 
     const size_t rayCount = samples.size() * skyPatches.size();
@@ -150,6 +75,8 @@ VertScalars  computeSkyViewFactor( const Mesh& terrain,
     const std::vector<MR::SkyPatch>& skyPatches,
     BitSet* outSkyRays )
 {
+    MR_TIMER
+
     const auto& tree = terrain.getAABBTree();
     const auto& nodes = tree.nodes();
 
@@ -166,38 +93,19 @@ VertScalars  computeSkyViewFactor( const Mesh& terrain,
     boost::to_block_range( validSamples, std::back_inserter( cudaValidSamplesblocks ) );
     DynamicArray<uint64_t> cudaValidSamples( cudaValidSamplesblocks );
 
-    std::vector<IntersectionPrecomputes> precs( skyPatches.size() );
-    for ( size_t i = 0; i < skyPatches.size(); ++i )
-    {
-        const auto& dir = skyPatches[i].dir;
-        auto& prec = precs[i];
-        findMaxVectorDim( prec.idxX, prec.idxY, prec.maxDimIdxZ, dir );
-
-        prec.sign.x = dir.x >= 0.0f ? 1 : 0;
-        prec.sign.y = dir.y >= 0.0f ? 1 : 0;
-        prec.sign.z = dir.z >= 0.0f ? 1 : 0;
-
-        prec.Sx = dir[prec.idxX] / dir[prec.maxDimIdxZ];
-        prec.Sy = dir[prec.idxY] / dir[prec.maxDimIdxZ];
-        prec.Sz = 1.0f / dir[prec.maxDimIdxZ];
-
-        prec.invDir.x = ( dir.x == 0 ) ? std::numeric_limits<float>::max() : 1.0f / dir.x;
-        prec.invDir.y = ( dir.y == 0 ) ? std::numeric_limits<float>::max() : 1.0f / dir.y;
-        prec.invDir.z = ( dir.z == 0 ) ? std::numeric_limits<float>::max() : 1.0f / dir.z;
-    }
-
+    const auto precs = calcPrecs( skyPatches );
     DynamicArray<IntersectionPrecomputes> cudaPrecs( precs );
     DynamicArray<SkyPatch> cudaSkyPatches( skyPatches );
 
     const size_t rayCount = samples.size() * skyPatches.size();
     DynamicArray<uint64_t> cudaOutSkyRays( ( rayCount + 63 ) / 64 );    
 
-    if ( outSkyRays )
+    if ( outSkyRays &&
+        findSkyRaysKernel( cudaNodes.data(), cudaMeshPoints.data(), cudaFaces.data(), cudaSamples.data(), cudaValidSamples.data(), cudaPrecs.data(), cudaOutSkyRays.data(), cudaOutSkyRays.size(), cudaSamples.size(), cudaPrecs.size() ) == cudaSuccess )
     {
-        findSkyRaysKernel( cudaNodes.data(), cudaMeshPoints.data(), cudaFaces.data(), cudaSamples.data(), cudaValidSamples.data(), cudaPrecs.data(), cudaOutSkyRays.data(), cudaOutSkyRays.size(), cudaSamples.size(), cudaPrecs.size() );
         std::vector<uint64_t> outSkyRaysBlocks;
         cudaOutSkyRays.toVector( outSkyRaysBlocks );
-        *outSkyRays = BitSet( outSkyRaysBlocks.begin(), outSkyRaysBlocks.end() );
+        *outSkyRays = BitSet( outSkyRaysBlocks.begin(), outSkyRaysBlocks.end() );     
     }
 
     DynamicArray<float> cudaRes( samples.size() );
--- source/MRCuda/MRCudaSolarRadiation.cu
+++ source/MRCuda/MRCudaSolarRadiation.cu
@@ -25,6 +25,7 @@ __global__ void rayKernel( const Node3* nodes, const float3* meshPoints, const F
     const size_t blockEnd = ( blockIndex == resBlockCount - 1) ? ( sampleCount * precCount ) : ( ( blockIndex + 1 ) << 6 );
 
     uint64_t currentBit = 1;
+    uint64_t block = 0;
     for ( size_t index = blockStart; index < blockEnd; ++index )
     {
         const size_t sample = index / precCount;
@@ -34,11 +35,13 @@ __global__ void rayKernel( const Node3* nodes, const float3* meshPoints, const F
 
             if ( rayMeshIntersect( nodes, meshPoints, faces, samples[sample], 0, FLT_MAX, precs[patch] ) < 0 )
             {
-                res[blockIndex] |= currentBit;         
+                block |= currentBit;         
             }
         }
         currentBit <<= 1;
     }
+
+    res[blockIndex] = block;
 }
 
 __global__ void radiationKernel( const Node3* nodes, const float3* meshPoints, const FaceToThreeVerts* faces, const float3* samples, const uint64_t* validSamples, const SkyPatch* skyPatches, const IntersectionPrecomputes* precs, const float rMaxRadiation, float* res, const size_t sampleCount, const size_t precCount, uint64_t* outSkyRays )
@@ -79,19 +82,25 @@ __global__ void radiationKernel( const Node3* nodes, const float3* meshPoints, c
     res[sampleVertId] = rMaxRadiation * totalRadiation;
 }
 
-void findSkyRaysKernel( const Node3* nodes, const float3* meshPoints, const FaceToThreeVerts* faces, const float3* samples, const uint64_t* validSamples, const IntersectionPrecomputes* precs, uint64_t* res, const size_t resBlockCount, const size_t sampleCount, const size_t precCount )
+cudaError_t findSkyRaysKernel( const Node3* nodes, const float3* meshPoints, const FaceToThreeVerts* faces, const float3* samples, const uint64_t* validSamples, const IntersectionPrecomputes* precs, uint64_t* res, const size_t resBlockCount, const size_t sampleCount, const size_t precCount )
 {
     constexpr int maxThreadsPerBlock = 640;
     int numBlocks = ( int( resBlockCount ) + maxThreadsPerBlock - 1 ) / maxThreadsPerBlock;
 
     rayKernel << < numBlocks, maxThreadsPerBlock >> > ( nodes, meshPoints, faces, samples, validSamples, precs, res, resBlockCount, sampleCount, precCount );
+    CUDA_EXEC_RETURN( cudaGetLastError() );
+
+    return cudaSuccess;
 }
 
-void computeSkyViewFactorKernel( const Node3* nodes, const float3* meshPoints, const FaceToThreeVerts* faces, const float3* samples, const uint64_t* validSamples, const SkyPatch* skyPatches, const IntersectionPrecomputes* precs, const float rMaxRadiation, float* res, const size_t sampleCount, const size_t precCount, uint64_t* outSkyRays )
+cudaError_t computeSkyViewFactorKernel( const Node3* nodes, const float3* meshPoints, const FaceToThreeVerts* faces, const float3* samples, const uint64_t* validSamples, const SkyPatch* skyPatches, const IntersectionPrecomputes* precs, const float rMaxRadiation, float* res, const size_t sampleCount, const size_t precCount, uint64_t* outSkyRays )
 {
     constexpr int maxThreadsPerBlock = 640;
     int numBlocks = ( int( sampleCount ) + maxThreadsPerBlock - 1 ) / maxThreadsPerBlock;
     radiationKernel << < numBlocks, maxThreadsPerBlock >> > ( nodes, meshPoints, faces, samples, validSamples, skyPatches, precs, rMaxRadiation, res, sampleCount, precCount, outSkyRays );
+    CUDA_EXEC_RETURN( cudaGetLastError() );
+
+    return cudaSuccess;
 }
 
 }

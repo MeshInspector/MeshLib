2d6c10fce5afc54327a613887dd395bf009f629f
Fedr <fchelnokov@adalisk.com>
2023-11-14 21:51:05 +0300

    struct DetectTunnelSettings with default limit on 1 maximum iterations (#1801)

--- source/MRMesh/MRTunnelDetector.h
+++ source/MRMesh/MRTunnelDetector.h
@@ -3,6 +3,7 @@
 #include "MREdgeMetric.h"
 #include "MRProgressCallback.h"
 #include "MRExpected.h"
+#include <cfloat>
 
 namespace MR
 {
@@ -16,9 +17,22 @@ namespace MR
 /// if no metric is given then discreteMinusAbsMeanCurvatureMetric will be used
 MRMESH_API Expected<std::vector<EdgeLoop>, std::string> detectBasisTunnels( const MeshPart& mp, EdgeMetric metric = {}, ProgressCallback progressCallback = {} );
 
+struct DetectTunnelSettings
+{
+    /// maximal length of tunnel loops to consider
+    float maxTunnelLength = FLT_MAX;
+    /// maximal number of iterations to detect all tunnels;
+    /// on a big mesh with many tunnels even one iteration can take a while
+    int maxIters = 1;
+    /// if no metric is given then discreteMinusAbsMeanCurvatureMetric will be used
+    EdgeMetric metric;
+    /// to report algorithm progress and cancel from outside
+    ProgressCallback progress;
+};
+
 /// returns tunnels as a number of faces;
 /// if you remove these faces and patch every boundary with disk, then the surface will be topology equivalent to sphere
-MRMESH_API Expected<FaceBitSet, std::string> detectTunnelFaces( const MeshPart& mp, float maxTunnelLength, EdgeMetric metric = {}, ProgressCallback progressCallback = {} );
+MRMESH_API Expected<FaceBitSet, std::string> detectTunnelFaces( const MeshPart& mp, const DetectTunnelSettings & settings = {} );
 
 /// \}
 

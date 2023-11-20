cc2d01774a0cf3bdacbe7c26da461a6433736951
Fedr <fchelnokov@adalisk.com>
2023-11-20 12:55:45 +0300

    every line of the viewer has been rewritten by now

--- source/MRViewer/MRViewer.h
+++ source/MRViewer/MRViewer.h
@@ -1,12 +1,4 @@
 #pragma once
-// This file is part of libigl, a simple c++ geometry processing library.
-//
-// Copyright (C) 2014 Daniele Panozzo <daniele.panozzo@gmail.com>
-//
-// This Source Code Form is subject to the terms of the Mozilla Public License
-// v. 2.0. If a copy of the MPL was not distributed with this file, You can
-// obtain one at http://mozilla.org/MPL/2.0/.
-
 
 #include "MRViewport.h"
 #include "MRViewerInstance.h"

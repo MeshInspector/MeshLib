2019944f11976de807d9c8a38e28ddeb3268d6ba
Grant Karapetyan <GKarapetyan@adalisk.com>
2023-11-21 16:36:58 +0300

    Fix excessive scene texture bindings (#1829)
    * Fix excessive scene texture bindings
    
    * Fix typos
    
    * fix 2

--- source/MRViewer/MRSceneTextureGL.cpp
+++ source/MRViewer/MRSceneTextureGL.cpp
@@ -10,11 +10,13 @@ namespace MR
 void SceneTextureGL::bind( bool clear )
 {
     fd_.bind( clear );
+    isBound_ = true;
 }
 
 void SceneTextureGL::unbind()
 {
     fd_.bindDefault();
+    isBound_ = false;
 }
 
 void SceneTextureGL::reset( const Vector2i& size, int msaa )
@@ -31,6 +33,7 @@ void SceneTextureGL::reset( const Vector2i& size, int msaa )
 void SceneTextureGL::copyTexture()
 {
     fd_.copyTextureBindDef();
+    isBound_ = false;
 }
 
 void SceneTextureGL::draw()

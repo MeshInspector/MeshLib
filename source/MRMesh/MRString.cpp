004e303127c69bdf6fc13f6f8be8d664e404a350
Grant Karapetyan <GKarapetyan@adalisk.com>
2023-11-27 21:35:13 +0300

    Add Damerau-Levenshtein distance (#1858)
    * Add Damerau-Levenshtein distance
    
    * fixes

--- source/MRMesh/MRString.cpp
+++ source/MRMesh/MRString.cpp
@@ -17,6 +17,40 @@ size_t findSubstringCaseInsensitive( const std::string& string, const std::strin
     return std::distance( string.begin(), iter );
 }
 
+int calcDamerauLevenshteinDistance( const std::string& stringA, const std::string& stringB )
+{
+    std::vector<int> map( stringA.size() * stringB.size() );
+    auto at = [&map,width = int( stringA.size() )] (int i, int j)->int&
+    {
+        return map[i + j * width];
+    };
+
+    for ( int i = 0; i < stringA.size(); ++i )
+    {
+        for ( int j = 0; j < stringB.size(); ++j )
+        {
+            if ( i == 0 || j == 0 )
+                at( i, j ) = std::max( i, j );
+            else if ( i > 1 && j > 1 && stringA[i] == stringB[j - 1] && stringA[i - 1] == stringB[j] )
+            {
+                at( i, j ) = std::min( { 
+                    at( i - 1,j ) + 1,
+                    at( i ,j - 1 ) + 1, 
+                    at( i - 1,j - 1 ) + ( stringA[i] == stringB[j] ? 0 : 1 ),
+                    at( i - 2,j - 2 ) + 1 } );
+            }
+            else
+            {
+                at( i, j ) = std::min( { 
+                    at( i - 1,j ) + 1,
+                    at( i ,j - 1 ) + 1,
+                    at( i - 1,j - 1 ) + ( stringA[i] == stringB[j] ? 0 : 1 ) } );
+            }
+        }
+    }
+    return at( int( stringA.size() ) - 1, int( stringB.size() ) - 1 );
+}
+
 std::vector<std::string> split( const std::string& string, const std::string& delimiter )
 {
     std::vector<std::string> res;

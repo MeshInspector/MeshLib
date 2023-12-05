b5b40b2eaf73280976c591b06dfd837914b07835
ASTROCOMP\astro <akoksin@adalisk.com>
2023-12-05 16:07:48 +0400

    code review

--- source/MRViewer/MRSelectCurvaturePreference.cpp
+++ source/MRViewer/MRSelectCurvaturePreference.cpp
@@ -4,16 +4,16 @@
 namespace MR
 {
 
-float SelectCurvaturePreference( CurvaturePreferenceMode* cp, float menuScaling )
+float SelectCurvaturePreference( PathPreference* pp, float menuScaling )
 {
     static constexpr float factors[3] = { 0.0f, -2.0f, 2.0f };
 
-    if ( !cp )
+    if ( !pp )
         return 0.0f;
     
-    UI::combo( "Curvature Preference", ( int* )cp, { "Geodesic", "Convex", "Concave" }, true, { "Select the shortest boundary", "Select longer boundary but going in more and more convex regions", "Select longer path but going in more and more concave regions" } );
+    UI::combo( "Curvature Preference", ( int* )pp, { "Geodesic", "Convex", "Concave" }, true, { "Select the shortest boundary", "Select longer boundary but going in convex regions", "Select longer path but going in concave regions" } );
     UI::setTooltipIfHovered( "Select to prefer in selection convex/concave angles or neither", menuScaling );
-    return factors[int( *cp )];
+    return factors[int( *pp )];
 }
 
 }

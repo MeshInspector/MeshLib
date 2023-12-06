bcea4551213049c29b658c186df97af2f039b6e1
Alexey Koksin <akoksin@adalisk.com>
2023-12-06 15:15:02 +0400

    make filtering components as an option (#1892)
    * make filtering components as an option
    
    * code review

--- source/MRMesh/MRFilterCreaseEdges.h
+++ source/MRMesh/MRFilterCreaseEdges.h
@@ -5,8 +5,8 @@ namespace MR
 {
 
  /// filters given edges using the following criteria:
- /// connected components with summary length of their edges less than \param critLength will be excluded
+ /// if \param filterComponents is true then connected components with summary length of their edges less than \param critLength will be excluded
  /// if \param filterBranches is true then branches shorter than \param critLength will be excluded
-MRMESH_API void filterCreaseEdges( const Mesh& mesh, UndirectedEdgeBitSet& creaseEdges, float critLength, bool filterBranches = false );
+MRMESH_API void filterCreaseEdges( const Mesh& mesh, UndirectedEdgeBitSet& creaseEdges, float critLength, bool filterComponents = true, bool filterBranches = false );
 
 } // namespace MR

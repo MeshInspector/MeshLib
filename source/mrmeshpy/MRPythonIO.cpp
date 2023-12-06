5f9d1de2410a9981f14917b1182b252e75687991
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2023-12-06 10:54:13 +0400

    Expose scene object loading and saving to Python

--- source/mrmeshpy/MRPythonIO.cpp
+++ source/mrmeshpy/MRPythonIO.cpp
@@ -19,6 +19,8 @@
 #include "MRMesh/MRSerializer.h"
 #include "MRMesh/MRLog.h"
 #include "MRMesh/MRExpected.h"
+#include "MRMesh/MRSceneLoad.h"
+#include "MRMesh/MRObjectSave.h"
 
 using namespace MR;
 
@@ -192,6 +194,23 @@ Expected<PointCloud, std::string> pythonLoadPointCloudFromAnyFormat( pybind11::o
     return MR::PointsLoad::fromAnySupportedFormat( ifs, extension );
 }
 
+Expected<std::shared_ptr<Object>> pythonLoadSceneObjectFromAnyFormat( const std::filesystem::path& path, ProgressCallback callback )
+{
+    auto result = SceneLoad::fromAnySupportedFormat( { path }, std::move( callback ) );
+    if ( !result.scene )
+        return unexpected( std::move( result.errorSummary ) );
+
+    if ( !result.isSceneConstructed || result.scene->children().size() != 1 )
+        return result.scene;
+    else
+        return result.scene->children().front();
+}
+
+VoidOrErrStr pythonSaveSceneObjectToAnySupportedFormat( const std::shared_ptr<Object>& object, const std::filesystem::path& path, ProgressCallback callback )
+{
+    return ObjectSave::toAnySupportedFormat( *object, path, std::move( callback ) );
+}
+
 MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, SaveMesh, [] ( pybind11::module_& m )
 {
     m.def( "saveMesh",
@@ -313,4 +332,20 @@ MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, LoadVoxels, [] ( pybind11::module_& m )
     },
         pybind11::arg( "path" ), pybind11::arg( "maxNumThreads" ) = 4, pybind11::arg( "callback" ) = ProgressCallback{},
         "Loads all volumetric data from DICOM file(s)" );
+} )
+
+MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, LoadSceneObject, [] ( pybind11::module_& m )
+{
+    m.def( "loadSceneObject",
+           MR::decorateExpected( pythonLoadSceneObjectFromAnyFormat ),
+           pybind11::arg( "path" ), pybind11::arg( "callback" ) = ProgressCallback(),
+           "detects the format from file extension and loads scene object from it" );
+} )
+
+MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, SaveSceneObject, [] ( pybind11::module_& m )
+{
+    m.def( "saveSceneObject",
+           MR::decorateExpected( pythonSaveSceneObjectToAnySupportedFormat ),
+           pybind11::arg( "object" ), pybind11::arg( "path" ), pybind11::arg( "callback" ) = ProgressCallback(),
+           "detects the format from file extension and saves scene object to it" );
 } )

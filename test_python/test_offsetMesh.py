12a80557e560238b1bd6f5023cb5ca86801e5aea
grantim <gkarapetyan@adalisk.com>
2023-12-21 19:03:25 +0300

    Expose thickenMesh to python

--- test_python/test_offsetMesh.py
+++ test_python/test_offsetMesh.py
@@ -11,3 +11,22 @@ def test_offset_mesh():
         assert False
 
     assert cube_offset.volume() / cube.volume() > 1.0
+
+def test_thicken_mesh():
+    cube = mrmesh.makeCube()
+    removeFaces = mrmesh.FaceBitSet()
+    removeFaces.resize(2,False)
+    removeFaces.set(mrmesh.FaceId(0),True)
+    removeFaces.set(mrmesh.FaceId(1),True)
+    cube.topology.deleteFaces(removeFaces)
+    cube.invalidateCaches()
+
+    params = mrmesh.OffsetParameters()
+    params.signDetectionMode = mrmesh.SignDetectionMode.Unsigned
+    params.voxelSize = mrmesh.suggestVoxelSize( cube, 5e6 )
+    try:
+        cube_thicken = mrmesh.thickenMesh(cube, 0.1, params)
+    except ValueError as e:
+        print(e)
+        assert False
+    assert cube_thicken.topology.findHoleRepresentiveEdges().size() == 2

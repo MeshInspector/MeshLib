5b2e809c0a1bc46c51cb690a99a2452c5e86205a
Fedr <fchelnokov@adalisk.com>
2023-12-25 00:17:15 +0300

    PointToPointAligningTransform: perform most computations without Eigen library (#1959)
    * PointToPointAligningTransform: perform most computations without Eigen library
    
    * void without nodiscard
    
    * our own eigenvectors of symmetrical matrix 3x3
    
    * fix order in .vcxproj

--- source/MRMesh/MRToFromEigen.h
+++ source/MRMesh/MRToFromEigen.h
@@ -8,6 +8,7 @@
 #include "MRSymMatrix3.h"
 #include "MRMatrix2.h"
 #include "MRMatrix3.h"
+#include "MRMatrix4.h"
 #include <MRPch/MREigenCore.h>
 
 namespace MR
@@ -94,6 +95,28 @@ template <typename T>
         { m( 0, 0 ), m( 0, 1 ), m( 0, 2 ) },
         { m( 1, 0 ), m( 1, 1 ), m( 1, 2 ) },
         { m( 2, 0 ), m( 2, 1 ), m( 2, 2 ) }
+    };
+}
+
+template <typename T>
+[[nodiscard]] inline Eigen::Matrix<T, 4, 4> toEigen( const Matrix4<T> & m )
+{
+    Eigen::Matrix<T, 4, 4> res;
+    res << m.x.x, m.x.y, m.x.z, m.x.w,
+           m.y.x, m.y.y, m.y.z, m.y.w,
+           m.z.x, m.z.y, m.z.z, m.x.w,
+           m.w.x, m.w.y, m.w.z, m.w.w;
+    return res;
+}
+
+template <typename T>
+[[nodiscard]] inline Matrix4<T> fromEigen( const Eigen::Matrix<T, 4, 4> & m )
+{
+    return Matrix4<T> {
+        { m( 0, 0 ), m( 0, 1 ), m( 0, 2 ), m( 0, 3 ) },
+        { m( 1, 0 ), m( 1, 1 ), m( 1, 2 ), m( 1, 3 ) },
+        { m( 2, 0 ), m( 2, 1 ), m( 2, 2 ), m( 2, 3 ) },
+        { m( 3, 0 ), m( 3, 1 ), m( 3, 2 ), m( 3, 3 ) }
     };
 }
 

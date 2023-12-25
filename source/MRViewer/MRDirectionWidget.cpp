53a40b8c528c1615255a6b9f4f3646b720815ad8
ASTROCOMP\astro <akoksin@adalisk.com>
2023-12-25 17:18:39 +0400

    support mouse

--- source/MRViewer/MRDirectionWidget.cpp
+++ source/MRViewer/MRDirectionWidget.cpp
@@ -5,6 +5,7 @@
 #include "MRMesh/MRSceneRoot.h"
 #include "MRViewer/MRAppendHistory.h"
 #include "MRMesh/MRChangeObjectAction.h"
+#include "MRViewer/MRViewport.h"
 
 namespace MR
 {
@@ -12,7 +13,8 @@ namespace MR
     : base_( base )
     , length_( length )
     {
-        updateDirection( dir );
+        connect( &getViewerInstance(), 10, boost::signals2::at_front );
+        updateDirection( dir, false );
     }
 
     DirectionWidget::~DirectionWidget()
@@ -22,15 +24,15 @@ namespace MR
             return bool( std::dynamic_pointer_cast<ChangeDirAction>( action ) );
         }, true );
         clear_();
+        disconnect();
     }
 
-    void DirectionWidget::updateDirection( const Vector3f& dir )
+    void DirectionWidget::updateDirection( const Vector3f& dir, bool needHistUpdate )
     {
-        dir_ = dir;
-        bool needHistUpdate = bool( directionObj_ );
+        dir_ = dir.normalized();
         if ( !directionObj_ )
         {
-            std::shared_ptr<Mesh> mesh = std::make_shared<Mesh>( makeArrow( base_, base_ + dir_ * length_, length_ * 0.02f, length_ * 0.04f, length_ * 0.08f ) );
+            std::shared_ptr<Mesh> mesh = std::make_shared<Mesh>( makeArrow( {}, dir_ * length_, length_ * 0.02f, length_ * 0.04f, length_ * 0.08f ) );
             directionObj_ = std::make_shared<ObjectMesh>();
             directionObj_->setMesh( mesh );
             directionObj_->setAncillary( true );
@@ -41,7 +43,7 @@ namespace MR
         if ( needHistUpdate )
             AppendHistory<ChangeDirAction>( *this, directionObj_ );
 
-        auto transform = AffineXf3f::translation( base_ ) * AffineXf3f::linear( Matrix3f::rotation( Vector3f::plusZ(), dir ) );
+        auto transform = AffineXf3f::translation(base_) * AffineXf3f::linear(Matrix3f::rotation(Vector3f::plusZ(), dir));
         directionObj_->setXf( transform );
     }
 
@@ -68,6 +70,52 @@ namespace MR
             directionObj_->detachFromParent();
             directionObj_.reset();
         }
+    }
+
+    bool DirectionWidget::onMouseDown_( Viewer::MouseButton button, int mod )
+    {
+        if ( button != Viewer::MouseButton::Left || mod != 0 )
+            return false;
+
+        auto viewer = Viewer::instance();
+        viewer->select_hovered_viewport();
+        const auto [obj, pof] = viewer->viewport().pick_render_object();
+        if ( obj != directionObj_ )
+            return false;
+
+        mousePressed_ = true;
+        worldStartPoint_ = directionObj_->worldXf()( pof.point );
+        viewportStartPointZ_ = viewer->viewport().projectToViewportSpace( worldStartPoint_ ).z;
+
+        AppendHistory<ChangeDirAction>( *this, directionObj_ );
+        return true;
+    }
+
+    bool DirectionWidget::onMouseMove_( int x, int y )
+    {
+        if ( !mousePressed_ )
+            return false;
+        
+        auto viewer = Viewer::instance();
+        const auto viewportEnd = viewer->screenToViewport( Vector3f( float( x ), float( y ), 0.f ), viewer->viewport().id );
+        const auto worldEndPoint = viewer->viewport().unprojectFromViewportSpace( { viewportEnd.x, viewportEnd.y, viewportStartPointZ_ } );
+        auto shift = worldEndPoint - worldStartPoint_;
+        shift = directionObj_->worldXf().A.inverse() * shift;
+
+        updateDirection( worldEndPoint - base_, false );
+        return true;
+    }
+
+    bool DirectionWidget::onMouseUp_( Viewer::MouseButton button, int )
+    {
+        if ( button != Viewer::MouseButton::Left )
+            return false;
+
+        if ( !mousePressed_ )
+            return false;
+
+        mousePressed_ = false;
+        return true;
     }
 
 }
--- source/MRViewer/MRDirectionWidget.h
+++ source/MRViewer/MRDirectionWidget.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "exports.h"
+#include "MRViewerEventsListener.h"
+#include "MRViewer/MRViewer.h"
 #include "MRMesh/MRMeshFwd.h"
 #include "MRMesh/MRVector3.h"
 #include "MRMesh/MRChangeXfAction.h"
@@ -8,12 +10,15 @@
 namespace MR
 {
 
-class MRVIEWER_CLASS DirectionWidget
+class MRVIEWER_CLASS DirectionWidget : public MultiListener<MouseDownListener, MouseMoveListener, MouseUpListener>
 {
     std::shared_ptr<ObjectMesh> directionObj_;
-    Vector3f dir_;
+    Vector3f& dir_;
     Vector3f base_;
     float length_;
+    bool mousePressed_ = false;
+    Vector3f worldStartPoint_;
+    float viewportStartPointZ_{ 0.0f };
 
     class ChangeDirAction : public ChangeXfAction
     {
@@ -40,9 +45,14 @@ public:
 
     MRVIEWER_API DirectionWidget( const Vector3f& dir, const Vector3f& base, float length );
     MRVIEWER_API ~DirectionWidget();
-    MRVIEWER_API void updateDirection( const Vector3f& dir );
+    MRVIEWER_API void updateDirection( const Vector3f& dir, bool needHistUpdate );
     MRVIEWER_API void updateArrow( const Vector3f& base, float length );
     MRVIEWER_API void setVisible( bool visible );
+
+private:
+    MRVIEWER_API virtual bool onMouseDown_( Viewer::MouseButton button, int modifier ) override;
+    MRVIEWER_API virtual bool onMouseUp_( Viewer::MouseButton button, int modifier ) override;
+    MRVIEWER_API virtual bool onMouseMove_( int mouse_x, int mouse_y ) override;
 };
 
 }

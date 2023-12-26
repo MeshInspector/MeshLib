e6570fc770e46698e4eac96c67895e79ba48a63d
AntonChernobrovkin <anton.chernobrovkin@smiledirectclub.com>
2023-12-26 13:09:28 +0400

    Autotest: Add comment to __init.py__ files

---
+++ test_regression/helpers/__init__.py
@@ -0,0 +1,5 @@
+"""
+This empty file makes package from folder to Python.
+It is required for PyTest. PyTest can export functions only from packages, so if we want to import helpers and
+constants - we need this file
+"""

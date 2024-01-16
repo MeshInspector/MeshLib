35aab9aa82d02d7db855895c1051470554e5b097
Fedr <fchelnokov@adalisk.com>
2024-01-16 13:20:08 +0300

    ImmediateGL: add modelMatrix in TriRenderParams

--- source/MRViewer/MRImmediateGL.h
+++ source/MRViewer/MRImmediateGL.h
@@ -2,6 +2,7 @@
 #include "exports.h"
 #include "MRViewportGL.h"
 #include "MRMesh/MRVector3.h"
+#include "MRMesh/MRMatrix4.h"
 
 namespace MR
 {
@@ -21,6 +22,7 @@ struct TriRenderParams : ViewportGL::BaseRenderParams
 {
     bool depthTest{ true };
     Vector3f lightPos;
+    Matrix4f modelMatrix; // transformation of each triangle in world space
 };
 
 // Draw lines immediately
@@ -29,18 +31,13 @@ MRVIEWER_API void drawLines( const std::vector<LineSegm3f>& lines, const std::ve
 // Draw points immediately
 MRVIEWER_API void drawPoints( const std::vector<Vector3f>& points, const std::vector<Vector4f>& colors, const ImmediateGL::RenderParams& params );
 
-struct Tri
-{
-    Vector3f a, b, c;
-};
-
 struct TriCornerColors
 {
     Vector4f a, b, c;
 };
 
 // Draw tris immediately (flat shaded)
-MRVIEWER_API void drawTris( const std::vector<Tri>& tris, const std::vector<TriCornerColors>& colors, const ImmediateGL::TriRenderParams& params );
+MRVIEWER_API void drawTris( const std::vector<Triangle3f>& tris, const std::vector<TriCornerColors>& colors, const ImmediateGL::TriRenderParams& params );
 
 }
 

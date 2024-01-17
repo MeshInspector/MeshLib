699bed746c239a4b3e95597923ae02995335949d
grantim <gkarapetyan@adalisk.com>
2024-01-17 13:12:09 +0300

    fix tes

--- test_python/test_convexHull.py
+++ test_python/test_convexHull.py
@@ -6,10 +6,7 @@ def test_convexHull():
     torus = mrmesh.makeTorus(2, 1, 10, 10, None)
 
     # Create the convex hull
-    hull = mrmesh.convexHull(torus)
-
-    # Check that all vertives of the torus are inside the hull
-    outside = mrmesh.boolean(torus, hull, mrmesh.BooleanOperation.OutsideB).mesh
+    hull = mrmesh.makeConvexHull(torus)
     
-    # There should be no vertices outside the hull
-    assert outside.points.size() == 0
+    # test hull volume
+    assert abs(hull.volume() - 43.184) < 0.01

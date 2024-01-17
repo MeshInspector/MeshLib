116ad1e587a84352d8436e8f2bb0a11bd6510a3c
Fedr <fchelnokov@adalisk.com>
2024-01-17 14:30:42 +0300

    Immediate drawing of points and lines became methods of Viewport (#2060)

--- source/MRViewer/MRImmediateGL.h
+++ source/MRViewer/MRImmediateGL.h
@@ -11,13 +11,6 @@ namespace MR
 namespace ImmediateGL
 {
 
-// Immediate render params for lines and points
-struct RenderParams : BaseRenderParams
-{
-    float width{1.0f};
-    bool depthTest{ true };
-};
-
 // Immediate render params for quads
 struct TriRenderParams : BaseRenderParams
 {
@@ -25,12 +18,6 @@ struct TriRenderParams : BaseRenderParams
     Vector3f lightPos;
     Matrix4f modelMatrix; // transformation of each triangle in world space
 };
-
-// Draw lines immediately
-MRVIEWER_API void drawLines( const std::vector<LineSegm3f>& lines, const std::vector<SegmEndColors>& colors, const ImmediateGL::RenderParams& params );
-
-// Draw points immediately
-MRVIEWER_API void drawPoints( const std::vector<Vector3f>& points, const std::vector<Vector4f>& colors, const ImmediateGL::RenderParams& params );
 
 struct TriCornerColors
 {

5d9b58b79656f9e34468df3825986fb92ea5e9b1
Fedr <fchelnokov@adalisk.com>
2024-01-17 13:38:32 +0300

    Immediate drawing of points and lines became methods of Viewport

---
+++ source/MRViewer/MRViewportDraw.cpp
@@ -0,0 +1,167 @@
+#include "MRViewport.h"
+#include "MRViewer.h"
+#include "MRGLMacro.h"
+#include "MRGLStaticHolder.h"
+#include "MRRenderGLHelpers.h"
+#include <MRMesh/MRVisualObject.h>
+#include <MRPch/MRSpdlog.h>
+
+namespace MR
+{
+
+void Viewport::draw( const VisualObject& obj, DepthFuncion depthFunc, bool alphaSort ) const
+{
+    draw( obj, obj.worldXf( id ), projM_, depthFunc, alphaSort );
+}
+
+void Viewport::draw(const VisualObject& obj, const AffineXf3f& xf,
+     DepthFuncion depthFunc, bool alphaSort ) const
+{
+    draw( obj, xf, projM_, depthFunc, alphaSort );
+}
+
+void Viewport::draw( const VisualObject& obj, const AffineXf3f& xf, const Matrix4f& projM,
+     DepthFuncion depthFunc, bool alphaSort ) const
+{
+    Matrix4f normM;
+    obj.render( getModelRenderParams( xf, projM, &normM, depthFunc, alphaSort ) );
+}
+
+ModelRenderParams Viewport::getModelRenderParams( const Matrix4f & modelM, const Matrix4f & projM,
+    Matrix4f * normM, DepthFuncion depthFunc, bool alphaSort ) const
+{
+    if ( normM )
+    {
+        auto normTemp = viewM_ * modelM;
+        if ( normTemp.det() == 0 )
+        {
+            auto norm = normTemp.norm();
+            if ( std::isnormal( norm ) )
+            {
+                normTemp /= norm;
+                normTemp.w = { 0, 0, 0, 1 };
+            }
+            else
+            {
+                spdlog::warn( "Object transform is degenerate" );
+            }
+        }
+        *normM = normTemp.inverse().transposed();
+    }
+
+    return ModelRenderParams
+    {
+        getBaseRenderParams( projM ),
+        modelM, normM,
+        params_.clippingPlane, depthFunc,
+        params_.lightPosition, alphaSort
+    };
+}
+
+void Viewport::drawLines( const std::vector<LineSegm3f>& lines, const std::vector<SegmEndColors>& colors, const LinePointImmediateRenderParams & params )
+{
+    if ( !Viewer::constInstance()->isGLInitialized() )
+        return;
+    // set GL_DEPTH_TEST specified for points 
+    GLuint lineVAO;
+    GL_EXEC( glGenVertexArrays( 1, &lineVAO ) );
+    GlBuffer lineBuffer, lineColorBuffer;
+
+    // set GL_DEPTH_TEST specified for lines
+    if ( params.depthTest )
+    {
+        GL_EXEC( glEnable( GL_DEPTH_TEST ) );
+    }
+    else
+    {
+        GL_EXEC( glDisable( GL_DEPTH_TEST ) );
+    }
+
+    GL_EXEC( glViewport( ( GLsizei )params.viewport.x, ( GLsizei )params.viewport.y,
+        ( GLsizei )params.viewport.z, ( GLsizei )params.viewport.w ) );
+    // Send lines data to GL, install lines properties 
+    GL_EXEC( glBindVertexArray( lineVAO ) );
+
+    auto shader = GLStaticHolder::getShaderId( GLStaticHolder::AdditionalLines );
+    GL_EXEC( glUseProgram( shader ) );
+
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "view" ), 1, GL_TRUE, params.viewMatrix.data() ) );
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "proj" ), 1, GL_TRUE, params.projMatrix.data() ) );
+
+    GL_EXEC( glUniform1f( glGetUniformLocation( shader, "offset" ), 0.0f ) );
+
+    GL_EXEC( GLint colorsId = glGetAttribLocation( shader, "color" ) );
+    lineColorBuffer.loadData( GL_ARRAY_BUFFER, colors );
+    GL_EXEC( glVertexAttribPointer( colorsId, 4, GL_FLOAT, GL_FALSE, 0, 0 ) );
+    GL_EXEC( glEnableVertexAttribArray( colorsId ) );
+
+    GL_EXEC( GLint positionId = glGetAttribLocation( shader, "position" ) );
+    lineBuffer.loadData( GL_ARRAY_BUFFER, lines );
+    GL_EXEC( glVertexAttribPointer( positionId, 3, GL_FLOAT, GL_FALSE, 0, 0 ) );
+    GL_EXEC( glEnableVertexAttribArray( positionId ) );
+
+    getViewerInstance().incrementThisFrameGLPrimitivesCount( Viewer::GLPrimitivesType::LineArraySize, lines.size() );
+
+    GL_EXEC( glBindVertexArray( lineVAO ) );
+    GL_EXEC( glLineWidth( params.width ) );
+    GL_EXEC( glDrawArrays( GL_LINES, 0, 2 * int( lines.size() ) ) );
+
+    GL_EXEC( glDeleteVertexArrays( 1, &lineVAO ) );
+}
+
+void Viewport::drawPoints( const std::vector<Vector3f>& points, const std::vector<Vector4f>& colors, const LinePointImmediateRenderParams & params )
+{
+    if ( !Viewer::constInstance()->isGLInitialized() )
+        return;
+    // set GL_DEPTH_TEST specified for points 
+    GLuint pointVAO;
+    // points 
+    GL_EXEC( glGenVertexArrays( 1, &pointVAO ) );
+    GlBuffer pointBuffer, pointColorBuffer;
+
+    if ( params.depthTest )
+    {
+        GL_EXEC( glEnable( GL_DEPTH_TEST ) );
+    }
+    else
+    {
+        GL_EXEC( glDisable( GL_DEPTH_TEST ) );
+    }
+
+    GL_EXEC( glViewport( ( GLsizei )params.viewport.x, ( GLsizei )params.viewport.y,
+        ( GLsizei )params.viewport.z, ( GLsizei )params.viewport.w ) );
+    // Send points data to GL, install points properties 
+    GL_EXEC( glBindVertexArray( pointVAO ) );
+
+    auto shader = GLStaticHolder::getShaderId( GLStaticHolder::AdditionalPoints );
+    GL_EXEC( glUseProgram( shader ) );
+
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "view" ), 1, GL_TRUE, params.viewMatrix.data() ) );
+    GL_EXEC( glUniformMatrix4fv( glGetUniformLocation( shader, "proj" ), 1, GL_TRUE, params.projMatrix.data() ) );
+
+    GL_EXEC( glUniform1f( glGetUniformLocation( shader, "offset" ), 0.0f ) );
+
+    GL_EXEC( GLint colorsId = glGetAttribLocation( shader, "color" ) );
+    pointColorBuffer.loadData( GL_ARRAY_BUFFER, colors );
+    GL_EXEC( glVertexAttribPointer( colorsId, 4, GL_FLOAT, GL_FALSE, 0, 0 ) );
+    GL_EXEC( glEnableVertexAttribArray( colorsId ) );
+
+    GL_EXEC( GLint positionId = glGetAttribLocation( shader, "position" ) );
+    pointBuffer.loadData( GL_ARRAY_BUFFER, points );
+    GL_EXEC( glVertexAttribPointer( positionId, 3, GL_FLOAT, GL_FALSE, 0, 0 ) );
+    GL_EXEC( glEnableVertexAttribArray( positionId ) );
+
+    getViewerInstance().incrementThisFrameGLPrimitivesCount( Viewer::GLPrimitivesType::PointArraySize, points.size() );
+
+    GL_EXEC( glBindVertexArray( pointVAO ) );
+#ifdef __EMSCRIPTEN__
+    GL_EXEC( glUniform1f( glGetUniformLocation( shader, "pointSize" ), params.width ) );
+#else
+    GL_EXEC( glPointSize( params.width ) );
+#endif
+    GL_EXEC( glDrawArrays( GL_POINTS, 0, int( points.size() ) ) );
+
+    GL_EXEC( glDeleteVertexArrays( 1, &pointVAO ) );
+}
+
+} //namespace MR

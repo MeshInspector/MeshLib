3aacf554aa7542c89e2f3103483569a30ac9bfb2
Gorbovskoy E.S <egorbovskoy@adalisk.com>
2024-01-18 20:20:49 +0300

    fix eigen in ubuntu for  decomposeMatrix3

--- source/MRMesh/MRMatrix3Decompose.h
+++ source/MRMesh/MRMatrix3Decompose.h
@@ -2,10 +2,18 @@
 
 #include "MRToFromEigen.h"
 
+#ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable:5054)  //operator '&': deprecated between enumerations of different types
 #pragma warning(disable:4127)  //C4127. "Consider using 'if constexpr' statement instead"
+#elif defined(__clang__)
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif 
+
 #include <Eigen/Dense>
+
 
 namespace MR
 {
@@ -47,4 +55,9 @@ bool isRigid( const Matrix3<T>& m )
 
 } // namespace MR
 
+#ifdef _MSC_VER
 #pragma warning(pop)
+#elif defined(__clang__)
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif 

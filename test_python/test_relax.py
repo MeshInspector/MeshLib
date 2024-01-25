1cd2652a4c52ea010b8ebbd8a6882fd2a061e840
Emil Peters <emil.peters@relu.eu>
2024-01-25 20:50:07 +0100

    Exposed more relax functions to python

--- test_python/test_relax.py
+++ test_python/test_relax.py
@@ -12,7 +12,7 @@ def test_relax():
     params.iterations = 5
     res = mrmesh.relax(torus, params)
 
-    assert (res)
+    assert res
 
 def test_relax_keep_volume():
     R1 = 2
@@ -24,4 +24,27 @@ def test_relax_keep_volume():
     params.iterations = 5
     res = mrmesh.relaxKeepVolume(keep_volume_torus, params)
 
-    assert (res)
+    assert res
+
+def test_relax_approx():
+    R1 = 2
+    R2_1 = 1
+    R2_2 = 2.5
+    torus = mrmesh.makeTorusWithSpikes(R1, R2_1, R2_2, 10, 12, None)
+
+    params = mrmesh.MeshApproxRelaxParams()
+    params.iterations = 5
+    res = mrmesh.relaxKeepVolume(torus, params)
+
+    assert res
+
+def test_smooth_region_boundary():
+    R1 = 2
+    R2_1 = 1
+    R2_2 = 2.5
+    keep_volume_torus = mrmesh.makeTorusWithSpikes(R1, R2_1, R2_2, 10, 12, None)
+
+    smooth_region = keep_volume_torus.topology.getValidFaces()
+
+    # This just checks that the function exists and can be called.
+    mrmesh.smoothRegionBoundary(keep_volume_torus, smooth_region)

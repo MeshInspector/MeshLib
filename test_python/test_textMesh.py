8ff1f05f1dd2f1d793c667c0c304b781495f1f65
Fedr <fchelnokov@adalisk.com>
2024-01-29 12:26:44 +0300

    fix test_textMesh.py

--- test_python/test_textMesh.py
+++ test_python/test_textMesh.py
@@ -5,7 +5,7 @@ import os
 def test_text_mesh():
     params = mrmesh.SymbolMeshParams()
     params.text = "Hello World"
-    params.pathToFontFile = os.path.dirname(os.path.abspath(__file__) + "/../thirdparty/Noto_Sans/NotoSans-Regular.ttf" )
+    params.pathToFontFile = os.path.dirname(os.path.abspath(__file__)) + "/../thirdparty/Noto_Sans/NotoSans-Regular.ttf"
     textMesh = mrmesh.createSymbolsMesh( params )
     assert textMesh.volume() > 0
 
@@ -17,7 +17,7 @@ def test_aligned_text_mesh():
     params.direction = mrmesh.Vector3f(1,0,0)
     params.text = "Hello World"
     params.fontHeight = 0.02
-    params.pathToFontFile = os.path.dirname(os.path.abspath(__file__) + "/../thirdparty/Noto_Sans/NotoSans-Regular.ttf" )
+    params.pathToFontFile = os.path.dirname(os.path.abspath(__file__)) + "/../thirdparty/Noto_Sans/NotoSans-Regular.ttf"
     # how deep to engrave
     params.surfaceOffset = 0.1
     alignedMesh = mrmesh.alignTextToMesh( sphere, params )

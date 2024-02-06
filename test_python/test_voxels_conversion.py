0179cc091edb9b84e5cd83ed3b2975200da8ccf9
Rodion Orlov <rorlov@adalisk.com>
2024-02-06 20:16:11 +0400

    Fix after review: enhance test script

--- test_python/test_voxels_conversion.py
+++ test_python/test_voxels_conversion.py
@@ -10,15 +10,15 @@ from meshlib import mrmeshnumpy
 
 def test_voxels_conversion():
     # Create array containing one octant with spherical distribution
-    mul = 1
-    np_array = np.zeros((20 * mul, 30 * mul, 40 * mul))
-    max_value = 3000 * mul * mul
-    center = (np_array.shape[0] / 10, np_array.shape[1] / 3, np_array.shape[2] / 3)
+    mul = 1  # Data size multiplier, set to larger value to test on larger arrays
+    np_array = np.zeros((12 * mul, 15 * mul, 20 * mul))
+    max_value = 1000 * mul * mul
+    center = (np_array.shape[0] / 12, np_array.shape[1] / 3, np_array.shape[2] / 3)
     for k in range(np_array.shape[2]):
         for j in range(np_array.shape[1]):
             for i in range(np_array.shape[0]):
                 x, y, z = i - center[0], j - center[1], k - center[2]
-                np_array[i, j, k] = 0.0 if x <= 0 or y <= 0 or z <= 0 else \
+                np_array[i, j, k] = 0.0 if x < 0 or y < 0 or z < 0 else \
                         max_value - (x * x + y * y + z * z)
 
     # Create initial volume object
@@ -28,19 +28,24 @@ def test_voxels_conversion():
 
     # Test conversion functions
     vdb_volume = mrmeshpy.simpleVolumeToVdbVolume(volume0)
-    volume = mrmeshpy.vdbVolumeToSimpleVolume(vdb_volume)
-    grid = mrmeshpy.simpleVolumeToDenseGrid(volume)
+    simple_volume = mrmeshpy.vdbVolumeToSimpleVolume(vdb_volume)
+    grid = mrmeshpy.simpleVolumeToDenseGrid(simple_volume)
     # Basic results validation
-    assert volume0.dims == vdb_volume.dims == volume.dims
-    assert volume0.voxelSize == vdb_volume.voxelSize == volume.voxelSize
-    assert volume0.min == vdb_volume.min == volume.min
-    assert volume0.max == vdb_volume.max == volume.max
+    assert volume0.dims == vdb_volume.dims == simple_volume.dims
+    assert volume0.voxelSize == vdb_volume.voxelSize == simple_volume.voxelSize
+    assert volume0.min == vdb_volume.min == simple_volume.min
+    assert volume0.max == vdb_volume.max == simple_volume.max
 
     # Test mesh build functions
-    radius = 15 * mul
+    radius = 8 * mul
     mesh1 = mrmeshpy.gridToMesh(vdb_volume, isoValue = max_value - radius * radius)
     mesh2 = mrmeshpy.gridToMesh(grid, voxelSize = volume0.voxelSize, isoValue = max_value - radius * radius)
-    # Test by roughly comparing mesh volume to expectation (volume of 1/8 sphere)
+    # Basic checks
+    for mesh in (mesh1, mesh2):
+        assert len(mrmeshpy.getAllComponents(mesh1)) == 1
+        assert mesh.topology.numValidFaces() + mesh.topology.numValidVerts() - \
+                mesh.topology.computeNotLoneUndirectedEdges() == 2  # Euler characteristic
+    # Check by roughly comparing mesh volume to expectation (volume of 1/8 sphere)
     vol = radius ** 3 * math.pi / 6 * (volume0.voxelSize.x * volume0.voxelSize.y * volume0.voxelSize.z)
-    assert 0.7 < mesh1.volume() / vol < 1.3
-    assert 0.7 < mesh2.volume() / vol < 1.3
+    for mesh in (mesh1, mesh2):
+        assert 0.9 < mesh.volume() / vol < 1.1

2c03f73fd5797af8fb167f775bafd5b8d71737a6
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2024-02-12 17:11:35 +0600

    Allow changing default measurement visibility. (#2206)
    * Allow changing default measurement visibility.
    
    * Try to fix build.

--- source/MRMesh/MRDefaultMeasurementObjects.h
+++ source/MRMesh/MRDefaultMeasurementObjects.h
@@ -6,20 +6,29 @@
 namespace MR
 {
 
-enum class AttachDefaultMeasurementsFlags
+enum class DefaultMeasurementKinds
+{
+    Radiuses = 1 << 0,
+    Distances = 1 << 1,
+    Angles = 1 << 2,
+    All = Radiuses | Distances | Angles,
+};
+MR_MAKE_FLAG_OPERATORS( DefaultMeasurementKinds )
+
+struct AttachDefaultMeasurementsParams
 {
     // If the measurements already exist, remove them and attach new ones.
     // Otherwise the existing measurements are left alone, even if they have some incorrect parameters.
-    Overwrite = 1 << 0,
-    Radiuses = 1 << 1,
-    Distances = 1 << 2,
-    Angles = 1 << 3,
-    AllKinds = Radiuses | Distances | Angles,
+    bool overwrite = false;
+
+    // If a measurement kind is not in this list, it's left alone.
+    DefaultMeasurementKinds enabledKinds = DefaultMeasurementKinds::All;
+    // If a measurement kind is not in this list, it's created invisible by default.
+    DefaultMeasurementKinds defaultVisibleKinds = DefaultMeasurementKinds::All;
 };
-MR_MAKE_FLAG_OPERATORS( AttachDefaultMeasurementsFlags )
 
 // Given a feature object, attach all necessary measurement subobjects to it.
 // They then automatically track the object dimensions.
-MRMESH_API void attachDefaultMeasurementsToObject( Object& object, AttachDefaultMeasurementsFlags flags = AttachDefaultMeasurementsFlags::AllKinds );
+MRMESH_API void attachDefaultMeasurementsToObject( Object& object, const AttachDefaultMeasurementsParams& params = {} );
 
 }

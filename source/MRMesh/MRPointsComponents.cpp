f463bff7e351dff87e1754a7ae37944d12d1e6a3
A.Burakov <aburakov@adalisk.com>
2024-02-13 15:12:54 +0300

    updated logic getUnionFindStructureVerts

--- source/MRMesh/MRPointsComponents.cpp
+++ source/MRMesh/MRPointsComponents.cpp
@@ -4,7 +4,7 @@
 #include "MRTimer.h"
 #include "MRPointsInBall.h"
 #include "MRProgressCallback.h"
-//#include <algorithm>
+#include "MRPch/MRTBB.h"
 
 namespace MR
 {
@@ -12,51 +12,41 @@ namespace MR
 namespace PointCloudComponents
 {
 
-Expected<std::vector<MR::VertBitSet>> getLargestComponents( const PointCloud& pointCloud, float maxDist, int minSize /*= -1*/, ProgressCallback pc /*= {}*/ )
+Expected<MR::VertBitSet> getLargestComponentsUnion( const PointCloud& pointCloud, float maxDist, int minSize, ProgressCallback pc /*= {}*/ )
 {
     MR_TIMER
 
     assert( maxDist > 0.f );
+    assert( minSize > 0 );
     const auto& validPoints = pointCloud.validPoints;
-    if ( minSize < 0 )
-        minSize = std::max( int( validPoints.count() ) / 100, 1 );
     ProgressCallback subPc = pc ? subprogress( pc, 0.f, 0.9f ) : pc;
-    auto unionStructsRes = PointCloudComponents::getUnionFindStructureVerts( pointCloud, maxDist, nullptr, subPc );
+    auto unionStructsRes = getUnionFindStructureVerts( pointCloud, maxDist, nullptr, subPc );
     if ( !unionStructsRes.has_value() )
-        return unexpected( unionStructsRes.error() );
+        return unexpectedOperationCanceled();
     auto& unionStructs = *unionStructsRes;
     auto allRoots = unionStructs.roots();
-    HashMap<VertId, int> rootMap;
-    std::vector<VertBitSet> components;
 
-    subPc = pc ? subprogress( pc, 0.9f, 1.f ) : pc;
+    subPc = pc ? subprogress( pc, 0.9f, 0.95f ) : pc;
     int counter = 0;
     const float counterMax = float( validPoints.count() );
     const int counterDivider = int( validPoints.count() ) / 100;
+    HashMap<VertId, int> root2size;
     for ( auto v : validPoints )
     {
-        VertId root = allRoots[v];
-        int componentId = -1;
-        if ( rootMap.contains( root ) )
-            componentId = rootMap[root];
-        else
-        {
-            componentId = int( components.size() );
-            rootMap[root] = componentId;
-            components.push_back( VertBitSet( validPoints.find_last() + 1 ) );
-        }
-
-        components[componentId].set( v );
-        ++counter;
+        ++root2size[allRoots[v]];
         if ( !reportProgress( subPc, counter / counterMax, counter, counterDivider ) )
-            return unexpected( std::string( "Operation canceled" ) );
+            return unexpectedOperationCanceled();
     }
 
-    std::vector<VertBitSet> result;
-    for ( int i = 0; i < components.size(); ++i )
+    subPc = pc ? subprogress( pc, 0.95f, 1.f ) : pc;
+    counter = 0;
+    VertBitSet result( validPoints.find_last() + 1 );
+    for ( auto v : validPoints )
     {
-        if ( components[i].count() > size_t( minSize ) )
-            result.push_back( components[i] );
+        if ( root2size[allRoots[v]] > minSize )
+            result.set( v );
+        if ( !reportProgress( subPc, counter / counterMax, counter, counterDivider ) )
+            return unexpectedOperationCanceled();
     }
 
     return result;
@@ -71,27 +61,94 @@ Expected<UnionFind<MR::VertId>> getUnionFindStructureVerts( const PointCloud& po
         vertsRegion &= *region;
 
     if ( !vertsRegion.any() )
-        return {};
+        return unexpected( std::string( "Chosen region empty" ));
 
-    UnionFind<VertId> unionFindStructure( vertsRegion.find_last() + 1 );
+    const VertBitSet* lastPassVerts = &vertsRegion;
+    const auto numVerts = vertsRegion.find_last() + 1;
+    UnionFind<VertId> unionFindStructure( numVerts );
+    const auto numThreads = int( tbb::global_control::active_value( tbb::global_control::max_allowed_parallelism ) );
 
-    VertId v1;
-    int counter = 0;
-    const float counterMax = float( vertsRegion.count() );
-    const int counterDivider = int( vertsRegion.count() ) / 100;
+    VertBitSet bdVerts;
+    ProgressCallback subPc = pc ? subprogress( pc, 0.f, 1.0f ) : pc;
+    if ( numThreads > 1 )
+    {
+        std::atomic<int> counterProcessedVerts = 0;
+        std::atomic<bool> canceled = false;
+        const float counterMax = float( vertsRegion.count() );
+        const int counterDivider = int( vertsRegion.count() ) / 100;
+        subPc = pc ? subprogress( pc, 0.f, 0.7f ) : pc;
 
-    for ( auto v0 : vertsRegion )
+        bdVerts.resize( numVerts );
+        lastPassVerts = &bdVerts;
+        const int endBlock = int( bdVerts.size() + bdVerts.bits_per_block - 1 ) / bdVerts.bits_per_block;
+        const auto bitsPerThread = ( endBlock + numThreads - 1 ) / numThreads * BitSet::bits_per_block;
+
+        tbb::parallel_for( tbb::blocked_range<int>( 0, numThreads ),
+            [&] ( const tbb::blocked_range<int>& range )
+        {
+            if ( canceled )
+                return;
+            const VertId vBeg{ range.begin() * bitsPerThread };
+            const VertId vEnd{ range.end() < numThreads ? range.end() * bitsPerThread : bdVerts.size() };
+            int counter = 0;
+            for ( auto v0 = vBeg; v0 < vEnd; ++v0 )
+            {
+                if ( !contains( vertsRegion, v0 ) )
+                    continue;
+
+                findPointsInBall( pointCloud.getAABBTree(), pointCloud.points[v0], maxDist,
+                    [&] ( VertId v1, const Vector3f& )
+                {
+                    if ( v0 < v1 && contains( vertsRegion, v1 ) )
+                    {
+                        if ( v1 >= vEnd )
+                            bdVerts.set( v0 ); // remember vert to unite later
+                        else
+                            unionFindStructure.unite( v0, v1 );
+                    }
+                } );
+                ++counter;
+                if ( pc && counter == counterDivider )
+                {
+                    if ( canceled )
+                        return;
+                    counterProcessedVerts += counter;
+                    counter = 0;
+                    if ( !subPc( counterProcessedVerts / counterMax ) )
+                    {
+                        canceled = true;
+                        return;
+                    }
+                }
+            }
+            counterProcessedVerts += counter;
+            if ( !reportProgress( subPc, counterProcessedVerts / counterMax, counter, counterDivider ) )
+                canceled = true;
+        } );
+        if ( canceled )
+            return unexpectedOperationCanceled();
+
+        subPc = pc ? subprogress( pc, 0.7f, 1.f ) : pc;
+    }
+
+    int counterProcessedVerts = 0;
+    const float counterMax = float( lastPassVerts->count() );
+    const int counterDivider = int( lastPassVerts->count() ) / 100;
+    for ( auto v0 : *lastPassVerts )
     {
         findPointsInBall( pointCloud.getAABBTree(), pointCloud.points[v0], maxDist,
             [&] ( VertId v1, const Vector3f& )
         {
-            if ( v1.valid() && vertsRegion.test( v1 ) && v1 < v0 )
+            if ( v0 < v1 && contains( vertsRegion, v1 ) )
+            {
                 unionFindStructure.unite( v0, v1 );
+            }
         } );
-        ++counter;
-        if ( !reportProgress( pc, counter / counterMax, counter, counterDivider ) )
-            return unexpected( std::string( "Operation canceled" ) );
+        ++counterProcessedVerts;
+        if ( !reportProgress( subPc, counterProcessedVerts / counterMax, counterProcessedVerts, counterDivider ) )
+            return unexpectedOperationCanceled();
     }
+
     return unionFindStructure;
 }
 
--- source/MRMesh/MRPointsComponents.h
+++ source/MRMesh/MRPointsComponents.h
@@ -13,9 +13,9 @@ namespace PointCloudComponents
 /// \ingroup ComponentsGroup
 /// \{
 
-/// get point cloud components contain more then minSize points and connected by a distance no greater than \param maxDist
-/// \param minSize if it less then 0, minSize = 1% of valid points
-MRMESH_API Expected<std::vector<VertBitSet>> getLargestComponents( const PointCloud& pointCloud, float maxDist, int minSize = -1, ProgressCallback pc = {} );
+
+///  returns the union of point cloud components containing more then \param minSize points and connected by a distance no greater than \param maxDist
+MRMESH_API Expected<VertBitSet> getLargestComponentsUnion( const PointCloud& pointCloud, float maxDist, int minSize, ProgressCallback pc = {} );
 
 /// gets union - find structure for vertices in \param region connected by a distance no greater than \param maxDist 
 MRMESH_API Expected<UnionFind<VertId>> getUnionFindStructureVerts( const PointCloud& pointCloud, float maxDist, const VertBitSet* region = nullptr, ProgressCallback pc = {} );

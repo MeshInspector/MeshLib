a2d0c2060f1e593c1229105c447f76f1c96315ee
A.Burakov <aburakov@adalisk.com>
2024-02-26 09:35:35 +0300

    added method getLargestComponents for search largest components of point cloud (similur to getLargestComponentsUnion)

--- source/MRMesh/MRPointsComponents.h
+++ source/MRMesh/MRPointsComponents.h
@@ -17,6 +17,10 @@ namespace PointCloudComponents
 /// \param minSize must be more than 1
 MRMESH_API Expected<VertBitSet> getLargestComponentsUnion( const PointCloud& pointCloud, float maxDist, int minSize, ProgressCallback pc = {} );
 
+/// returns vector of point cloud components containing at least minSize points and connected by a distance no greater than \param maxDist
+/// \param minSize must be more than 1
+MRMESH_API Expected<std::vector<VertBitSet>> getLargestComponents( const PointCloud& pointCloud, float maxDist, int minSize, ProgressCallback pc = {} );
+
 /// gets union - find structure for vertices in \param region connected by a distance no greater than \param maxDist 
 MRMESH_API Expected<UnionFind<VertId>> getUnionFindStructureVerts( const PointCloud& pointCloud, float maxDist, const VertBitSet* region = nullptr, ProgressCallback pc = {} );
 

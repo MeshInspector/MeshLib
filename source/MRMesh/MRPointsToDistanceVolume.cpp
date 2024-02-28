1e3fb8fb012d13b3c351195058f51001f46f8c47
Fedr <fchelnokov@adalisk.com>
2024-02-28 13:47:09 +0300

    pointsToDistanceVolume

---
+++ source/MRMesh/MRPointsToDistanceVolume.cpp
@@ -0,0 +1,50 @@
+#include "MRPointsToDistanceVolume.h"
+#include "MRVolumeIndexer.h"
+#include "MRParallelFor.h"
+#include "MRPointCloud.h"
+#include "MRPointsInBall.h"
+#include "MRTimer.h"
+#include <limits>
+
+namespace MR
+{
+
+Expected<SimpleVolume> pointsToDistanceVolume( const PointCloud & cloud, const PointsToDistanceVolumeParams& params )
+{
+    MR_TIMER
+    assert( params.truncationRadius > 0 );
+    assert( params.influenceRadius >= params.truncationRadius );
+    assert( cloud.hasNormals() );
+
+    SimpleVolume res;
+    res.voxelSize = params.voxelSize;
+    res.dims = params.dimensions;
+    VolumeIndexer indexer( res.dims );
+    res.data.resize( indexer.size(), std::numeric_limits<float>::quiet_NaN() );
+
+    if ( !ParallelFor( size_t( 0 ), indexer.size(), [&]( size_t i )
+    {
+        auto coord = Vector3f( indexer.toPos( VoxelId( i ) ) ) + Vector3f::diagonal( 0.5f );
+        auto voxelCenter = params.origin + mult( params.voxelSize, coord );
+
+        float sumDist = 0;
+        int num = 0;
+        findPointsInBall( cloud, voxelCenter, params.influenceRadius, [&]( VertId v, const Vector3f& p )
+        {
+            auto tsdf = std::clamp( dot( cloud.normals[v], voxelCenter - p ),
+                -params.truncationRadius, params.truncationRadius );
+            sumDist += tsdf;
+            ++num;
+        } );
+
+        if ( num > 0 )
+            res.data[i] = sumDist / num;
+    }, params.cb ) )
+        return unexpectedOperationCanceled();
+
+    res.min = -params.truncationRadius;
+    res.max =  params.truncationRadius;
+    return res;
+}
+
+} //namespace MR
---
+++ source/MRMesh/MRPointsToDistanceVolume.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "MRDistanceVolumeParams.h"
+#include "MRSimpleVolume.h"
+#include "MRExpected.h"
+
+namespace MR
+{
+
+struct PointsToDistanceVolumeParams : DistanceVolumeParams
+{
+    /// distance of influence of a point, beyond that radius the influence is zero
+    float influenceRadius = 0;
+
+    /// signed distance is truncated (reaches its maximum or minimum) at this distance
+    float truncationRadius = 0;
+};
+
+/// makes SimpleVolume filled with signed distances to points with normals
+[[nodiscard]] MRMESH_API Expected<SimpleVolume> pointsToDistanceVolume( const PointCloud & cloud, const PointsToDistanceVolumeParams& params );
+
+} //namespace MR

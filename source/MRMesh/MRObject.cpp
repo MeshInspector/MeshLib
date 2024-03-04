245885e7e671a85ed715dc31206d33bc1718898e
Egor Grebenchenko <egrebenchenko@adalisk.com>
2024-03-04 17:28:07 +0300

    fix

--- source/MRMesh/MRObject.cpp
+++ source/MRMesh/MRObject.cpp
@@ -251,16 +251,15 @@ bool Object::addChild( std::shared_ptr<Object> child, bool recognizedChild )
     child->parent_ = this;
     if ( recognizedChild )
     {
-        child->propagateWorldXfChangedSignal_();
-        children_.push_back( std::move( child ) );
+        children_.push_back( child );
     }
     else
     {
         // remove invalid children before adding new one
         std::erase_if( bastards_, [](const auto & b) { return !b.lock(); } );
-        child->propagateWorldXfChangedSignal_();
-        bastards_.push_back( std::move( child ) );
+        bastards_.push_back( child );
     }
+    child->propagateWorldXfChangedSignal_();
     needRedraw_ = true;
     return true;
 }
@@ -300,8 +299,8 @@ bool Object::addChildBefore( std::shared_ptr<Object> newChild, const std::shared
         oldParent->removeChild( newChild );
 
     newChild->parent_ = this;
+    children_.insert( it1, newChild );
     newChild->propagateWorldXfChangedSignal_();
-    children_.insert( it1, std::move( newChild ) );
     needRedraw_ = true;
     return true;
 }

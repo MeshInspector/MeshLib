8b5ad8b2e636752afbe424d18477c561eec7737f
AntonChernobrovkin <anton.chernobrovkin@smiledirectclub.com>
2024-03-05 18:17:46 +0400

    Cosmetic improvements

--- test_regression/test_algorithms/test_make_bridge.py
+++ test_regression/test_algorithms/test_make_bridge.py
@@ -11,23 +11,26 @@ import pytest
 @pytest.mark.parametrize("input_case", ["torus", "crocodile"])
 def test_make_bridge_edge(input_case, tmp_path):
     """
-    Test make bridge edge algorithm
+    Test makeBridgeEdge algorithm
     """
     #  Load input meshes
     input_folder = Path(test_files_path) / "algorithms" / "bridge" / input_case
     case_name = f"bridge_edge_{input_case}"
     mesh = mlib.loadMesh(input_folder / "input.mrmesh")
+
+    # make bridge edge
     topl = mesh.topology
+    edges_num_before = topl.edgeSize()
+    faces_num_before = topl.numValidFaces()
+
     edges = topl.findHoleRepresentiveEdges()
-    edges_num = topl.edgeSize()
-    faces_num = topl.numValidFaces()
     mrmesh.makeBridgeEdge(topl, edges[0], edges[1])
 
     # === Verification
     with check:
-        assert topl.edgeSize() - edges_num == 2, "Edges number should be increased on 2"
+        assert topl.edgeSize() - edges_num_before == 2, "Edges number should be increased on 2"
     with check:
-        assert topl.numValidFaces() == faces_num, "Faces number should be the same"
+        assert topl.numValidFaces() == faces_num_before, "Faces number should be the same"
     mlib.saveMesh(mesh, tmp_path / f"{case_name}.mrmesh")
     ref_mesh_path = input_folder / f"{case_name}.mrmesh"
     ref_mesh = mlib.loadMesh(ref_mesh_path)
@@ -39,23 +42,25 @@ def test_make_bridge_edge(input_case, tmp_path):
 @pytest.mark.parametrize("input_case", ["torus", "crocodile"])
 def test_make_bridge(input_case, tmp_path):
     """
-    Test make bridge edge algorithm
+    Test makeBridge algorithm
     """
     #  Load input meshes
     input_folder = Path(test_files_path) / "algorithms" / "bridge" / input_case
     case_name = f"bridge_{input_case}"
     mesh = mlib.loadMesh(input_folder / "input.mrmesh")
+
+    # make bridge edge
     topl = mesh.topology
     edges = topl.findHoleRepresentiveEdges()
-    edges_num = topl.edgeSize()
-    faces_num = topl.numValidFaces()
+    edges_num_before = topl.edgeSize()
+    faces_num_before = topl.numValidFaces()
     mrmesh.makeBridge(topl, edges[0], edges[1])
 
     # === Verification
     with check:
-        assert mesh.topology.edgeSize() - edges_num == 6, "Edges number should be increased on 6"
+        assert mesh.topology.edgeSize() - edges_num_before == 6, "Edges number should be increased on 6"
     with check:
-        assert topl.numValidFaces() - faces_num == 2, "Edges number should be increased on 2"
+        assert topl.numValidFaces() - faces_num_before == 2, "Faces number should be increased on 2"
 
     mlib.saveMesh(mesh, tmp_path / f"{case_name}.mrmesh")
     ref_mesh_path = input_folder / f"{case_name}.mrmesh"

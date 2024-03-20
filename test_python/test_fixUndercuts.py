54b59f83313e97127e4b34e2355614f9f6223725
Grant Karapetyan <GKarapetyan@adalisk.com>
2024-03-20 14:46:34 +0300

    Expose findUndercuts to python (#2434)

--- test_python/test_fixUndercuts.py
+++ test_python/test_fixUndercuts.py
@@ -9,6 +9,10 @@ def test_fix_undercuts():
     dir.x = 0
     dir.y = 0
     dir.z = 1
+    
+    undercuts = mrmesh.FaceBitSet()
+    mrmesh.findUndercuts(torus, dir, undercuts)
+    assert undercuts.count() > 0
 
     mrmesh.fixUndercuts(torus, dir, 0.2, 0.0)
 

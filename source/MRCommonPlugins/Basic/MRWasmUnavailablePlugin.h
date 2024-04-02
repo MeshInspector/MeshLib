c5f77f733f80be37e94304703b4cbc8967605656
grantim <gkarapetyan@adalisk.com>
2024-04-02 15:08:18 +0300

    Wasm unavailable plugins perform scene checks MIC#3756

--- source/MRCommonPlugins/Basic/MRWasmUnavailablePlugin.h
+++ source/MRCommonPlugins/Basic/MRWasmUnavailablePlugin.h
@@ -13,14 +13,33 @@ public:
         StatePlugin( name ){}
     
     MRCOMMONPLUGINS_API virtual void drawDialog( float menuScaling, ImGuiContext* ) override;
+    virtual bool blocking() const override { return false; }
 private:
     virtual bool onEnable_() override { openPopup_ = true; return true; }
     bool openPopup_{true};
 };
 
+
+template<typename AvailabilityCheckClass>
+class WasmUnavailablePluginWithSceneCheck : public WasmUnavailablePlugin, public AvailabilityCheckClass {
+public:
+    WasmUnavailablePluginWithSceneCheck( const std::string& name ) :
+        WasmUnavailablePlugin( name ) {}
+};
+
+class WasmUnavailableObjectVoxels
+{
+public:
+    constexpr static const char* TypeName() noexcept { return "ObjectVoxels"; }
+};
+
+
 }
 
 #define MR_REGISTER_WASM_UNAVAILABLE_ITEM( pluginType, name )\
     static MR::RibbonMenuItemAdder<MR::WasmUnavailablePlugin> ribbonMenuItemAdder##pluginType##_(name);
+
+#define MR_REGISTER_WASM_UNAVAILABLE_ITEM_CHECK( pluginType, availCheckType, name )\
+    static MR::RibbonMenuItemAdder<MR::WasmUnavailablePluginWithSceneCheck<availCheckType>> ribbonMenuItemAdder##pluginType##_(name);
 
 #endif

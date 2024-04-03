a3240dd0db75e32756dee8973452578132762c6b
grantim <gkarapetyan@adalisk.com>
2024-04-03 19:02:31 +0300

    Introduce AncillaryImGuiLabel

--- source/MRViewer/MRAncillaryLabels.cpp
+++ source/MRViewer/MRAncillaryLabels.cpp
@@ -2,6 +2,11 @@
 #include "MRMesh/MRObjectLabel.h"
 #include "MRMesh/MRSceneColors.h"
 #include "MRMesh/MRSceneRoot.h"
+#include "MRViewer.h"
+#include "MRViewport.h"
+#include "ImGuiMenu.h"
+#include "MRImGuiMeasurementIndicators.h"
+#include "imgui.h"
 
 namespace MR
 {
@@ -49,6 +54,66 @@ void AncillaryLabel::setDepthTest( bool depthTest )
 void AncillaryLabel::setPosition( const Vector3f& pos )
 {
     obj->setLabel( PositionedText{ obj->getLabel().text, pos } );
+}
+
+void AncillaryImGuiLabel::make( Object& parent, const PositionedText& text )
+{
+    reset();
+    make( text );
+    parentXfConnection_ = parent.worldXfChangedSignal.connect( [&] ()
+    {
+        labelData_.position = parent.worldXf()( labelData_.position );
+    } );
+    labelData_.position = parent.worldXf()( labelData_.position );
+}
+
+void AncillaryImGuiLabel::make( const PositionedText& text )
+{
+    reset();
+    labelData_ = text;
+    // these parameters emulate plugins
+    connect( &getViewerInstance(), 10, boost::signals2::at_front );
+}
+
+void AncillaryImGuiLabel::reset()
+{
+    disconnect();
+    if ( parentXfConnection_.connected() )
+        parentXfConnection_.disconnect();
+    labelData_ = {};
+}
+
+void AncillaryImGuiLabel::preDraw_()
+{
+    if ( labelData_.text.empty() )
+        return;
+    auto menu = getViewerInstance().getMenuPlugin();
+    if ( !menu )
+        return;
+    
+    ImGuiMeasurementIndicators::Params params;
+    if ( !params.list )
+        return;
+    params.colorTextOutline.a = 220;
+    auto scaling = menu->menu_scaling();
+    const ImGuiMeasurementIndicators::StringWithIcon sWithI( labelData_.text );
+
+    for ( const auto& vp : getViewerInstance().viewport_list )
+    {
+        auto rect = vp.getViewportRect();
+        ImVec2 minRect( rect.min.x,
+                 getViewerInstance().framebufferSize.y - ( rect.min.y + height( rect ) ) );
+        ImVec2 maxRect( rect.min.x + width( rect ),
+                 getViewerInstance().framebufferSize.y - rect.min.y );
+
+
+        Vector3f coord = vp.projectToViewportSpace( labelData_.position );
+        auto viewerCoord = getViewerInstance().viewportToScreen( coord, vp.id );
+
+        params.list->PushClipRect( minRect, maxRect );
+        ImGuiMeasurementIndicators::text( ImGuiMeasurementIndicators::Element::both, scaling, params, ImVec2( viewerCoord.x, viewerCoord.y ), sWithI );
+        params.list->PopClipRect();
+    }
 }
 
 } //namespace MR

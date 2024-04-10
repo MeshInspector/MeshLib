a7d7e03884061a12fb90c0ff9af4f3fb82714d0a
Egor Grebenchenko <egrebenchenko@adalisk.com>
2024-04-10 15:11:01 +0300

    add progress bar

--- source/MRMesh/MRAddNoise.cpp
+++ source/MRMesh/MRAddNoise.cpp
@@ -1,6 +1,6 @@
 #include "MRAddNoise.h"
 
-#include "MRPch/MRTBB.h"
+#include "MRParallelFor.h"
 #include "MRMesh/MRBitSet.h"
 
 #include <random>
@@ -8,30 +8,27 @@
 namespace MR
 {
 
-void addNoise( VertCoords& points, const VertBitSet& validVerts, float sigma, unsigned int seed )
+void addNoise( VertCoords& points, const VertBitSet& validVerts, float sigma, unsigned int seed, ProgressCallback callback )
 {
     if ( validVerts.count() > 1000 )
     {
         const size_t numBlock = 128;
         const size_t step = validVerts.size() / numBlock;
-        tbb::parallel_for( tbb::blocked_range<size_t>( 0, numBlock ),
-        [&] ( const tbb::blocked_range<size_t>& range )
+        ParallelFor( size_t( 0 ), numBlock,
+        [&] ( size_t block )
         {
-            for ( size_t block = range.begin(); block < range.end(); block++ )
+            std::mt19937 gen_{ seed + ( unsigned int )block };
+            std::normal_distribution d{ 0.0f, sigma };
+            auto end = step * ( block + 1 );
+            if ( end > validVerts.size() )
+                end = validVerts.size();
+            for ( auto i = step * block; i < end; i++ )
             {
-                std::mt19937 gen_{ seed + ( unsigned int )block };
-                std::normal_distribution d{ 0.0f, sigma };
-                auto end = step * ( block + 1 );
-                if ( end > validVerts.size() )
-                    end = validVerts.size();
-                for ( auto i = step * block; i < end; i++ )
-                {
-                    if ( !validVerts.test( VertId( i ) ) )
-                        continue;
-                    points[VertId( i )] += Vector3f( d( gen_ ), d( gen_ ), d( gen_ ) );
-                }
+                if ( !validVerts.test( VertId( i ) ) )
+                    continue;
+                points[VertId( i )] += Vector3f( d( gen_ ), d( gen_ ), d( gen_ ) );
             }
-        } );
+        }, callback );
     }
     else
     {
--- source/MRMesh/MRAddNoise.h
+++ source/MRMesh/MRAddNoise.h
@@ -7,6 +7,11 @@ namespace MR
 {
 // Adds noise to the points, using a normal distribution
 // seed - start state of the generator engine
-MRMESH_API void addNoise( VertCoords& points, const VertBitSet& validVerts, float sigma, unsigned int seed );
+MRMESH_API void addNoise( 
+    VertCoords& points, 
+    const VertBitSet& validVerts, 
+    float sigma, 
+    unsigned int seed, 
+    ProgressCallback callback );
 
 }

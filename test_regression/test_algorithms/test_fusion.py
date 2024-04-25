28034078655d0c0e301973204959689d0cc88654
AntonChernobrovkin <anton.chernobrovkin@smiledirectclub.com>
2024-04-25 15:21:50 +0400

    Add fusion autotest

---
+++ test_regression/test_algorithms/test_fusion.py
@@ -0,0 +1,35 @@
+from module_helper import *
+from pathlib import Path
+from pytest_check import check
+from constants import test_files_path
+from helpers.meshlib_helpers import compare_meshes_similarity, compare_mesh
+import meshlib.mrmeshpy as mlib
+
+import pytest
+
+
+@pytest.mark.smoke
+@pytest.mark.parametrize('voxel', [0.2, 0.4])
+def test_fusion(tmp_path, voxel):
+    """
+    Test triangulation algorithm
+    """
+    #  Load input point
+    case_name = f"voxel_{voxel}"
+    input_folder = Path(test_files_path) / "algorithms" / "fusion"
+    points = mlib.loadPoints(input_folder / "input.ctm")
+
+    # Make mesh
+    params = mlib.PointsToMeshParameters()
+    params.voxelSize = voxel
+    params.sigma = 0.8
+    params.minWeight = 6
+    mesh = mlib.pointsToMeshFusion(pointCloud=points, params=params)
+
+    # === Verification
+    mlib.saveMesh(mesh, tmp_path / f"{case_name}.ctm")  # used to store
+    ref_mesh_path = input_folder / f"{case_name}.ctm"
+    ref_mesh = mlib.loadMesh(ref_mesh_path)
+
+    with check:
+        compare_meshes_similarity(mesh, ref_mesh)

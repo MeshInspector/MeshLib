fc103cb57b438774a2860e0ec3da8cf18a247f66
Fedr <fchelnokov@adalisk.com>
2024-04-26 10:00:57 +0300

    Start replacing DistanceMapToWorld with AffineXf3f (#2654)
    * Start replacing DistanceMapToWorld with AffineXf3f
    
    * MeshToDistanceMapParams::operator AffineXf3f()

--- source/MRMesh/MRDistanceMapParams.h
+++ source/MRMesh/MRDistanceMapParams.h
@@ -55,6 +55,12 @@ struct MeshToDistanceMapParams
     MRMESH_API MeshToDistanceMapParams( const AffineXf3f& xf, const Vector2i& resolution, const Vector2f& size );
     MRMESH_API MeshToDistanceMapParams( const AffineXf3f& xf, const Vector2f& pixelSize, const Vector2i& resolution );
 
+    /// converts in transformation
+    operator AffineXf3f() const
+    {
+        return { Matrix3f::fromColumns( xRange / float( resolution.x ), yRange / float( resolution.y ), direction ), orgPoint };
+    }
+
     Vector3f xRange = Vector3f( 1.f, 0.f, 0.f ); ///< Cartesian range vector between distance map borders in X direction
     Vector3f yRange = Vector3f( 0.f, 1.f, 0.f ); ///< Cartesian range vector between distance map borders in Y direction
     Vector3f direction = Vector3f( 0.f, 0.f, 1.f ); ///< direction of intersection ray
@@ -127,6 +133,15 @@ struct DistanceMapToWorld
 
     /// Init fields by `ContourToDistanceMapParams` struct
     MRMESH_API DistanceMapToWorld( const ContourToDistanceMapParams& params );
+
+    /// Converts from AffineXf3f
+    DistanceMapToWorld( const AffineXf3f & xf )
+        : orgPoint( xf.b )
+        , pixelXVec( xf.A.col( 0 ) )
+        , pixelYVec( xf.A.col( 1 ) )
+        , direction( xf.A.col( 2 ) )
+    {
+    }
     
 
     /// get world coordinate by depth map info
@@ -138,6 +153,11 @@ struct DistanceMapToWorld
         return orgPoint + x * pixelXVec + y * pixelYVec + depth * direction;
     }
 
+    /// converts in transformation X: X(p) == toWorld( p.x, p.y, p.z )
+    operator AffineXf3f() const
+    {
+        return { Matrix3f::fromColumns( pixelXVec, pixelYVec, direction ), orgPoint };
+    }
 
     /// world coordinates of distance map origin corner
     Vector3f orgPoint;

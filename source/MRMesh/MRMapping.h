fd7d994caccdd780e5273ed4962df3a78afcf5d0
Egor Grebenchenko <egrebenchenko@adalisk.com>
2024-04-26 21:05:35 +0300

    refactoring

--- source/MRMesh/MRMapping.h
+++ source/MRMesh/MRMapping.h
@@ -8,40 +8,41 @@ namespace MR
 // newToOld - how to compare indexes
 // newIndex - indexes of the new object to be mapped
 template<typename ValueT, typename IndexT>
-Vector<ValueT, Id<IndexT>> mapNewToOldVector(
+void mapNewToOldVector(
     const Vector<ValueT, Id<IndexT>>& oldData,
     const Vector<Id<IndexT>, Id<IndexT>>& newToOld,
-    const TaggedBitSet<IndexT>& newIndex )
+    const TaggedBitSet<IndexT>& newIndex,
+    Vector<ValueT, Id<IndexT>>& newData )
 {
-    Vector<ValueT, Id<IndexT>> newMap( newIndex.find_last() + 1 );
+    if ( newData.size() < newIndex.find_last() )
+        newData.resizeNoInit( newIndex.find_last() + 1 );
+
     for ( auto curId : newIndex )
     {
         if ( auto id = newToOld[curId] )
         {
-            newMap[curId] = oldData[id];
+            newData[curId] = oldData[id];
         }
     }
-    return newMap;
 }
 
 // oldData - the data that needs to be compared
 // oldToNew - how to compare indexes
 // newIndex - indexes of the new object to be mapped
 template<typename ValueT, typename IndexT>
-Vector<ValueT, Id<IndexT>> mapOldToNewVector(
+void mapOldToNewVector(
     const Vector<ValueT, Id<IndexT>>& oldData,
     const Vector<Id<IndexT>, Id<IndexT>>& oldToNew,
-    const TaggedBitSet<IndexT>& newIndex )
+    const TaggedBitSet<IndexT>& newIndex,
+    Vector<ValueT, Id<IndexT>>& newData )
 {
-    Vector<ValueT, Id<IndexT>> newMap( newIndex.find_last() + 1 );
     for ( auto curId : newIndex )
     {
         if ( auto id = oldToNew[curId] )
         {
-            newMap[id] = oldData[curId];
+            newData[id] = oldData[curId];
         }
     }
-    return newMap;
 }
 
 }

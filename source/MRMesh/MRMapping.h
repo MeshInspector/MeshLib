2ce644951c801e95675136ddde496c3a89859625
Egor Grebenchenko <egrebenchenko@adalisk.com>
2024-04-26 20:53:02 +0300

    refactoring template

--- source/MRMesh/MRMapping.h
+++ source/MRMesh/MRMapping.h
@@ -7,13 +7,13 @@ namespace MR
 // oldData - the data that needs to be compared
 // newToOld - how to compare indexes
 // newIndex - indexes of the new object to be mapped
-template<typename ValueT, typename IndexT, typename IndexF>
-Vector<ValueT, IndexT> mapNewToOldVector(
-    const Vector<ValueT, IndexT>& oldData,
-    const Vector<IndexT, IndexT>& newToOld,
-    const TaggedBitSet<IndexF>& newIndex )
+template<typename ValueT, typename IndexT>
+Vector<ValueT, Id<IndexT>> mapNewToOldVector(
+    const Vector<ValueT, Id<IndexT>>& oldData,
+    const Vector<Id<IndexT>, Id<IndexT>>& newToOld,
+    const TaggedBitSet<IndexT>& newIndex )
 {
-    Vector<ValueT, IndexT> newMap( validIndex.find_last() + 1 );
+    Vector<ValueT, Id<IndexT>> newMap( newIndex.find_last() + 1 );
     for ( auto curId : newIndex )
     {
         if ( auto id = newToOld[curId] )
@@ -27,16 +27,16 @@ Vector<ValueT, IndexT> mapNewToOldVector(
 // oldData - the data that needs to be compared
 // oldToNew - how to compare indexes
 // newIndex - indexes of the new object to be mapped
-template<typename ValueT, typename IndexT, typename IndexF>
-Vector<ValueT, IndexT> mapOldToNewVector(
-    const Vector<ValueT, IndexT>& oldData,
-    const Vector<IndexT, IndexT>& oldToNew,
-    const TaggedBitSet<IndexF>& newIndex )
+template<typename ValueT, typename IndexT>
+Vector<ValueT, Id<IndexT>> mapOldToNewVector(
+    const Vector<ValueT, Id<IndexT>>& oldData,
+    const Vector<Id<IndexT>, Id<IndexT>>& oldToNew,
+    const TaggedBitSet<IndexT>& newIndex )
 {
-    Vector<ValueT, IndexT> newMap( validIndex.find_last() + 1 );
+    Vector<ValueT, Id<IndexT>> newMap( newIndex.find_last() + 1 );
     for ( auto curId : newIndex )
     {
-        if ( auto id = newToOld[curId] )
+        if ( auto id = oldToNew[curId] )
         {
             newMap[id] = oldData[curId];
         }

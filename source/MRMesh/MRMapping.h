ca4956c55a4a311523e9c09d4c75ab4ed275e667
Egor Grebenchenko <egrebenchenko@adalisk.com>
2024-04-26 20:20:25 +0300

    refactoring

--- source/MRMesh/MRMapping.h
+++ source/MRMesh/MRMapping.h
@@ -8,20 +8,37 @@ namespace MR
 // theseToThose - how to compare indexes
 // validIndex - which indexes should be compared
 template<typename ValueT, typename IndexT, typename IndexF>
-Vector<ValueT, IndexT> mapTheseToThoseVector(
-    const Vector<ValueT, IndexT>& theseData,
-    const Vector<IndexT, IndexT>& theseToThose,
-    const TaggedBitSet<IndexF>& validIndex )
+Vector<ValueT, IndexT> mapNewToOldVector(
+    const Vector<ValueT, IndexT>& oldData,
+    const Vector<IndexT, IndexT>& newToOld,
+    const TaggedBitSet<IndexF>& newIndex )
 {
-    Vector<ValueT, IndexT> newColorMap( validIndex.find_last() + 1 );
-    for ( auto id : validIndex )
+    Vector<ValueT, IndexT> newMap( validIndex.find_last() + 1 );
+    for ( auto curId : newIndex )
     {
-        if ( auto curId = theseToThose[id] )
+        if ( auto id = newToOld[curId] )
         {
-            newColorMap[id] = theseData[curId];
+            newMap[curId] = oldData[id];
         }
     }
-    return newColorMap;
+    return newMap;
+}
+
+template<typename ValueT, typename IndexT, typename IndexF>
+Vector<ValueT, IndexT> mapOldToNewVector(
+    const Vector<ValueT, IndexT>& oldData,
+    const Vector<IndexT, IndexT>& oldToNew,
+    const TaggedBitSet<IndexF>& newIndex )
+{
+    Vector<ValueT, IndexT> newMap( validIndex.find_last() + 1 );
+    for ( auto curId : newIndex )
+    {
+        if ( auto id = newToOld[curId] )
+        {
+            newMap[id] = oldData[curId];
+        }
+    }
+    return newMap;
 }
 
 }

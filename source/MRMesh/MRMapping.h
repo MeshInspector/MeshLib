5c2a9988551ada1208047734cf48d0a37d92d940
Egor Grebenchenko <egrebenchenko@adalisk.com>
2024-04-27 12:05:53 +0300

    refacoting function

--- source/MRMesh/MRMapping.h
+++ source/MRMesh/MRMapping.h
@@ -6,43 +6,30 @@ namespace MR
 
 // oldData - the data that needs to be compared
 // newToOld - how to compare indexes
-// newIndex - indexes of the new object to be mapped
+// for greater optimality, it is worth making a resize for newData in advance
 template<typename ValueT, typename IndexT>
 void mapNewToOldVector(
     const Vector<ValueT, Id<IndexT>>& oldData,
     const Vector<Id<IndexT>, Id<IndexT>>& newToOld,
-    const TaggedBitSet<IndexT>& newIndex,
     Vector<ValueT, Id<IndexT>>& newData )
 {
-    if ( newData.size() < newIndex.find_last() )
-        newData.resizeNoInit( newIndex.find_last() + 1 );
-
-    for ( auto curId : newIndex )
-    {
-        if ( auto id = newToOld[curId] )
-        {
-            newData[curId] = oldData[id];
-        }
-    }
+    for ( Id<IndexT> newId( 0 ); newId < newToOld.size(); ++newId )
+        if ( auto oldId = newToOld[newId] )
+            newData.autoResizeSet( newId, oldData[oldId] );
 }
 
 // oldData - the data that needs to be compared
-// oldToNew - how to compare indexes
-// newIndex - indexes of the new object to be mapped
+// newToOld - how to compare indexes
+// for greater optimality, it is worth making a resize for newData in advance
 template<typename ValueT, typename IndexT>
 void mapOldToNewVector(
     const Vector<ValueT, Id<IndexT>>& oldData,
-    const Vector<Id<IndexT>, Id<IndexT>>& oldToNew,
-    const TaggedBitSet<IndexT>& newIndex,
+    const Vector<Id<IndexT>, Id<IndexT>>& newToOld,
     Vector<ValueT, Id<IndexT>>& newData )
 {
-    for ( auto curId : newIndex )
-    {
-        if ( auto id = oldToNew[curId] )
-        {
-            newData[id] = oldData[curId];
-        }
-    }
+    for ( Id<IndexT> oldId( 0 ); oldId < newToOld.size(); ++oldId )
+        if ( auto newId = newToOld[oldId] )
+            newData.autoResizeSet( newId, oldData[oldId] );
 }
 
 }

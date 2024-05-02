5cd3e08d1e8662edd057637f6646b1e0a1c56de4
Fedr <fchelnokov@adalisk.com>
2024-05-03 00:03:02 +0300

    pip-build: Linux python 3.12 support (#2673)
    * pip-build: Linux python 3.12 support
    
    * fix Clang build on macOS
    
    * Revert "fix Clang build on macOS"
    
    This reverts commit 0f040c05a30a42c4863018186cbaf119cc4860e2.
    
    * checkout actual ref
    
    * add tests
    
    * MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, FaceMap, MR::FaceMap )
    
    * fixes
    
    * fix other Vectors
    
    * MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, PointPairs, MR::PointPairs )
    
    * more
    
    * fix python list operations
    
    * more
    
    * pip install typing-extensions
    
    * final touches
    
    * revert std::vector for python
    
    * return makeMovementBuildBody
    
    * another try
    
    * yet another
    
    * Artur's approach
    
    * restore pip-build.yml
    
    ---------
    
    Co-authored-by: Maxim Rayskiy <mraiskii@adalisk.com>

--- source/mrmeshpy/MRPythonMeshPlugins.cpp
+++ source/mrmeshpy/MRPythonMeshPlugins.cpp
@@ -266,20 +266,6 @@ MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, PlaneSections, [] ( pybind11::module_& m )
 
 MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, MovementBody, [] ( pybind11::module_& m )
 {
-    pybind11::class_<MovementBuildBodyParams>( m, "MovementBuildBodyParams" ).
-        def( pybind11::init<>() ).
-        def_readwrite( "allowRotation", &MovementBuildBodyParams::allowRotation,
-            "if this flag is set, rotate body in trajectory vertices\n"
-            "according to its rotation\n"
-            "otherwise body movement will be done without any rotation" ).
-        def_readwrite( "center", &MovementBuildBodyParams::center,
-            "point in body space that follows trajectory\n"
-            "if not set body bounding box center is used" ).
-        def_readwrite( "bodyNormal", &MovementBuildBodyParams::bodyNormal,
-            "facing direction of body, used for initial rotation (if allowRotation)\n"
-            "if not set body accumulative normal is used" ).
-        def_readwrite( "b2tXf", &MovementBuildBodyParams::b2tXf, "optional transform body space to trajectory space" );
-
     m.def( "makeMovementBuildBody", &makeMovementBuildBody,
         pybind11::arg( "body" ), pybind11::arg( "trajectory" ), pybind11::arg_v( "params", MovementBuildBodyParams(), "MovementBuildBodyParams()" ),
         "makes mesh by moving `body` along `trajectory`\n"

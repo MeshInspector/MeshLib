af87fc595a787534612ec6012bb7e51ba246b627
Fedr <fchelnokov@adalisk.com>
2024-05-02 22:07:49 +0300

    Artur's approach

--- source/mrmeshpy/MRPythonMovementBody.cpp
+++ source/mrmeshpy/MRPythonMovementBody.cpp
@@ -3,24 +3,17 @@
 #include "MRMesh/MRAffineXf3.h"
 #include "MRMesh/MRMesh.h"
 
-using namespace MR;
-
-// must be before #include <pybind11/stl.h>
-auto defMovementBody = [] ( pybind11::module_& m )
-{
-    m.def( "makeMovementBuildBody", &makeMovementBuildBody,
-        pybind11::arg( "body" ), pybind11::arg( "trajectory" ), pybind11::arg_v( "params", MovementBuildBodyParams(), "MovementBuildBodyParams()" ),
-        "makes mesh by moving `body` along `trajectory`\n"
-        "if allowRotation rotate it in corners" );
-};
-
 // for automatic conversion of std::optional into python type,
 // but it also affects all std::vector's making them another python type not like in other translation units
 #include <pybind11/stl.h>
 
-MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, MovementBuildBodyParams, [] ( pybind11::module_& m )
+using namespace MR;
+
+MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, MovementBuildBodyParams, MovementBuildBodyParams )
+
+MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, MovementBuildBodyParams, [] ( pybind11::module_& )
 {
-    pybind11::class_<MovementBuildBodyParams>( m, "MovementBuildBodyParams" ).
+    MR_PYTHON_CUSTOM_CLASS( MovementBuildBodyParams ).
         def( pybind11::init<>() ).
         def_readwrite( "allowRotation", &MovementBuildBodyParams::allowRotation,
             "if this flag is set, rotate body in trajectory vertices\n"
@@ -34,6 +27,3 @@ MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, MovementBuildBodyParams, [] ( pybind11::modu
             "if not set body accumulative normal is used" ).
         def_readwrite( "b2tXf", &MovementBuildBodyParams::b2tXf, "optional transform body space to trajectory space" );
 } )
-
-// must be after MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, MovementBuildBodyParams ... )
-MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, MovementBody, defMovementBody )

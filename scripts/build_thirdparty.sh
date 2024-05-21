0ae012585e08cbd986e35cf2ddab30562af9da77
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2024-05-21 14:27:46 +0200

    WIP: Customize thirdparty output dir location

--- scripts/build_thirdparty.sh
+++ scripts/build_thirdparty.sh
@@ -10,6 +10,12 @@ dt=$(date '+%d-%m-%Y_%H:%M:%S');
 logfile="`pwd`/install_thirdparty_${dt}.log"
 printf "Thirdparty build script started.\nYou could find output in ${logfile}\n"
 
+SCRIPT_DIR=$(dirname $(realpath "$0"))
+BASE_DIR=$(realpath "${SCRIPT_DIR}/..")
+
+THIRDPARTY_DIR=${BASE_DIR}/thirdparty/
+THIRDPARTY_BUILD_DIR="${THIRDPARTY_BUILD_DIR:-${BASE_DIR}/thirdparty_build/}"
+THIRDPARTY_ROOT_DIR="${THIRDPARTY_ROOT_DIR:-${BASE_DIR}/thirdparty_root/}"
 
 if [[ $OSTYPE == 'darwin'* ]]; then
   echo "Host system: MacOS"
@@ -55,63 +61,56 @@ if [ $MR_EMSCRIPTEN == "ON" ]; then
   fi
 elif [ -n "${INSTALL_REQUIREMENTS}" ]; then
   echo "Check requirements. Running ${INSTALL_REQUIREMENTS} ..."
-  ./scripts/$INSTALL_REQUIREMENTS
+  ${SCRIPT_DIR}/$INSTALL_REQUIREMENTS
 else
   echo "Unsupported system. Installing dependencies is your responsibility."
 fi
 
-
-MR_THIRDPARTY_DIR="thirdparty/"
-MR_THIRDPARTY_BUILD_DIR="thirdparty_build"
-MR_THIRDPARTY_LIB_DIR="lib/"
-MR_THIRDPARTY_INCLUDE_DIR="include/"
-
-#build Third party
-rm -rf "${MR_THIRDPARTY_BUILD_DIR}"
-rm -rf "${MR_THIRDPARTY_LIB_DIR}"
-rm -rf "${MR_THIRDPARTY_INCLUDE_DIR}"
-
-mkdir -p "${MR_THIRDPARTY_BUILD_DIR}"
-mkdir -p "${MR_THIRDPARTY_LIB_DIR}"
-mkdir -p "${MR_THIRDPARTY_INCLUDE_DIR}"
+# FIXME: make it optional
+rm -rf "${THIRDPARTY_BUILD_DIR}"
+mkdir -p "${THIRDPARTY_BUILD_DIR}"
+# FIXME: make it optional
+rm -rf "${THIRDPARTY_ROOT_DIR}"
+mkdir -p "${THIRDPARTY_ROOT_DIR}"
 
 # build
 echo "Starting build..."
-cd "${MR_THIRDPARTY_BUILD_DIR}"
+pushd "${THIRDPARTY_BUILD_DIR}"
 if [ "${MR_EMSCRIPTEN}" == "ON" ]; then
-  emcmake cmake -DMR_EMSCRIPTEN=1 -DMR_EMSCRIPTEN_SINGLETHREAD=${MR_EMSCRIPTEN_SINGLETHREAD} ../${MR_THIRDPARTY_DIR} -DCMAKE_INSTALL_PREFIX=../
+  emcmake cmake -DMR_EMSCRIPTEN=1 -DMR_EMSCRIPTEN_SINGLETHREAD=${MR_EMSCRIPTEN_SINGLETHREAD} ${THIRDPARTY_DIR} -DCMAKE_INSTALL_PREFIX=${THIRDPARTY_ROOT_DIR}
   emmake make -j `nproc` #VERBOSE=1
   make install
 
   # build OpenVDB separately
   OPENVDB_CMAKE_OPTIONS="-DOPENVDB_ENABLE_UNINSTALL=OFF -DOPENVDB_ENABLE_INSTALL=OFF -DOPENVDB_CORE_SHARED=OFF -DOPENVDB_CORE_STATIC=ON -DOPENVDB_BUILD_BINARIES=OFF -DOPENVDB_BUILD_VDB_PRINT=OFF -DUSE_BLOSC=OFF -DUSE_EXPLICIT_INSTANTIATION=OFF"
   # fix TBB lookup
-  OPENVDB_CMAKE_OPTIONS="${OPENVDB_CMAKE_OPTIONS} -DCMAKE_FIND_ROOT_PATH=$(pwd)/../ -DTbb_VERSION=2021.12"
+  OPENVDB_CMAKE_OPTIONS="${OPENVDB_CMAKE_OPTIONS} -DCMAKE_FIND_ROOT_PATH=${THIRDPARTY_ROOT_DIR} -DTbb_VERSION=2021.12"
   # fix linkage
   if [[ ${MR_EMSCRIPTEN_SINGLETHREAD} == 0 ]] ; then
     OPENVDB_CMAKE_OPTIONS="${OPENVDB_CMAKE_OPTIONS} -DCMAKE_CXX_FLAGS=-pthread"
   fi
   # mock the missing dependencies that are not actually used
   # if you want to use the OpenVDB file functions (see MRMESH_OPENVDB_USE_IO), you'll have to build these libraries by yourself
-  touch ../lib/libboost_iostreams.a ../lib/libboost_regex.a
-  emcmake cmake -S ../thirdparty/openvdb/v10/openvdb/ -B openvdb_build -DCMAKE_INSTALL_PREFIX="$(pwd)/../" ${OPENVDB_CMAKE_OPTIONS}
+  touch ${THIRDPARTY_ROOT_DIR}/lib/libboost_iostreams.a ${THIRDPARTY_ROOT_DIR}/lib/libboost_regex.a
+  emcmake cmake -S ${THIRDPARTY_DIR}/openvdb/v10/openvdb/ -B openvdb_build -DCMAKE_INSTALL_PREFIX="${THIRDPARTY_ROOT_DIR}" ${OPENVDB_CMAKE_OPTIONS}
   emmake cmake --build openvdb_build -j `nproc`
   emmake cmake --install openvdb_build
 else
-  cmake ../${MR_THIRDPARTY_DIR} -DCMAKE_INSTALL_PREFIX=../ -DCMAKE_BUILD_TYPE=Release
+  cmake ${THIRDPARTY_DIR} -DCMAKE_INSTALL_PREFIX=${THIRDPARTY_ROOT_DIR} -DCMAKE_BUILD_TYPE=Release
   cmake --build . -j `nproc`  #VERBOSE=1
   cmake --install .
 fi
-cd ..
+popd
 
 # copy libs (some of them are handled by their `cmake --install`, but some are not)
 echo "Copying thirdparty libs.."
 if [[ $OSTYPE == 'darwin'* ]]; then
-  cp "${MR_THIRDPARTY_BUILD_DIR}"/*.dylib "${MR_THIRDPARTY_LIB_DIR}"/
+  LIB_SUFFIX="*.dylib"
 elif [ "${MR_EMSCRIPTEN}" = "ON" ]; then
-  cp "${MR_THIRDPARTY_BUILD_DIR}"/*.a "${MR_THIRDPARTY_LIB_DIR}"/
+  LIB_SUFFIX="*.a"
 else
-  cp "${MR_THIRDPARTY_BUILD_DIR}"/*.so "${MR_THIRDPARTY_LIB_DIR}"/
+  LIB_SUFFIX="*.so"
 fi
+cp "${THIRDPARTY_BUILD_DIR}"/${LIB_SUFFIX} "${THIRDPARTY_ROOT_DIR}/lib/"
 
 printf "\rThirdparty build script successfully finished. Required libs located in ./lib folder. You could run ./scripts/build_source.sh\n\n"

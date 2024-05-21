e3bb01f139d74a35a8488bd78a7cb19e2cff675f
Artur Sharafutdinov <asharafutdinov@adalisk.com>
2024-05-21 15:57:58 +0200

    WIP: Rename variables

--- scripts/build_thirdparty.sh
+++ scripts/build_thirdparty.sh
@@ -13,9 +13,9 @@ printf "Thirdparty build script started.\nYou could find output in ${logfile}\n"
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 BASE_DIR=$(realpath "${SCRIPT_DIR}/..")
 
-THIRDPARTY_DIR=${BASE_DIR}/thirdparty/
-THIRDPARTY_BUILD_DIR="${THIRDPARTY_BUILD_DIR:-${BASE_DIR}/thirdparty_build/}"
-THIRDPARTY_ROOT_DIR="${THIRDPARTY_ROOT_DIR:-${BASE_DIR}/thirdparty_root/}"
+MESHLIB_THIRDPARTY_DIR=${BASE_DIR}/thirdparty/
+MESHLIB_THIRDPARTY_BUILD_DIR="${MESHLIB_THIRDPARTY_BUILD_DIR:-${BASE_DIR}/thirdparty_build/}"
+MESHLIB_THIRDPARTY_ROOT_DIR="${MESHLIB_THIRDPARTY_ROOT_DIR:-${BASE_DIR}/thirdparty_root/}"
 
 if [[ $OSTYPE == 'darwin'* ]]; then
   echo "Host system: MacOS"
@@ -67,36 +67,36 @@ else
 fi
 
 # FIXME: make it optional
-rm -rf "${THIRDPARTY_BUILD_DIR}"
-mkdir -p "${THIRDPARTY_BUILD_DIR}"
+rm -rf "${MESHLIB_THIRDPARTY_BUILD_DIR}"
+mkdir -p "${MESHLIB_THIRDPARTY_BUILD_DIR}"
 # FIXME: make it optional
-rm -rf "${THIRDPARTY_ROOT_DIR}"
-mkdir -p "${THIRDPARTY_ROOT_DIR}"
+rm -rf "${MESHLIB_THIRDPARTY_ROOT_DIR}"
+mkdir -p "${MESHLIB_THIRDPARTY_ROOT_DIR}"
 
 # build
 echo "Starting build..."
-pushd "${THIRDPARTY_BUILD_DIR}"
+pushd "${MESHLIB_THIRDPARTY_BUILD_DIR}"
 if [ "${MR_EMSCRIPTEN}" == "ON" ]; then
-  emcmake cmake -DMR_EMSCRIPTEN=1 -DMR_EMSCRIPTEN_SINGLETHREAD=${MR_EMSCRIPTEN_SINGLETHREAD} ${THIRDPARTY_DIR} -DCMAKE_INSTALL_PREFIX=${THIRDPARTY_ROOT_DIR}
+  emcmake cmake -DMR_EMSCRIPTEN=1 -DMR_EMSCRIPTEN_SINGLETHREAD=${MR_EMSCRIPTEN_SINGLETHREAD} ${MESHLIB_THIRDPARTY_DIR} -DCMAKE_INSTALL_PREFIX=${MESHLIB_THIRDPARTY_ROOT_DIR}
   emmake make -j `nproc` #VERBOSE=1
   make install
 
   # build OpenVDB separately
   OPENVDB_CMAKE_OPTIONS="-DOPENVDB_ENABLE_UNINSTALL=OFF -DOPENVDB_ENABLE_INSTALL=OFF -DOPENVDB_CORE_SHARED=OFF -DOPENVDB_CORE_STATIC=ON -DOPENVDB_BUILD_BINARIES=OFF -DOPENVDB_BUILD_VDB_PRINT=OFF -DUSE_BLOSC=OFF -DUSE_EXPLICIT_INSTANTIATION=OFF"
   # fix TBB lookup
-  OPENVDB_CMAKE_OPTIONS="${OPENVDB_CMAKE_OPTIONS} -DCMAKE_FIND_ROOT_PATH=${THIRDPARTY_ROOT_DIR} -DTbb_VERSION=2021.12"
+  OPENVDB_CMAKE_OPTIONS="${OPENVDB_CMAKE_OPTIONS} -DCMAKE_FIND_ROOT_PATH=${MESHLIB_THIRDPARTY_ROOT_DIR} -DTbb_VERSION=2021.12"
   # fix linkage
   if [[ ${MR_EMSCRIPTEN_SINGLETHREAD} == 0 ]] ; then
     OPENVDB_CMAKE_OPTIONS="${OPENVDB_CMAKE_OPTIONS} -DCMAKE_CXX_FLAGS=-pthread"
   fi
   # mock the missing dependencies that are not actually used
   # if you want to use the OpenVDB file functions (see MRMESH_OPENVDB_USE_IO), you'll have to build these libraries by yourself
-  touch ${THIRDPARTY_ROOT_DIR}/lib/libboost_iostreams.a ${THIRDPARTY_ROOT_DIR}/lib/libboost_regex.a
-  emcmake cmake -S ${THIRDPARTY_DIR}/openvdb/v10/openvdb/ -B openvdb_build -DCMAKE_INSTALL_PREFIX="${THIRDPARTY_ROOT_DIR}" ${OPENVDB_CMAKE_OPTIONS}
+  touch ${MESHLIB_THIRDPARTY_ROOT_DIR}/lib/libboost_iostreams.a ${MESHLIB_THIRDPARTY_ROOT_DIR}/lib/libboost_regex.a
+  emcmake cmake -S ${MESHLIB_THIRDPARTY_DIR}/openvdb/v10/openvdb/ -B openvdb_build -DCMAKE_INSTALL_PREFIX="${MESHLIB_THIRDPARTY_ROOT_DIR}" ${OPENVDB_CMAKE_OPTIONS}
   emmake cmake --build openvdb_build -j `nproc`
   emmake cmake --install openvdb_build
 else
-  cmake ${THIRDPARTY_DIR} -DCMAKE_INSTALL_PREFIX=${THIRDPARTY_ROOT_DIR} -DCMAKE_BUILD_TYPE=Release
+  cmake ${MESHLIB_THIRDPARTY_DIR} -DCMAKE_INSTALL_PREFIX=${MESHLIB_THIRDPARTY_ROOT_DIR} -DCMAKE_BUILD_TYPE=Release
   cmake --build . -j `nproc`  #VERBOSE=1
   cmake --install .
 fi
@@ -111,6 +111,6 @@ elif [ "${MR_EMSCRIPTEN}" = "ON" ]; then
 else
   LIB_SUFFIX="*.so"
 fi
-cp "${THIRDPARTY_BUILD_DIR}"/${LIB_SUFFIX} "${THIRDPARTY_ROOT_DIR}/lib/"
+cp "${MESHLIB_THIRDPARTY_BUILD_DIR}"/${LIB_SUFFIX} "${MESHLIB_THIRDPARTY_ROOT_DIR}/lib/"
 
 printf "\rThirdparty build script successfully finished. Required libs located in ./lib folder. You could run ./scripts/build_source.sh\n\n"

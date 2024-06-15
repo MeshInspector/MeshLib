cc4adcaabc7051e997054a19586764d812ff6bf4
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-06-15 19:44:07 +0300

    AABBTreeMaker: partition leaves by the center of their boxes (#2842)
    * AABBTreeMaker: partition leaves by the center of their boxes
    
    * Restart pipeline
    
    * Update link on autotest data
    
    * closestPointInTriangle: comments
    
    * closestPointInTriangle: handle rounding errors
    
    ---------
    
    Co-authored-by: AntonChernobrovkin <Anton.Chernobrovkin@meshinspector.com>

--- source/MRMesh/MRClosestPointInTriangle.h
+++ source/MRMesh/MRClosestPointInTriangle.h
@@ -24,6 +24,7 @@ namespace MR
 template <typename T>
 static std::pair<Vector3<T>, TriPoint<T>> closestPointInTriangle( const Vector3<T>& p, const Vector3<T>& a, const Vector3<T>& b, const Vector3<T>& c )
 {
+    // https://stackoverflow.com/a/74395029/7325599
     const Vector3<T> ab = b - a;
     const Vector3<T> ac = c - a;
     const Vector3<T> ap = p - a;
@@ -31,25 +32,25 @@ static std::pair<Vector3<T>, TriPoint<T>> closestPointInTriangle( const Vector3<
     const T d1 = dot( ab, ap );
     const T d2 = dot( ac, ap );
     if ( d1 <= 0 && d2 <= 0 ) 
-        return { a, { 0, 0 } };
+        return { a, { 0, 0 } }; //#1
 
     const Vector3<T> bp = p - b;
     const T d3 = dot( ab, bp );
     const T d4 = dot( ac, bp );
     if ( d3 >= 0 && d4 <= d3 ) 
-        return { b, { 1, 0 } };
+        return { b, { 1, 0 } }; //#2
 
     const Vector3<T> cp = p - c;
     const T d5 = dot( ab, cp );
     const T d6 = dot( ac, cp );
     if ( d6 >= 0 && d5 <= d6 ) 
-        return { c, { 0, 1 } };
+        return { c, { 0, 1 } }; //#3
 
     const T vc = d1 * d4 - d3 * d2;
     if ( vc <= 0 && d1 >= 0 && d3 <= 0 )
     {
         const T v = d1 / ( d1 - d3 );
-        return { a + v * ab, { v, 0 } };
+        return { a + v * ab, { v, 0 } }; //#4
     }
 
     const T vb = d5 * d2 - d1 * d6;
@@ -57,21 +58,29 @@ static std::pair<Vector3<T>, TriPoint<T>> closestPointInTriangle( const Vector3<
     {
         assert( d2 >= 0 );
         const T v = d2 / ( d2 - d6 );
-        return { a + v * ac, { 0, v } };
+        return { a + v * ac, { 0, v } }; //#5
     }
 
     const T va = d3 * d6 - d5 * d4;
     if ( va <= 0 )
     {
-        assert( d4 >= d3 && d5 >= d6 );
+        // d4-d3 = dot(bc,bp) >= 0 in #6
+        if ( d4 < d3 ) // floating-point rounding errors
+            return { b, { 1, 0 } }; //#2
+
+        // d5-d6 = dot(cb,cp) >= 0 in #6
+        if ( d5 < d6 ) // floating-point rounding errors
+            return { c, { 0, 1 } }; //#3
+
+        // ( d4 - d3 ) + ( d5 - d6 ) = bc^2 >= 0
         const T v = ( d4 - d3 ) / ( ( d4 - d3 ) + ( d5 - d6 ) );
-        return { b + v * ( c - b ), { 1 - v, v } };
+        return { b + v * ( c - b ), { 1 - v, v } }; //#6
     }
 
     const T denom = 1 / ( va + vb + vc );
     const T v = vb * denom;
     const T w = vc * denom;
-    return { a + v * ab + w * ac, { v, w } };
+    return { a + v * ab + w * ac, { v, w } }; //#0
 }
 
 }

119fbe7fd2a73dce8b602309eedd1eaa5d583971
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-06-20 15:05:23 +0300

    increase eps

--- source/MRMesh/MRBestFitParabola.cpp
+++ source/MRMesh/MRBestFitParabola.cpp
@@ -10,15 +10,15 @@ template class BestFitParabola<double>;
 
 TEST(MRMesh, BestFitParabola)
 {
-    Parabolad p( 1, 2, 3 );
+    const Parabolad p( 1, 2, 3 );
 
     BestFitParabolad fitter;
     fitter.addPoint( 1, p( 1 ) );
     fitter.addPoint( 2, p( 2 ) );
     fitter.addPoint( 3, p( 3 ) );
 
-    auto p1 = fitter.getBestParabola();
-    constexpr double eps = 1e-11;
+    const auto p1 = fitter.getBestParabola();
+    constexpr double eps = 2e-11;
     EXPECT_NEAR( std::abs( p.a - p1.a ), 0., eps );
     EXPECT_NEAR( std::abs( p.b - p1.b ), 0., eps );
     EXPECT_NEAR( std::abs( p.c - p1.c ), 0., eps );

0cb45719bff8938b41e2f0232458de721f08bf60
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-06-25 19:34:10 +0300

    FindInnerShellSettings (#2892)

--- source/MRMesh/MRInnerShell.h
+++ source/MRMesh/MRInnerShell.h
@@ -12,24 +12,28 @@ enum class Side
     Positive
 };
 
+struct FindInnerShellSettings
+{
+    /// specifies which side of shell is of interest: negative or positive relative to mesh normals
+    Side side = Side::Negative;
+
+    /// specifies maximum squared distance from shell parts of interest to source mesh
+    float maxDistSq = FLT_MAX;
+};
+
 /// Tests \param shellPoint from bidirectional shell constructed for an open \param mp;
-/// \param side specifies which side of shell is of interest: negative or positive relative to mesh normals;
-/// \return whether the distance from given point to given mesh part is of same sign as \param side,
+/// \return whether the distance from given point to given mesh part is of same sign as settings.side,
 /// always returning false for the points projecting on mesh boundary
-/// if squared distance to the point is more than \param maxAbsDistSq, returns false
-[[nodiscard]] MRMESH_API bool isInnerShellVert( const MeshPart & mp, const Vector3f & shellPoint, Side side, float maxAbsDistSq = FLT_MAX );
+[[nodiscard]] MRMESH_API bool isInnerShellVert( const MeshPart & mp, const Vector3f & shellPoint, const FindInnerShellSettings & settings = {} );
 
 /// Finds inner-shell vertices on bidirectional \param shell constructed for an open \param mp;
-/// \param side specifies which side of shell is of interest: negative or positive relative to mesh normals;
-/// The function will return all shell vertices that have distance to mesh of same sign as \param side
+/// The function will return all shell vertices that have distance to mesh of same sign as settings.side
 /// excluding the vertices projecting on mesh boundary
-/// Vertices with squared distances to the mesh more than \param maxAbsDistSq will not be considered as shell vertices
-[[nodiscard]] MRMESH_API VertBitSet findInnerShellVerts( const MeshPart & mp, const Mesh & shell, Side side, float maxAbsDistSq = FLT_MAX );
+[[nodiscard]] MRMESH_API VertBitSet findInnerShellVerts( const MeshPart & mp, const Mesh & shell, const FindInnerShellSettings & settings = {} );
 
 /// Finds inner-shell faces on bidirectional \param shell constructed for an open \param mp;
-/// \param side specifies which side of shell is of interest: negative or positive relative to mesh normals;
-/// The function will return all shell faces (after some subdivision) that have distance to mesh of same sign as \param side
+/// The function will return all shell faces (after some subdivision) that have distance to mesh of same sign as settings.side
 /// excluding the face projecting on mesh boundary
-[[nodiscard]] MRMESH_API FaceBitSet findInnerShellFacesWithSplits( const MeshPart & mp, Mesh & shell, Side side );
+[[nodiscard]] MRMESH_API FaceBitSet findInnerShellFacesWithSplits( const MeshPart & mp, Mesh & shell, const FindInnerShellSettings & settings = {} );
 
 } // namespace MR

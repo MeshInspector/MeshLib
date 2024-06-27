d55504acce6b7c1b27b92d8f0619194e11afa035
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-06-27 19:13:16 +0300

    classifyShellVert

--- source/MRMesh/MRInnerShell.h
+++ source/MRMesh/MRInnerShell.h
@@ -28,10 +28,26 @@ struct FindInnerShellSettings
     float windingNumberThreshold = 0.25f;
 };
 
+/// information about shell vertex
+struct ShellVertexInfo
+{
+    /// true when shell vertex is within settings.maxDist from source mesh
+    bool inRange = false;
+
+    /// shell vertex projects on source mesh boundary (never true for winding rule mode)
+    bool projOnBd = false;
+
+    /// whether shell vertex is on requested side of source mesh
+    bool rightSide = false;
+
+    /// returns true if shell vertex is in range, does not project on boundary and located on proper side
+    bool valid() const { return inRange && !projOnBd && rightSide; }
+};
+
 /// Tests \param shellPoint from bidirectional shell constructed for an open \param mp;
 /// \return whether the distance from given point to given mesh part is of same sign as settings.side,
 /// if useWindingNumber = false, returns false for all points projecting on mesh boundary
-[[nodiscard]] MRMESH_API bool isInnerShellVert( const MeshPart & mp, const Vector3f & shellPoint, const FindInnerShellSettings & settings = {} );
+[[nodiscard]] MRMESH_API ShellVertexInfo classifyShellVert( const MeshPart & mp, const Vector3f & shellPoint, const FindInnerShellSettings & settings = {} );
 
 /// Finds inner-shell vertices on bidirectional \param shell constructed for an open \param mp;
 /// The function will return all shell vertices that have distance to mesh of same sign as settings.side

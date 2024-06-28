83a9d7e0150822bf5d2d42fb6063c9c932fe9e1f
Egor Grebenchenko <egrebenchenko@adalisk.com>
2024-06-28 12:38:09 +0300

    fix

--- source/MRMesh/MRICP.cpp
+++ source/MRMesh/MRICP.cpp
@@ -420,7 +420,7 @@ NumSum getSumSqDistToPoint( const IPointPairs& pairs, double* inaccuracy )
     {
         const auto& vp = pairs[idx];
         if ( inaccuracy )
-            res.sum += std::abs( vp.distSq + *inaccuracy * *inaccuracy );
+            res.sum += std::abs( vp.distSq - *inaccuracy * *inaccuracy );
         else
             res.sum += vp.distSq;
         ++res.num;
@@ -436,7 +436,7 @@ NumSum getSumSqDistToPlane( const IPointPairs& pairs, double* inaccuracy )
         const auto& vp = pairs[idx];
         auto v = dot( vp.tgtNorm, vp.tgtPoint - vp.srcPoint );
         if ( inaccuracy )
-            res.sum += std::abs( sqr( v ) + *inaccuracy * *inaccuracy );
+            res.sum += std::abs( sqr( v ) - *inaccuracy * *inaccuracy );
         else
             res.sum += sqr( v );
         ++res.num;

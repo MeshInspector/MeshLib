271c6b70914ef61a2948183a83bd00763c9791ac
Egor Mikhailov <egor.mikhaylov@meshinspector.com>
2024-07-02 03:27:25 -0500

    Rework singletons.

--- source/MRMesh/MRSceneColors.cpp
+++ source/MRMesh/MRSceneColors.cpp
@@ -3,20 +3,65 @@
 
 namespace MR
 {
-SceneColors& SceneColors::instance_()
+
+namespace
 {
-    static SceneColors instance_;
-    return instance_;
+struct ColorState
+{
+    std::array<Color, size_t( SceneColors::Count )> colors;
+
+    ColorState()
+    {
+        using namespace SceneColors;
+
+        // color of object when it is selected
+        colors[SelectedObjectMesh] = colors[SelectedObjectPoints] =
+            colors[SelectedObjectLines] = colors[SelectedObjectVoxels] =
+            colors[SelectedObjectDistanceMap] = Color( Vector4f{ 1.0f,0.9f,0.23f,1.0f } );
+        // color if object when it is NOT selected
+        colors[UnselectedObjectMesh] = colors[UnselectedObjectPoints] =
+            colors[UnselectedObjectLines] = colors[UnselectedObjectVoxels] =
+            colors[UnselectedObjectDistanceMap] = Color( Vector4f{ 1.0f, 1.0f, 1.0f, 1.0f } );
+        // color of back faces
+        colors[BackFaces] = Color( Vector4f{ 0.519f,0.625f,0.344f,1.0f } );
+        // color of labels
+        colors[Labels] = Color( Vector4f{ 0.0f,0.0f,0.0f,1.0f } );
+        // color of edges
+        colors[Edges] = Color( Vector4f{ 0.0f,0.0f,0.0f,1.0f } );
+        // color of selected faces
+        colors[SelectedFaces] = Color( Vector4f{ 0.8f,0.2f,0.2f,1.0f } );
+        // color of selected edges
+        colors[SelectedEdges] = Color( Vector4f{ 0.7f,0.2f,0.7f,1.0f } );
+        // color of selected points
+        colors[SelectedPoints] = Color( Vector4f{ 0.8f,0.2f,0.2f,1.0f } );
+
+        colors[SelectedFeatures] = Color( 193, 40, 107, 255 );
+        colors[UnselectedFeatures] = Color( 216, 128, 70, 255 );
+        colors[FeatureBackFaces] = Color( 176, 124, 91, 255 );
+
+        colors[SelectedFeatureDecorations] = Color( 255, 64, 192, 255 );
+        colors[UnselectedFeatureDecorations] = Color( 255, 64, 192, 255 );
+
+        colors[SelectedMeasurements] = Color( 50, 255, 240, 255 );
+        colors[UnselectedMeasurements] = Color( 255, 255, 255, 255 );
+    }
+};
+
+ColorState &GetColorState()
+{
+    static ColorState ret;
+    return ret;
 }
+} // namespace
 
 const Color& SceneColors::get( Type type )
 {
-    return instance_().colors_[type];
+    return GetColorState().colors[type];
 }
 
 void SceneColors::set( Type type, const Color& color )
 {
-    instance_().colors_[type] = color;
+    GetColorState().colors[type] = color;
 }
 
 const char* SceneColors::getName( Type type )
@@ -50,40 +95,6 @@ const char* SceneColors::getName( Type type )
     }
     assert( false && "Invalid enum." );
     return nullptr;
-}
-
-SceneColors::SceneColors()
-{
-    // color of object when it is selected
-    colors_[SelectedObjectMesh] = colors_[SelectedObjectPoints] =
-        colors_[SelectedObjectLines] = colors_[SelectedObjectVoxels] =
-        colors_[SelectedObjectDistanceMap] = Color( Vector4f{ 1.0f,0.9f,0.23f,1.0f } );
-    // color if object when it is NOT selected
-    colors_[UnselectedObjectMesh] = colors_[UnselectedObjectPoints] =
-        colors_[UnselectedObjectLines] = colors_[UnselectedObjectVoxels] =
-        colors_[UnselectedObjectDistanceMap] = Color( Vector4f{ 1.0f, 1.0f, 1.0f, 1.0f } );
-    // color of back faces
-    colors_[BackFaces] = Color( Vector4f{ 0.519f,0.625f,0.344f,1.0f } );
-    // color of labels
-    colors_[Labels] = Color( Vector4f{ 0.0f,0.0f,0.0f,1.0f } );
-    // color of edges
-    colors_[Edges] = Color( Vector4f{ 0.0f,0.0f,0.0f,1.0f } );
-    // color of selected faces
-    colors_[SelectedFaces] = Color( Vector4f{ 0.8f,0.2f,0.2f,1.0f } );
-    // color of selected edges
-    colors_[SelectedEdges] = Color( Vector4f{ 0.7f,0.2f,0.7f,1.0f } );
-    // color of selected points
-    colors_[SelectedPoints] = Color( Vector4f{ 0.8f,0.2f,0.2f,1.0f } );
-
-    colors_[SelectedFeatures] = Color( 193, 40, 107, 255 );
-    colors_[UnselectedFeatures] = Color( 216, 128, 70, 255 );
-    colors_[FeatureBackFaces] = Color( 176, 124, 91, 255 );
-
-    colors_[SelectedFeatureDecorations] = Color( 255, 64, 192, 255 );
-    colors_[UnselectedFeatureDecorations] = Color( 255, 64, 192, 255 );
-
-    colors_[SelectedMeasurements] = Color( 50, 255, 240, 255 );
-    colors_[UnselectedMeasurements] = Color( 255, 255, 255, 255 );
 }
 
 }

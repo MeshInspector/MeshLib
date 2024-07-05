e05739b91e2794ea94ea447258c1ec6fe2cfe2ec
Alex Koksin <alex.koksin@meshinspector.com>
2024-07-05 16:46:43 +0400

    Add MRDotNetTests project

---
+++ source/MRDotNetTests/Program.cs
@@ -0,0 +1,8 @@
+﻿using MR.DotNet;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Vector3f a = new Vector3f(1, 2, 3);
+Vector3f b = new Vector3f(4, 5, 6);
+var c = a + b;
+Console.WriteLine( "{0} {1} {2}", c.x, c.y, c.z );
+

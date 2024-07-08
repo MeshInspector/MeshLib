95051b7872677b3492f17a82ce3e7d5043428f16
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-07-08 14:20:49 +0300

    PythonMeshExposing: add modern aliases to deprecated names (#2951)
    * PythonMeshExposing: add modern aliases to deprecated names
    
    * Deprecate types
    
    ---------
    
    Co-authored-by: Grant Karapetyan <grant.karapetyan@meshinspector.com>

--- source/mrmeshpy/MRPythonMeshExposing.cpp
+++ source/mrmeshpy/MRPythonMeshExposing.cpp
@@ -69,17 +69,37 @@ MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, MeshTopology, [] ( pybind11::module_& )
 } )
 
 // these declarations fix "Invalid expression" errors in pybind11_stubgen
-MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, VertColorMap, MR::VertColors )
+MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, VertColors, MR::VertColors )
 MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, VertCoords, MR::VertCoords )
 MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, FaceMap, MR::FaceMap )
 MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, VertMap, MR::VertMap )
 MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, WholeEdgeMap, MR::WholeEdgeMap )
 MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, UndirectedEdgeMap, MR::UndirectedEdgeMap )
 MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, EdgeMap, MR::EdgeMap )
-MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, VectorFloatByVert, MR::VertScalars )
+MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, VertScalars, MR::VertScalars )
 MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, FaceNormals, MR::FaceNormals )
 using VertCoords2 = Vector<Vector2f, VertId>;
 MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, VertCoords2, VertCoords2 )
+
+class DeprecatedVertColorMap : public MR::VertColors
+{
+public:
+    DeprecatedVertColorMap()
+    {
+        PyErr_WarnEx( PyExc_DeprecationWarning, "mrmeshpy.VertColorMap is deprecated, use mrmeshpy.VertColors type instead", 1 );
+    }
+};
+class DeprecatedVectorFloatByVert : public MR::VertScalars
+{
+public:
+    DeprecatedVectorFloatByVert()
+    {
+        PyErr_WarnEx( PyExc_DeprecationWarning, "mrmeshpy.VectorFloatByVert is deprecated, use mrmeshpy.VertScalars type instead", 1 );
+    }
+};
+
+MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, VertColorMap, DeprecatedVertColorMap )
+MR_ADD_PYTHON_CUSTOM_CLASS( mrmeshpy, VectorFloatByVert, DeprecatedVectorFloatByVert )
 
 MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, Vector, [] ( pybind11::module_& )
 {
@@ -111,6 +131,10 @@ MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, Vector, [] ( pybind11::module_& )
         def( pybind11::init<>() ).
         def_readwrite( "vec", &VertScalars::vec_ );
 
+    MR_PYTHON_CUSTOM_CLASS( VertScalars ).
+        def( pybind11::init<>() ).
+        def_readwrite( "vec", &VertScalars::vec_ );
+
     MR_PYTHON_CUSTOM_CLASS( FaceNormals ).
         def( pybind11::init<>() ).
         def_readwrite( "vec", &FaceNormals::vec_ );
@@ -120,6 +144,10 @@ MR_ADD_PYTHON_CUSTOM_DEF( mrmeshpy, Vector, [] ( pybind11::module_& )
         def_readwrite( "vec", &VertCoords2::vec_ );
 
     MR_PYTHON_CUSTOM_CLASS( VertColorMap ).
+        def( pybind11::init<>() ).
+        def_readwrite( "vec", &VertColors::vec_ );
+
+    MR_PYTHON_CUSTOM_CLASS( VertColors ).
         def( pybind11::init<>() ).
         def_readwrite( "vec", &VertColors::vec_ );
 } )

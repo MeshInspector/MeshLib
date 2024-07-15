e362aa8c68c5060ef0707fa6edf35149f37efe34
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2024-07-15 07:45:00 -0500

    Load mrbind settings from files.

--- scripts/mrbind/run.sh
+++ scripts/mrbind/run.sh
@@ -7,12 +7,12 @@ make -f _mrbind/scripts/apply_to_files.mk \
     INPUT_FILES_BLACKLIST="\$(file <$DIR/input_file_blacklist.txt)" \
     OUTPUT_DIR=build/binds \
     INPUT_GLOBS='*.h' \
-    MRBIND='_mrbind/build/mrbind --format=macros --ignore-pch-flags --ignore :: --allow MR --ignore MR::detail --ignore MR::Signal --ignore MR::UniquePtr --ignore MR::OpenVdbFloatGrid --ignore MR::RegisterRenderObjectConstructor --ignore MR::Config --allow std::integral_constant --skip-base boost::dynamic_bitset' \
-    COMPILER_FLAGS="-std=c++20 -Wno-nonportable-include-path -Wno-enum-constexpr-conversion -Wno-deprecated-enum-enum-conversion -I. -Iinclude -Isource -I/usr/include/jsoncpp -isystem/usr/include/freetype2 -isystem/usr/include/gdcm-3.0 $(pkg-config --cflags python3-embed)" \
-    COMPILER_FLAGS_LIBCLANG=-DMR_PARSING_FOR_PB11_BINDINGS \
-    COMPILER="$CXX -I_mrbind/include -Wno-deprecated-declarations -Wno-implicitly-unsigned-literal -fPIC -DMR_COMPILING_PB11_BINDINGS -DMRBIND_HEADER='<mrbind/targets/pybind11.h>' -DMB_PB11_MODULE_NAME=mrmesh -DMB_PB11_ADJUST_NAMES='\"s/\\\\bMR:://;s/\\\\bvec_\\\\b/vec/\"' -DMB_PB11_ENABLE_CXX_STYLE_CONTAINER_METHODS" \
+    MRBIND="_mrbind/build/mrbind \$(file <$DIR/mrbind_flags.txt)" \
+    COMPILER_FLAGS="\$(file <$DIR/common_compiler_parser_flags.txt) $(pkg-config --cflags python3-embed)" \
+    COMPILER_FLAGS_LIBCLANG="\$(file <$DIR/parser_only_flags.txt)" \
+    COMPILER="$CXX \$(file <$DIR/compiler_only_flags.txt)" \
     LINKER_OUTPUT=build/Release/bin/mrmesh$(python3-config --extension-suffix) \
     LINKER="$CXX -fuse-ld=lld" \
-    LINKER_FLAGS="$(pkg-config --libs python3-embed) -Lbuild/Release/bin -lMRMesh -Llib -ljsoncpp -lopenvdb -lTKernel -shared" \
+    LINKER_FLAGS="$(pkg-config --libs python3-embed) -Lbuild/Release/bin -lMRMesh -shared \$(file <$DIR/linker_flags.txt)" \
     NUM_FRAGMENTS=4 \
     "$@"

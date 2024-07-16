4440bdb9b035b69cab04e9cdf948cf3fbe9f6ddd
Alex Koksin <alex.koksin@meshinspector.com>
2024-07-16 17:37:05 +0400

    wrap std::expected for C++/CLI environment

--- source/MRMesh/MRExpected.h
+++ source/MRMesh/MRExpected.h
@@ -3,18 +3,34 @@
 #include "MRMeshFwd.h"
 
 #include <version>
-//#if __cpp_lib_expected >= 202211
-//#include <expected>
-//#else
+#if __cpp_lib_expected >= 202211
+#include <expected>
+#else
 #include <tl/expected.hpp>
-//#endif
+#ifdef MR_DOT_NET_BUILD
+namespace std
+{
+template<typename T, typename E>
+class expected : public tl::expected<T, E>
+{
+    using tl::expected<T, E>::expected;
+};
+
+template <class E>
+inline auto unexpected( E &&e )
+{
+    return tl::make_unexpected( std::forward<E>( e ) );
+}
+}
+#endif
+#endif
 
 #include <string>
 
 namespace MR
 {
 
-/*#if __cpp_lib_expected >= 202211
+#if ( __cpp_lib_expected >= 202211  ||  defined( MR_DOT_NET_BUILD ) )
 
 template<class T, class E = std::string>
 using Expected = std::expected<T, E>;
@@ -25,7 +41,7 @@ inline auto unexpected( E &&e )
     return std::unexpected( std::forward<E>( e ) );
 }
 
-#else*/
+#else
 
 template<class T, class E = std::string>
 using Expected = tl::expected<T, E>;
@@ -36,7 +52,7 @@ inline auto unexpected( E &&e )
     return tl::make_unexpected( std::forward<E>( e ) );
 }
 
-//#endif
+#endif
 
 /// return type for a void function that can produce an error string
 using VoidOrErrStr = Expected<void, std::string>;
@@ -50,7 +66,7 @@ inline std::string stringOperationCanceled()
 /// Returns Expected error with `stringOperationCanceled()`
 inline auto unexpectedOperationCanceled()
 {
-    return unexpected( stringOperationCanceled() );
+    return MR::unexpected(stringOperationCanceled());
 }
 
 } //namespace MR

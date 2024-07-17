294b1ba50f8293b9639f3a0b57ba8857bddf42bb
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2024-07-17 16:29:23 +0200

    Add new function

--- source/MRMeshC/MRPointCloud.cpp
+++ source/MRMeshC/MRPointCloud.cpp
@@ -2,7 +2,18 @@
 
 #include "MRMesh/MRPointCloud.h"
 
+#include <span>
+
 using namespace MR;
+
+MRPointCloud* mrPointCloudNew( const MRVector3f* points_, size_t pointsNum )
+{
+    std::span points { reinterpret_cast<const Vector3f*>( points_ ), pointsNum };
+
+    PointCloud res;
+    res.points = { points.begin(), points.end() };
+    return reinterpret_cast<MRPointCloud*>( new PointCloud( std::move( res ) ) );
+}
 
 const MRVector3f* mrPointCloudPoints( const MRPointCloud* pc_ )
 {
--- source/MRMeshC/MRPointCloud.h
+++ source/MRMeshC/MRPointCloud.h
@@ -4,6 +4,9 @@
 
 MR_EXTERN_C_BEGIN
 
+/// creates a new PointCloud object
+MRMESHC_API MRPointCloud* mrPointCloudNew( const MRVector3f* points, size_t pointsNum );
+
 /// coordinates of points
 MRMESHC_API const MRVector3f* mrPointCloudPoints( const MRPointCloud* pc );
 

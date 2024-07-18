a5fd0ef12eb2dce6da699cecc00815ddab748a25
Alex Koksin <alex.koksin@meshinspector.com>
2024-07-18 18:46:59 +0400

    add configs

--- scripts/run_c_unit_test_script.py
+++ scripts/run_c_unit_test_script.py
@@ -6,6 +6,10 @@ configuration = 'Release'
 if (len(sys.argv)>1):
 	if (sys.argv[1]=='Debug'):
 		configuration = 'Debug'
+	if (sys.argv[1]=='Debug_vs2019'
+		configuration = 'Debug_vs2019'
+	if (sys.argv[1]=='Release_vs2019'
+		configuration = 'Release_vs2019'
 
 run_tests_cmd = 'source\\x64\\'+configuration+'\\MRTestC.exe'
 

129d225c460c66636cc124c7b4919b065a8d6c16
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-07-19 17:07:46 +0300

    RecentFilesStore::onUpdate

--- source/MRViewer/MRRecentFilesStore.h
+++ source/MRViewer/MRRecentFilesStore.h
@@ -11,31 +11,34 @@ namespace MR
 
 using FileNamesStack = std::vector<std::filesystem::path>;
 
-// This class exists to store and load recently opened files
-// from system storage (registry for Windows)
+/// This class exists to store and load recently opened files
+/// from system storage (registry for Windows)
 class RecentFilesStore
 {
 public:
     RecentFilesStore() = default;
-    // Capacity is maximum stored filenames,
-    // it needs app name to store data to correct place in system storage
+    /// Capacity is maximum stored filenames,
+    /// it needs app name to store data to correct place in system storage
     RecentFilesStore( std::string appName, int capacity = 10 ) :
         appName_{std::move(appName)}, capacity_{capacity} {}
 
-    // Saves filename on top of recently opened stack,
-    // if file is already in the storage put it on top
+    /// Saves filename on top of recently opened stack,
+    /// if file is already in the storage put it on top
     MRVIEWER_API void storeFile( const std::filesystem::path& file ) const;
 
-    // Returns filenames from storage
+    /// Returns filenames from storage
     MRVIEWER_API std::vector<std::filesystem::path> getStoredFiles() const;
 
-    // Returns maximum size of recently opened files stack
+    /// Returns maximum size of recently opened files stack
     int getCapacity() const { return capacity_; }
 
-    // this signal is called when storage is updated
-    boost::signals2::signal<void( const FileNamesStack& files )> storageUpdateSignal;
+    /// Connects given slot to receive signal on every update of this store
+    MRVIEWER_API boost::signals2::connection onUpdate( const boost::function<void( const FileNamesStack& files )> & slot, boost::signals2::connect_position position = boost::signals2::at_back );
+
 private:
     std::string appName_;
     int capacity_{5};
+    boost::signals2::signal<void( const FileNamesStack& files )> updateSignal_;
 };
-}
+
+} //namespace MR

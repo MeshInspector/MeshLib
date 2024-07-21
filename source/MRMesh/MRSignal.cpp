fdbd77cd48a9f957d66939c00f659c76004c98e4
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-07-21 22:19:58 +0300

    fix warning

--- source/MRMesh/MRSignal.cpp
+++ source/MRMesh/MRSignal.cpp
@@ -12,7 +12,7 @@ boost::signals2::connection Signal<T>::connect( const boost::function<T> & slot,
 #define INSTANTIATE(T) \
     template boost::signals2::connection Signal<T>::connect( const boost::function<T> & slot, boost::signals2::connect_position position );
 
-INSTANTIATE(void());
-INSTANTIATE(void(uint32_t));
+INSTANTIATE(void())
+INSTANTIATE(void(uint32_t))
 
 } //namespace MR

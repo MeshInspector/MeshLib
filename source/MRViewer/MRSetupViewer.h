854ba448262be78a5bc496f964ab0646c0995acb
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-07-22 17:10:33 +0300

    More cleanup in Viewer::launchShut() to allow plugins unloading

--- source/MRViewer/MRSetupViewer.h
+++ source/MRViewer/MRSetupViewer.h
@@ -9,24 +9,32 @@ namespace MR
 class MRVIEWER_CLASS ViewerSetup
 {
 public:
-    // explicitly define ctors to avoid warning C5267: definition of implicit copy constructor is deprecated because it has a user-provided destructor
+    /// explicitly define ctors to avoid warning C5267: definition of implicit copy constructor is deprecated because it has a user-provided destructor
     ViewerSetup() = default;
     ViewerSetup( const ViewerSetup & ) = default;
     ViewerSetup( ViewerSetup && ) = default;
+
     virtual ~ViewerSetup() = default;
-    // Setups Menu Save and Open plugins
+
+    /// Setups Menu Save and Open plugins
     MRVIEWER_API virtual void setupBasePlugins( Viewer* /*viewer*/ ) const;
-    // Setups modifiers to Menu plugin if it is present in viewer
+
+    /// Setups modifiers to Menu plugin if it is present in viewer
     virtual void setupCommonModifiers( Viewer* /*viewer*/ ) const {}
-    // Setups custom plugins to viewer
+
+    /// Setups custom plugins to viewer
     virtual void setupCommonPlugins( Viewer* /*viewer*/ ) const {}
-    // Sets custom viewer settings manager to viewer
+
+    /// Sets custom viewer settings manager to viewer
     MRVIEWER_API virtual void setupSettingsManager( Viewer* viewer, const std::string& appName ) const;
-    // Sets start configuration for viewer
-    // use to override default configuration of viewer
+
+    /// Sets start configuration for viewer
+    /// use to override default configuration of viewer
     MRVIEWER_API virtual void setupConfiguration( Viewer* viewer ) const;
-    // use to load additional libraries with plugins.
-    // all libraries are taken from GetResourcesDirectory() *.ui.json files
+
+    /// use to load additional libraries with plugins.
+    /// all libraries are taken from GetResourcesDirectory() *.ui.json files
     MRVIEWER_API virtual void setupExtendedLibraries() const;
 };
-}
+
+} // namespace MR

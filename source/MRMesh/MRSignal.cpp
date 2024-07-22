4fc004787a6929265b1ebf49dc1efe93defa5de4
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-07-22 10:06:04 +0300

    Signal<Signature, Combiner>

--- source/MRMesh/MRSignal.cpp
+++ source/MRMesh/MRSignal.cpp
@@ -3,16 +3,19 @@
 namespace MR
 {
 
-template<typename T>
-boost::signals2::connection Signal<T>::connect( const boost::function<T> & slot, boost::signals2::connect_position position )
+template<typename Signature, typename Combiner>
+boost::signals2::connection Signal<Signature, Combiner>::connect( const boost::function<Signature> & slot, boost::signals2::connect_position position )
 {
     return Parent::connect( slot, position );
 }
 
-#define INSTANTIATE(T) \
-    template boost::signals2::connection Signal<T>::connect( const boost::function<T> & slot, boost::signals2::connect_position position );
+#define INSTANTIATE(Signature) \
+    template boost::signals2::connection Signal<Signature>::connect( const boost::function<Signature> & slot, boost::signals2::connect_position position );
 
 INSTANTIATE(void())
 INSTANTIATE(void(uint32_t))
+
+#define INSTANTIATE2(Signature, Combiner) \
+    template boost::signals2::connection Signal<Signature, Combiner>::connect( const boost::function<Signature> & slot, boost::signals2::connect_position position );
 
 } //namespace MR
--- source/MRMesh/MRSignal.h
+++ source/MRMesh/MRSignal.h
@@ -21,10 +21,11 @@ namespace MR
 
 /// This class wraps boost::signals2::signal adding copy constructor and copy assignment operator,
 /// which do nothing, but allow putting this as a member in copyable classes
-template<typename T>
-struct Signal : boost::signals2::signal<T>
+template<typename Signature,
+         typename Combiner = boost::signals2::optional_last_value<typename boost::function_traits<Signature>::result_type>>
+struct Signal : boost::signals2::signal<Signature, Combiner>
 {
-    using Parent = boost::signals2::signal<T>;
+    using Parent = boost::signals2::signal<Signature, Combiner>;
 
     Signal() noexcept = default;
     Signal( const Signal& ) noexcept : Parent() {}
@@ -34,7 +35,7 @@ struct Signal : boost::signals2::signal<T>
     Signal& operator =( Parent&& b ) noexcept { *static_cast<Parent*>(this) = std::move( b ); return *this; }
 
     /// implementation of connect method in MRMesh shared library allows the caller shared library to be safely unloaded
-    MRMESH_API boost::signals2::connection connect( const boost::function<T> & slot, boost::signals2::connect_position position = boost::signals2::at_back );
+    MRMESH_API boost::signals2::connection connect( const boost::function<Signature> & slot, boost::signals2::connect_position position = boost::signals2::at_back );
 };
 
 } //namespace MR

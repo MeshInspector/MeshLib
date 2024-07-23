c5b103adf0123d58df8c599493e91113ad2c386e
Alex Koksin <akoksin@adalisk.com>
2024-07-23 19:06:18 +0400

    distribute .NET bindings (#3040)
    * get rid of native headers
    
    * zip and upload .NET distribution
    
    * code review

--- scripts/zip_distribution.py
+++ scripts/zip_distribution.py
@@ -2,15 +2,20 @@ import os
 import sys
 
 if (len(sys.argv)<2):
+	print("No path to the folder given")
+	sys.exit(1)
+    
+if (len(sys.argv)<3):
 	print("No path to distribute given")
 	sys.exit(1)
 
-res_file = str(sys.argv[1])
+src_folder = str(sys.argv[1])
+res_file = str(sys.argv[2])
 
 base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..')
-path_to_install_folder = os.path.join(base_path,'install')
+path_to_install_folder = os.path.join(base_path,src_folder)
 
-zip_cmd = 'tar -a -c -f '+ ' ' + res_file + ' install' 
+zip_cmd = 'tar -a -c -f '+ ' ' + res_file + ' ' + src_folder
 
 os.chdir(base_path)
 res = os.system(zip_cmd)

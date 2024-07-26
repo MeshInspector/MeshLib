1d709c9a88c76cdcd1bf2dab96fc73282896f032
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-07-26 13:11:15 +0300

    findPointsInBall: update comment

--- source/MRMesh/MRPointsInBall.h
+++ source/MRMesh/MRPointsInBall.h
@@ -14,7 +14,7 @@ using FoundPointCallback = std::function<void( VertId, const Vector3f& )>;
 MRMESH_API void findPointsInBall( const PointCloud& pointCloud, const Vector3f& center, float radius, 
     const FoundPointCallback& foundCallback, const AffineXf3f* xf = nullptr );
 
-/// Finds all valid points of the mesh that are inside or on the surface of given ball (center, radius)
+/// Finds all valid vertices of the mesh that are inside or on the surface of given ball (center, radius)
 /// \ingroup AABBTreeGroup
 /// \param xf points-to-center transformation, if not specified then identity transformation is assumed
 MRMESH_API void findPointsInBall( const Mesh& mesh, const Vector3f& center, float radius, 

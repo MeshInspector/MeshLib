3b255477ea72eb0e285546c108cea0c39257e409
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-07-28 07:54:00 +0300

    findInSphere: only one call to findTrisInBall (#3086)

--- source/MRMesh/MRMeshThickness.h
+++ source/MRMesh/MRMeshThickness.h
@@ -37,7 +37,7 @@ struct InSphereSearchSettings
     /// for almost closed meshes the article recommends maxRadius = 0.5f * std::min( { boxSize.x, boxSize.y, boxSize.z } )
     float maxRadius = 1;
 
-    /// maximum number of shrinking iterations
+    /// maximum number of shrinking iterations for one triangle
     int maxIters = 16;
 
     /// iterations stop if next radius is larger than minShrinkage times previous radius

440befa587f36e3614254b7550018026b36cbb8a
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2024-07-29 10:13:13 +0200

    Enable color loading for STEP files

--- source/MRMesh/MRMeshLoadStep.cpp
+++ source/MRMesh/MRMeshLoadStep.cpp
@@ -92,8 +92,8 @@ using namespace MR;
 #define STEP_READSTREAM_SUPPORTED ( ( ( OCC_VERSION_MAJOR > 7 ) || ( OCC_VERSION_MAJOR == 7 && OCC_VERSION_MINOR >= 5 ) ) )
 // reading STEP data fixed
 #define STEP_READER_FIXED ( ( ( OCC_VERSION_MAJOR > 7 ) || ( OCC_VERSION_MAJOR == 7 && OCC_VERSION_MINOR >= 7 ) ) )
-// TODO: full color support
-#define STEP_LOAD_COLORS 0
+// enable full color support
+#define STEP_LOAD_COLORS 1
 
 #if MODERN_MESSAGE_SUPPORTED
 /// spdlog adaptor for OpenCASCADE logging system

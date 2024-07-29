9a5e20106e4411ffc699d46e5f24accf69d9bcba
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2024-07-29 10:23:47 +0200

    Fix build

--- source/MRMesh/MRMeshLoadStep.cpp
+++ source/MRMesh/MRMeshLoadStep.cpp
@@ -173,7 +173,7 @@ private:
 #pragma message( "Progress indication is currently unsupported for OpenCASCADE versions prior to 7.4" )
 #endif
 
-#if STEP_LOAD_COLORS
+#if defined( MRMESH_OPENCASCADE_USE_XDE ) && STEP_LOAD_COLORS
 Color toColor( const Quantity_ColorRGBA& rgba )
 {
     const auto& rgb = rgba.GetRGB();

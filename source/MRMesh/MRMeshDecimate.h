38c5acaa90af5acf16f7578068b64aba36473911
Egor Grebenchenko <egor.grebenchenko@meshinspector.com>
2024-08-06 20:16:48 +0300

    add default EdgeToCollapse func

--- source/MRMesh/MRMeshDecimate.h
+++ source/MRMesh/MRMeshDecimate.h
@@ -109,6 +109,39 @@ struct DecimateSettings
      */
     std::function<bool( EdgeId edgeToCollapse, const Vector3f & newEdgeOrgPos)> preCollapse;
 
+    template <typename M, typename T>
+    static std::function<bool( EdgeId edgeToCollapse, const Vector3f& newEdgeOrgPos )> defaultEdgeToCollapse( M& mesh, T& data )
+    {
+        auto preCollapse = [&] ( EdgeId edgeToCollapse, const Vector3f& newEdgeOrgPos )
+        {
+            const auto org = mesh.topology.org( edgeToCollapse );
+            const auto dest = mesh.topology.dest( edgeToCollapse );
+            const auto orgPos = mesh.orgPnt( edgeToCollapse );
+            const auto destPos = mesh.destPnt( edgeToCollapse );
+
+            const auto ab = destPos - orgPos;
+            const auto dt = dot( newEdgeOrgPos - orgPos, ab );
+            const auto abLengthSq = ab.lengthSq();
+            if ( dt <= 0 )
+            {
+                return true;
+            }
+
+            if ( dt >= abLengthSq )
+            {
+                data[org] = data[dest];
+                return true;
+            }
+
+            const auto ratio = dt / abLengthSq;
+            data[org] = ( 1 - ratio ) * data[org] + ratio * data[dest];
+
+            return true;
+        };
+
+        return preCollapse;
+    }
+
     /**
      * \brief The user can provide this optional callback for adjusting error introduced by this
      * edge collapse and the collapse position.

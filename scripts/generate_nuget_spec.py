d1b99aeeef3fac46c62455416479b6d805d4541f
Alex Koksin <alex.koksin@meshinspector.com>
2024-08-08 15:45:34 +0400

    Nuget package (#3127)
    * PLEASE DO NOT MERGE
    
    * add app version
    
    * move needs
    
    * hardcode version for testing
    
    * sign package with cert
    
    * Revert "sign package with cert"
    
    This reverts commit 9283b832cc1c5a8fa868d142d70e2374ac3890e5.
    
    * get proper build number
    
    * fix formatting
    
    * change runner
    
    * parse version tag
    
    * test uploading package
    
    * fix a typo
    
    * generate nupkg without version in name
    
    * codesign package
    
    * add needed job
    
    * delete step
    
    * use jsign tool
    
    * uplload signed package
    
    * use nuget instead of dotnet
    
    * download nuget executable
    
    * comment lines
    
    * check signature and upload package
    
    * build nuget package only if fulll-ci is enabled
    
    * build NuGet package only for Release config
    
    * move uploading to a separate file
    
    * distribute nuget package
    
    * fix fomatting
    
    * add checkout step
    
    * fix formatting
    
    * fix scripts
    
    * fix formating
    
    * rename artifact
    
    * fix a typo
    
    * move setup job
    
    * move job
    
    * add line
    
    * remove spaces
    
    * remove extra dependency
    
    * delete path to file
    
    * write relative path
    
    * modify scripts
    
    * fix formatting
    
    * fix formatting
    
    * rename build file
    
    * fix substring to find
    
    * fix build scripts
    
    * rename some vars
    
    * rename
    
    * add dependency
    
    ---------
    
    Co-authored-by: ADALISK\akoksin <akoksin@adalis.com>

---
+++ scripts/generate_nuget_spec.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+import sys
+
+base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..')
+path_to_objects = base_path
+path_to_spec = os.path.join(base_path, 'Package.nuspec')
+print( path_to_spec )
+print('\n')
+
+excluded_modules = ['MRCommonPlugins', 'MRCuda', 'MRMeshC', 'MRViewer', 'MRMeshViewer', 'MRTest', 'MRTestC']
+path_to_copyright_header = os.path.join(os.path.dirname(os.path.abspath(__file__)),'copyright_header.txt')
+copyright_header = open(path_to_copyright_header,'r').read()[3:]
+
+f = open(path_to_spec, 'w')
+f.write('<?xml version="1.0" encoding="utf-8"?>\n')
+f.write('<package>\n')
+f.write('\t<metadata>\n')
+
+f.write('\t\t<id>MeshLib</id>\n')
+
+f.write('\t\t<version>')
+f.write(sys.argv[1][1:])
+f.write('\t\t</version>\n')
+
+f.write('\t\t<authors>AMV Consulting</authors>\n')
+f.write('\t\t<owners>AMV Consulting</owners>\n')
+f.write('\t\t<projectUrl>https://meshinspector.com</projectUrl>\n')
+f.write('\t\t<description>Mesh processing library</description>\n')
+
+f.write('\t\t<releaseNotes>https://github.com/MeshInspector/MeshLib/releases</releaseNotes>\n')
+
+f.write('\t\t<copyright>')
+f.write(copyright_header)
+f.write('</copyright>\n')
+
+f.write('\t</metadata>\n')
+
+f.write('\t<files>\n')
+
+folder = os.walk(path_to_objects)
+for address, dirs, files in folder:
+	for file in files:
+		if ((file.endswith('.dll') and not any(map(file.startswith, excluded_modules)))):
+			src = os.path.join(address,file)
+			print(src)
+			f.write('\t\t<file src="./source/x64/Release/')
+			f.write(file)
+			f.write('" target="lib/net8.0/"></file>\n')
+            
+f.write('\t</files>\n')
+f.write('</package>\n')
+f.close()

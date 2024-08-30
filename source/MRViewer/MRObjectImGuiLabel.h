eb0c64942533f08999d2cc00c71214c4d248da65
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2024-08-30 13:23:25 +0200

    Move ObjectImGuiLabel class to MRViewer

--- source/MRViewer/MRObjectImGuiLabel.h
+++ source/MRViewer/MRObjectImGuiLabel.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include "MRSymbolMeshFwd.h"
-#include "MRSymbolMesh.h"
+#include "exports.h"
 
 #include "MRMesh/MRVisualObject.h"
 
@@ -10,10 +9,10 @@ namespace MR
 
 /// Renders a label using ImGui, on top of all geometry without z-buffering.
 /// But those are still depth-sorted among each other.
-class MRSYMBOLMESH_CLASS ObjectImGuiLabel : public VisualObject
+class MRVIEWER_CLASS ObjectImGuiLabel : public VisualObject
 {
 public:
-    MRSYMBOLMESH_API ObjectImGuiLabel();
+    MRVIEWER_API ObjectImGuiLabel();
 
     ObjectImGuiLabel( ObjectImGuiLabel&& ) noexcept = default;
     ObjectImGuiLabel& operator = ( ObjectImGuiLabel&& ) noexcept = default;
@@ -29,26 +28,26 @@ public:
 
     bool hasVisualRepresentation() const override { return true; }
 
-    MRSYMBOLMESH_API std::shared_ptr<Object> clone() const override;
-    MRSYMBOLMESH_API std::shared_ptr<Object> shallowClone() const override;
+    MRVIEWER_API std::shared_ptr<Object> clone() const override;
+    MRVIEWER_API std::shared_ptr<Object> shallowClone() const override;
 
     /// \note this ctor is public only for std::make_shared used inside clone()
-    MRSYMBOLMESH_API ObjectImGuiLabel( ProtectedStruct, const ObjectImGuiLabel& obj );
+    MRVIEWER_API ObjectImGuiLabel( ProtectedStruct, const ObjectImGuiLabel& obj );
 
-    [[nodiscard]] MRSYMBOLMESH_API const std::string& getLabel() const;
-    MRSYMBOLMESH_API void setLabel( std::string value );
+    [[nodiscard]] MRVIEWER_API const std::string& getLabel() const;
+    MRVIEWER_API void setLabel( std::string value );
 
 protected:
     ObjectImGuiLabel( const ObjectImGuiLabel& other ) = default;
 
     /// swaps this object with other
-    MRSYMBOLMESH_API void swapBase_( Object& other ) override;
+    MRVIEWER_API void swapBase_( Object& other ) override;
 
-    MRSYMBOLMESH_API void serializeFields_( Json::Value& root ) const override;
+    MRVIEWER_API void serializeFields_( Json::Value& root ) const override;
 
-    MRSYMBOLMESH_API void deserializeFields_( const Json::Value& root ) override;
+    MRVIEWER_API void deserializeFields_( const Json::Value& root ) override;
 
-    MRSYMBOLMESH_API void setupRenderObject_() const override;
+    MRVIEWER_API void setupRenderObject_() const override;
 
 private:
     std::string labelText_;

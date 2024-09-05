12395e223d429980be46e9fbe36e531ef2c5c08d
Egor Grebenchenko <egor.grebenchenko@meshinspector.com>
2024-09-05 17:57:57 +0300

    MIC#4835_support_texture_in_Mesh_Healer_add_new_func (#3305)

---
+++ source/MRMesh/MRProjectionMeshAttribute.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "MRMesh.h"
+#include "MRBitSetParallelFor.h"
+#include "MRMeshProject.h"
+#include "MRMeshAttributesToUpdate.h"
+
+namespace MR
+{
+
+// projecting the vertex attributes of the old onto the new one
+template<typename F>
+void projectVertAttribute( const Mesh& newMesh, const Mesh& oldMesh, F&& func, ProgressCallback progressCb );
+
+// projecting the face attributes of the old onto the new one
+template<typename F>
+void projectFaceAttribute( const Mesh& newMesh, const Mesh& oldMesh, F&& func, ProgressCallback progressCb );
+
+
+template<typename F>
+void projectVertAttribute( const Mesh& newMesh, const Mesh& oldMesh, F&& func, ProgressCallback progressCb )
+{
+    BitSetParallelFor( newMesh.topology.getValidVerts(), [&] ( VertId id )
+        {
+            auto projectionResult = findProjection( newMesh.points[id], oldMesh );
+            auto res = projectionResult.mtp;
+            VertId v1 = oldMesh.topology.org( res.e );
+            VertId v2 = oldMesh.topology.dest( res.e );
+            VertId v3 = oldMesh.topology.dest( oldMesh.topology.next( res.e ) );
+            func( id, projectionResult, v1, v2, v3 );
+        },
+    progressCb );
+}
+
+template<typename F>
+void projectFaceAttribute( const Mesh& newMesh, const Mesh& oldMesh, F&& func, ProgressCallback progressCb )
+{
+    BitSetParallelFor( newMesh.topology.getValidFaces(), [&] ( FaceId newFaceId )
+    {
+        auto verts = oldMesh.topology.getTriVerts( newFaceId );
+        auto point = oldMesh.points[verts[0]] / 3.0f + oldMesh.points[verts[1]] / 3.0f + oldMesh.points[verts[2]] / 3.0f;
+        auto projectionResult = findProjection( point, oldMesh );
+        func( newFaceId, projectionResult );
+    },
+    progressCb );
+}
+
+}

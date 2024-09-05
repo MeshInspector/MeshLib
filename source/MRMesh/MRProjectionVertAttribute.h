4d4a494c1ea93b89613004d415bf99008fff82f6
Egor Grebenchenko <egor.grebenchenko@meshinspector.com>
2024-09-05 07:25:51 +0300

    add new func

---
+++ source/MRMesh/MRProjectionVertAttribute.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "MRMeshFwd.h"
+#include "MRMeshProject.h"
+
+namespace MR
+{
+
+template<typename T>
+T calcNewVertAttribute( const Mesh& newMesh, const Mesh& oldMesh, const T& data, ProgressCallback progressCb )
+{
+    T newData;
+    if ( !data.empty() )
+    {
+        newData.resize( newMesh->topology.lastValidVert() + 1 );
+        BitSetParallelFor( newMesh->topology.getValidVerts(), [&] ( VertId id )
+            {
+                auto res = findProjection( newMesh->points[id], *oldMesh ).mtp;
+                VertId v1 = oldMesh->topology.org( res.e );
+                VertId v2 = oldMesh->topology.dest( res.e );
+                VertId v3 = oldMesh->topology.dest( oldMesh->topology.next( res.e ) );
+                newData[id] = res.bary.interpolate( data[v1], data[v2], data[v3] );
+            },
+        progressCb );
+    }
+
+    return newData;
+}
+
+}

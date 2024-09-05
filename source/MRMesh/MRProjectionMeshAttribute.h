071a36f902156cf4ff50d7ef65726906915f23a6
Egor Grebenchenko <egor.grebenchenko@meshinspector.com>
2024-09-05 21:07:14 +0300

    refactoring for face attribute

--- source/MRMesh/MRProjectionMeshAttribute.h
+++ source/MRMesh/MRProjectionMeshAttribute.h
@@ -37,8 +37,7 @@ void projectFaceAttribute( const Mesh& newMesh, const Mesh& oldMesh, F&& func, P
 {
     BitSetParallelFor( newMesh.topology.getValidFaces(), [&] ( FaceId newFaceId )
     {
-        auto verts = oldMesh.topology.getTriVerts( newFaceId );
-        auto point = oldMesh.points[verts[0]] / 3.0f + oldMesh.points[verts[1]] / 3.0f + oldMesh.points[verts[2]] / 3.0f;
+        auto point = newMesh.triCenter( newFaceId );
         auto projectionResult = findProjection( point, oldMesh );
         func( newFaceId, projectionResult );
     },

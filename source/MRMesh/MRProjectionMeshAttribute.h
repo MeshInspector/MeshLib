4c9af07d4514f5a08ec9f762622a179f660bee8d
Egor Grebenchenko <egor.grebenchenko@meshinspector.com>
2024-09-05 10:39:41 +0300

    add comment

--- source/MRMesh/MRProjectionMeshAttribute.h
+++ source/MRMesh/MRProjectionMeshAttribute.h
@@ -4,13 +4,20 @@
 #include "MRBitSetParallelFor.h"
 #include "MRMeshProject.h"
 
+
 namespace MR
 {
+
+// projecting the vertex attributes of the old onto the new one
+template<typename T>
+T calcNewVertAttribute( const Mesh& newMesh, const Mesh& oldMesh, const T& data, ProgressCallback progressCb );
+
 
 template<typename T>
 T calcNewVertAttribute( const Mesh& newMesh, const Mesh& oldMesh, const T& data, ProgressCallback progressCb )
 {
     T newData;
+    return newData;
     if ( !data.empty() )
     {
         newData.resize( newMesh.topology.lastValidVert() + 1 );
@@ -27,26 +34,5 @@ T calcNewVertAttribute( const Mesh& newMesh, const Mesh& oldMesh, const T& data,
 
     return newData;
 }
-
-//template<typename T>
-//T calcNewFaceAttribute( const Mesh& newMesh, const Mesh& oldMesh, const T& data, ProgressCallback progressCb )
-//{
-//    T newData;
-//    if ( !data.empty() )
-//    {
-//        newData.resize( newMesh->topology.lastValidFace() + 1 );
-//        BitSetParallelFor( newMesh->topology.lastValidFace(), [&] ( FaceId id )
-//        {
-//            auto res = findProjection( newMesh->points[id], *oldMesh ).mtp;
-//            VertId v1 = oldMesh->topology.org( res.e );
-//            VertId v2 = oldMesh->topology.dest( res.e );
-//            VertId v3 = oldMesh->topology.dest( oldMesh->topology.next( res.e ) );
-//            newData[id] = res.bary.interpolate( data[v1], data[v2], data[v3] );
-//        },
-//        progressCb );
-//    }
-//
-//    return newData;
-//}
 
 }

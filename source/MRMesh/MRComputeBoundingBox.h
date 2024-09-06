bf4ea126ffbfeac1a1b65dcaca7c453575e6fee3
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-09-06 16:01:28 +0300

    Generalize computeBoundingBox function (#3318)

--- source/MRMesh/MRComputeBoundingBox.h
+++ source/MRMesh/MRComputeBoundingBox.h
@@ -6,12 +6,33 @@
 namespace MR
 {
 
-/// passes through all region points and finds the minimal bounding box containing all of them;
+/// passes through all region points 
+/// 1) in the range [firstVert, lastVert)
+/// 2) corresponding to set bits in region (if provided)
+/// and finds the minimal bounding box containing all of them;
 /// if toWorld transformation is given then returns minimal bounding box in world space
 /// \ingroup MathGroup
 template<typename V>
-Box<V> computeBoundingBox( const Vector<V, VertId> & points, const VertBitSet & region, const AffineXf<V> * toWorld = nullptr );
+Box<V> computeBoundingBox( const Vector<V, VertId> & points, VertId firstVert, VertId lastVert, const VertBitSet* region = nullptr, const AffineXf<V>* toWorld = nullptr );
+
+/// passes through all region points corresponding to set bits in region (if provided)
+/// and finds the minimal bounding box containing all of them;
+/// if toWorld transformation is given then returns minimal bounding box in world space
+/// \ingroup MathGroup
 template<typename V>
-Box<V> computeBoundingBox( const Vector<V, VertId>& points, const VertBitSet* region = nullptr, const AffineXf<V>* toWorld = nullptr );
+inline Box<V> computeBoundingBox( const Vector<V, VertId> & points, const VertBitSet* region = nullptr, const AffineXf<V>* toWorld = nullptr )
+{
+    return computeBoundingBox( points, points.beginId(), points.endId(), region, toWorld );
+}
+
+/// passes through all region points corresponding to set bits in region
+/// and finds the minimal bounding box containing all of them;
+/// if toWorld transformation is given then returns minimal bounding box in world space
+/// \ingroup MathGroup
+template<typename V>
+inline Box<V> computeBoundingBox( const Vector<V, VertId> & points, const VertBitSet & region, const AffineXf<V> * toWorld = nullptr )
+{
+    return computeBoundingBox( points, points.beginId(), points.endId(), &region, toWorld );
+}
 
 } // namespace MR

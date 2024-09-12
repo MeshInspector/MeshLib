1721dabdafbd2decfcedef4f269899831ad1350e
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-09-12 14:11:25 +0300

    pointsToDistanceFunctionVolume (#3343)

--- source/MRVoxels/MRPointsToDistanceVolume.cpp
+++ source/MRVoxels/MRPointsToDistanceVolume.cpp
@@ -8,49 +8,46 @@
 #include "MRMesh/MRColor.h"
 #include "MRMesh/MRTimer.h"
 #include <limits>
+#include "MRMesh/MRIsNaN.h"
 
 namespace MR
 {
 
-Expected<SimpleVolume> pointsToDistanceVolume( const PointCloud & cloud, const PointsToDistanceVolumeParams& params )
+FunctionVolume pointsToDistanceFunctionVolume( const PointCloud & cloud, const PointsToDistanceVolumeParams& params )
 {
-    MR_TIMER
     assert( params.sigma > 0 );
     assert( params.minWeight > 0 );
     assert( params.ptNormals || cloud.hasNormals() );
 
-    const VertNormals& normals = params.ptNormals ? *params.ptNormals : cloud.normals;
-
-    SimpleVolume res;
-    res.voxelSize = params.voxelSize;
-    res.dims = params.dimensions;
-    VolumeIndexer indexer( res.dims );
-    res.data.resize( indexer.size(), std::numeric_limits<float>::quiet_NaN() );
-
-    const auto inv2SgSq = -0.5f / sqr( params.sigma );
-    if ( !ParallelFor( size_t( 0 ), indexer.size(), [&]( size_t i )
+    return FunctionVolume
     {
-        auto coord = Vector3f( indexer.toPos( VoxelId( i ) ) ) + Vector3f::diagonal( 0.5f );
-        auto voxelCenter = params.origin + mult( params.voxelSize, coord );
-
-        float sumDist = 0;
-        float sumWeight = 0;
-        findPointsInBall( cloud, voxelCenter, 3 * params.sigma, [&]( VertId v, const Vector3f& p )
+        .data = [&cloud, params, inv2SgSq = -0.5f / sqr( params.sigma ),
+            &normals = params.ptNormals ? *params.ptNormals : cloud.normals] ( const Vector3i& pos ) -> float
         {
-            const auto distSq = ( voxelCenter - p ).lengthSq();
-            const auto w = std::exp( distSq * inv2SgSq );
-            sumWeight += w;
-            sumDist += dot( normals[v], voxelCenter - p ) * w;
-        } );
+            auto coord = Vector3f( pos ) + Vector3f::diagonal( 0.5f );
+            auto voxelCenter = params.origin + mult( params.voxelSize, coord );
 
-        if ( sumWeight >= params.minWeight )
-            res.data[i] = sumDist / sumWeight;
-    }, params.cb ) )
-        return unexpectedOperationCanceled();
+            float sumDist = 0;
+            float sumWeight = 0;
+            findPointsInBall( cloud, voxelCenter, 3 * params.sigma, [&]( VertId v, const Vector3f& p )
+            {
+                const auto distSq = ( voxelCenter - p ).lengthSq();
+                const auto w = std::exp( distSq * inv2SgSq );
+                sumWeight += w;
+                sumDist += dot( normals[v], voxelCenter - p ) * w;
+            } );
 
-    res.max =  params.sigma * std::exp( -0.5f );
-    res.min = -res.max;
-    return res;
+            return sumWeight >= params.minWeight ? sumDist / sumWeight : cQuietNan;
+        },
+        .dims = params.dimensions,
+        .voxelSize = params.voxelSize
+    };
+}
+
+Expected<SimpleVolume> pointsToDistanceVolume( const PointCloud & cloud, const PointsToDistanceVolumeParams& params )
+{
+    MR_TIMER
+    return functionVolumeToSimpleVolume( pointsToDistanceFunctionVolume( cloud, params ), params.cb );
 }
 
 Expected<VertColors> calcAvgColors( const PointCloud & cloud, const VertColors & colors,

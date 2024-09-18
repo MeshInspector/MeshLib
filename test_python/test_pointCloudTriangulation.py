38e29e8c199463e184866e082a1cee57dae8ff05
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2024-09-18 15:20:39 +0300

    Add tests and expose normals creation

--- test_python/test_pointCloudTriangulation.py
+++ test_python/test_pointCloudTriangulation.py
@@ -12,3 +12,16 @@ def test_point_cloud_triangulation():
     assert len(restored.points.vec) == 1024
     assert restored.topology.getValidVerts().count() == 1024
     assert restored.topology.findHoleRepresentiveEdges().size() == 0
+
+def test_create_normals():
+    torusMesh = mrmesh.makeTorus(2, 1, 32, 32, None)
+    torusPointCloud = mrmesh.meshToPointCloud(torusMesh, False, None)
+    settings = mrmesh.TriangulationHelpersSettings()
+    settings.numNeis = 16
+
+    allLocal = mrmesh.buildUnitedLocalTriangulations(torusPointCloud, settings)
+    unoriented = mrmesh.makeUnorientedNormals(torusPointCloud,allLocal,None,mrmesh.OrientNormals.TowardOrigin)
+    oriented = mrmesh.makeOrientedNormals(torusPointCloud,allLocal)
+
+    assert unoriented.vec.size() == torusPointCloud.points.vec.size()
+    assert oriented.vec.size() == torusPointCloud.points.vec.size()

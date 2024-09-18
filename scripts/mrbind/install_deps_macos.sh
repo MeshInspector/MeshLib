a892213393019fc16d872c555927a32dfbe88863
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2024-09-18 08:38:09 -0500

    Deps script for macos.

---
+++ scripts/mrbind/install_deps_macos.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+# Installs everything needed to generate and build MRBind bindings.
+# We assume `brew` is already installed. Automatic its installation is too much,
+#   especially because of the conflicts that happen if several users install it.
+
+# Read the Clang version from `preferred_clang_version.txt`. `xargs` trims the whitespace.
+SCRIPT_DIR="$(realpath "$(dirname "$BASH_SOURCE")")"
+CLANG_VER="$(cat $SCRIPT_DIR/preferred_clang_version.txt | xargs)"
+[[ $CLANG_VER ]] || (echo "Not sure what version of Clang to use." && false)
+
+brew install make gawk llvm@$CLANG_VER

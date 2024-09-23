3b349532dbed8a03d613317d58ac6bd532f272d6
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2024-09-23 05:28:24 -0500

    Fix undefined reference?

--- scripts/mrbind/helpers.cpp
+++ scripts/mrbind/helpers.cpp
@@ -155,7 +155,7 @@ namespace MR::Extra
     }
 }
 
-#if !MR_PARSING_FOR_PB11_BINDINGS && MB_DEFINE_IMPLEMENTATION
+#if !MR_PARSING_FOR_PB11_BINDINGS
 namespace MR
 {
     // This is something the old `mrmeshpy` library exported, presumably to have something to link against, to help load the shared library. Hmm.

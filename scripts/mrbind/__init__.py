2419b228fb2333552fffea91bc5e5a94d8a72538
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2024-09-24 13:45:09 +0200

    Fix script

--- scripts/mrbind/__init__.py
+++ scripts/mrbind/__init__.py
@@ -77,7 +77,7 @@ def _override_resources_dir():
     import pathlib
     from . import mrmeshpy as mr
 
-    mr.SystemPath.overrideDirectory(mr.SystemPath.Directory.Resources, pathlib.Path(__file__).parent.resolve())
+    mr.SystemPath.overrideDirectory(mr.SystemPath_Directory.Resources, pathlib.Path(__file__).parent.resolve())
 
 _override_resources_dir()
 del _override_resources_dir

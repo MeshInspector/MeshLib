4b02bd7746928dcf60356f1cfe63c1df3e0320b8
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2024-09-24 13:23:33 +0200

    Update comments

--- source/MRVoxels/MRScalarConvert.h
+++ source/MRVoxels/MRScalarConvert.h
@@ -5,7 +5,7 @@
 namespace MR
 {
 
-/// ...
+/// scalar value's binary format type
 enum class ScalarType
 {
     UInt8,
@@ -23,7 +23,10 @@ enum class ScalarType
     Count
 };
 
-/// ...
+/// get a function to convert binary data of specified format type to a scalar value
+/// \param scalarType - binary format type
+/// \param range - (for integer types only) the range of possible values
+/// \param min - (for integer types only) the minimal value
 MRVOXELS_API std::function<float ( const char* )> getTypeConverter( ScalarType scalarType, uint64_t range, int64_t min );
 
 } // namespace MR
--- source/MRVoxels/MRScanHelpers.h
+++ source/MRVoxels/MRScanHelpers.h
@@ -10,30 +10,36 @@
 namespace MR
 {
 
-/// ...
+/// slice information
+/// \sa SliceInfo
 struct SliceInfoBase
 {
+    /// instance number
     int instanceNum = 0;
+    /// layer height
     double z = 0;
+    /// file index
     int fileNum = 0;
+
     auto operator <=>( const SliceInfoBase & ) const = default;
 };
 
-/// ...
+/// slice information
+/// these fields will be ignored in sorting
+/// \sa SliceInfoBase
 struct SliceInfo : SliceInfoBase
 {
-    // these fields will be ignored in sorting
+    /// image position
     Vector3d imagePos;
 };
 
-/// ...
+/// Sort scan files in given vector by given slice information
 MRVOXELS_API void sortScansByOrder( std::vector<std::filesystem::path>& scans, std::vector<SliceInfo>& zOrder );
 
-/// ...
+/// Read layer heights from given scan file names
 MRVOXELS_API void putScanFileNameInZ( const std::vector<std::filesystem::path>& scans, std::vector<SliceInfo>& zOrder );
 
-/// Sort files in given vector by names (respect numbers in it)
-/// usually needed for scans
+/// Sort scan files in given vector by names (respect numbers in it)
 MRVOXELS_API void sortScanFilesByName( std::vector<std::filesystem::path>& scans );
 
 } // namespace MR

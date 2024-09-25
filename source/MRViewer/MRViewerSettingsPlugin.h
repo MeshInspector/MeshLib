c390c82af83a5389a716fb785e05b8ae1b15c15b
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2024-09-26 00:04:27 +0200

    Fix Emscripten build

--- source/MRViewer/MRViewerSettingsPlugin.h
+++ source/MRViewer/MRViewerSettingsPlugin.h
@@ -100,7 +100,9 @@ private:
     int storedSamples_{ 0 };
     int maxSamples_{ 0 };
     bool needReset_{ false };
+#ifndef __EMSCRIPTEN__
     bool gpuOverridesMSAA_{ false };
+#endif
 
     Vector4f backgroundColor_;
 

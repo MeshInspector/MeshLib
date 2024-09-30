89e626d84ff9b21884902943f1738b85236977ec
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2024-09-30 17:47:41 +0300

    try .NET Framwork 4.7.1 target in MR::DotNet (#3344)
    * try other way nuget
    
    * no c# tests
    
    * enable c# tests
    
    * enable building of MRDotNetTest
    
    * filter dlls
    
    * Revert "filter dlls"
    
    This reverts commit 8980579be2c30dececd44fc1a6ac2d66577ed2df.
    
    * move native dlls to a separate folder & add .targets file
    
    * add targets fo net6.0 and net8.0
    
    * fix path to temp output
    
    * delete unnecessary version fo .net 6
    
    * Update documentation
    
    * fix syntax
    
    * fix a typo
    
    * add icon license and readme
    
    * change icon
    
    * rename project and fix typos
    
    * highlight link
    
    * revert change
    
    ---------
    
    Co-authored-by: Alex Koksin <alex.koksin@meshinspector.com>
    Co-authored-by: ADALISK\akoksin <akoksin@adalis.com>

--- scripts/generate_nuget_spec.py
+++ scripts/generate_nuget_spec.py
@@ -3,14 +3,24 @@ import shutil
 import sys
 
 base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..')
-path_to_objects = base_path
+path_to_objects = os.path.join( base_path, "source/x64/Release/" )
+print( path_to_objects )
 path_to_spec = os.path.join(base_path, 'Package.nuspec')
+path_to_targets = os.path.join(base_path, 'MeshLib.targets')
 print( path_to_spec )
 print('\n')
+
+path_to_license = os.path.join(base_path, 'LICENSE')
+shutil.copy(path_to_license, path_to_license + '.txt')
 
 excluded_modules = ['MRCommonPlugins', 'MRCuda', 'MRMeshC', 'MRViewer', 'MRMeshViewer', 'MRTest', 'MRTestC']
 path_to_copyright_header = os.path.join(os.path.dirname(os.path.abspath(__file__)),'copyright_header.txt')
 copyright_header = open(path_to_copyright_header,'r').read()[3:]
+
+fTargets = open(path_to_targets, 'w')
+fTargets.write('<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">\n')
+fTargets.write('\t<ItemGroup>\n')
+
 
 f = open(path_to_spec, 'w')
 f.write('<?xml version="1.0" encoding="utf-8"?>\n')
@@ -34,19 +44,57 @@ f.write('\t\t<copyright>')
 f.write(copyright_header)
 f.write('</copyright>\n')
 
+f.write('\t\t<dependencies>\n')
+f.write('\t\t\t<group targetFramework="net6.0"/>\n')
+f.write('\t\t\t<group targetFramework=".NETFramework4.7.1"/>\n')
+f.write('\t\t</dependencies>\n')
+
+f.write('\t\t<icon>images/MeshInspector_icon.png</icon>\n')
+f.write('\t\t<license type="file">LICENSE.txt</license>\n')
+f.write('\t\t<readme>docs/readme_dotnet.md</readme>\n')
 f.write('\t</metadata>\n')
 
 f.write('\t<files>\n')
-
+f.write('\t\t<file src="./macos/MeshInspector_icon.png" target="images/"></file>\n')
+f.write('\t\t<file src="./LICENSE.txt" target=""></file>\n')
+f.write('\t\t<file src="./readme_dotnet.md" target="docs/"></file>\n')
 folder = os.walk(path_to_objects)
 for address, dirs, files in folder:
 	for file in files:
-		if ((file.endswith('.dll') and not any(map(file.startswith, excluded_modules)))):
+		if file.startswith('nunit'):
+			continue
+            
+		if (file.endswith('.dll') and not any(map(file.startswith, excluded_modules)) and not file.startswith('System') and not file.startswith('MRDotNet')):
 			src = os.path.join(address,file)
 			print(src)
 			f.write('\t\t<file src="./source/x64/Release/')
 			f.write(file)
+			f.write('" target="runtimes/win-x64/native/"></file>\n')
+			fTargets.write('\t\t<None Include="$(MSBuildThisFileDirectory)\\..\\runtimes\\win-x64\\native\\')
+			fTargets.write(file)
+			fTargets.write('">\n')
+			fTargets.write('\t\t\t<Link>')
+			fTargets.write(file)
+			fTargets.write('</Link>\n')
+			fTargets.write('\t\t\t<CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>\n')
+			fTargets.write('\t\t</None>\n')
+		elif file == 'MRDotNet.dll':
+			src = os.path.join(address,file)
+			print(src)
+			f.write('\t\t<file src="./source/x64/Release/')
+			f.write(file)
+			f.write('" target="lib/net471/"></file>\n')
+		elif file == 'MRDotNet6.dll':
+			f.write('\t\t<file src="./source/x64/Release/')
+			f.write(file)
 			f.write('" target="lib/net6.0/"></file>\n')
+            
+fTargets.write('\t</ItemGroup>\n')
+fTargets.write('</Project>\n')
+fTargets.close()
+
+f.write('\t\t<file src="./MeshLib.targets" target="build/"></file>\n')
+
             
 f.write('\t</files>\n')
 f.write('</package>\n')

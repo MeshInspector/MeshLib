958986941f688d97938283636e4223d1f3f25dc2
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2024-10-01 12:28:46 +0200

    Fix

--- source/MRMesh/MRScopedValue.h
+++ source/MRMesh/MRScopedValue.h
@@ -3,5 +3,5 @@
 #include "MRFinally.h"
 
 /// change a variable's value until the current scope's end
-#define MR_SCOPED_VALUE( var, value ) \
-const auto MR_CONCAT( _prev_value_, __LINE__ ) = ( var ); ( var ) = ( value ); MR_FINALLY { ( var ) = MR_CONCAT( _prev_value_, __LINE__ ); };
+#define MR_SCOPED_VALUE( var, ... ) \
+const auto MR_CONCAT( _prev_value_, __LINE__ ) = ( var ); ( var ) = ( __VA_ARGS__ ); MR_FINALLY { ( var ) = MR_CONCAT( _prev_value_, __LINE__ ); }

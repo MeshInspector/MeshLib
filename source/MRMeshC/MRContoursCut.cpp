628c8aac42b8ab54d0c26b10104465fa6f63a42d
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2024-10-10 10:29:20 +0200

    WIP

---
+++ source/MRMeshC/MRContoursCut.cpp
@@ -0,0 +1,44 @@
+#include "MRContoursCut.h"
+
+#include "detail/TypeCast.h"
+#include "detail/Vector.h"
+
+#include "MRMesh/MRContoursCut.h"
+
+using namespace MR;
+
+REGISTER_AUTO_CAST( AffineXf3f )
+REGISTER_AUTO_CAST( ContinuousContours )
+REGISTER_AUTO_CAST( CoordinateConverters )
+REGISTER_AUTO_CAST( Mesh )
+REGISTER_AUTO_CAST( OneMeshContours )
+
+static_assert( sizeof( MROneMeshIntersection ) == sizeof( OneMeshIntersection ) );
+
+const MROneMeshContour mrOneMeshContoursGet( const MROneMeshContours* contours_, size_t index )
+{
+    ARG( contours );
+    const auto& result = contours[index];
+    return {
+        .intersections = cast_to<MRVectorOneMeshIntersection>( vector_ref_wrapper( result.intersections ) ),
+        .closed = result.closed,
+    };
+}
+
+size_t mrOneMeshContoursSize( const MROneMeshContours* contours_ )
+{
+    ARG( contours );
+    return contours.size();
+}
+
+void mrOneMeshContoursFree( MROneMeshContours* contours_ )
+{
+    ARG_PTR( contours );
+    delete contours;
+}
+
+MROneMeshContours* mrGetOneMeshIntersectionContours( const MRMesh* meshA_, const MRMesh* meshB_, const MRContinuousContours* contours_, bool getMeshAIntersections, const MRCoordinateConverters* converters_, const MRAffineXf3f* rigidB2A_ )
+{
+    ARG( meshA ); ARG( meshB ); ARG( contours ); ARG( converters ); ARG_PTR( rigidB2A );
+    RETURN_NEW( getOneMeshIntersectionContours( meshA, meshB, contours, getMeshAIntersections, converters, rigidB2A ) );
+}

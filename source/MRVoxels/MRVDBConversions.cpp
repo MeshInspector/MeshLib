d2f8ca204ea4900be220939ca1f05661d0bf2949
Andrey Aralov <andrey.aralov@meshinspector.com>
2024-10-14 12:08:38 +0200

    Change scaling method in `vdbVolumeToSimpleVolumeImpl`. (#3489)
    * Change scaling method in `vdbVolumeToSimpleVolumeImpl`.
    
    Previously, the interval `[min; max]` was rescaled to the interval of the target integer type, where `min` and `max` are the actual minimum and maximum values of the data. This means, that the real absolute values of voxels is lost (as it is spanned into full target type interval anyway).
    
    I propose to always scale `[0; 1]` to the target interval, which should better preserve the values of voxels.
    
    * fix spelling
    
    ---------
    
    Co-authored-by: Fedor Chelnokov <fedor.chelnokov@meshinspector.com>

--- source/MRVoxels/MRVDBConversions.cpp
+++ source/MRVoxels/MRVDBConversions.cpp
@@ -319,10 +319,10 @@ Expected<VoxelsVolumeMinMax<std::vector<T>>> vdbVolumeToSimpleVolumeImpl(
         res.min = 0;
         res.max = std::numeric_limits<T>::max();
     }
-    [[maybe_unused]] const float oMin = float( res.min );
-    [[maybe_unused]] const float oMax = float( res.max );
-    [[maybe_unused]] const float k =
-        vdbVolume.max > vdbVolume.min ? ( oMax - oMin ) / ( vdbVolume.max - vdbVolume.min ) : 0.0f;
+    [[maybe_unused]] const float targetMin = float( res.min );
+    [[maybe_unused]] const float targetMax = float( res.max );
+    // voxels are scaled to [0; 1] when loaded
+    [[maybe_unused]] const float k = targetMax - targetMin;
 
     VolumeIndexer indexer( res.dims );
     res.data.resize( indexer.size() );
@@ -342,7 +342,7 @@ Expected<VoxelsVolumeMinMax<std::vector<T>>> vdbVolumeToSimpleVolumeImpl(
         if constexpr ( isFloat && !Norm )
             res.data[i] = T( value );
         else
-            res.data[i] = T( std::clamp( ( value - vdbVolume.min ) * k + oMin, oMin, oMax ) );
+            res.data[i] = T( std::clamp( value * k + targetMin, targetMin, targetMax ) );
     }, cb ) )
         return unexpectedOperationCanceled();
     return res;

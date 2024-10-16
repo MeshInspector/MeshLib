4adc595673ccd2230927d6a7ccb3e1973c23a804
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-10-16 21:55:23 +0300

    MRCudaPointsToDistanceVolume.cu: fix warning

--- source/MRCuda/MRCudaPointsToDistanceVolume.cu
+++ source/MRCuda/MRCudaPointsToDistanceVolume.cu
@@ -88,9 +88,9 @@ namespace Cuda
         constexpr int maxThreadsPerBlock = 640;
         const size_t size = size_t( params.dimensions.x ) * params.dimensions.y * params.dimensions.z;
 
-        size_t numBlocks = ( size_t( size ) + maxThreadsPerBlock - 1) / maxThreadsPerBlock;
-        kernel << < numBlocks, maxThreadsPerBlock >> > ( nodes, points, normals, volume, params);
+        auto numBlocks = (unsigned int)( ( size_t( size ) + maxThreadsPerBlock - 1 ) / maxThreadsPerBlock );
+        kernel << < numBlocks, maxThreadsPerBlock >> > ( nodes, points, normals, volume, params );
         return ( cudaGetLastError() == cudaSuccess );
     }
-}
-}
+} // namespace Cuda
+} // namespace MR

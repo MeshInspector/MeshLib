d63ccac37ac0305da5d4048523da7c29a446f178
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-10-28 20:09:54 +0300

    more

--- source/MRViewer/MRObjectPointsHistory.cpp
+++ source/MRViewer/MRObjectPointsHistory.cpp
@@ -6,9 +6,6 @@
 #include <MRMesh/MRObjectPoints.h>
 #include <MRMesh/MRPointCloud.h>
 #include <MRMesh/MRTimer.h>
-#include <MRMesh/MRParallelFor.h>
-#include <MRMesh/MRBitSetParallelFor.h>
-#include <MRMesh/MRBuffer.h>
 
 namespace MR
 {
@@ -20,42 +17,27 @@ static void packPointsWithHistoryCore( const std::shared_ptr<ObjectPoints>& objP
     if ( !objPoints || !objPoints->pointCloud() )
         return;
 
-    Historian<ChangePointCloudAction> h( "set cloud", objPoints );
+    auto packed = pack( *objPoints, reorder, newValidVerts );
 
-    if ( newValidVerts )
     {
-        objPoints->varPointCloud()->validPoints = std::move( *newValidVerts );
-        objPoints->varPointCloud()->invalidateCaches();
+        Historian<ChangePointCloudAction> h( "set cloud", objPoints );
+        std::shared_ptr<PointCloud> tmp;
+        packed->swapPointCloud( tmp );    // tmp := packed
+        objPoints->swapPointCloud( tmp ); // objPoints := tmp
     }
 
-    const auto map = objPoints->varPointCloud()->pack( reorder );
-
-    if ( !objPoints->getVertsColorMap().empty() )
     {
         Historian<ChangeVertsColorMapAction> hCM( "color map update", objPoints );
-        VertColors newColors;
-        newColors.resizeNoInit( map.tsize );
-        const auto & oldColors = objPoints->getVertsColorMap();
-        ParallelFor( 0_v, map.b.endId(), [&] ( VertId oldv )
-        {
-            auto newv = map.b[oldv];
-            if ( !newv )
-                return;
-            newColors[newv] = oldColors[oldv];
-        } );
-        objPoints->setVertsColorMap( std::move( newColors ) );
+        VertColors tmp;
+        packed->updateVertsColorMap( tmp );    // tmp := packed
+        objPoints->updateVertsColorMap( tmp ); // objPoints := tmp
     }
 
-    // update points in the selection
-    const auto & oldSel = objPoints->getSelectedPoints();
-    if ( oldSel.any() )
     {
         Historian<ChangePointPointSelectionAction> hs( "selection", objPoints );
-        VertBitSet newSel( map.tsize );
-        for ( auto oldv : oldSel )
-            if ( auto newv = map.b[ oldv ] )
-                newSel.set( newv );
-        objPoints->selectPoints( std::move( newSel ) );
+        VertBitSet tmp;
+        packed->updateSelectedPoints( tmp );    // tmp := packed
+        objPoints->updateSelectedPoints( tmp ); // objPoints := tmp
     }
 }
 

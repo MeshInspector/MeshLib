3c0c7b0544b233315b2dacca199142a7ccecd44b
Egor Grebenchenko <egor.grebenchenko@meshinspector.com>
2024-10-30 20:08:12 +0300

    delete comment

--- source/MRVoxels/MRVDBConversions.cpp
+++ source/MRVoxels/MRVDBConversions.cpp
@@ -296,7 +296,6 @@ FloatGrid simpleVolumeToDenseGrid( const SimpleVolume& simpleVolume,
 VdbVolume simpleVolumeToVdbVolume( const SimpleVolumeMinMax& simpleVolume, ProgressCallback cb /*= {} */ )
 {
     VdbVolume res;
-    // use min value as background
     res.data = simpleVolumeToDenseGrid( simpleVolume, simpleVolume.min, cb );
     res.dims = simpleVolume.dims;
     res.voxelSize = simpleVolume.voxelSize;

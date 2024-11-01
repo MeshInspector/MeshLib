641a77d6beb06bdf2c9e1062d534a1f113fa7ef6
AntonChernobrovkin <anton.chernobrovkin@meshinspector.com>
2024-11-01 19:48:29 +0400

    Update python decimation sample

--- examples/python-examples/MeshDecimate.dox.py
+++ examples/python-examples/MeshDecimate.dox.py
@@ -8,9 +8,19 @@ import meshlib.mrmeshpy as mrmeshpy
 # Load mesh
 mesh = mrmeshpy.loadMesh("mesh.stl")
 
+# Repack mesh optimally.
+# It's not necessary but highly recommended to achieve the best performance in parallel processing
+mesh.packOptimally()
+
 # Setup decimate parameters
 settings = mrmeshpy.DecimateSettings()
-settings.maxError = 0.05
+# Decimation stop thresholds, you may specify one or both
+settings.maxDeletedFaces = 1000 # Number of faces to be deleted
+settings.maxError = 0.05 # Maximum error when decimation stops
+
+# Number of parts to simultaneous processing, greatly improves performance by cost of minor quality loss
+# Recommended to set to number of CPU cores available for the best performance
+settings.subdivideParts = 8
 
 # Decimate mesh
 mrmeshpy.decimateMesh(mesh, settings)

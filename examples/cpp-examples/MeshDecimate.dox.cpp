f05c736b193309035bc57ca598291da69eddbc1c
AntonChernobrovkin <anton.chernobrovkin@meshinspector.com>
2024-11-05 19:20:29 +0400

    Update cpp decimation sample

--- examples/cpp-examples/MeshDecimate.dox.cpp
+++ examples/cpp-examples/MeshDecimate.dox.cpp
@@ -8,20 +8,33 @@
 #include "MRMesh/MRMeshSave.h"
 #include "MRMesh/MRMesh.h"
 #include "MRMesh/MRMeshDecimate.h"
+#include "MRMesh/MRBuffer.h"
 
 int main()
 {
     // Load mesh
-    MR::Mesh mesh = *MR::MeshLoad::fromAnySupportedFormat( "mesh.stl" );
+    MR::Mesh mesh = *MR::MeshLoad::fromAnySupportedFormat("mesh.stl");
+
+    // Repack mesh optimally.
+    // It's not necessary but highly recommended to achieve the best performance in parallel processing
+    mesh.packOptimally();
+
 
     // Setup decimate parameters
     MR::DecimateSettings settings;
-    settings.maxError = 0.05f;
+
+    // Decimation stop thresholds, you may specify one or both
+    settings.maxDeletedFaces = 1000; // Number of faces to be deleted
+    settings.maxError = 0.05f; // Maximum error when decimation stops
+
+    // Number of parts to simultaneous processing, greatly improves performance by cost of minor quality loss.
+    // Recommended to set to number of CPU cores or more available for the best performance
+    settings.subdivideParts = 64;
 
     // Decimate mesh
-    MR::decimateMesh( mesh, settings );
+    MR::decimateMesh(mesh, settings);
 
     // Save result
-    MR::MeshSave::toAnySupportedFormat( mesh, "decimatedMesh.stl" );
+    MR::MeshSave::toAnySupportedFormat(mesh, "decimatedMesh.stl");
 }
 /// \endcode

53efd4caec0a64a6e7bb2130a5508a6cd98489d8
Alex Koksin <alex.koksin@meshinspector.com>
2024-11-07 16:21:05 +0400

    add comments

--- source/MRMeshC/MRMeshBuilder.h
+++ source/MRMeshC/MRMeshBuilder.h
@@ -4,9 +4,16 @@
 
 MR_EXTERN_C_BEGIN
 
+/// the function finds groups of mesh vertices located closer to each other than \param closeDist, and unites such vertices in one;
+/// then the mesh is rebuilt from the remaining triangles
+/// \param optionalVertOldToNew is the mapping of vertices: before -> after
+/// \param uniteOnlyBd if true then only boundary vertices can be united, all internal vertices (even close ones) will remain
+/// \return the number of vertices united, 0 means no change in the mesh
 MRMESHC_API int mrMeshBuilderUniteCloseVertices( MRMesh* mesh, float closeDist, bool uniteOnlyBd, MRVertMap* optionalVertOldToNew );
 
+/// creates new instance of mapping from old to new vertices
 MRMESHC_API MRVertMap mrMeshBuilderVertMapNew( void );
+/// deletes the mapping
 MRMESHC_API void mrMeshBuilderVertMapFree( MRVertMap* vertOldToNew );
 
 MR_EXTERN_C_END
--- source/MRMeshC/MRMeshComponents.h
+++ source/MRMeshC/MRMeshComponents.h
@@ -10,12 +10,15 @@ MR_EXTERN_C_BEGIN
 
 MR_VECTOR_LIKE_DECL( Face2RegionMap, RegionId )
 
+
+/// stores reference on Face2RegionMap ( key: face id, value: region id ) and number of components
 typedef struct MRMeshComponentsMap
 {
     MRFace2RegionMap* faceMap;
     int numComponents;
 } MRMeshComponentsMap;
 
+/// stores reference on bitset of faces and number of regions
 typedef struct MRMeshRegions
 {
     MRFaceBitSet* faces;
@@ -28,10 +31,17 @@ typedef enum MRFaceIncidence
     MRFaceIncidencePerVertex ///< face can have neighbor via vertex
 } MRFaceIncidence;
 
+/// gets all connected components of mesh part as
+/// 1. the mapping: FaceId -> Component ID in [0, 1, 2, ...)
+/// 2. the total number of components
 MRMESHC_API MRMeshComponentsMap mrMeshComponentsGetAllComponentsMap( const MRMeshPart* mp, MRFaceIncidence incidence );
 
+/// returns
+/// 1. the union of all regions with area >= minArea
+/// 2. the number of such regions
 MRMESHC_API MRMeshRegions mrMeshComponentsGetLargeByAreaRegions( const MRMeshPart* mp, const MRFace2RegionMap* face2RegionMap, int numRegions, float minArea );
 
+/// deletes allocated map
 MRMESHC_API void mrMeshComponentsAllComponentsMapFree( const MRMeshComponentsMap* map );
 
 MR_EXTERN_C_END

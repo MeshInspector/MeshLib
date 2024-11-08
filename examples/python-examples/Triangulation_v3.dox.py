e97327452736403484de42cbf69b17371b8d3fec
AntonChernobrovkin <anton.chernobrovkin@meshinspector.com>
2024-11-08 19:00:12 +0400

    Add test for Python samples.

---
+++ examples/python-examples/Triangulation_v3.dox.py
@@ -0,0 +1,34 @@
+## \page ExamplePythonTriangulationV3 Simple triangulation V3
+##
+## Simple triangulation
+##
+## \code
+from Demos.mmapfile_demo import offset
+from meshlib import mrmeshpy as mm
+from meshlib import mrmeshnumpy as mn
+import numpy as np
+
+u, v = np.mgrid[0:2 * np.pi:100j, 0:np.pi:100j]
+x = np.cos(u) * np.sin(v)
+y = np.sin(u) * np.sin(v)
+z = np.cos(v)
+
+# Prepare for MeshLib PointCloud
+verts = np.stack((x.flatten(), y.flatten(), z.flatten()), axis=-1).reshape(-1, 3)
+# Create MeshLib PointCloud from np ndarray
+pc = mn.pointCloudFromPoints(verts)
+# Remove duplicate points
+samplingSettings = mm.UniformSamplingSettings()
+samplingSettings.distance = 1e-3
+pc.validPoints = mm.pointUniformSampling(pc, samplingSettings)
+pc.invalidateCaches()
+
+# Triangulate it
+triangulated_pc = mm.triangulatePointCloud(pc)
+
+# Fix possible issues
+offsetParams = mm.OffsetParameters()
+offsetParams.voxelSize = 0.1
+triangulated_pc = mm.offsetMesh(triangulated_pc, 0.0, offsetParams)
+
+## \endcode

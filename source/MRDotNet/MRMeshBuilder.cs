562b0e6803027837354d87de4f11e594651182bf
Alex Koksin <alex.koksin@meshinspector.com>
2024-11-11 13:39:51 +0400

    add MeshComponents and MeshBuilder to C# (#3638)
    * introduce MeshComponents & UniteCloseVertices
    
    * add more tests and functions for MeshComponents
    
    * add comments
    
    * code review

---
+++ source/MRDotNet/MRMeshBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using static MR.DotNet.MeshComponents;
+
+namespace MR.DotNet
+{   
+    public class MeshBuilder
+    {
+        [DllImport("MRMeshC.dll", CharSet = CharSet.Auto)]
+        unsafe private static extern MRVertMap* mrVertMapNew();
+
+        [DllImport("MRMeshC.dll", CharSet = CharSet.Auto)]
+        unsafe private static extern void mrVertMapFree(MRVertMap* vertMap);
+
+        [DllImport("MRMeshC.dll", CharSet = CharSet.Auto)]
+        unsafe private static extern int mrMeshBuilderUniteCloseVertices(IntPtr mesh, float closeDist, bool uniteOnlyBd, MRVertMap* optionalVertOldToNew);
+
+        /// the function finds groups of mesh vertices located closer to each other than \param closeDist, and unites such vertices in one;
+        /// then the mesh is rebuilt from the remaining triangles
+        /// \param mesh is passed by ref. The function changes it
+        /// \param optionalVertOldToNew is the mapping of vertices: before -> after
+        /// \param uniteOnlyBd if true then only boundary vertices can be united, all internal vertices (even close ones) will remain
+        /// \return the number of vertices united, 0 means no change in the mesh
+        unsafe public static int UniteCloseVertices( ref Mesh mesh, float closeDist, bool uniteOnlyBd, List<VertId>? optionalVertOld2New = null )
+        {            
+            if ( optionalVertOld2New == null )
+                return mrMeshBuilderUniteCloseVertices(mesh.mesh_, closeDist, uniteOnlyBd, null);
+
+            MRVertMap* vertMap = mrVertMapNew();
+            var res = mrMeshBuilderUniteCloseVertices(mesh.mesh_, closeDist, uniteOnlyBd, vertMap);
+            optionalVertOld2New.Clear();
+
+            for (int i = 0; i < (int)vertMap->size; i++)
+            {
+                var vertId =  Marshal.ReadInt32(IntPtr.Add(vertMap->data, i * sizeof(int)));
+                optionalVertOld2New.Add(new VertId(vertId));
+            }
+
+            mrVertMapFree(vertMap);
+            return res;         
+        }
+    }
+}
---
+++ source/MRDotNetTest/MeshComponents.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+
+namespace MR.DotNet.Test
+{
+    [TestFixture]
+    public class MeshComponentsTests
+    {
+
+        static MeshPart CreateMesh()
+        {
+            var bigCube = Mesh.MakeCube(Vector3f.Diagonal(1), Vector3f.Diagonal(-0.5f));
+            var smallCube = Mesh.MakeCube(Vector3f.Diagonal(0.1f), Vector3f.Diagonal(1.0f));
+            var boolResult = MeshBoolean.Boolean(bigCube, smallCube, BooleanOperation.Union);
+            return new MeshPart(boolResult.mesh);
+        }
+
+        [Test]
+        public void TestComponentsMap()
+        {
+            var mp = CreateMesh();
+            var map = MeshComponents.GetAllComponentsMap(mp, FaceIncidence.PerEdge);
+
+            Assert.That(map.NumComponents == 2);
+            Assert.That(map.Count == 24);
+            Assert.That(map[0].Id == 0);
+            Assert.That(map[12].Id == 1);
+        }
+
+        [Test]
+        public void TestLargeRegions()
+        {
+            var mp = CreateMesh();
+            var map = MeshComponents.GetAllComponentsMap(mp, FaceIncidence.PerEdge);
+            var res = MeshComponents.GetLargeByAreaRegions(mp, map, map.NumComponents, 0.1f);
+
+            Assert.That(res.numRegions == 1);
+            Assert.That(res.faces.Test(0));
+            Assert.That(!res.faces.Test(12));
+        }
+
+        [Test]
+        public void TestLargestComponent()
+        {
+            var mp = CreateMesh();
+            int numSmallerComponents = 0;
+            var components = MeshComponents.GetLargestComponent(mp, FaceIncidence.PerEdge, 0.1f, out numSmallerComponents);
+            Assert.That(numSmallerComponents == 1);
+            Assert.That(components.Test(0));
+            Assert.That(!components.Test(12));
+        }
+
+        [Test]
+        public void TestLargeComponents()
+        {
+            var mp = CreateMesh();
+            var components = MeshComponents.GetLargeByAreaComponents(mp, 0.1f);
+            Assert.That(components.Test(0));
+            Assert.That(!components.Test(12));
+        }
+
+        [Test]
+        public void TestComponent()
+        {
+            var mp = CreateMesh();
+            var component = MeshComponents.GetComponent(mp, new FaceId(12), FaceIncidence.PerEdge);
+            Assert.That(!component.Test(0));
+            Assert.That(component.Test(12));
+        }
+    }
+}

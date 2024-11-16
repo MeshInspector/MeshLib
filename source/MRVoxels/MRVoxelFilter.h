232485549dcbcec87e27479e27aca1edb1805853
Andrey Aralov <andrey.aralov@meshinspector.com>
2024-11-16 18:10:49 +0100

    Add comment.

--- source/MRVoxels/MRVoxelFilter.h
+++ source/MRVoxels/MRVoxelFilter.h
@@ -13,6 +13,9 @@ enum class VoxelFilterType : int
     Gaussian
 };
 
+/// Performs voxels filtering.
+/// @param type Type of fitler
+/// @param width Width of the filtering window, must be an odd number greater or equal to 1.
 MRVOXELS_API VdbVolume voxelFilter( const VdbVolume& volume, VoxelFilterType type, int width );
 
 }

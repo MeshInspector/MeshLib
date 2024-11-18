5b0abbeadf9a56a525a127eac74cde59eb04d743
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-11-18 19:34:48 +0300

    MRSurroundingContour.h: more comments

--- source/MRMesh/MRSurroundingContour.h
+++ source/MRMesh/MRSurroundingContour.h
@@ -9,10 +9,13 @@ namespace MR
 {
 
 /**
- * \brief Find the best closed edge loop passing through given edges, which minimizes the sum of given edge metric
+ * \brief Find the best closed edge loop passing through given edges, which minimizes the sum of given edge metric.
+ * The algorithm assumes that input edges can be projected on the plane orthogonal to given direction,
+ * then the center point of all input edges is found, and each segment of the searched loop is within infinite pie sector
+ * with this center and the borders passing via two sorted input edges.
  * \ingroup MeshSegmentationGroup
  * 
- * \param includeEdges contain all edges that must be present in the returned loop, probably with reversed direction (should have at least 2 elements)
+ * \param includeEdges contain all edges in arbitrary order that must be present in the returned loop, probably with reversed direction (should have at least 2 elements)
  * \param edgeMetric returned loop will minimize the sum of this metric
  * \param dir direction approximately orthogonal to the loop
  */
@@ -24,10 +27,13 @@ namespace MR
 );
 
 /**
- * \brief Find the best closed edge loop passing through given vertices, which minimizes the sum of given edge metric
+ * \brief Find the best closed edge loop passing through given vertices, which minimizes the sum of given edge metric.
+ * The algorithm assumes that input vertices can be projected on the plane orthogonal to given direction,
+ * then the center point of all input vertices is found, and each segment of the searched loop is within infinite pie sector
+ * with this center and the borders passing via two sorted input vertices.
  * \ingroup MeshSegmentationGroup
  * 
- * \param keyVertices contain all vertices that returned loop must pass (should have at least 2 elements)
+ * \param keyVertices contain all vertices in arbitrary order that returned loop must pass (should have at least 2 elements)
  * \param edgeMetric returned loop will minimize the sum of this metric
  * \param dir direction approximately orthogonal to the loop
  */

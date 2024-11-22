52a6b0bacbd9503c34b4417540d18f840d2070a9
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2024-11-22 12:48:01 +0300

    introduce calcLength function (#3732)
    * intorduce calcLength function
    
    * fix build

--- source/MRMesh/MRContour.cpp
+++ source/MRMesh/MRContour.cpp
@@ -33,4 +33,33 @@ TEST( MRMesh, calcOrientedArea )
     EXPECT_NEAR( area3d.z, 0.5, 1e-12 );
 }
 
+TEST( MRMesh, calcLength )
+{
+    Contour2f contour2 = {
+        { 0, 0 },
+        { 1, 0 },
+        { 1, 1 },
+        { 0, 1 },
+        { 0, 0 }
+    };
+    auto length2 = calcLength( contour2 );
+    EXPECT_NEAR( length2, 4.0f, 1e-6f );
+
+    auto length2d = calcLength<Vector2f, double>( contour2 );
+    EXPECT_NEAR( length2d, 4.0, 1e-12 );
+
+    Contour3f contour3 = {
+        { 0, 0, 0 },
+        { 1, 0, 0 },
+        { 1, 1, 0 },
+        { 0, 1, 0 },
+        { 0, 0, 0 }
+    };
+    auto length3 = calcLength( contour3 );
+    EXPECT_NEAR( length3, 4.0f, 1e-6f );
+
+    auto length3d = calcLength<Vector3f, double>( contour3 );
+    EXPECT_NEAR( length3d, 4.0, 1e-12 );
+}
+
 } //namespace MR

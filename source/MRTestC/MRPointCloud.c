84625d809d3f5230f5a0e5cb24f528236055f9ed
Alex Koksin <alex.koksin@meshinspector.com>
2024-11-25 17:15:16 +0400

    add MeshToPointCloud and update test

--- source/MRTestC/MRPointCloud.c
+++ source/MRTestC/MRPointCloud.c
@@ -8,46 +8,31 @@
 #include "MRMeshC/MRPointCloud.h"
 #include "MRMeshC/MRPointCloudTriangulation.h"
 #include "MRMeshC/MRVector3.h"
-
-MRPointCloud* makeCube( void )
-{
-    MRPointCloud* pc = mrPointCloudNew();
-    MRVector3f point = mrVector3fDiagonal( 0.0f );
-    mrPointCloudAddPoint( pc, &point);
-
-    point.x = 1.0f; point.y = 0.0f; point.z = 0.0f;
-    mrPointCloudAddPoint( pc, &point );
-
-    point.x = 0.0f; point.y = 1.0f; point.z = 0.0f;
-    mrPointCloudAddPoint( pc, &point );
-
-    point.x = 1.0f; point.y = 1.0f; point.z = 0.0f;
-    mrPointCloudAddPoint( pc, &point );
-
-    point.x = 0.0f; point.y = 0.0f; point.z = 1.0f;
-    mrPointCloudAddPoint( pc, &point );
-
-    point.x = 1.0f; point.y = 0.0f; point.z = 1.0f;
-    mrPointCloudAddPoint( pc, &point );
-
-    point.x = 0.0f; point.y = 1.0f; point.z = 1.0f;
-    mrPointCloudAddPoint( pc, &point );
-
-    point.x = 1.0f; point.y = 1.0f; point.z = 1.0f;
-    mrPointCloudAddPoint( pc, &point );
-
-    return pc;
-}
+#include "MRMeshC/MRTorus.h"
+#include "MRMeshC/MRMeshToPointCloud.h"
 
 void testTriangulation( void )
 {
-    MRPointCloud* pc = makeCube();
-    MRMesh* mesh = mrTriangulatePointCloud( pc, NULL );
-    const size_t pointsCount = mrMeshPointsNum( mesh );
-    TEST_ASSERT( pointsCount == 8 );
-    const MRFaceBitSet* faces = mrMeshTopologyGetValidFaces( mrMeshTopology( mesh ) );
-    const size_t count = mrBitSetCount( (MRBitSet*)faces );
-    TEST_ASSERT( count > 0 );
+    MRMakeTorusParameters torusParams;
+    torusParams.primaryRadius = 2.f;
+    torusParams.secondaryRadius = 1.0f;
+    torusParams.primaryResolution = 32;
+    torusParams.secondaryResolution = 32;
+
+    MRMesh* mesh = mrMakeTorus( &torusParams );
+
+    MRPointCloud* pc = mrMeshToPointCloud( mesh, true, NULL );
+    const MRTriangulationParameters params = mrTriangulationParametersNew();
+    MRMesh* restored = mrTriangulatePointCloud( pc, &params );
+    TEST_ASSERT( mrMeshPointsNum( restored ) == 1024 );
+    const MRMeshTopology* top = mrMeshTopology( restored );
+    TEST_ASSERT( mrBitSetCount( ( MRBitSet* )mrMeshTopologyGetValidVerts( top ) ) == 1024 );
+    MREdgePath* holes = mrMeshTopologyFindHoleRepresentiveEdges( top );
+    
+    TEST_ASSERT( holes->size == 0 );
+
+    mrEdgePathFree( holes );
     mrMeshFree( mesh );
+    mrMeshFree( restored );
     mrPointCloudFree( pc );
 }

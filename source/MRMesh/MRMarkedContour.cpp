0f5d387b19cac729c8e36d7404b85a3b13f24b68
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-11-26 17:02:08 +0300

    support for closed splines

--- source/MRMesh/MRMarkedContour.cpp
+++ source/MRMesh/MRMarkedContour.cpp
@@ -97,16 +97,20 @@ MarkedContour3f makeSpline( MarkedContour3f mc, float markStability )
     if ( mc.contour.empty() )
         return mc;
     assert( firstLastMarked( mc ) );
+    const bool closed = mc.contour.size() > 1 && mc.contour.front() == mc.contour.back();
 
     const auto sz = mc.contour.size();
     const auto mz = mc.marks.count();
 
     std::vector<Eigen::Triplet<double>> mTriplets;
-    mTriplets.reserve( 3 * ( sz - 2 ) + mz );
+    const auto numVars = closed ? sz - 1 : sz;
+    const auto numEqs = sz + mz - 2; // closed has one more smooth middle equation and one less marked point equation
+    const auto nonZeros = closed ? 3 * ( sz - 1 ) + mz - 1 : 3 * ( sz - 2 ) + mz;
+    mTriplets.reserve( nonZeros );
 
     Eigen::VectorXd r[3];
     for ( int i = 0; i < 3; ++i )
-        r[i].resize( sz - 2 + mz );
+        r[i].resize( numEqs );
     auto setR = [&r]( int row, Vector3f p )
     {
         r[0][row] = p.x;
@@ -115,28 +119,43 @@ MarkedContour3f makeSpline( MarkedContour3f mc, float markStability )
     };
 
     // Smoothness at middle points
-    for ( int i = 0; i + 2 < sz; ++i )
+    for ( int i = 0; i + 1 < sz; ++i )
         r[0][i] = r[1][i] = r[2][i] = 0;
 
-    for ( int i = 0; i + 2 < sz; ++i )
+    int nextRow = 0;
+    for ( ; nextRow + 2 + closed < sz; ++nextRow )
     {
-        mTriplets.emplace_back( i, i    , -0.5 );
-        mTriplets.emplace_back( i, i + 1,  1.0 );
-        mTriplets.emplace_back( i, i + 2, -0.5 );
+        mTriplets.emplace_back( nextRow, nextRow    , -0.5 );
+        mTriplets.emplace_back( nextRow, nextRow + 1,  1.0 );
+        mTriplets.emplace_back( nextRow, nextRow + 2, -0.5 );
+    }
+    if ( closed )
+    {
+        mTriplets.emplace_back( nextRow, int( sz - 3 ), -0.5 );
+        mTriplets.emplace_back( nextRow, int( sz - 2 ),  1.0 );
+        mTriplets.emplace_back( nextRow, 0,             -0.5 );
+        ++nextRow;
+
+        mTriplets.emplace_back( nextRow, int( sz - 2 ), -0.5 );
+        mTriplets.emplace_back( nextRow, 0,              1.0 );
+        mTriplets.emplace_back( nextRow, 1,             -0.5 );
+        ++nextRow;
     }
 
     // Stabilization mc marked points
-    int nextRow = int( sz - 2 );
     for ( auto i : mc.marks )
     {
+        if ( closed && i + 1 == sz )
+            break;
         mTriplets.emplace_back( nextRow, int( i ), markStability );
         setR( nextRow, markStability * mc.contour[i] );
         ++nextRow;
     }
+    assert( nextRow == numEqs );
+    assert( mTriplets.size() == nonZeros );
 
     Eigen::SparseMatrix<double,Eigen::RowMajor> C;
-    C.resize( sz - 2 + mz, sz );
-    assert( mTriplets.size() == 3 * ( sz - 2 ) + mz );
+    C.resize( numEqs, numVars );
     C.setFromTriplets( mTriplets.begin(), mTriplets.end() );
 
     // minimum squares solution:
@@ -154,8 +173,10 @@ MarkedContour3f makeSpline( MarkedContour3f mc, float markStability )
     x[2] = chol.solve( b[2] );
 
     // produce output
-    for ( int i = 0; i < sz; ++i )
+    for ( int i = 0; i < numVars; ++i )
         mc.contour[i] = Vector3f( (float)x[0][i], (float)x[1][i], (float)x[2][i] );
+    if ( closed )
+        mc.contour.back() = mc.contour.front();
     return mc;
 }
 
@@ -175,6 +196,23 @@ TEST(MRMesh, MarkedContour)
     EXPECT_EQ( rc.contour.size(), 4 );
 
     auto spline = makeSpline( rc );
+    EXPECT_EQ( spline.contour.size(), 4 );
+}
+
+TEST(MRMesh, MakeClosedSpline)
+{
+    Contour3f c{
+        Vector3f{ 0, 0, 0 },
+        Vector3f{ 1, 0, 0 },
+        Vector3f{ 1, 1, 0 },
+        Vector3f{ 0, 1, 0 },
+        Vector3f{ 0, 0, 0 }
+    };
+    SplineSettings s{ .samplingStep = 0.4f };
+    auto spline = makeSpline( c, s );
+    EXPECT_EQ( spline.contour.size(), 13 );
+    EXPECT_EQ( spline.contour.front(), spline.contour.back() );
+    EXPECT_EQ( spline.marks.count(), 5 );
 }
 
 } //namespace MR

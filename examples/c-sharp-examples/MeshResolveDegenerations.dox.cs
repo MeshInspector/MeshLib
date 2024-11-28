6bd285c42c4e8f224f3f973b929dfb22bf143610
Alex Koksin <alex.koksin@meshinspector.com>
2024-11-28 14:16:01 +0400

    add ResolveMeshDegenerations func and example

---
+++ examples/c-sharp-examples/MeshResolveDegenerations.dox.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using static MR.DotNet;
+
+public static class MeshResolveDegenerationsExample
+{
+    public static void Run(string[] args)
+    {
+        try
+        {
+            if (args.Length != 2 && args.Length != 3)
+            {
+                Console.WriteLine("Usage: {0} MeshResolveDegenerationsExample INPUT [OUTPUT]", Assembly.GetExecutingAssembly().GetName().Name);
+                return;
+            }
+
+            string inputFile = args[1];
+            string outputFile = args.Length == 3 ? args[2] : inputFile;
+
+            var mesh = MeshLoad.FromAnySupportedFormat(inputFile);
+            var parameters = new ResolveMeshDegenParameters();
+            parameters.maxDeviation = mesh.BoundingBox.Diagonal() * 1e-5f;
+            parameters.tinyEdgeLength = 1e-3f;
+
+            if (!ResolveMeshDegenerations(ref mesh, parameters))
+            {
+                Console.WriteLine("No changes were made");
+            }
+            else
+            {
+                Console.WriteLine("Degenerations resolved");
+                MeshSave.ToAnySupportedFormat(mesh, outputFile);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: {0}", e.Message);
+        }
+    }
+}

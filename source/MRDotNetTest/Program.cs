330ea7aa0e74c5b215e05f9ed45f015f4863c6f0
Alex Koksin <alex.koksin@meshinspector.com>
2024-12-02 14:37:18 +0400

    Fix c# unit tests (#3789)

--- source/MRDotNetTest/Program.cs
+++ source/MRDotNetTest/Program.cs
@@ -5,10 +5,10 @@ namespace MR.Test
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             Console.WriteLine("Starting tests...");
-            new AutoRun().Execute(args);
+            return new AutoRun().Execute(args);
         }
     }
 }

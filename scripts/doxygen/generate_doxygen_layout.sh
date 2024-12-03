fbc9959a54ac154eb9cef459b17d6beb6f39213c
Alexandr Burakov <alexandr.burakov@meshinspector.com>
2024-12-03 11:45:41 +0300

    fix doxyfile generation script

--- scripts/doxygen/generate_doxygen_layout.sh
+++ scripts/doxygen/generate_doxygen_layout.sh
@@ -14,19 +14,19 @@ else
     BEGIN_URL="https://${2}"
 fi
 
-sed -e "s|BEGIN_URL|${BEGIN_URL}/${1}/html/|" -e "s|END_URL|${END_URL}|" ${DOXYGEN_DIR}/layout_templates/base_struct.xml > ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+sed -e "s|BEGIN_URL|${BEGIN_URL}/Main/html/|" -e "s|END_URL|${END_URL}|" ${DOXYGEN_DIR}/layout_templates/base_struct.xml > ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 
 if [ "$1" != "Cpp" ]; then
-    sed -e "s|\s*API_CPP_PAGE|      <tab type=\"user\" url=\"${BEGIN_URL}/Cpp/html/APICppPage.html\" title=\"C++\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "s|      <!-- API_CPP_PAGE -->|      <tab type=\"user\" url=\"${BEGIN_URL}/Cpp/html/APICppPage.html\" title=\"C++\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 else
-    sed -e "s|\s*API_CPP_PAGE|$(cat ${DOXYGEN_DIR}/layout_templates/API_part.xml)|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "/      <!-- API_CPP_PAGE -->/r ${DOXYGEN_DIR}/layout_templates/API_part.xml" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
     sed -e "s|API_PAGE_URL|${BEGIN_URL}/Cpp/html/APICppPage.html|" -e "s|API_PAGE_NAME|C++|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 fi
 
 if [ "$1" != "Py" ]; then
-    sed -e "s|\s*API_PY_PAGE|      <tab type=\"user\" url=\"${BEGIN_URL}/Py/html/APIPyPage.html\" title=\"Python\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "s|      <!-- API_PY_PAGE -->|      <tab type=\"user\" url=\"${BEGIN_URL}/Py/html/APIPyPage.html\" title=\"Python\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 else
-    sed -e "s|\s*API_PY_PAGE|$(cat ${DOXYGEN_DIR}/layout_templates/API_part.xml)|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "/      <!-- API_PY_PAGE -->/r ${DOXYGEN_DIR}/layout_templates/API_part.xml" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
     sed -e "s|API_PAGE_URL|${BEGIN_URL}/Py/html/APIPyPage.html|" -e "s|API_PAGE_NAME|Python|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 fi
 

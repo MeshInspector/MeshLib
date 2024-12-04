6e79f35b81ceae5c3aab7c5bb889f1a709b29a8e
Anton Chernobrovkin <anton.chernobrovkin@meshinspector.com>
2024-12-04 15:17:24 +0400

    Add c# sample path configuration

--- test_regression/conftest.py
+++ test_regression/conftest.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+import pytest
+
 def pytest_addoption(parser):
     parser.addoption(
         "--run-c-sharp-samples",
@@ -5,3 +8,23 @@ def pytest_addoption(parser):
         default=False,
         help="Run tests for c# samples",
     )
+    parser.addoption(
+        "--csharp-sample-dir",
+        action="store",
+        help="Directory with c# sample",
+        type=str,
+        default=None
+    )
+
+@pytest.fixture
+def csharp_sample_dir(request):
+    if request.config.getoption("--csharp-sample-dir"):
+        try:
+            sample_exec_dir = Path(request.config.getoption("--csharp-sample-dir"))
+        except Exception as e:
+            print(e)
+            raise Exception("Invalid --csharp-sample-dir")
+    else:
+        sample_exec_dir = Path("..\\source\\x64\\Release")
+        print(f"WARNING: using default path for C# sample: {sample_exec_dir}")
+    yield sample_exec_dir

f52a2bd17ade462641dd7b33fd18506f45f0c94f
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-12-13 14:17:26 +0300

    Open File adds object type in undo name

--- source/MRViewer/MRViewer.cpp
+++ source/MRViewer/MRViewer.cpp
@@ -1207,10 +1207,14 @@ bool Viewer::loadFiles( const std::vector<std::filesystem::path>& filesList, con
             const bool wasEmptyScene = SceneRoot::get().children().empty();
             const bool wasEmptyUndo = globalHistoryStore_ && globalHistoryStore_->getStackPointer() == 0;
 
+            std::string undoName = options.undoPrefix + commonFilesName( result.loadedFiles );
+            if ( result.scene->children().size() == 1 )
+                undoName += " as " + result.scene->children().front()->getClassName();
+
             if ( options.forceReplaceScene || ( result.loadedFiles.size() == 1 && ( !result.isSceneConstructed || wasEmptyScene ) ) )
             {
                 // the scene is taken as is from a single file, replace the current scene with it
-                AppendHistory<SwapRootAction>( options.undoPrefix + commonFilesName( result.loadedFiles ) );
+                AppendHistory<SwapRootAction>( undoName );
                 auto newRoot = result.scene;
                 std::swap( newRoot, SceneRoot::getSharedPtr() );
                 setSceneDirty();
@@ -1222,7 +1226,7 @@ bool Viewer::loadFiles( const std::vector<std::filesystem::path>& filesList, con
                 for ( const auto& file : result.loadedFiles )
                     recentFilesStore().storeFile( file );
 
-                SCOPED_HISTORY( options.undoPrefix + commonFilesName( result.loadedFiles ) );
+                SCOPED_HISTORY( undoName );
 
                 const auto children = result.scene->children();
                 result.scene->removeAllChildren();

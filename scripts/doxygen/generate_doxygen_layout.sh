f53cf209f13812d18d9493a59cb8173942787819
Alexandr Burakov <alexandr.burakov@meshinspector.com>
2024-12-13 10:45:42 +0300

    fix link to main page in docs

--- scripts/doxygen/generate_doxygen_layout.sh
+++ scripts/doxygen/generate_doxygen_layout.sh
@@ -8,17 +8,20 @@ DOXYGEN_DIR=$(readlink -f "${SCRIPTPATH}/../../doxygen")
 BEGIN_URL=""
 END_URL=".html"
 
-if [ "$2" == "127.0.0.1:8000/MeshLib/local" ]; then
+if [ "$2" = "127.0.0.1:8000/MeshLib/local" ]; then
     BEGIN_URL="http://${2}"
 else
     BEGIN_URL="https://${2}"
 fi
 
 # edit main block
+cp ${DOXYGEN_DIR}/layout_templates/base_struct.xml ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 if [ "$1" = "Main" ]; then
-    sed -e "s|__BEGIN_URL__|@ref |" -e "s|__END_URL__||" ${DOXYGEN_DIR}/layout_templates/base_struct.xml > ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "s|__MAIN_PAGE_TAB__|<tab type=\"mainpage\" visible=\"yes\" title=\"About\g"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "s|__BEGIN_URL__|@ref |" -e "s|__END_URL__||" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 else
-    sed -e "s|__BEGIN_URL__|${BEGIN_URL}/html/|" -e "s|__END_URL__|${END_URL}|" ${DOXYGEN_DIR}/layout_templates/base_struct.xml > ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "s|__MAIN_PAGE_TAB__|<tab type=\"user\" url=\"${BEGIN_URL}/html/index.html\" title=\"About\"/>|"  -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "s|__BEGIN_URL__|${BEGIN_URL}/html/|" -e "s|__END_URL__|${END_URL}|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 fi
 
 # edit C++ API block

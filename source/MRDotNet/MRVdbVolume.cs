454094d95eab8291c254352be8a4333179fd1f67
Alex Koksin <alex.koksin@meshinspector.com>
2024-12-16 17:56:46 +0400

    Add voxel resampling and accessorc to c# (#3877)

--- source/MRDotNet/MRVdbVolume.cs
+++ source/MRDotNet/MRVdbVolume.cs
@@ -4,11 +4,31 @@ using static MR.DotNet.Vector3i;
 using static MR.DotNet.Vector3f;
 using static MR.DotNet.VdbVolume;
 using System.Runtime.InteropServices;
+using static MR.DotNet.Box3f;
+using static MR.DotNet.Box3i;
 
 namespace MR
 {
     public partial class DotNet
-    {
+    {        
+        [DllImport("MRMeshC.dll", CharSet = CharSet.Ansi)]
+        private static extern IntPtr mrFloatGridResampledUniformly( IntPtr grid, float voxelScale, IntPtr cb );
+
+        [DllImport("MRMeshC.dll", CharSet = CharSet.Ansi)]
+        private static extern IntPtr mrFloatGridResampled( IntPtr grid, ref MRVector3f voxelScale, IntPtr cb );
+
+        [DllImport("MRMeshC.dll", CharSet = CharSet.Ansi)]
+        private static extern IntPtr mrFloatGridCropped( IntPtr grid, ref MRBox3i box, IntPtr cb );
+
+        [DllImport("MRMeshC.dll", CharSet = CharSet.Ansi)]
+        private static extern float mrFloatGridGetValue( IntPtr grid, ref MRVector3i p );
+
+        [DllImport("MRMeshC.dll", CharSet = CharSet.Ansi)]
+        private static extern void mrFloatGridSetValue(IntPtr grid, ref MRVector3i p, float value );
+
+        [DllImport("MRMeshC.dll", CharSet = CharSet.Ansi)]
+        private static extern void mrFloatGridSetValueForRegion(IntPtr grid, IntPtr region, float value );
+
         /// stores a pointer to a native OpenVDB object
         public class FloatGrid
         {
@@ -18,6 +38,34 @@ namespace MR
                 this.mrFloatGrid = mrFloatGrid;
             }
         }
+        
+        /// resample this grid with voxel size uniformly scaled by voxelScale
+        public static FloatGrid Resampled(FloatGrid grid, float voxelScale)
+        {
+            return new FloatGrid(mrFloatGridResampledUniformly(grid.mrFloatGrid, voxelScale, IntPtr.Zero));
+        }
+
+        /// resample this grid with voxel size scaled by voxelScale in each dimension
+        public static FloatGrid Resampled(FloatGrid grid, Vector3f voxelScale)
+        {
+            return new FloatGrid(mrFloatGridResampled(grid.mrFloatGrid, ref voxelScale.vec_, IntPtr.Zero));
+        }
+
+        /// returns cropped grid
+        public static FloatGrid Cropped(FloatGrid grid, Box3i box)
+        {
+            return new FloatGrid(mrFloatGridCropped(grid.mrFloatGrid, ref box.boxRef(), IntPtr.Zero));
+        }
+
+        /// returns the value at given voxel
+        public static float GetValue(FloatGrid grid, Vector3i p) => mrFloatGridGetValue(grid.mrFloatGrid, ref p.vec_);
+
+        /// sets given voxel
+        public static void SetValue(FloatGrid grid, Vector3i p, float value) => mrFloatGridSetValue(grid.mrFloatGrid, ref p.vec_, value);
+        
+        /// sets given region voxels value
+        /// \note region is in grid space (0 voxel id is minimum active voxel in grid)
+        public static void SetValue(FloatGrid grid, VoxelBitSet region, float value) => mrFloatGridSetValueForRegion(grid.mrFloatGrid, region.bs_, value);
         
         /// represents a box in 3D space subdivided on voxels stored in data;
         /// and stores minimum and maximum values among all valid voxels

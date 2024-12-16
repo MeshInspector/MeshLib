8cf298ddb206c9abf58454a9d47061b5a928b395
Anton Chernobrovkin <anton.chernobrovkin@meshinspector.com>
2024-12-16 19:18:07 +0400

    Add offset cuda sample

--- examples/python-examples/CudaOffset.dox.py
+++ examples/python-examples/CudaOffset.dox.py
@@ -4,9 +4,9 @@ from meshlib import mrcudapy as mc
 
 mesh = mlib.loadMesh("mesh.stl")
 offset_params = mlib.GeneralOffsetParameters()
-offset_params.signDetectionMode = mlib.SignDetectionMode.WindingRule
-offset_params.fwn = mc.FastWindingNumber(mesh)
+offset_params.signDetectionMode = mlib.SignDetectionMode.HoleWindingRule
+offset_params.fwn = mc.FastWindingNumber(mesh) # Enables usage of CUDA
 offset_params.voxelSize = mlib.suggestVoxelSize(mesh, 5e6)
 
-new_mesh = mlib.generalOffsetMesh(mp=mesh, offset=1, params=offset_params)
+new_mesh = mlib.generalOffsetMesh(mp=mesh, offset=0, params=offset_params)
 mlib.saveMesh(new_mesh, f"cuda_offset.ctm")

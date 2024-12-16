b68be6ca93573fb332d73ddde5c8d13a93b01090
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2024-12-16 17:32:02 +0300

    SignDetectionMode: better comments (#3879)

--- source/MRMesh/MRSignDetectionMode.h
+++ source/MRMesh/MRSignDetectionMode.h
@@ -8,11 +8,22 @@ namespace MR
 /// how to determine the sign of distances from a mesh
 enum class SignDetectionMode
 {
-    Unsigned,         ///< unsigned distance, useful for bidirectional `Shell` offset
-    OpenVDB,          ///< sign detection from OpenVDB library, which is good and fast if input geometry is closed
-    ProjectionNormal, ///< the sign is determined based on pseudonormal in closest mesh point (unsafe in case of self-intersections)
-    WindingRule,      ///< ray intersection counter, significantly slower than ProjectionNormal and does not support holes in mesh
-    HoleWindingRule   ///< computes winding number generalization with support of holes in mesh, slower than WindingRule
+    /// unsigned distance, useful for bidirectional `Shell` offset
+    Unsigned,
+
+    /// sign detection from OpenVDB library, which is good and fast if input geometry is closed
+    OpenVDB,
+
+    /// the sign is determined based on pseudonormal in closest mesh point (unsafe in case of self-intersections)
+    ProjectionNormal,
+
+    /// ray intersection counter, significantly slower than ProjectionNormal and does not support holes in mesh;
+    /// this mode is slow, and it does NOT have CUDA acceleration at this moment
+    WindingRule,
+
+    /// computes robust winding number generalization with support of holes and self-intersections in mesh,
+    /// it is the slowest sign detection mode, but it CAN be accelerated with CUDA if this mode activated e.g. in OffsetParameters.fwn
+    HoleWindingRule
 };
 
 /// returns string representation of enum values

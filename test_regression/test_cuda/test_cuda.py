1675c2c0110295c197861b309a36507c6a6d82d6
Anton Chernobrovkin <anton.chernobrovkin@meshinspector.com>
2024-12-18 12:05:40 +0400

    Add tests on cuda in CI/CD

--- test_regression/test_cuda/test_cuda.py
+++ test_regression/test_cuda/test_cuda.py
@@ -2,6 +2,8 @@ import pytest
 
 from meshlib import mrcudapy as mc
 
+
+@pytest.mark.smoke
 @pytest.mark.skipif(
     "not config.getoption('--run-cuda')=='negative'",
     reason="Only run when --run-cuda is 'n'",
@@ -10,6 +12,7 @@ def test_cuda_not_available():
     assert mc.isCudaAvailable() is False, "Check if cuda not available and it's reported correctly"
 
 
+@pytest.mark.smoke
 @pytest.mark.skipif(
     "not config.getoption('--run-cuda')=='positive'",
     reason="Only run when --run-cuda is 'p'",

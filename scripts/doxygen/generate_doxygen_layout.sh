4776e8ce21e3313ec7a2761555179dd00009609c
Alexandr Burakov <alexandr.burakov@meshinspector.com>
2024-12-18 15:31:50 +0300

    reworked to using relative url paths

--- scripts/doxygen/generate_doxygen_layout.sh
+++ scripts/doxygen/generate_doxygen_layout.sh
@@ -5,14 +5,7 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "${SCRIPT}")
 DOXYGEN_DIR=$(readlink -f "${SCRIPTPATH}/../../doxygen")
 
-BEGIN_URL=""
 END_URL=".html"
-
-if [ "$2" = "127.0.0.1:8000/MeshLib/local" ]; then
-    BEGIN_URL="http://${2}"
-else
-    BEGIN_URL="https://${2}"
-fi
 
 # edit main block
 cp ${DOXYGEN_DIR}/layout_templates/base_struct.xml ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
@@ -20,21 +13,25 @@ if [ "$1" = "Main" ]; then
     sed -e "s|__MAIN_PAGE_TAB__|<tab type=\"mainpage\" visible=\"yes\" title=\"About\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
     sed -e "s|__BEGIN_URL__|@ref |" -e "s|__END_URL__||" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 else
-    sed -e "s|__MAIN_PAGE_TAB__|<tab type=\"user\" url=\"${BEGIN_URL}/html/index.html\" title=\"About\"/>|"  -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
-    sed -e "s|__BEGIN_URL__|${BEGIN_URL}/html/|" -e "s|__END_URL__|${END_URL}|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "s|__MAIN_PAGE_TAB__|<tab type=\"user\" url=\"../index.html\" title=\"About\"/>|"  -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+    sed -e "s|__BEGIN_URL__|../|" -e "s|__END_URL__|${END_URL}|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 fi
 
 # edit C++ API block
-if [ ! "$1" = "Cpp" ]; then
-    sed -e "s|      <!-- API_CPP_PAGE -->|      <tab type=\"user\" url=\"${BEGIN_URL}/html/Cpp/APICppPage.html\" title=\"C++\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+if [ "$1" = "Main" ]; then
+    sed -e "s|      <!-- API_CPP_PAGE -->|      <tab type=\"user\" url=\"Cpp/APICppPage.html\" title=\"C++\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+elif [ "$1" = "Py" ]; then
+    sed -e "s|      <!-- API_CPP_PAGE -->|      <tab type=\"user\" url=\"../Cpp/APICppPage.html\" title=\"C++\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 else
     sed -e "/      <!-- API_CPP_PAGE -->/r ${DOXYGEN_DIR}/layout_templates/API_part.xml" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
     sed -e "s|__API_PAGE_URL__|@ref APICppPage|" -e "s|__API_PAGE_NAME__|C++|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 fi
 
 # edit Python API block
-if [ ! "$1" = "Py" ]; then
-    sed -e "s|      <!-- API_PY_PAGE -->|      <tab type=\"user\" url=\"${BEGIN_URL}/html/Py/APIPyPage.html\" title=\"Python\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+if [ "$1" = "Main" ]; then
+    sed -e "s|      <!-- API_PY_PAGE -->|      <tab type=\"user\" url=\"Py/APIPyPage.html\" title=\"Python\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
+elif [ "$1" = "Cpp" ]; then
+    sed -e "s|      <!-- API_PY_PAGE -->|      <tab type=\"user\" url=\"../Py/APIPyPage.html\" title=\"Python\"/>|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 else
     sed -e "/      <!-- API_PY_PAGE -->/r ${DOXYGEN_DIR}/layout_templates/API_part.xml" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
     sed -e "s|__API_PAGE_URL__|@ref APIPyPage|" -e "s|__API_PAGE_NAME__|Python|" -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
@@ -53,5 +50,3 @@ else
         -e "s|__NAMESPACES_MEMBERS_TITLE__||" \
         -i ${DOXYGEN_DIR}/DoxygenLayout${1}.xml
 fi
-
-sed -e "s|__BEGIN_URL__|${BEGIN_URL}|" -i ${DOXYGEN_DIR}/APIPage.dox

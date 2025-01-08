86ae7e3d92db84d9ad31cbe08fe1648fac2f0cb8
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-01-08 13:28:50 +0300

    findSignedDistances

--- source/MRMesh/MRPointsToMeshProjector.cpp
+++ source/MRMesh/MRPointsToMeshProjector.cpp
@@ -2,6 +2,7 @@
 #include "MRMesh.h"
 #include "MRAffineXf3.h"
 #include "MRMatrix3Decompose.h"
+#include "MRParallelFor.h"
 #include "MRTimer.h"
 #include "MRPch/MRTBB.h"
 
@@ -50,4 +51,64 @@ size_t PointsToMeshProjector::projectionsHeapBytes( size_t ) const
     return 0;
 }
 
+VertScalars findSignedDistances(
+    const Mesh& refMesh,
+    const VertCoords & testPoints, const VertBitSet * validTestPoints,
+    const MeshProjectionParameters & params,
+    IPointsToMeshProjector * projector )
+{
+    MR_TIMER
+
+    AffineXf3f testToRefXf;
+    if ( params.refXf )
+        testToRefXf = params.refXf->inverse();
+    if ( params.xf )
+        testToRefXf = testToRefXf * ( *params.xf );
+
+    VertScalars res( testPoints.size(), std::sqrt( params.upDistLimitSq ) );
+
+    if ( projector )
+    {
+        projector->updateMeshData( &refMesh );
+        std::vector<MeshProjectionResult> mpRes( testPoints.size() );
+        projector->findProjections( mpRes, testPoints.vec_, params.xf, params.refXf, params.upDistLimitSq, params.loDistLimitSq );
+
+        ParallelFor( 0_v, testPoints.endId(), [&] ( VertId v )
+        {
+            if ( validTestPoints && !validTestPoints-> test( v ) )
+                return;
+            const auto& mpResV = mpRes[v.get()];
+            if ( mpResV.mtp.e )
+                res[v] = refMesh.signedDistance( testToRefXf( testPoints[v] ), mpResV );
+            else
+                res[v] = std::sqrt( mpResV.distSq );
+        } );
+    }
+    else
+    {
+        // no projector is given, use fully CPU computation
+        ParallelFor( 0_v, testPoints.endId(), [&] ( VertId v )
+        {
+            if ( validTestPoints && !validTestPoints-> test( v ) )
+                return;
+            const auto pt = testToRefXf( testPoints[v] );
+            const auto mpResV = findProjection( pt, refMesh, params.upDistLimitSq, nullptr, params.loDistLimitSq );
+            if ( mpResV.mtp.e )
+                res[v] = refMesh.signedDistance( pt, mpResV );
+            else
+                res[v] = std::sqrt( mpResV.distSq );
+        } );
+    }
+    return res;
 }
+
+VertScalars findSignedDistances(
+    const Mesh& refMesh,
+    const Mesh& mesh,
+    const MeshProjectionParameters & params,
+    IPointsToMeshProjector * projector )
+{
+    return findSignedDistances( refMesh, mesh.points, &mesh.topology.getValidVerts(), params, projector );
+}
+
+} //namespace MR

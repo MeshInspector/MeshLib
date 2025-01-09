6afdb4729099a7deb431360eea4e4629ec467a43
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-01-09 17:57:04 +0300

    Generalize splitEdge on not-triangular faces

--- source/MRMesh/MRMeshTopology.cpp
+++ source/MRMesh/MRMeshTopology.cpp
@@ -11,9 +11,10 @@
 #include "MRParallelFor.h"
 #include "MRProgressReadWrite.h"
 #include "MRGridSettings.h"
-#include "MRPch/MRTBB.h"
 #include "MRParallelFor.h"
 #include "MRIOParsing.h"
+#include "MRGTest.h"
+#include "MRPch/MRTBB.h"
 #include <atomic>
 #include <initializer_list>
 
@@ -2361,6 +2362,30 @@ bool MeshTopology::checkValidity( ProgressCallback cb, bool allVerts ) const
 
 void loadMeshDll()
 {
+}
+
+TEST( MRMesh, splitEdge )
+{
+    MeshTopology topology;
+    EdgeId a = topology.makeEdge();
+    EdgeId b = topology.makeEdge();
+    EdgeId c = topology.makeEdge();
+    EdgeId d = topology.makeEdge();
+    topology.splice( b, a.sym() );
+    topology.splice( c, b.sym() );
+    topology.splice( d, c.sym() );
+    topology.splice( a, d.sym() );
+    EXPECT_TRUE( topology.isLeftQuad( a ) );
+    EXPECT_TRUE( topology.isLeftQuad( a.sym() ) );
+    topology.setLeft( a, topology.addFaceId() );
+    EdgeId a0 = topology.splitEdge( a );
+    EXPECT_TRUE( topology.isLeftTri( a0 ) );
+    EXPECT_TRUE( topology.isLeftQuad( a ) );
+    EXPECT_EQ( topology.getLeftDegree( a.sym() ), 5 );
+    EdgeId a1 = topology.splitEdge( a.sym() );
+    EXPECT_TRUE( topology.isLeftTri( a1.sym() ) );
+    EXPECT_TRUE( topology.isLeftQuad( a ) );
+    EXPECT_EQ( topology.getLeftDegree( a.sym() ), 6 );
 }
 
 } //namespace MR

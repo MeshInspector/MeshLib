e27d6d6b219284b711bb3f14d7320fd23b66ce0f
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-01-28 07:44:13 -0500

    Tweak the script to install all python versions.

--- scripts/mrbind-pybind11/install_all_python_versions_macos.sh
+++ scripts/mrbind-pybind11/install_all_python_versions_macos.sh
@@ -11,6 +11,26 @@ PY_VERSIONS="$(cat $SCRIPT_DIR/python_versions.txt | xargs)"
 # Should be `/opt/homebrew` on ARM and `/usr/local` on x86.
 [[ ${HOMEBREW_DIR:=} ]] || HOMEBREW_DIR="$(brew --prefix)"
 
+if [[ ${ENABLE_SUDO:=} == 1 ]]; then
+    SUDO=sudo
+elif [[ ${ENABLE_SUDO:=} == 0 ]]; then
+    SUDO=
+elif which sudo >/dev/null 2>/dev/null; then
+    SUDO=sudo
+else
+    SUDO=
+fi
+
+# ??
+$SUDO find "$HOMEBREW_DIR/bin" -lname '*/Library/Frameworks/Python.framework/*' -delete
+
+if [[ ${ALLOW_DELETING_EXISTING_PYTHON:=} == 1 ]]; then
+    $SUDO rm -rf /Library/Frameworks/Python.framework/
+elif [[ -d /Library/Frameworks/Python.framework/ ]]; then
+    echo "WARNING: You have a potentially conflicting Python installation at: /Library/Frameworks/Python.framework/"
+    echo "  Consider deleting it using the following command: sudo rm -rf /Library/Frameworks/Python.framework/"
+fi
+
 brew update
 
 for ver in $PY_VERSIONS; do

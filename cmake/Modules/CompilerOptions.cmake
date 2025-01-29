1302f03ebf75c5ad43faf731535db6b5afb302b1
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-01-29 14:11:36 +0100

    Add experimental wasm64 support (#4026)
    * WIP: Wasm: Add Memory64 support
    
    * WIP: Fix callback
    
    * WIP: Remove debug lines
    
    * WIP: Add option to build Wasm64 version
    
    * WIP: Fix thirdparty build
    
    * WIP: Add option to build Wasm64 version
    
    * WIP: Fix build
    
    * WIP: Fix build
    
    * WIP: Revert changes
    
    * WIP: Revert changes
    
    * WIP: Fix laz-perf build
    
    * WIP: Fix GLFW callbacks
    
    * WIP: Fix GLFW callbacks
    
    * WIP: Fix GLFW callbacks
    
    * WIP: Fix file loading
    
    * WIP: Fix file loading
    
    * Revert ABI change
    
    * Revert file loading changes
    
    * Fix bitness check
    
    * Enable wasm64 build
    
    * WIP: Disable other platforms' builds
    
    * WIP: Disable other platforms' builds
    
    * WIP: Disable other platforms' builds
    
    * Revert changes
    
    * Do not build wasm64 on every run
    
    * Move code out of header file
    
    * Re-organize CMake config
    
    * Re-organize CMake config
    
    * Re-organize CMake config
    
    * Re-organize CMake config
    
    * Re-organize CMake config
    
    * Add comments

--- cmake/Modules/CompilerOptions.cmake
+++ cmake/Modules/CompilerOptions.cmake
@@ -1,10 +1,4 @@
-set(MR_CXX_STANDARD 20 CACHE STRING "Version of the C++ standard used to compile the project")
-IF(DEFINED ENV{MR_USE_CPP_23} AND "$ENV{MR_USE_CPP_23}" STREQUAL "ON")
-  message("MR_USE_CPP_23 variable is deprecated; consider setting MR_CXX_STANDARD to 23")
-  set(MR_CXX_STANDARD 23)
-ENDIF()
-set(CMAKE_CXX_STANDARD ${MR_CXX_STANDARD})
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
+# this file must be included AFTER the `project' command because it relies on the detected compiler information
 
 set(MR_PCH_DEFAULT OFF)
 # for macOS, GCC, and Clang<15 builds: PCH not only does not give any speedup, but even vice versa
@@ -19,16 +13,8 @@ IF(MR_PCH AND NOT MR_EMSCRIPTEN)
 ENDIF()
 message("MR_PCH=${MR_PCH}")
 
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -D_DEBUG -DDEBUG")
-# turn on warnings as errors
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-missing-field-initializers -Wno-unknown-pragmas -Wno-sign-compare -Werror -fvisibility=hidden -pedantic-errors -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS -DIMGUI_ENABLE_FREETYPE")
-
-IF(NOT MR_EMSCRIPTEN_SINGLETHREAD)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
-ENDIF() # NOT MR_EMSCRIPTEN_SINGLETHREAD
-
-IF(WIN32 AND MINGW)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wa,-mbig-obj")
+IF(MR_EMSCRIPTEN AND NOT MR_EMSCRIPTEN_SINGLETHREAD)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-pthreads-mem-growth") # look https://github.com/emscripten-core/emscripten/issues/8287
 ENDIF()
 
 # make link to fail if there are unresolved symbols (GCC and Clang)
@@ -57,44 +43,3 @@ ENDIF()
 IF(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-maybe-uninitialized")
 ENDIF()
-
-# TODO: __aarch64__ ?
-IF(NOT APPLE AND NOT CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
-  message("CMAKE_SYSTEM_PROCESSOR is ${CMAKE_SYSTEM_PROCESSOR}")
-  add_compile_definitions(__ARM_CPU__)
-  message("ARM cpu detected")
-ENDIF()
-
-IF(MR_EMSCRIPTEN)
-  # reference: https://github.com/emscripten-core/emscripten/blob/main/src/settings.js
-  string(JOIN " " CMAKE_EXE_LINKER_FLAGS
-    "${CMAKE_EXE_LINKER_FLAGS}"
-    "-s EXPORTED_RUNTIME_METHODS=[ccall]"
-    "-s ALLOW_MEMORY_GROWTH=1"
-    "-s MAXIMUM_MEMORY=4GB"
-    "-s LLD_REPORT_UNDEFINED=1"
-    "-s USE_WEBGL2=1"
-    "-s USE_GLFW=3"
-    "-s USE_ZLIB=1"
-    "-s FULL_ES3=1"
-    "-s USE_LIBPNG=1"
-  )
-
-  IF(MR_EMSCRIPTEN_SINGLETHREAD)
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -s ENVIRONMENT=web")
-  ELSE()
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -s ENVIRONMENT=web,worker -pthread -s PTHREAD_POOL_SIZE_STRICT=0 -s PTHREAD_POOL_SIZE=navigator.hardwareConcurrency")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-pthreads-mem-growth") # look https://github.com/emscripten-core/emscripten/issues/8287
-  ENDIF() # NOT MR_EMSCRIPTEN_SINGLETHREAD
-
-  IF(NOT MR_DISABLE_EMSCRIPTEN_ASYNCIFY)
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -s ASYNCIFY -Wno-limited-postlink-optimizations")
-    add_compile_definitions(MR_EMSCRIPTEN_ASYNCIFY)
-  ENDIF() # NOT MR_DISABLE_EMSCRIPTEN_ASYNCIFY
-
-  add_compile_definitions(SPDLOG_FMT_EXTERNAL)
-  add_compile_definitions(SPDLOG_WCHAR_FILENAMES) # hack to make it work with new version of fmt
-  
-  # FIXME: comment required
-  add_compile_definitions(EIGEN_STACK_ALLOCATION_LIMIT=0)
-ENDIF() # MR_EMSCRIPTEN

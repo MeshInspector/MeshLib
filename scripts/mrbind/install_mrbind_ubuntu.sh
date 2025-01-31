1314a32609f55f5f420ef92295842b466bc42a40
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-01-31 11:50:24 +0300

    Move mrbind to a submodule. (#4039)
    * Move mrbind to a submodule.
    
    * Typo.
    
    * Try again.
    
    * Update mrbind.
    
    * Try again.

--- scripts/mrbind/install_mrbind_ubuntu.sh
+++ scripts/mrbind/install_mrbind_ubuntu.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
-# Downloads the source code for MRBind and builds it at `MeshLib/mrbind/build`.
+# Build the MRBind submodule at `MeshLib/thirdparty/mrbind/build`.
 
 set -euxo pipefail
 
 SCRIPT_DIR="$(realpath "$(dirname "$BASH_SOURCE")")"
 
-[[ -v MRBIND_DIR ]] || MRBIND_DIR="$(realpath "$SCRIPT_DIR/../../mrbind")"
-[[ -v MRBIND_COMMIT ]] || MRBIND_COMMIT="$(cat "$SCRIPT_DIR/mrbind_commit.txt" | xargs)"
+[[ -v MRBIND_DIR ]] || MRBIND_DIR="$(realpath "$SCRIPT_DIR/../../thirdparty/mrbind")"
 
 # Read the Clang version from `clang_version.txt`. `xargs` trims the whitespace.
 CLANG_VER="$(cat "$SCRIPT_DIR/clang_version.txt" | xargs)"
 [[ $CLANG_VER ]] || (echo "Not sure what version of Clang to use." && false)
 
-# Clone mrbind source.
-if [[ ! -d $MRBIND_DIR ]]; then
-    mkdir -p "$MRBIND_DIR"
-    git clone https://github.com/MeshInspector/mrbind "$MRBIND_DIR"
-    cd "$MRBIND_DIR"
-else
-    cd "$MRBIND_DIR"
-    git fetch
-fi
-
-git checkout "$MRBIND_COMMIT"
-
+cd "$MRBIND_DIR"
 rm -rf build
 
 

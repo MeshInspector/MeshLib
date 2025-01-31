7abb7df45b3ff09c64463e51bfaa7100df86114e
Anton Chernobrovkin <anton.chernobrovkin@meshinspector.com>
2025-01-31 14:47:32 +0400

    Add tests on distanceMapFromContours on cuda and non-cuda modes

--- test_regression/test_cuda/test_cuda_dm_from_contour.py
+++ test_regression/test_cuda/test_cuda_dm_from_contour.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest_check import check
 
 from module_helper import *
@@ -6,15 +7,17 @@ from constants import test_files_path
 from helpers.meshlib_helpers import compare_distance_maps
 
 
+@pytest.mark.skipif(
+    "not config.getoption('--run-cuda')=='positive'",
+    reason="Only run when --run-cuda is 'positive'",
+)
 def test_dm_from_contour(cuda_module, tmp_path):
-    """
-
-    """
     #  Load input point
     input_folder = Path(test_files_path) / "cuda" / "lines_to_dm"
     pl3 = mrmeshpy.loadLines(input_folder / "input.mrlines")
     pl2 = mrmeshpy.Polyline2(pl3.contours2())
 
+    # process
     params = mrmeshpy.ContourToDistanceMapParams()
     params.pixelSize = mrmeshpy.Vector2f(0.1, 0.1)
     params.resolution = mrmeshpy.Vector2i(100, 100)

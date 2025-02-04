4a1b7f30d91d206f14d22db18ea5d06aff6df755
Max Raiskii <max.raiskii@meshinspector.com>
2025-02-05 01:08:12 +0300

    Revert "ignore -Warray-bounds on gcc 13 fmt v9"
    This reverts commit c6d6e9a9553767073fe1694379a6599d23ab7af9.

--- source/MRPch/MRFmt.h
+++ source/MRPch/MRFmt.h
@@ -2,20 +2,9 @@
 
 #pragma warning(push)
 #pragma warning(disable:4275) // non dll-interface class 'std::runtime_error' used as base for dll-interface class 'fmt::v10::format_error'
-
-#if defined(__GNUC__) && __GNUC__ == 13 && defined(FMT_VERSION) && (FMT_VERSION >= 90000 && FMT_VERSION < 100000)
-  #pragma GCC diagnostic push
-  #pragma GCC diagnostic ignored "-Warray-bounds"
-#endif
-
 #include <spdlog/tweakme.h>
 #include <spdlog/fmt/fmt.h>
 #include <spdlog/fmt/ostr.h>
-
-#if defined(__GNUC__) && __GNUC__ == 13 && defined(FMT_VERSION) && (FMT_VERSION >= 90000 && FMT_VERSION < 100000)
-  #pragma GCC diagnostic pop
-#endif
-
 #pragma warning(pop)
 
 #include "MRPch/MRBindingMacros.h"

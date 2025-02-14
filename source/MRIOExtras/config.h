5dc17f8c3374776249961eedd9aa1ba3513a70fb
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-02-14 10:33:27 +0100

    Enable config header for MSVC (#4110)

--- source/MRIOExtras/config.h
+++ source/MRIOExtras/config.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#if defined( _MSC_VER )
-// no config file support yet
-#elif __has_include( "config_cmake.h" )
+#if __has_include( "config_cmake.h" )
 #include "config_cmake.h"
 #endif

505af81dda0fa64510375d617554addab52406d4
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-02-20 09:42:06 +0100

    Add comments

--- source/MRTestC/TestFunctions.h
+++ source/MRTestC/TestFunctions.h
@@ -2,8 +2,11 @@
 
 #include <time.h>
 
+// returns a timespec object holding the current time
 struct timespec timespec_now( void );
 
+// returns a timespec object holding a duration between two time values
 struct timespec timespec_get_duration( const struct timespec* before, const struct timespec* after );
 
+// converts a timespec object to a timestamp in seconds
 double timespec_to_seconds( const struct timespec* ts );

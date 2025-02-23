4ca4ee253f202de6e6e57ccb626502a661539306
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-02-23 14:59:12 +0300

    typo

--- examples/python-examples/OpenDicomFiles.dox.py
+++ examples/python-examples/OpenDicomFiles.dox.py
@@ -3,7 +3,7 @@ from meshlib import mrmeshpy
 # Path to folder, that contains mutiple numbered files, like: 3DSlice1.dcm, 3DSlice2.dcm, 3DSlice3.dcm
 # Folder can contain subfolders - each will be loaded separately
 dicom_folder = 'path/to/folder'
-# Load dicom objects from folder and it's subfolders
+# Load dicom objects from folder and its subfolders
 dicoms = mrmeshpy.VoxelsLoad.loadDicomsFolderTreeAsVdb(dicom_folder)
 # Result contains list of multiple objects, each object is a separate dicom volume, that correspond to each folder
 # Getting the first one here

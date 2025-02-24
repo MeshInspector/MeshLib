ac81a0849a485a47a497b43024ad2527b44a8b7f
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-02-24 16:32:21 +0300

    PickPointManager: fix click on sphere

--- source/MRViewer/MRPickPointManager.cpp
+++ source/MRViewer/MRPickPointManager.cpp
@@ -341,6 +341,9 @@ bool PickPointManager::onMouseDown_( Viewer::MouseButton button, int mod )
     if ( !obj )
         return false;
 
+    if ( myPickSpheres_.contains( obj.get() ) )
+        return false; // we can be here only if pick sphere under cursor was not properly hovered and thus ignored mouse down
+
     if ( ( params.surfacePointParams.pickInBackFaceObject == false ) && ( SurfacePointWidget::isPickIntoBackFace( obj, pick, getViewerInstance().viewport().getCameraPoint() ) ) )
         return false;
 

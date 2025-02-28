080cf39c0506323146aa7fb52f9863b46dfdcfc8
Max Raiskii <max.raiskii@meshinspector.com>
2025-03-01 01:53:41 +0300

    find vcpkg version

--- thirdparty/install.bat
+++ thirdparty/install.bat
@@ -4,7 +4,6 @@ REM use "aws s3 ls s3://vcpkg-export/" to ls all available tags
 set VCPKG_DEFAULT_TRIPLET=x64-windows-meshlib
 setlocal enabledelayedexpansion
 
-
 REM Default settings
 set VCPKG_TAG=no-tag
 set VCPKG_DEFAULT_TRIPLET=x64-windows-meshlib
@@ -26,9 +25,12 @@ if not defined vcpkg_path (
     echo vcpkg not found. Setting VCPKG_TAG to "no-tag".
     set VCPKG_TAG=no-tag
 ) else (
-    REM Extract the first valid version number (YYYY-MM-DD format)
-    for /f "tokens=4 delims= " %%V in ('vcpkg version 2^>nul ^| findstr /R "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]"') do set VCPKG_TAG=%%V
+    REM Extract version number (YYYY-MM-DD-hash) and cut first 10 characters
+    for /f "tokens=6" %%V in ('vcpkg version 2^>nul ^| findstr /R "vcpkg package management program version [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]"') do set FULL_VCPKG_TAG=%%V
+    set VCPKG_TAG=!FULL_VCPKG_TAG:~0,10!
 )
+
+echo Using vcpkg version: %VCPKG_TAG%
 
 REM Check for --write-s3 option
 set "write_s3_option=false"
@@ -36,15 +38,13 @@ for %%i in (%*) do (
     if /I "%%i"=="--write-s3" set "write_s3_option=true"
 )
 
-echo Using vcpkg version: %VCPKG_TAG%
-
 REM Configure VCPKG_BINARY_SOURCES
 if "!write_s3_option!"=="true" (
     set "VCPKG_BINARY_SOURCES=clear;x-aws,s3://vcpkg-export/%VCPKG_TAG%/x64-windows-meshlib/,readwrite;"
-    echo "Using AWS authentication"
+    echo "Using AWS authentication for vcpkg binary caching."
 ) else (
     set "VCPKG_BINARY_SOURCES=clear;x-aws-config,no-sign-request;x-aws,s3://vcpkg-export/%VCPKG_TAG%/x64-windows-meshlib/,readwrite;"
-    echo "Using no authentication"
+    echo "Using no authentication for vcpkg binary caching."
 )
 
 REM Ensure vcpkg downloads folder exists
@@ -57,8 +57,10 @@ for /f "delims=" %%i in ('type "%~dp0..\requirements\windows.txt"') do (
     set packages=!packages! %%i
 )
 
-REM Install dependencies
+REM Install vcpkg core dependencies
 vcpkg install vcpkg-cmake vcpkg-cmake-config --host-triplet %VCPKG_DEFAULT_TRIPLET% --overlay-triplets "%~dp0vcpkg\triplets" --debug --x-abi-tools-use-exact-versions || goto :error
+
+REM Install all required dependencies
 vcpkg install !packages! --host-triplet %VCPKG_DEFAULT_TRIPLET% --overlay-triplets "%~dp0vcpkg\triplets" --debug --x-abi-tools-use-exact-versions || goto :error
 
 endlocal
@@ -67,5 +69,6 @@ goto :EOF
 REM Error handling
 :error
 echo Failed with error #%errorlevel%.
+echo This may be due to missing AWS credentials, incorrect package names, or missing triplets.
 endlocal
 exit /b %errorlevel%

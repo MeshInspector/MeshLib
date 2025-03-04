169a520c58b0a5314eb3c935607903bfb76e5e73
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-03-04 16:25:30 +0300

    auto CubicBezierCurve<V>::getWeights( T t ) -> std::array<T, 4>

--- source/MRMesh/MRBezier.h
+++ source/MRMesh/MRBezier.h
@@ -2,6 +2,7 @@
 
 #include "MRMeshFwd.h"
 #include "MRVectorTraits.h"
+#include <array>
 
 namespace MR
 {
@@ -19,6 +20,9 @@ struct CubicBezierCurve
 
     /// computes point on the curve from parameter value
     V getPoint( T t ) const;
+
+    /// computes weights of every control point for given parameter value, the sum of all weights is equal to 1
+    static std::array<T, 4> getWeights( T t );
 };
 
 template <typename V>
@@ -34,6 +38,20 @@ inline V CubicBezierCurve<V>::getPoint( T t ) const
         r[i] = lerp( q[i], q[i+1], t );
 
     return lerp( r[0], r[1], t );
+}
+
+template <typename V>
+inline auto CubicBezierCurve<V>::getWeights( T t ) -> std::array<T, 4>
+{
+    T s = 1 - t;
+    std::array<T, 4> w =
+    {
+            s * s * s,
+        3 * s * s * t,
+        3 * s * t * t
+    };
+    w[3] = 1 - w[0] - w[1] - w[2];
+    return w;
 }
 
 } //namespace MR

66fc40cd69299095ff9e49392edb6890ec83884c
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-03-07 12:02:38 +0100

    Update comments

--- source/MRVoxels/MRPointsToMeshFusion.h
+++ source/MRVoxels/MRPointsToMeshFusion.h
@@ -32,11 +32,12 @@ struct PointsToMeshParameters
     /// Progress callback
     ProgressCallback progress;
 
+    /// Callback for volume creation by parts. If both volume creation functions are null - volume will be created with memory efficient pointsToDistanceFunctionVolume function
+    /// This callback takes precedence over createVolumeCallback
+    std::function<Expected<void>( const PointCloud& cloud, const PointsToDistanceVolumeParams& params, std::function<Expected<void>( const SimpleVolumeMinMax& volume )> addPart )> createVolumeCallbackByParts;
+
     /// Callback for volume creation. If both volume creation functions are null - volume will be created with memory efficient pointsToDistanceFunctionVolume function
     std::function<Expected<SimpleVolumeMinMax>( const PointCloud& cloud, const PointsToDistanceVolumeParams& params )> createVolumeCallback;
-
-    /// Callback for volume creation by parts. If both volume creation functions are null - volume will be created with memory efficient pointsToDistanceFunctionVolume function
-    std::function<Expected<void>( const PointCloud& cloud, const PointsToDistanceVolumeParams& params, std::function<Expected<void>( const SimpleVolumeMinMax& volume )> addPart )> createVolumeCallbackByParts;
 
     /// Callback for checking whether it's possible to use the volume creation function
     std::function<bool ( const PointCloud& cloud, const PointsToDistanceVolumeParams& params )> canCreateVolume;

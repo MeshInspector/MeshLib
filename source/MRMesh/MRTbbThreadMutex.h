94f42d95c3fe538060314875819fe9763bd04675
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-03-19 15:13:23 +0100

    Update comments

--- source/MRMesh/MRTbbThreadMutex.h
+++ source/MRMesh/MRTbbThreadMutex.h
@@ -8,14 +8,29 @@
 namespace MR
 {
 
-/// helper class used to ensure that the specific thread is used only once
+/// helper class used to ensure that the specific thread is not re-used by TBB
 /// use it if you have nested TBB operations (e.g. parallel_for inside another parallel_for)
+/// \code
+/// TbbThreadMutex reportThreadMutex;
+/// tbb::parallel_for( range, [&] ( auto&& range )
+/// {
+///     const auto reportThreadLock = reportThreadMutex.tryLock();
+///     for ( auto i = range.begin(); i != range.end(); ++i )
+///     {
+///         // if you have a nested `parallel_for` call here, you might come back to the same main loop with another range
+///         // since TBB can re-use 'stale' threads
+///         tbb::parallel_for( ... );
+///         if ( reportThreadLock )
+///             report( ... );
+///     }
+/// } );
+/// \endcode
 class TbbThreadMutex
 {
 public:
     MRMESH_API explicit TbbThreadMutex( std::thread::id id = std::this_thread::get_id() );
 
-    /// ...
+    /// RAII-style lock guard for the mutex; releases it on destruction
     class LockGuard
     {
         friend class TbbThreadMutex;
@@ -27,6 +42,8 @@ public:
         MRMESH_API ~LockGuard();
     };
 
+    /// try to lock the mutex
+    /// returns a lock guard if the current thread id is equal to the mutex's one and the mutex is not locked yet
     MRMESH_API std::optional<LockGuard> tryLock();
 
 private:

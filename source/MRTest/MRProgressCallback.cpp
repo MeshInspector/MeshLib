d5bc0dbb4614cf1370f95a6b49e043dcc58f2b30
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-03-19 16:51:59 +0100

    Add progress callback unit tests

---
+++ source/MRTest/MRProgressCallback.cpp
@@ -0,0 +1,26 @@
+#include <MRMesh/MRProgressCallback.h>
+
+#include <gtest/gtest.h>
+
+namespace MR
+{
+
+TEST( MRMesh, ProgressCallback )
+{
+    std::vector<float> progressValues;
+    ProgressCallback cb = [&progressValues] ( float v )
+    {
+        progressValues.emplace_back( v );
+        return true;
+    };
+
+    cb = subprogress( cb, 0.40f, 1.00f );
+    cb = subprogress( cb, 0.80f, 1.00f );
+    cb = subprogress( cb, 0.00f, 0.30f );
+
+    cb( (float)10817394 / 10817408 );
+    cb( (float)10817401 / 10817408 );
+    ASSERT_LE( progressValues[0], progressValues[1] );
+}
+
+} // namespace MR

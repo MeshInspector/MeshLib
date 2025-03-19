ec62af19408842900ec283432f83db37890e087e
Anton Chernobrovkin <anton.chernobrovkin@meshinspector.com>
2025-03-19 14:42:45 +0400

    Add samples on signed distances

--- examples/python-examples/SignedDistances.dox.py
+++ examples/python-examples/SignedDistances.dox.py
@@ -4,9 +4,8 @@ ref_mesh = mrmeshpy.loadMesh("mesh1.ctm")
 mesh = mrmeshpy.loadMesh("mesh2.ctm")
 
 # get object of mrmeshpy.VertScalars - set of distances between points of target mesh and reference mesh
-a = mrmeshpy.findSignedDistances(ref_mesh, mesh)
+vert_distances = mrmeshpy.findSignedDistances(ref_mesh, mesh)
 
-print("Id of closest point is " + a.frontId().get())
-print("Distance between reference mesh and the closest point of target mesh is " + a.front())
-print("Id of farest point is " + a.backId().get())
-print("Distance between reference mesh and the farthest point of target mesh is " + a.back())
+# mrmeshpy.VertScalars is iterable so we can use min() or max() functions
+print("Distance between reference mesh and the closest point of target mesh is " + str(min(vert_distances)))
+print("Distance between reference mesh and the farthest point of target mesh is " + str(max(vert_distances)))

c18db565e536d269ea10f60667bb6f87c73bad97
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-03-19 18:12:54 +0100

    Extend test data set

--- source/MRTest/MRProgressCallback.cpp
+++ source/MRTest/MRProgressCallback.cpp
@@ -19,8 +19,11 @@ TEST( MRMesh, ProgressCallback )
     cb = subprogress( cb, 0.00f, 0.30f );
 
     cb( (float)10817394 / 10817408 );
+    cb( (float)10817398 / 10817408 );
     cb( (float)10817401 / 10817408 );
-    ASSERT_LE( progressValues[0], progressValues[1] );
+    EXPECT_LE( progressValues[0], progressValues[1] );
+    EXPECT_LE( progressValues[0], progressValues[2] );
+    EXPECT_LE( progressValues[1], progressValues[2] );
 }
 
 } // namespace MR

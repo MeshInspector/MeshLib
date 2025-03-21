7f2fddc2346664738b102e83518d90a5121f37a8
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2025-03-21 19:09:26 +0300

    More functions to cut self-intersection contours (#4306)
    * More functions to cut self-intersectoin contours
    
    * CR Fix

--- source/MRMesh/MRContoursCut.h
+++ source/MRMesh/MRContoursCut.h
@@ -53,12 +53,17 @@ using OneMeshContours = std::vector<OneMeshContour>;
 MRMESH_API void subdivideLoneContours( Mesh& mesh, const OneMeshContours& contours, FaceHashMap* new2oldMap = nullptr );
 
 // Converts ordered continuous contours of two meshes to OneMeshContours
-// converters is required for better precision in case of degenerations
+// converters are required for better precision in case of degenerations
 // note that contours should not have intersections
 [[nodiscard]]
 MRMESH_API OneMeshContours getOneMeshIntersectionContours( const Mesh& meshA, const Mesh& meshB, const ContinuousContours& contours, bool getMeshAIntersections,
     const CoordinateConverters& converters, const AffineXf3f* rigidB2A = nullptr );
 
+// Converts ordered continuous self contours of single meshes to OneMeshContours
+// converters are required for better precision in case of degenerations
+[[nodiscard]]
+MRMESH_API OneMeshContours getOneMeshSelfIntersectionContours( const Mesh& mesh, const ContinuousContours& contours,
+    const CoordinateConverters& converters, const AffineXf3f* rigidB2A = nullptr );
 
 // Converts OneMeshContours contours representation to Contours3f: set of coordinates
 [[nodiscard]]

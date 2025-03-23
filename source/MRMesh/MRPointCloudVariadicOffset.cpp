da4bfbe2df607028e79c40b2347ef645ba01de69
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-03-23 14:58:30 +0300

    findClosestWeightedPoint

---
+++ source/MRMesh/MRPointCloudVariadicOffset.cpp
@@ -0,0 +1,50 @@
+#include "MRPointCloudVariadicOffset.h"
+#include "MRPointsInBall.h"
+
+namespace MR
+{
+
+PointAndDistance findClosestWeightedPoint( const Vector3f & loc,
+    const AABBTreePoints& tree, const VertMetric& pointWeights, const VariadicOffsetParams& params )
+{
+    assert( params.maxWeightGrad >= 0 );
+    // if params.maxWeightGrad == 0 then you need to find euclidean closest point - a much simpler algorithm than below
+    assert( params.maxWeightGrad < 1 ); // otherwise some points with smaller weights can be completely ignored
+
+    PointAndDistance res;
+    auto maxSearchRadius = params.maxDistance + params.maxWeight;
+    if ( maxSearchRadius < 0 )
+        return res;
+
+    /// the weight of hypothetical point at loc is no more than this value
+    auto maxLocWeight = params.maxWeight;
+
+    findPointsInBall( tree, { loc, sqr( maxSearchRadius ) }, [&]( const PointsProjectionResult & found, const Vector3f &, Ball3f & ball )
+    {
+        const auto r = std::sqrt( found.distSq );
+        const auto w = pointWeights( found.vId );
+        auto dist = r - w;
+        if ( !res || res.dist > dist )
+        {
+            res.dist = dist;
+            res.vId = found.vId;
+        }
+        // assess the maximal weight of hypothetical point at loc
+        const auto locWeight = w + r * params.maxWeightGrad;
+        if ( locWeight < maxLocWeight )
+        {
+            // try to reduce search radius knowing that the weights of nearby points are limited by known gradient
+            maxLocWeight = locWeight;
+            const auto searchRadius = ( params.maxDistance + maxLocWeight ) / ( 1 - params.maxWeightGrad );
+            if ( searchRadius < maxSearchRadius )
+            {
+                maxSearchRadius = searchRadius;
+                ball.radiusSq = sqr( maxSearchRadius );
+            }
+        }
+        return Processing::Continue;
+    } );
+    return res;
+}
+
+} //namespace MR
---
+++ source/MRMesh/MRPointCloudVariadicOffset.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "MRMeshFwd.h"
+#include "MRId.h"
+
+namespace MR
+{
+
+struct PointAndDistance
+{
+    /// a cloud's point
+    VertId vId;
+
+    /// the distance from input location to point vId considering point's weight
+    float dist = 0;
+
+    /// check for validity, otherwise there is no point closer than maxDistance
+    [[nodiscard]] bool valid() const { return vId.valid(); }
+    [[nodiscard]] explicit operator bool() const { return vId.valid(); }
+};
+
+struct VariadicOffsetParams
+{
+    /// find closest point only if the distance to it is less than given value
+    float maxDistance = 0;
+
+    /// maximal weight among all points in the cloud; too big value here results in more computations
+    float maxWeight = 0;
+
+    /// maximal magnitude of gradient of points' weight in the cloud; too big value here results in more computations
+    float maxWeightGrad = 0;
+};
+
+/// consider a point cloud where each point has additive weight,
+/// and the distance to a point is considered equal to (euclidean distance - weight),
+/// finds the point with minimal distance to given 3D location
+[[nodiscard]] MRMESH_API PointAndDistance findClosestWeightedPoint( const Vector3f & loc,
+    const AABBTreePoints& tree, const VertMetric& pointWeights, const VariadicOffsetParams& params );
+
+} //namespace MR

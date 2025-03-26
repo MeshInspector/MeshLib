86a170101dcc327abab28ab49dc16b456b531e22
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-03-26 15:23:07 +0300

    BallRadiusAssessor

--- source/MRMesh/MRClosestWeightedPoint.cpp
+++ source/MRMesh/MRClosestWeightedPoint.cpp
@@ -5,6 +5,63 @@
 namespace MR
 {
 
+namespace
+{
+
+class BallRadiusAssessor
+{
+public:
+    explicit BallRadiusAssessor( const DistanceFromWeightedPointsComputeParams& params )
+        : params_( params )
+        , maxSearchRadius_( params.maxDistance + params.maxWeight )
+        , maxLocWeight_ ( params.maxWeight )
+    {
+    }
+
+    /// call this function on each found point:
+    /// \param r distance from requested location to found point
+    /// \param w weight of found point
+    /// \return true if maxSearchRadius was reduced during the call
+    bool pointFound( float r, float w );
+
+    float maxSearchRadius() const { return maxSearchRadius_; }
+
+private:
+    const DistanceFromWeightedPointsComputeParams& params_;
+
+    float maxSearchRadius_ = 0;
+
+    /// the weight of hypothetical point at loc is no more than this value
+    float maxLocWeight_ = 0;
+};
+
+bool BallRadiusAssessor::pointFound( float r, float w )
+{
+    assert( r >= 0 );
+    assert( w <= params_.maxWeight );
+    assert( w <= maxLocWeight_ + r * params_.maxWeightGrad );
+    if ( params_.maxWeightGrad < 1 )
+    {
+        // assess the maximal weight of hypothetical point at loc
+        const auto locWeight = w + r * params_.maxWeightGrad;
+        if ( locWeight < maxLocWeight_ )
+        {
+            // try to reduce search radius knowing that the weights of nearby points are limited by known gradient
+            maxLocWeight_ = locWeight;
+            const auto searchRadius = ( params_.maxDistance + maxLocWeight_ ) / ( 1 - params_.maxWeightGrad );
+            assert( searchRadius >= 0 ); // if params_.maxDistance + maxLocWeight_ >= 0 and params_.maxWeightGrad < 1
+            if ( searchRadius < maxSearchRadius_ )
+            {
+                maxSearchRadius_ = searchRadius;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+} // anonymous namespace
+
 PointAndDistance findClosestWeightedPoint( const Vector3f & loc,
     const AABBTreePoints& tree, const DistanceFromWeightedPointsComputeParams& params )
 {
@@ -17,19 +74,14 @@ PointAndDistance findClosestWeightedPoint( const Vector3f & loc,
     assert( params.pointWeight );
     if ( !params.pointWeight )
         return res;
-    auto maxSearchRadius = params.maxDistance + params.maxWeight;
-    if ( maxSearchRadius < 0 )
+    BallRadiusAssessor ballRadiusAssessor( params );
+    if ( ballRadiusAssessor.maxSearchRadius() < 0 )
         return res;
 
-    /// the weight of hypothetical point at loc is no more than this value
-    auto maxLocWeight = params.maxWeight;
-
-    findPointsInBall( tree, { loc, sqr( maxSearchRadius ) }, [&]( const PointsProjectionResult & found, const Vector3f &, Ball3f & ball )
+    findPointsInBall( tree, { loc, sqr( ballRadiusAssessor.maxSearchRadius() ) }, [&]( const PointsProjectionResult & found, const Vector3f &, Ball3f & ball )
     {
         const auto r = std::sqrt( found.distSq );
         const auto w = params.pointWeight( found.vId );
-        assert( w <= params.maxWeight );
-        assert( w <= maxLocWeight + r * params.maxWeightGrad );
         auto dist = r - w;
         if ( dist < res.dist )
         {
@@ -38,22 +90,8 @@ PointAndDistance findClosestWeightedPoint( const Vector3f & loc,
             if ( dist < params.minDistance )
                 return Processing::Stop;
         }
-        if ( params.maxWeightGrad < 1 )
-        {
-            // assess the maximal weight of hypothetical point at loc
-            const auto locWeight = w + r * params.maxWeightGrad;
-            if ( locWeight < maxLocWeight )
-            {
-                // try to reduce search radius knowing that the weights of nearby points are limited by known gradient
-                maxLocWeight = locWeight;
-                const auto searchRadius = ( params.maxDistance + maxLocWeight ) / ( 1 - params.maxWeightGrad );
-                if ( searchRadius < maxSearchRadius )
-                {
-                    maxSearchRadius = searchRadius;
-                    ball.radiusSq = sqr( maxSearchRadius );
-                }
-            }
-        }
+        if ( ballRadiusAssessor.pointFound( r, w ) )
+            ball.radiusSq = sqr( ballRadiusAssessor.maxSearchRadius() );
         return Processing::Continue;
     } );
     return res;
@@ -63,12 +101,24 @@ MeshPointAndDistance findClosestWeightedMeshPoint( const Vector3f& loc,
     const MeshPart& mp, const DistanceFromWeightedPointsComputeParams& params )
 {
     MeshPointAndDistance res;
+    assert( params.pointWeight );
+    if ( !params.pointWeight )
+        return res;
+    BallRadiusAssessor ballRadiusAssessor( params );
+    if ( ballRadiusAssessor.maxSearchRadius() < 0 )
+        return res;
+
     // first consider only mesh vertices ignoring triangles
     {
         auto ptRes = findClosestWeightedPoint( loc, mp.mesh.getAABBTreePoints(), params );
         res.dist = ptRes.dist;
         if ( ptRes.vId )
+        {
             res.mtp = MeshTriPoint( mp.mesh.topology, ptRes.vId );
+            const auto r = distance( loc, mp.mesh.points[ptRes.vId] );
+            const auto w = params.pointWeight( ptRes.vId );
+            ballRadiusAssessor.pointFound( r, w );
+        }
     }
 
     return res;

fc2512e26445cd10d9ac8196a995a1111c8343f1
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-03-31 13:56:13 +0300

    EdgeLengthMesh::isDelone and MeshTopology::getLeftTriEdges (#4348)

--- source/MRMesh/MREdgeLengthMesh.h
+++ source/MRMesh/MREdgeLengthMesh.h
@@ -15,6 +15,19 @@ struct [[nodiscard]] EdgeLengthMesh
 
     /// construct EdgeLengthMesh from an ordinary mesh
     [[nodiscard]] MRMESH_API static EdgeLengthMesh fromMesh( const Mesh& mesh );
+
+    /// computes cotangent of the angle in the left( e ) triangle opposite to e,
+    /// and returns 0 if left face does not exist
+    [[nodiscard]] MRMESH_API float leftCotan( EdgeId e ) const;
+
+    /// computes sum of cotangents of the angle in the left and right triangles opposite to given edge,
+    /// consider cotangents zero for not existing triangles
+    [[nodiscard]] float cotan( UndirectedEdgeId ue ) const { EdgeId e{ ue }; return leftCotan( e ) + leftCotan( e.sym() ); }
+
+    /// returns true if given edge satisfies Delaunay conditions,
+    /// returns false if the edge needs to be flipped to satisfy Delaunay conditions,
+    /// passing negative threshold makes more edges satisfy Delaunay conditions
+    [[nodiscard]] bool isDelone( UndirectedEdgeId ue, float threshold = 0 ) const { return cotan( ue ) >= threshold; }
 };
 
 } //namespace MR

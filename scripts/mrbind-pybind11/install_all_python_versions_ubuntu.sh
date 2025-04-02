7624821aad4c7f03980f907050c44653f528178a
Max Raiskii <max.raiskii@meshinspector.com>
2025-04-03 00:57:02 +0300

    mrbind install_all_python_versions_ubuntu: rm sudo

--- scripts/mrbind-pybind11/install_all_python_versions_ubuntu.sh
+++ scripts/mrbind-pybind11/install_all_python_versions_ubuntu.sh
@@ -10,16 +10,16 @@ PY_VERSIONS="$(cat $SCRIPT_DIR/python_versions.txt | xargs)"
 echo "Python versions: $PY_VERSIONS"
 
 # Add the PPA.
-sudo apt -y update && sudo apt -y upgrade && sudo apt -y install software-properties-common curl
-sudo add-apt-repository -y ppa:deadsnakes/ppa
+apt -y update && apt -y upgrade && apt -y install software-properties-common curl
+add-apt-repository -y ppa:deadsnakes/ppa
 
 # Install the packages.
-sudo apt -y install $(echo $PY_VERSIONS | perl -pe 's/(\S+)/python\1-dev python\1-venv/g')
+apt -y install $(echo $PY_VERSIONS | perl -pe 's/(\S+)/python\1-dev python\1-venv/g')
 
 # Install distutils (needed for pip), but only for Python versions for which it exists.
 for ver in $PY_VERSIONS; do
     if apt show python$ver-distutils >/dev/null 2>/dev/null; then
-        sudo apt -y install python$ver-distutils
+        apt -y install python$ver-distutils
     fi
 done
 

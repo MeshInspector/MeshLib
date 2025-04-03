28bea58485ae5b29a2af86fa4557b3e2796be4fc
Anton Chernobrovkin <anton.chernobrovkin@meshinspector.com>
2025-04-03 19:55:52 +0400

    Add test and sample on variable offset

--- examples/python-examples/MeshOffsetWeighted.dox.py
+++ examples/python-examples/MeshOffsetWeighted.dox.py
@@ -10,13 +10,13 @@ verts_num = mesh.points.size()
 # Create VertScalars obj with weights for every vertice
 scalars = mrmeshpy.VertScalars(verts_num)
 for i in range(verts_num):
-    weight = abs(mesh.points.vec[i].x / 5) + 0.1 # Offset size for point
+    weight = abs(mesh.points.vec[i].x / 5) + 0.2 # Offset size for point
     scalars.vec[i] = weight
 
 # ===params
 
 params = mrmeshpy.WeightedPointsShellParameters()
-params.offset = 0.1 # common offset that will be added for every point to weighted
+params.offset = 0.0 # common offset that will be added for every point to weighted
 # Algorithm is voxel based, voxel size affects performance and form of result mesh
 params.voxelSize = mrmeshpy.suggestVoxelSize(mesh, 10000)
 params.dist.maxWeight = max(scalars.vec) # should always have maximum between weights provided

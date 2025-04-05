bf6af9759f6ed8d63df2879f9a974fbd7e1ab92a
Andrey Aralov <andrey.aralov@meshinspector.com>
2025-04-05 14:27:14 +0200

    Add region based `weightedMeshShell` overload.

--- source/MRVoxels/MRWeightedPointsShell.cpp
+++ source/MRVoxels/MRWeightedPointsShell.cpp
@@ -7,6 +7,7 @@
 #include "MRMesh/MRAABBTreePoints.h"
 #include "MRMesh/MRTimer.h"
 #include "MRMesh/MRIsNaN.h"
+#include "MRMesh/MRPointsInBall.h"
 
 namespace MR
 {
@@ -65,7 +66,7 @@ FunctionVolume weightedMeshToDistanceFunctionVolume( const Mesh & mesh, const We
     };
 }
 
-Expected<Mesh> weightedPointsShell( const PointCloud & cloud, const WeightedPointsShellParameters& params )
+Expected<Mesh> weightedPointsShell( const PointCloud & cloud, const WeightedPointsShellParametersMetric& params )
 {
     MR_TIMER
 
@@ -103,15 +104,8 @@ Expected<Mesh> weightedPointsShell( const PointCloud & cloud, const WeightedPoin
     return marchingCubes( weightedPointsToDistanceFunctionVolume( cloud, wp2vparams ), vmParams );
 }
 
-Expected<Mesh> weightedPointsShell( const PointCloud & cloud, const VertScalars& pointWeights,
-    const WeightedPointsShellParameters& params0 )
-{
-    auto params = params0;
-    params.dist.pointWeight = [&pointWeights]( VertId v ){ return pointWeights[v]; };
-    return weightedPointsShell( cloud, params );
-}
 
-Expected<Mesh> weightedMeshShell( const Mesh & mesh, const WeightedPointsShellParameters& params )
+Expected<Mesh> weightedMeshShell( const Mesh & mesh, const WeightedPointsShellParametersMetric& params )
 {
     MR_TIMER
 
@@ -149,11 +143,63 @@ Expected<Mesh> weightedMeshShell( const Mesh & mesh, const WeightedPointsShellPa
     return marchingCubes( weightedMeshToDistanceFunctionVolume( mesh, wp2vparams ), vmParams );
 }
 
-Expected<Mesh> weightedMeshShell( const Mesh & mesh, const VertScalars& vertWeights, const WeightedPointsShellParameters& params0 )
+Expected<Mesh> weightedMeshShell( const Mesh& mesh, const WeightedPointsShellParametersRegions& params )
 {
-    auto params = params0;
-    params.dist.pointWeight = [&vertWeights]( VertId v ){ return vertWeights[v]; };
-    return weightedMeshShell( mesh, params );
+    VertBitSet allVerts;
+    for ( const auto& reg : params.regions )
+        allVerts |= allVerts | reg.verts;
+
+    const auto interRadSq = sqr( params.interpolationDist );
+    auto pointWeight = [&params, &mesh, &allVerts, interRadSq] ( VertId v )
+    {
+        if ( params.regions.empty() )
+            return 0.f;
+        if ( !allVerts.test( v ) )
+            return 0.f;
+        float res = 0.f;
+        size_t n = 0;
+
+        const auto pt = mesh.points[v];
+        findPointsInBall( mesh, Ball3f( pt, interRadSq ), [&n, &res, &params]
+            ( const PointsProjectionResult & found, const Vector3f &, Ball3f & )
+        {
+            auto vv = found.vId;
+            for ( const auto& reg : params.regions )
+            {
+                if ( reg.verts.test( vv ) )
+                    res += reg.value;
+            }
+            n += 1;
+            return Processing::Continue;
+        } );
+
+        return res / static_cast<float>( n );
+    };
+
+    // precalculate the weights
+    Vector<float, VertId> weights( allVerts.find_last() + 1 );
+    for ( auto v : allVerts )
+        weights[v] = pointWeight( v );
+
+    DistanceFromWeightedPointsParams distParams;
+    distParams.pointWeight = [weights = std::move( weights ), allVerts] ( VertId v ) mutable
+    {
+        if ( allVerts.test( v ) )
+            return weights[v];
+        else
+            return 0.f;
+    };
+
+    distParams.maxWeight = std::numeric_limits<float>::min();
+    for ( auto v : allVerts )
+    {
+        distParams.maxWeight = std::max( distParams.maxWeight, distParams.pointWeight( v ) );
+    }
+
+    WeightedPointsShellParametersMetric resParams{ static_cast<WeightedPointsShellParametersBase>( params ), distParams };
+
+    return weightedMeshShell( mesh, resParams );
 }
+
 
 } //namespace MR
--- source/MRVoxels/MRWeightedPointsShell.h
+++ source/MRVoxels/MRWeightedPointsShell.h
@@ -5,6 +5,7 @@
 #include "MRMesh/MRExpected.h"
 #include "MRMesh/MRProgressCallback.h"
 #include "MRMesh/MRClosestWeightedPoint.h"
+#include "MRMesh/MRBitSet.h"
 #include "MRPch/MRBindingMacros.h"
 
 namespace MR
@@ -27,7 +28,7 @@ struct WeightedPointsToDistanceVolumeParams
 /// makes FunctionVolume representing minimal distance to mesh with weighted vertices
 [[nodiscard]] MRVOXELS_API FunctionVolume weightedMeshToDistanceFunctionVolume( const Mesh & mesh, const WeightedPointsToDistanceVolumeParams& params );
 
-struct WeightedPointsShellParameters
+struct WeightedPointsShellParametersBase
 {
     /// build iso-surface of minimal distance to points corresponding to this value
     float offset = 0;
@@ -35,9 +36,6 @@ struct WeightedPointsShellParameters
     /// Size of voxel in grid conversions;
     /// The user is responsible for setting some positive value here
     float voxelSize = 0;
-
-    /// parameters of distance finding
-    DistanceFromWeightedPointsParams dist;
 
     /// if true, then the distance will get its sign from the normal of the closest point (positive values in the half space pointed by normal);
     /// initial distances must be unsigned then (e.g. all point weights are negative);
@@ -52,26 +50,40 @@ struct WeightedPointsShellParameters
     MR_BIND_PREFER_UNLOCK_GIL_WHEN_USED_AS_PARAM
 };
 
+struct WeightedPointsShellParametersMetric : WeightedPointsShellParametersBase
+{
+    /// parameters of distance finding
+    DistanceFromWeightedPointsParams dist;
+};
+
+struct WeightedPointsShellParametersRegions : WeightedPointsShellParametersBase
+{
+    struct PartialVertScalars
+    {
+        VertBitSet verts;
+        float value = 0.f;
+    };
+
+    /// list of regions (overlappings are allowed) with corresponding offsets
+    std::vector<PartialVertScalars> regions;
+
+    /// interpolation factor between the weights of the regions
+    float interpolationDist = 0;
+};
+
 /// consider a point cloud where each point has additive weight,
 /// and the distance to a point is considered equal to (euclidean distance - weight),
 /// constructs iso-surface of such distance field corresponding to params.offset value using marching cubes
 // MR_BIND_IGNORE to hide this function in Python API because calling Python's Lambda will be extremely slow anyway
-[[nodiscard]] MR_BIND_IGNORE MRVOXELS_API Expected<Mesh> weightedPointsShell( const PointCloud & cloud, const WeightedPointsShellParameters& params );
-
-/// consider a point cloud where each point has additive weight (taken from pointWeights and not from params),
-/// and the distance to a point is considered equal to (euclidean distance - weight),
-/// constructs iso-surface of such distance field corresponding to params.offset value using marching cubes
-[[nodiscard]] MRVOXELS_API Expected<Mesh> weightedPointsShell( const PointCloud & cloud, const VertScalars& pointWeights, const WeightedPointsShellParameters& params );
+[[nodiscard]] MR_BIND_IGNORE MRVOXELS_API Expected<Mesh> weightedPointsShell( const PointCloud & cloud, const WeightedPointsShellParametersMetric& params );
 
 /// consider a mesh where each vertex has additive weight, and this weight is linearly interpolated in mesh triangles,
 /// and the distance to a point is considered equal to (euclidean distance - weight),
 /// constructs iso-surface of such distance field corresponding to params.offset value using marching cubes
 // MR_BIND_IGNORE to hide this function in Python API because calling Python's Lambda will be extremely slow anyway
-[[nodiscard]] MR_BIND_IGNORE MRVOXELS_API Expected<Mesh> weightedMeshShell( const Mesh & mesh, const WeightedPointsShellParameters& params );
+[[nodiscard]] MR_BIND_IGNORE MRVOXELS_API Expected<Mesh> weightedMeshShell( const Mesh & mesh, const WeightedPointsShellParametersMetric& params );
 
-/// consider a mesh where each vertex has additive weight (taken from vertWeights and not from params), and this weight is linearly interpolated in mesh triangles,
-/// and the distance to a point is considered equal to (euclidean distance - weight),
-/// constructs iso-surface of such distance field corresponding to params.offset value using marching cubes
-[[nodiscard]] MRVOXELS_API Expected<Mesh> weightedMeshShell( const Mesh & mesh, const VertScalars& vertWeights, const WeightedPointsShellParameters& params );
+/// this overload supports linear interpolation between the regions with different weight
+[[nodiscard]] MR_BIND_IGNORE MRVOXELS_API Expected<Mesh> weightedMeshShell( const Mesh& mesh, const WeightedPointsShellParametersRegions& params );
 
 } //namespace MR

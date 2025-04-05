0bd4be3154ba3bd4da5ad18e79d4c3123e52ff21
Max Raiskii <max.raiskii@meshinspector.com>
2025-04-05 15:54:35 +0300

    mv install_all_python_versions_ubuntu to dockerfile

--- scripts/mrbind-pybind11/install_all_python_versions_ubuntu.sh
+++ scripts/mrbind-pybind11/install_all_python_versions_ubuntu.sh
@@ -10,16 +10,16 @@ PY_VERSIONS="$(cat $SCRIPT_DIR/python_versions.txt | xargs)"
 echo "Python versions: $PY_VERSIONS"
 
 # Add the PPA.
-apt -y update && apt -y upgrade && apt -y install software-properties-common curl
-add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt -y update && sudo apt -y upgrade && sudo apt -y install software-properties-common curl
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 
 # Install the packages.
-apt -y install $(echo $PY_VERSIONS | perl -pe 's/(\S+)/python\1-dev python\1-venv/g')
+sudo apt -y install $(echo $PY_VERSIONS | perl -pe 's/(\S+)/python\1-dev python\1-venv/g')
 
 # Install distutils (needed for pip), but only for Python versions for which it exists.
 for ver in $PY_VERSIONS; do
     if apt show python$ver-distutils >/dev/null 2>/dev/null; then
-        apt -y install python$ver-distutils
+        sudo apt -y install python$ver-distutils
     fi
 done
 

2acabcdb485bc48d27090577beb58c6dafd65a57
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-04-14 00:45:10 +0300

    improvements

--- source/MRMesh/MRPrecisePredicates2ccw.py
+++ source/MRMesh/MRPrecisePredicates2ccw.py
@@ -6,7 +6,9 @@ pay=ay+e
 pbx=bx+e**8
 pby=by+e**4
 d=det(Matrix([[pax,pay],[pbx,pby]]))
-for i in range(7):
+for i in range(1000):
      c = d.coeff(e,i)
      if c != 0:
          print(f'{i}: {c}')
+     if c == 1 or c == -1:
+         break

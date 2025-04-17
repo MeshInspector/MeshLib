42954d638a5582a28402cc4c0201a0991261a880
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2025-04-17 19:51:10 +0300

    Support wall angle in find undercuts (#4469)

--- source/MRVoxels/MRToolPath.cpp
+++ source/MRVoxels/MRToolPath.cpp
@@ -95,7 +95,7 @@ Expected<Mesh> preprocessMesh( const Mesh& inputMesh, const ToolPathParams& para
     if ( !reportProgress( params.cb, 0.15f ) )
         return unexpectedOperationCanceled();
     
-    FixUndercuts::fix( meshCopy, { .upDirection = Vector3f::plusZ(),.voxelSize = params.voxelSize } );
+    FixUndercuts::fix( meshCopy, { .findParameters = {.upDirection = Vector3f::plusZ()},.voxelSize = params.voxelSize } );
     
     if ( !reportProgress( params.cb, 0.20f ) )
         return unexpectedOperationCanceled();

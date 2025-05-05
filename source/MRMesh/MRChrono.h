3368f0e01bb332db6506584e76b1f3ebc30f49f2
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-05-05 06:54:01 -0500

    Don't bind `MR::Localtime()`.

--- source/MRMesh/MRChrono.h
+++ source/MRMesh/MRChrono.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "MRMeshFwd.h"
+#include "MRPch/MRBindingMacros.h"
 
 #include <ctime>
 #include <optional>
@@ -9,9 +10,9 @@ namespace MR
 {
 
 // A threadsafe equivalent for `std::localtime()`. Returns null on failure.
-[[nodiscard]] MRMESH_API std::optional<std::tm> Localtime( std::time_t time );
+[[nodiscard]] MRMESH_API MR_BIND_IGNORE std::optional<std::tm> Localtime( std::time_t time );
 
 // Same, but returns a struct full of zeroes on error.
-[[nodiscard]] MRMESH_API std::tm LocaltimeOrZero( std::time_t time );
+[[nodiscard]] MRMESH_API MR_BIND_IGNORE std::tm LocaltimeOrZero( std::time_t time );
 
 }

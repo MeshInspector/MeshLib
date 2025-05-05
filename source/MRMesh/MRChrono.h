5d50c1b125eb5c5acde86c3089fcb738c994e978
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-05-05 05:56:18 -0500

    Fix formatting.

--- source/MRMesh/MRChrono.h
+++ source/MRMesh/MRChrono.h
@@ -8,6 +8,6 @@ namespace MR
 {
 
 // A threadsafe equivalent for `std::localtime()`. Returns null on failure.
-[[nodiscard]] MRMESH_API std::optional<std::tm> Localtime(std::time_t time);
+[[nodiscard]] MRMESH_API std::optional<std::tm> Localtime( std::time_t time );
 
 }

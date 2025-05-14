0999539009ce2026b0ffdccf854b4504be6c7344
Andrey Aralov <andrey.aralov@meshinspector.com>
2025-05-14 03:38:44 +0200

    Add continuity test with a weight.

--- source/MRTest/MRPointCloudVariadicOffsetTests.cpp
+++ source/MRTest/MRPointCloudVariadicOffsetTests.cpp
@@ -2,6 +2,9 @@
 #include <MRMesh/MRPointCloud.h>
 #include <MRMesh/MRMesh.h>
 #include <MRMesh/MRGTest.h>
+#include <MRMesh/MRCube.h>
+#include <MRMesh/MRMeshComponents.h>
+#include <MRMesh/MRParallelFor.h>
 
 namespace MR
 {
@@ -93,6 +96,58 @@ TEST( MRMesh, findClosestWeightedMeshPoint )
         for ( float z = -2; z <= 2; z += 0.1f )
             EXPECT_NEAR( distance( Vector3f( 0, 0, z ) ),  1 + std::abs( z ), 1e-7f );
     }
+}
+
+TEST( MRMesh, findClosestWeightedMeshPointContinuity )
+{
+    auto cube = makeCube();
+    DistanceFromWeightedPointsComputeParams params;
+    auto distance = [&]( Vector3f loc )
+    {
+        auto pd = findClosestWeightedMeshPoint( loc, cube, params );
+        assert( !pd.mtp.onEdge( mesh.topology ) );
+        return pd.dist;
+    };
+
+    params.pointWeight  = [] ( VertId ) { return 0.f; };
+    params.maxWeight = 0.f;
+    params.bidirectionalMode = false;
+    params.pointWeight = [v0 = cube.topology.getValidVerts().find_first()] ( VertId v ) { return ( v == v0 ) ? 0.1f : 0.f; };
+
+    constexpr float step = 0.05f;
+    constexpr float gradStep = 0.001f;
+    constexpr float rangeMin = -0.8f;
+    constexpr float rangeMax = 0.8f;
+    std::vector<float> zVals;
+    for ( float z = rangeMin; z < rangeMax; z += step )
+        zVals.push_back( z );
+
+    // check that distance is continuous
+    tbb::enumerable_thread_specific<float> threadMaxGrad;
+    ParallelFor( zVals, [&] ( size_t iz ) {
+        float z = zVals[iz];
+        float localMaxGrad = 0.f;
+        for ( float y = rangeMin; y < rangeMax; y += step )
+        {
+            for ( float x = rangeMin; x < rangeMax; x += step )
+            {
+                Vector3f pt{ x, y, z };
+                Vector3f grad{
+                        ( distance( pt + Vector3f::plusX() * gradStep ) - distance( pt - Vector3f::plusX() * gradStep ) ) / ( 2.f * gradStep ),
+                        ( distance( pt + Vector3f::plusY() * gradStep ) - distance( pt - Vector3f::plusY() * gradStep ) ) / ( 2.f * gradStep ),
+                        ( distance( pt + Vector3f::plusZ() * gradStep ) - distance( pt - Vector3f::plusZ() * gradStep ) ) / ( 2.f * gradStep )
+                };
+                localMaxGrad = std::max( localMaxGrad, grad.length() );
+            }
+        }
+        threadMaxGrad.local() = localMaxGrad;
+    } );
+
+    float maxGrad = 0.f;
+    for ( float val : threadMaxGrad )
+        maxGrad = std::max( maxGrad, val );
+
+    EXPECT_NEAR( maxGrad, 1.f, 0.1f ); // gradient should be 1 as distance should change linearly
 }
 
 } //namespace MR

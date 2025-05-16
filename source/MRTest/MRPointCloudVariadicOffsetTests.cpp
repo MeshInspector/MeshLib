6923e7311a8388bb8c72002a90a191a1d026ff1d
Andrey Aralov <andrey.aralov@meshinspector.com>
2025-05-16 14:49:52 +0200

    New test with problem near sharp angle.

--- source/MRTest/MRPointCloudVariadicOffsetTests.cpp
+++ source/MRTest/MRPointCloudVariadicOffsetTests.cpp
@@ -216,4 +216,42 @@ TEST( MRMesh, WeightedClosed )
     ASSERT_FLOAT_EQ( maxDiff, 1.f );
 }
 
+TEST( MRMesh, findClosestWeightedMeshPointSharpAngle )
+{
+    Triangulation t{
+        { 0_v, 2_v, 1_v },
+        { 0_v, 1_v, 3_v }
+    };
+    VertCoords vs{
+        { 1, 0, 0 },        //0_v
+        { 0, 0, 0 },        //1_v
+        { 0.5, 0.5, 1 },    //2_v
+        { -0.5, -0.5, 1 }   //3_v
+    };
+    auto mesh = Mesh::fromTriangles( std::move( vs ), t );
+
+    DistanceFromWeightedPointsComputeParams params;
+    params.pointWeight = [&]( VertId ) { return 0.05f; };
+    params.maxWeight = 0;
+    params.bidirectionalMode = false;
+
+    auto smartDistance = [&]( Vector3f loc )
+    {
+        auto pd = findClosestWeightedMeshPoint( loc, mesh, params );
+        assert( !pd.mtp.onEdge( mesh.topology ) );
+        return pd.dist();
+    };
+
+    std::ofstream f( "/tmp/test.csv" );
+    f << "z,d\n";
+    for ( float z = -0.5; z <= 0.5; z += 0.05f )
+        f << z << ',' << smartDistance( Vector3f( 0.1, 0.1, z ) ) << '\n';
+    f.close();
+
+//    float maxDiff = 0.f;
+//    for ( float z = -1; z <= 4; z += 0.1f )
+//        maxDiff = std::max( maxDiff, std::abs( smartDistance( Vector3f{ 0.f, 0.f, z - 0.5f } ) - smartDistance( Vector3f{ 0.f, 0.f, z + 0.5f } ) ) );
+//    ASSERT_FLOAT_EQ( maxDiff, 1.f );
+}
+
 } //namespace MR

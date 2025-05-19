6741129057a5c9cab7a5b343a1af3145a124a2e1
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-05-19 13:58:41 +0300

    MapOrHashMap

---
+++ source/MRMesh/MRMapOrHashMap.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "MRVector.h"
+#include "MRphmap.h"
+#include <variant>
+
+namespace MR
+{
+
+template <typename K, typename V>
+struct MapOrHashMap
+{
+    using Map = Vector<V, K>;
+    using HashMap = HashMap<K, V>;
+    std::variant<Map, HashMap> var;
+
+    static MapOrHashMap createMap( size_t size = 0 );
+    static MapOrHashMap createHashMap( size_t capacity = 0 );
+
+    bool isMap() const;
+    bool isHashMap() const { return !isMap(); }
+};
+
+template <typename K, typename V>
+inline MapOrHashMap<K,V> MapOrHashMap<K,V>::createMap( size_t size )
+{
+    MapOrHashMap<K,V> res;
+    res.var = Map( size );
+    return res;
+}
+
+template <typename K, typename V>
+inline MapOrHashMap<K,V> MapOrHashMap<K,V>::createHashMap( size_t capacity )
+{
+    MapOrHashMap<K,V> res;
+    HashMap hmap;
+    if ( capacity > 0 )
+        hmap.reserve( capacity );
+    res.var = std::move( hmap );
+    return res;
+}
+
+template <typename K, typename V>
+inline bool MapOrHashMap<K,V>::isMap() const
+{
+    std::visit( overloaded{
+        []( const Map& map ) { return true; },
+        []( const HashMap& hashMap ) { return false; }
+    }, var );
+}
+
+template <typename K, typename V>
+inline V getAt( const MapOrHashMap<K, V> & m, K key, V def = {} )
+{
+    std::visit( overloaded{
+        []( const Vector<V, K>& map ) { getAt( map, key, def ); },
+        []( const HashMap<K, V>& hashMap ) { getAt( hashMap, key, def ); }
+    }, m.var );
+}
+
+template <typename K, typename V>
+inline void setAt( MapOrHashMap<K, V> & m, K key, V val )
+{
+    std::visit( overloaded{
+        []( Vector<V, K>& map ) { map[key] = val; },
+        []( HashMap<K, V>& hashMap ) { hashMap[key] = val; }
+    }, m.var );
+}
+
+} //namespace MR
--- source/MRMesh/MRPartMapping.h
+++ source/MRMesh/MRPartMapping.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "MRphmap.h"
+#include "MRMapOrHashMap.h"
 
 namespace MR
 {

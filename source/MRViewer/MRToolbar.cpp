4f46662151fd3ab4c25f8017e4ec9758b85eeeda
Alexandr Burakov <alexandr.burakov@meshinspector.com>
2025-05-21 20:50:54 +0300

    fix

--- source/MRViewer/MRToolbar.cpp
+++ source/MRViewer/MRToolbar.cpp
@@ -404,7 +404,7 @@ void Toolbar::drawCustomizeModal_()
     {
         searchResult_.clear();
         searchResult_.resize( RibbonSchemaHolder::schema().tabsMap.size() );
-        auto searchResRaw_ = RibbonSchemaHolder::search( searchString_ );
+        auto searchResRaw_ = RibbonSchemaHolder::search( searchString_, {} );
         for ( const auto& sr : searchResRaw_ )
         {
             if ( sr.tabIndex < 0 )

994ded9e049edd5894749974236984abd95baa18
sergeypetrochenkov <sergey.a.petrochenkov@gmail.com>
2025-05-28 22:01:22 +0400

    Add comments

--- source/MRMesh/MRGeomDataKeeper.h
+++ source/MRMesh/MRGeomDataKeeper.h
@@ -2,11 +2,19 @@
 
 namespace MR
 {
+
+// If the Object is GeomDataKeeper please call these methods before and after changing the geometry
+
 class MRMESH_CLASS GeomDataKeeper
 {
 public:
     virtual ~GeomDataKeeper() = default;
+
+    /// Call it before changing geometry
     MRMESH_API virtual void beforeGeometryChange() = 0;
+
+    /// Call it right after geometry changed. Notice that it creates additional history actions and store it in the history store.
+    /// So you must use SCOPED_HISORY to agregate all history actions into one record
     MRMESH_API virtual void onGeometryChanged() = 0;
 };
 } // namespace MR

2119eab9af0cf2fb45731b2a628e48cf8bfd7260
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-05-29 13:18:57 +0300

    history

--- source/MRCommonPlugins/ViewerButtons/MRRotatorPlugin.cpp
+++ source/MRCommonPlugins/ViewerButtons/MRRotatorPlugin.cpp
@@ -4,9 +4,12 @@
 #include "MRViewer/MRUIStyle.h"
 #include "MRViewer/MRViewerInstance.h"
 #include "MRViewer/MRViewport.h"
+#include "MRViewer/MRAppendHistory.h"
 #include "MRMesh/MRLine3.h"
 #include "MRMesh/MRSceneRoot.h"
 #include "MRMesh/MRObjectsAccess.h"
+#include "MRMesh/MRChangeXfAction.h"
+#include "MRMesh/MRCombinedHistoryAction.h"
 
 namespace MR
 {
@@ -23,9 +26,13 @@ private:
     bool onEnable_() override;
     bool onDisable_() override;
     void preDraw_() override;
+    /// returns true only if on the top of undo stack there is not our own action (not to make new undo on each frame)
+    bool shouldCreateNewHistoryAction_( const std::vector<std::shared_ptr<Object>>& selObjs ) const;
 
     float rotationSpeed_ = 5 * PI_F / 180;
     bool rotateCamera_ = true;
+
+    std::weak_ptr<CombinedHistoryAction> myLastHistoryAction_;
 };
 
 RotatorPlugin::RotatorPlugin() :
@@ -56,7 +63,32 @@ bool RotatorPlugin::onEnable_()
 
 bool RotatorPlugin::onDisable_()
 {
+    myLastHistoryAction_.reset();
     return true;
+}
+
+bool RotatorPlugin::shouldCreateNewHistoryAction_( const std::vector<std::shared_ptr<Object>>& selObjs ) const
+{
+    if ( selObjs.empty() )
+        return false;
+    const auto& hs = HistoryStore::getViewerInstance();
+    if ( !hs )
+        return false;
+    auto a = myLastHistoryAction_.lock();
+    if ( !a )
+        return true;
+    if ( hs->getLastAction( HistoryAction::Type::Undo ) != a )
+        return true;
+    if ( a->getStack().size() != selObjs.size() )
+        return true;
+    for ( int i = 0; i < selObjs.size(); ++i )
+    {
+        auto c = dynamic_cast<ChangeXfAction*>( a->getStack()[i].get() );
+        assert( c );
+        if ( !c || c->obj() != selObjs[i] )
+            return true;
+    }
+    return false;
 }
 
 void RotatorPlugin::preDraw_()
@@ -74,11 +106,21 @@ void RotatorPlugin::preDraw_()
     }
     else
     {
+        const auto selObjs = getAllObjectsInTree<Object>( &SceneRoot::get(), ObjectSelectivityType::Selected );
+        bool appendHistory = shouldCreateNewHistoryAction_( selObjs );
+        std::optional<ScopeHistory> scope;
+        if ( appendHistory )
+        {
+            scope.emplace( "Rotator" );
+            myLastHistoryAction_ = scope->combinedAction();
+        }
         const auto rotMat = Matrix3f::rotation( viewport.getUpDirection(), deltaAngle );
-        for ( const auto & obj : getAllObjectsInTree<Object>( &SceneRoot::get(), ObjectSelectivityType::Selected ) )
+        for ( const auto & obj : selObjs )
         {
             const auto wXf = obj->worldXf();
             const auto wCenter = obj->getWorldTreeBox().center();
+            if ( appendHistory )
+                AppendHistory<ChangeXfAction>( obj->name(), obj );
             obj->setWorldXf( AffineXf3f::xfAround( rotMat, wCenter ) * wXf );
         }
     }

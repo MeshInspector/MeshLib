a34895bd6030f0e1f3eed9dd191c27145627ef7e
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2025-06-09 23:39:31 +0400

    Unite corner controller code (#4677)
    * Unite corner controller code
    
    * fix header

--- source/MRViewer/MRViewportCornerController.h
+++ source/MRViewer/MRViewportCornerController.h
@@ -1,8 +1,55 @@
 #pragma once
 #include "MRViewerFwd.h"
+#include "MRMesh/MRViewportId.h"
+#include "MRMesh/MRId.h"
+#include "MRMesh/MRSignal.h"
 
 namespace MR
 {
+
+enum class SideRegions
+{
+    CCWArrow = 26,
+    CWArrow = 27
+};
+
+/// class that holds and manages corner controller object
+class MRVIEWER_CLASS CornerControllerObject
+{
+public:
+    struct PickedIds
+    {
+        ViewportId vId;
+        RegionId rId;
+        bool operator==( const PickedIds& other ) const = default;
+    };
+
+    CornerControllerObject() = default;
+
+    /// initialize this object with default settings
+    MRVIEWER_API void initDefault();
+
+    /// enables or disables this object by provided mask
+    MRVIEWER_API void enable( ViewportMask mask );
+
+    /// draw this object in given viewport in position of basis axes
+    MRVIEWER_API void draw( const Viewport& vp, const AffineXf3f& rotXf, const AffineXf3f& vpInvXf );
+
+    /// returns true if objects requires redraw
+    MRVIEWER_API bool getRedrawFlag( ViewportMask mask ) const;
+
+    /// marks this object as drawn
+    MRVIEWER_API void resetRedrawFlag();
+private:
+    std::shared_ptr<Object> rootObj_;
+    std::vector<boost::signals2::scoped_connection> connections_;
+
+    PickedIds pick_( const Vector2f& mousePos ) const;
+    void hover_( const Vector2f& mousePos );
+    bool press_( const Vector2f& mousePos );
+    PickedIds pickedId_;
+};
+
 
 /// <summary>
 /// Makes cube mesh with specified face structure for each 3-rank corner, each 2-rank corner and each side:\n
@@ -11,12 +58,6 @@ namespace MR
 /// <param name="cornerRatio">ratio of side length that is used for corners</param>
 /// <returns>Cube mesh with specified face structure</returns>
 MRVIEWER_API Mesh makeCornerControllerMesh( float size, float cornerRatio = 0.2f );
-
-enum class SideRegions
-{
-    CCWArrow = 26,
-    CWArrow = 27
-};
 
 /// <summary>
 /// Makes planar arrow mesh that will be used for controlling in plane rotation in corner near cube controller
@@ -54,6 +95,6 @@ MRVIEWER_API RegionId getCornerControllerRegionByFace( FaceId face );
 MRVIEWER_API TexturePerFace getCornerControllerHoveredTextureMap( RegionId rId );
 
 /// setup camera for selected viewport by corner controller region
-MRVIEWER_API void updateCurrentViewByControllerRegion( RegionId rId );
+MRVIEWER_API void updateCurrentViewByControllerRegion( CornerControllerObject::PickedIds pickedId );
 
 }

94f59b284f9489409db0af5f0403d6621ccd9611
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-06-10 14:18:02 +0300

    Polyline2RayIntersectDouble unit test

--- source/MRTest/MRPolyline2IntersectTests.cpp
+++ source/MRTest/MRPolyline2IntersectTests.cpp
@@ -21,4 +21,21 @@ TEST( MRMesh, Polyline2RayIntersect )
     ASSERT_EQ( res->distanceAlongLine, 1.0f / 4 );
 }
 
+TEST( MRMesh, Polyline2RayIntersectDouble )
+{
+    const Line2d lineA( { 21.226973, -29.397297 }, { 0.9549145, 0.29688108 } );
+    const Line2d lineB( lineA.p + lineA.d.normalized() * 7., lineA.d );
+    const Contour2f cnt = { { 25.131077f, -16.692158f }, { 28.388832f, -27.170689f }, { 28.726366f, -29.492802f } };
+    Polyline2 pl;
+    pl.addFromPoints( cnt.data(), cnt.size() );
+
+    const auto projResA = rayPolylineIntersect( pl, lineA ); //no intersection in floats
+    EXPECT_TRUE( projResA.has_value() );
+    EXPECT_NEAR( projResA->distanceAlongLine, 7.5f, 1e-6f );
+
+    const auto projResB = rayPolylineIntersect( pl, lineB );
+    EXPECT_TRUE( projResB.has_value() );
+    EXPECT_NEAR( projResB->distanceAlongLine, 0.5f, 1e-6f );
+}
+
 } //namespace MR

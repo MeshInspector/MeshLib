69626dfd763eae0d0a7fbae7dd5a1a3777e5d264
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-06-20 17:56:38 +0300

    better comments

--- source/MRTestC/MRMeshCollidePrecise.c
+++ source/MRTestC/MRMeshCollidePrecise.c
@@ -41,10 +41,10 @@ void testMeshCollidePrecise( void )
     TEST_ASSERT_INT_EQUAL( (int)mrContinuousContoursSize( contours ), 4 )
     TEST_ASSERT_INT_EQUAL( (int)mrContinuousContoursGet( contours, 0 ).size, 71 )
     TEST_ASSERT_INT_EQUAL( (int)mrContinuousContoursGet( contours, 1 ).size, 7 )
-    TEST_ASSERT( mrContinuousContoursGet( contours, 2 ).size == 69 || // x86
-                 mrContinuousContoursGet( contours, 2 ).size == 71 ); // ARM (FMA)
-    TEST_ASSERT( mrContinuousContoursGet( contours, 3 ).size == 9 ||  // x86
-                 mrContinuousContoursGet( contours, 3 ).size == 7 );  // ARM (FMA)
+    TEST_ASSERT( mrContinuousContoursGet( contours, 2 ).size == 69 || // without FMA instruction (default settings for x86 or old compilers for ARM)
+                 mrContinuousContoursGet( contours, 2 ).size == 71 ); // with FMA instruction (modern compilers for ARM)
+    TEST_ASSERT( mrContinuousContoursGet( contours, 3 ).size == 9 ||  // without FMA instruction (default settings for x86 or old compilers for ARM)
+                 mrContinuousContoursGet( contours, 3 ).size == 7 );  // with FMA instruction (modern compilers for ARM)
 
     MROneMeshContours* meshAContours = mrGetOneMeshIntersectionContours( meshA, meshB, contours, true, &conv, NULL );
     MROneMeshContours* meshBContours = mrGetOneMeshIntersectionContours( meshA, meshB, contours, false, &conv, NULL );

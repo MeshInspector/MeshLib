6619a9b058f2a3c228f1013db157f7db17d36370
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-06-24 16:35:54 +0300

    comment spelling

--- source/MRViewer/MRSurfaceManipulationWidget.h
+++ source/MRViewer/MRSurfaceManipulationWidget.h
@@ -59,7 +59,7 @@ public:
     /// reset widget state
     MRVIEWER_API void reset();
     /// lock the mesh region (vertices in this region cannot be moved, added or deleted)
-    /// @note boundary edges can be splitted to improve quality of the patch
+    /// @note boundary edges can be split to improve quality of the patch
     MRVIEWER_API void setFixedRegion( const FaceBitSet& region );
 
     /// set widget settings (mesh change settings)

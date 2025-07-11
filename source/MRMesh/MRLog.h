3e88c5a40ffa77bb1f50c45f5a5b9f34a34da0c1
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-07-11 12:36:09 +0200

    Move cylinder approximator definition out of header file
    - Remove obsolete Logger methods.
    - Move cylinder approximation test to MRTest to check the extern
      template visibility.

--- source/MRMesh/MRLog.h
+++ source/MRMesh/MRLog.h
@@ -24,15 +24,6 @@ class MR_BIND_IGNORE Logger
 public:
     MRMESH_API static Logger& instance();
 
-    /// simple methods to log messages
-    /// for more optimal methods use getSpdLogger()
-    MRMESH_API static void trace( std::string_view msg );
-    MRMESH_API static void debug( std::string_view msg );
-    MRMESH_API static void info( std::string_view msg );
-    MRMESH_API static void warn( std::string_view msg );
-    MRMESH_API static void error( std::string_view msg );
-    MRMESH_API static void critical( std::string_view msg );
-
     /// store this pointer if need to prolong logger life time (necessary to log something from destructors)
     MRMESH_API const std::shared_ptr<spdlog::logger>& getSpdLogger() const;
 

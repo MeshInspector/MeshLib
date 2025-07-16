e18017ce74c46d70426582c808b04aeb5190f33e
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-07-16 18:01:09 +0200

    Fix Clang build

--- scripts/mrbind/install_mrbind_rockylinux.sh
+++ scripts/mrbind/install_mrbind_rockylinux.sh
@@ -15,5 +15,5 @@ rm -rf build
 # Guess the number of build threads.
 [[ ${JOBS:=} ]] || JOBS=$(nproc)
 
-CC=clang CXX=clang++ cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+CC=clang CXX=clang++ cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_FLAGS="-fuse-ld=lld" -DCMAKE_CXX_FLAGS="-fuse-ld=lld"
 cmake --build build -j$JOBS

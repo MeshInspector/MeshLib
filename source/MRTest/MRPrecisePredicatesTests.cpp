61717f38a4de03da30d79c3c4a74db9b7f7508a0
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-07-16 17:50:56 +0300

    ccwLess SoS predicate (#4839)

--- source/MRTest/MRPrecisePredicatesTests.cpp
+++ source/MRTest/MRPrecisePredicatesTests.cpp
@@ -71,6 +71,48 @@ TEST( MRMesh, PrecisePredicates2more )
     EXPECT_TRUE(  ccw( { vs[2],vs[3],vs[0] } ) );
 }
 
+TEST( MRMesh, ccwLess )
+{
+    std::array<PreciseVertCoords2, 5> vs =
+    {
+        PreciseVertCoords2{ 0_v, Vector2i{ 0, 0 } },
+        PreciseVertCoords2{ 1_v, Vector2i( 1, 0 ) },
+        PreciseVertCoords2{ 2_v, Vector2i{ 0, 1 } },
+        PreciseVertCoords2{ 3_v, Vector2i{ 0, 2 } },
+        PreciseVertCoords2{ 4_v, Vector2i{ 0, 1 } } // vs[4].pt == vs[2].pt
+    };
+
+    // not-degenerate
+    EXPECT_TRUE(  ccwLess( { vs[0], vs[1], vs[2], vs[3] } ) );
+    EXPECT_FALSE( ccwLess( { vs[0], vs[1], vs[3], vs[2] } ) );
+    EXPECT_FALSE( ccwLess( { vs[1], vs[0], vs[2], vs[3] } ) );
+
+    // partially degenerate
+    EXPECT_TRUE(  ccwLess( { vs[0], vs[1], vs[4], vs[2] } ) );
+    EXPECT_FALSE( ccwLess( { vs[0], vs[1], vs[2], vs[4] } ) );
+    EXPECT_FALSE( ccwLess( { vs[1], vs[0], vs[4], vs[2] } ) );
+}
+
+TEST( MRMesh, ccwLessFullDegen )
+{
+    std::array<PreciseVertCoords2, 4> vs = 
+    { 
+        PreciseVertCoords2{ 0_v, Vector2i( 0, 0 ) },
+        PreciseVertCoords2{ 1_v, Vector2i( 0, 0 ) },
+        PreciseVertCoords2{ 2_v, Vector2i( 0, 0 ) },
+        PreciseVertCoords2{ 3_v, Vector2i( 0, 0 ) }
+    };
+
+    EXPECT_TRUE(  ccwLess( { vs[0], vs[1], vs[2], vs[3] } ) );
+
+    // test that maximum degree in ccwLess can cope with most degenerate situation possible
+    do
+    {
+        (void)ccwLess( { vs[0], vs[1], vs[2], vs[3] } );
+    }
+    while ( std::next_permutation( vs.begin(), vs.end(), []( const auto & l, const auto & r ) { return l.id < r.id; } ) );
+}
+
 TEST( MRMesh, PrecisePredicates2InCircle )
 {
     std::array<PreciseVertCoords2, 4> vs =
@@ -121,7 +163,7 @@ TEST( MRMesh, PrecisePredicates2More )
     EXPECT_FALSE( segmentIntersectionOrder( { vs[4], vs[5], vs[0], vs[2], vs[0], vs[3] } ) );
 }
 
-TEST( MRMesh, PrecisePredicates2FullDegen )
+TEST( MRMesh, segmentIntersectionOrder2dFullDegen )
 {
     std::array<PreciseVertCoords2, 6> vs = 
     { 

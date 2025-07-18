fe577ea0e1590ef0992ccc8e6b6e084e4b6e92f4
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-07-18 10:24:13 +0200

    Revert "Faulty commit"
    This reverts commit ca47892d213aee883d807db948f0a627b8474085.

--- source/MRMesh/MRFillContours2D.cpp
+++ source/MRMesh/MRFillContours2D.cpp
@@ -10,6 +10,7 @@
 #include "MRMakeSphereMesh.h"
 #include "MRMeshTrimWithPlane.h"
 #include "MRPlane3.h"
+#include "MRGTest.h"
 #include "MRMeshSave.h"
 #include "MRFillContour.h"
 #include "MRObjectMeshData.h"
@@ -280,6 +281,26 @@ Expected<void> fillPlanarHole( ObjectMeshData& data, std::vector<EdgeLoop>& hole
     }
 
     return {};
+}
+
+TEST( MRMesh, fillContours2D )
+{
+    Mesh sphereBig = makeUVSphere( 1.0f, 32, 32 );
+    Mesh sphereSmall = makeUVSphere( 0.7f, 16, 16 );
+
+    sphereSmall.topology.flipOrientation();
+    sphereBig.addMesh( sphereSmall );
+
+    trimWithPlane( sphereBig, TrimWithPlaneParams{ .plane = Plane3f::fromDirAndPt( Vector3f::plusZ(), Vector3f() ) } );
+    sphereBig.pack();
+
+    auto firstNewFace = sphereBig.topology.lastValidFace() + 1;
+    auto v = fillContours2D( sphereBig, sphereBig.topology.findHoleRepresentiveEdges() );
+    EXPECT_TRUE( v.has_value() );
+    for ( FaceId f = firstNewFace; f <= sphereBig.topology.lastValidFace(); ++f )
+    {
+        EXPECT_TRUE( std::abs( dot( sphereBig.dirDblArea( f ).normalized(), Vector3f::minusZ() ) - 1.0f ) < std::numeric_limits<float>::epsilon() );
+    }
 }
 
 }

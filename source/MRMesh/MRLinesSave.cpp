7dc37eb75bcf281b86714a8de44d821c954b20d5
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-07-18 19:38:09 +0300

    save polylines in PLY format

--- source/MRMesh/MRLinesSave.cpp
+++ source/MRMesh/MRLinesSave.cpp
@@ -154,6 +154,96 @@ Expected<void> toDxf( const Polyline3& polyline, std::ostream& out, const SaveSe
     return {};
 }
 
+Expected<void> toPly( const Polyline3& polyline, const std::filesystem::path & file, const SaveSettings & settings )
+{
+    std::ofstream out( file, std::ofstream::binary );
+    if ( !out )
+        return unexpected( std::string( "Cannot open file for writing " ) + utf8string( file ) );
+
+    return toPly( polyline, out, settings );
+}
+
+Expected<void> toPly( const Polyline3& polyline, std::ostream & out, const SaveSettings & settings )
+{
+    MR_TIMER;
+
+    const VertRenumber vertRenumber( polyline.topology.getValidVerts(), settings.onlyValidPoints );
+    const int numPoints = vertRenumber.sizeVerts();
+    const VertId lastVertId = polyline.topology.lastValidVert();
+    const bool saveColors = settings.colors && settings.colors->size() > lastVertId;
+
+    out << "ply\nformat binary_little_endian 1.0\ncomment MeshInspector.com\n"
+        "element vertex " << numPoints << "\nproperty float x\nproperty float y\nproperty float z\n";
+    if ( saveColors )
+        out << "property uchar red\nproperty uchar green\nproperty uchar blue\n";
+
+    const auto ueLast = polyline.topology.lastNotLoneUndirectedEdge();
+    const auto numSaveEdges = settings.packPrimitives ? polyline.topology.computeNotLoneUndirectedEdges() : int( ueLast + 1 );
+    out <<  "element edge " << numSaveEdges << "\nproperty int vertex1\nproperty int vertex2\nend_header\n";
+
+    static_assert( sizeof( Vector3f ) == 12, "wrong size of Vector3f" );
+#pragma pack(push, 1)
+    struct PlyColor
+    {
+        unsigned char r = 0, g = 0, b = 0;
+    };
+#pragma pack(pop)
+    static_assert( sizeof( PlyColor ) == 3, "check your padding" );
+
+    // write vertices
+    int numSaved = 0;
+    for ( VertId i{ 0 }; i <= lastVertId; ++i )
+    {
+        if ( settings.onlyValidPoints && !polyline.topology.hasVert( i ) )
+            continue;
+        const Vector3f p = applyFloat( settings.xf, polyline.points[i] );
+        out.write( ( const char* )&p, 12 );
+        if ( settings.colors )
+        {
+            const auto c = ( *settings.colors )[i];
+            PlyColor pc{ .r = c.r, .g = c.g, .b = c.b };
+            out.write( ( const char* )&pc, 3 );
+        }
+        ++numSaved;
+        if ( settings.progress && !( numSaved & 0x3FF ) && !settings.progress( float( numSaved ) / numPoints * 0.5f ) )
+            return unexpectedOperationCanceled();
+    }
+
+    // write edges
+    #pragma pack(push, 1)
+    struct PlyEdge
+    {
+        int v1 = 0, v2 = 0;
+    };
+    #pragma pack(pop)
+    static_assert( sizeof( PlyEdge ) == 8, "check your padding" );
+
+    PlyEdge edge;
+    int savedEdges = 0;
+    for ( auto ue = 0_ue; ue <= ueLast; ++ue )
+    {
+        if ( !polyline.topology.isLoneEdge( ue ) )
+        {
+            edge.v1 = polyline.topology.org( ue );
+            edge.v2 = polyline.topology.dest( ue );
+        }
+        else if ( !settings.packPrimitives )
+            edge = {};
+        else
+            continue;
+        out.write( (const char *)&edge, sizeof( PlyEdge ) );
+        ++savedEdges;
+        if ( settings.progress && !( savedEdges & 0x3FF ) && !settings.progress( float( savedEdges ) / numSaveEdges * 0.5f + 0.5f ) )
+            return unexpectedOperationCanceled();
+    }
+
+    if ( !out )
+        return unexpected( std::string( "Error saving in PLY-format" ) );
+
+    reportProgress( settings.progress, 1.f );
+    return {};
+}
+
 Expected<void> toAnySupportedFormat( const Polyline3& polyline, const std::filesystem::path& file, const SaveSettings & settings )
 {
     auto ext = utf8string( file.extension() );
@@ -183,6 +273,7 @@ Expected<void> toAnySupportedFormat( const Polyline3& polyline, const std::strin
 
 MR_ADD_LINES_SAVER_WITH_PRIORITY( IOFilter( "MrLines (.mrlines)", "*.mrlines" ), toMrLines, -1 )
 MR_ADD_LINES_SAVER( IOFilter( "PTS (.pts)", "*.pts" ), toPts )
+MR_ADD_LINES_SAVER( IOFilter( "PLY (.ply)", "*.ply" ), toPly )
 MR_ADD_LINES_SAVER( IOFilter( "Drawing exchange format (.dxf)", "*.dxf" ), toDxf )
 
 } //namespace LinesSave

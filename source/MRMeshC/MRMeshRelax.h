6f93b30f708d4019e92355d836ce7b5a8e2b8235
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2025-07-23 16:04:01 +0300

    Expose mesh relax to C#

--- source/MRMeshC/MRMeshRelax.h
+++ source/MRMeshC/MRMeshRelax.h
@@ -11,4 +11,9 @@ MR_EXTERN_C_BEGIN
 /// \return true if was finished successfully, false if was interrupted by progress callback
 MRMESHC_API bool mrRelax( MRMesh* mesh, const MRRelaxParams* params, MRProgressCallback* cb );
 
+/// applies given number of relaxation iterations to the whole mesh ( or some region if it is specified ) \n
+/// do not really keeps volume but tries hard
+/// \return true if the operation completed successfully, and false if it was interrupted by the progress callback.
+MRMESHC_API bool mrRelaxKeepVolume( MRMesh* mesh, const MRRelaxParams* params, MRProgressCallback* cb );
+
 MR_EXTERN_C_END

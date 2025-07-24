250a9d27a36b09fe66987b1deecfd909a5f55c77
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-07-24 09:54:21 +0200

    Add object factory registry

--- source/MRMesh/MRObjectRegistry.h
+++ source/MRMesh/MRObjectRegistry.h
@@ -79,4 +79,49 @@ private:
     }
 };
 
+template <typename T>
+class ObjectFactoryRegistry
+{
+public:
+    using Factory = std::function<std::shared_ptr<T> ( void )>;
+
+    static bool add( std::string id, Factory&& factory, int priority = 0 )
+    {
+        static auto& registry = detail::GenericObjectRegistry::get( typeid( T ) );
+        auto object = std::make_shared<Factory>( std::forward<Factory>( factory ) );
+        return registry.add( std::move( id ), cast_<void>( std::move( object ) ), priority );
+    }
+
+    static void remove( const std::string& id )
+    {
+        static auto& registry = detail::GenericObjectRegistry::get( typeid( T ) );
+        registry.remove( id );
+    }
+
+    static std::shared_ptr<Factory> find( const std::string& id )
+    {
+        static const auto& registry = detail::GenericObjectRegistry::get( typeid( T ) );
+        return cast_<Factory>( registry.find( id ) );
+    }
+
+    static std::shared_ptr<T> make( const std::string& id )
+    {
+        static const auto& registry = detail::GenericObjectRegistry::get( typeid( T ) );
+        auto factory = cast_<Factory>( registry.find( id ) );
+        return factory ? (*factory)() : std::shared_ptr<T>{};
+    }
+
+private:
+    template <typename To, typename From>
+    static std::shared_ptr<To> cast_( std::shared_ptr<From>&& object )
+    {
+        auto* ptr = object.get();
+        return { std::forward<std::shared_ptr<From>>( object ), reinterpret_cast<To*>( ptr ) };
+    }
+};
+
+#define MR_FACTORY( Interface, Class ) [] () -> std::shared_ptr<Interface> { return std::make_shared<Class>(); }
+
+#define MR_REGISTER_FACTORY( Interface, Class, Name, ... ) MR::ObjectFactoryRegistry<Interface>::add( Name, MR_FACTORY( Interface, Class ) __VA_OPT__(,) __VA_ARGS__ )
+
 } // namespace MR

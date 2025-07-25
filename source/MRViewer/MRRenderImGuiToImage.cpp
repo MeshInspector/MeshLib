a45ede505d855234408988b7b88d325d52296164
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-07-25 15:15:51 +0200

    Call the function synchronously

--- source/MRViewer/MRRenderImGuiToImage.cpp
+++ source/MRViewer/MRRenderImGuiToImage.cpp
@@ -12,61 +12,58 @@
 namespace MR
 {
 
-void renderImGuiToImage( Vector2i resolution, Color backgroundColor, std::function<void ( void )> renderFunc, std::function<void ( Image )> imageCallback )
+Image renderImGuiToImage( const Vector2i& resolution, const Color& backgroundColor, const std::function<void ( void )>& renderFunc )
 {
-    CommandLoop::appendCommand( [=]
-    {
-        auto& viewer = Viewer::instanceRef();
-        if ( !viewer.isGLInitialized() )
-            return;
+    auto& viewer = Viewer::instanceRef();
+    if ( !viewer.isGLInitialized() )
+        return {};
 
-        FramebufferData fd;
-        fd.gen( resolution, 1 );
-        fd.bind( false );
+    FramebufferData fd;
+    fd.gen( resolution, 1 );
+    fd.bind( false );
 
-        GL_EXEC( glClearColor( backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a ) );
-        GL_EXEC( glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ) );
+    GL_EXEC( glClearColor( backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a ) );
+    GL_EXEC( glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ) );
 
-        // backup ImGui context
-        auto* backupCtx = ImGui::GetCurrentContext();
-        auto* ctx = ImGui::CreateContext( backupCtx->IO.Fonts );
-        ImGui::SetCurrentContext( ctx );
+    // backup ImGui context
+    auto* backupCtx = ImGui::GetCurrentContext();
+    auto* ctx = ImGui::CreateContext( backupCtx->IO.Fonts );
+    ImGui::SetCurrentContext( ctx );
 
-        // render ImGui
-        #ifdef __EMSCRIPTEN__
-            const char* glsl_version = "#version 300 es";
-        #else
-            const char* glsl_version = "#version 150";
-        #endif
-        ImGui_ImplOpenGL3_Init( glsl_version );
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui::GetIO().DisplaySize = { (float)resolution.x, (float)resolution.y };
-        if ( viewer.hasScaledFramebuffer() )
-            ImGui::GetIO().DisplayFramebufferScale = { 1.f, 1.f };
-        ImGui::NewFrame();
-        renderFunc();
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData( ImGui::GetDrawData() );
+    // render ImGui
+    #ifdef __EMSCRIPTEN__
+        const char* glsl_version = "#version 300 es";
+    #else
+        const char* glsl_version = "#version 150";
+    #endif
+    ImGui_ImplOpenGL3_Init( glsl_version );
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui::GetIO().DisplaySize = { (float)resolution.x, (float)resolution.y };
+    if ( viewer.hasScaledFramebuffer() )
+        ImGui::GetIO().DisplayFramebufferScale = { 1.f, 1.f };
+    ImGui::NewFrame();
+    renderFunc();
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData( ImGui::GetDrawData() );
 
-        // restore ImGui context
-        ImGui::SetCurrentContext( backupCtx );
-        ImGui::DestroyContext( ctx );
+    // restore ImGui context
+    ImGui::SetCurrentContext( backupCtx );
+    ImGui::DestroyContext( ctx );
 
-        fd.copyTextureBindDef();
-        fd.bindTexture();
+    fd.copyTextureBindDef();
+    fd.bindTexture();
 
-        // copy texture to image
-        Image result;
-        result.resolution = resolution;
-        result.pixels.resize( (size_t)resolution.x * resolution.y );
-        GL_EXEC( glGetTexImage( GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void*)result.pixels.data() ) );
+    // copy texture to image
+    Image result;
+    result.resolution = resolution;
+    result.pixels.resize( (size_t)resolution.x * resolution.y );
+    GL_EXEC( glGetTexImage( GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void*)result.pixels.data() ) );
 
-        fd.del();
+    fd.del();
 
-        viewer.bindSceneTexture( true );
+    viewer.bindSceneTexture( true );
 
-        imageCallback( std::move( result ) );
-    } );
+    return result;
 }
 
 } // namespace MR
--- source/MRViewer/MRRenderImGuiToImage.h
+++ source/MRViewer/MRRenderImGuiToImage.h
@@ -5,16 +5,14 @@
 namespace MR
 {
 
-/// Render ImGui to an image using OpenGL texture. Called between frames' rendering in a separate context.
+/// Render ImGui to an image using OpenGL texture. Uses a new ImGui context with the shared font atlas.
 /// \param resolution - image resolution, also sets the ImGui's display size
 /// \param backgroundColor - image background color
 /// \param renderFunc - callback to execute ImGui draw functions, called immediately after ImGui::NewFrame() and before ImGui::Render()
-/// \param imageCallback - callback with the output image
-MRVIEWER_API void renderImGuiToImage(
-    Vector2i resolution,
-    Color backgroundColor,
-    std::function<void ( void )> renderFunc,
-    std::function<void ( Image )> imageCallback
+MRVIEWER_API Image renderImGuiToImage(
+    const Vector2i& resolution,
+    const Color& backgroundColor,
+    const std::function<void ( void )>& renderFunc
 );
 
 } // namespace MR

228cdda673b57adcbed60695b8cb5081b8d16858
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-07-25 15:39:02 +0200

    Fix Wasm build

--- source/MRViewer/MRRenderImGuiToImage.cpp
+++ source/MRViewer/MRRenderImGuiToImage.cpp
@@ -61,7 +61,19 @@ Image renderImGuiToImage( const Vector2i& resolution, const Color& backgroundCol
     Image result;
     result.resolution = resolution;
     result.pixels.resize( (size_t)resolution.x * resolution.y );
+#ifdef __EMSCRIPTEN__
+    GLuint fbo;
+    GL_EXEC( glGenFramebuffers( 1, &fbo ) );
+    GL_EXEC( glBindFramebuffer( GL_FRAMEBUFFER, fbo ) );
+    GL_EXEC( glFramebufferTexture2D( GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fd.getTexture(), 0 ) );
+
+    GL_EXEC( glReadPixels( 0, 0, newRes.x, newRes.y, GL_RGBA, GL_UNSIGNED_BYTE, (void*)result.pixels.data() ) );
+
+    glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+    glDeleteFramebuffers( 1, &fbo );
+#else
     GL_EXEC( glGetTexImage( GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void*)result.pixels.data() ) );
+#endif
 
     fd.del();
 

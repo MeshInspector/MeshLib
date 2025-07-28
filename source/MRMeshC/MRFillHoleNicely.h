00cb3c11f935b8e9cd10f8dc71d4a5c50c42dc4b
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-07-28 16:41:46 +0300

    FillHoleNicelySettings::notFlippable option added (#4899)

--- source/MRMeshC/MRFillHoleNicely.h
+++ source/MRMeshC/MRFillHoleNicely.h
@@ -25,22 +25,34 @@ typedef struct MRFillHoleNicelyParams
 {
     /// how to triangulate the hole, must be specified by the user
     MRFillHoleParams triangulateParams;
+
     /// If false then additional vertices are created inside the patch for best mesh quality
     bool triangulateOnly;
+
+    /// in triangulateOnly = false mode, edges specified by this bit-set will never be flipped, but they can be split so it is updated during the operation
+    MRUndirectedEdgeBitSet* notFlippable;
+
     /// Subdivision is stopped when all edges inside or on the boundary of the region are not longer than this value
     float maxEdgeLen;
+
     /// Maximum number of edge splits allowed during subdivision
     int maxEdgeSplits;
+
     /// Improves local mesh triangulation by doing edge flips if it does not change dihedral angle more than on this value (in radians)
     float maxAngleChangeAfterFlip;
+
     /// Whether to make patch over the hole smooth both inside and on its boundary with existed surface
     bool smoothCurvature;
+
     /// Additionally smooth 3 layers of vertices near hole boundary both inside and outside of the hole
     bool naturalSmooth;
+
     /// edge weighting scheme for smoothCurvature mode
     MREdgeWeights edgeWeights;
+
     /// vertex mass scheme for smoothCurvature mode
     MRVertexMass vmass;
+
 } MRFillHoleNicelyParams;
 
 MRMESHC_API MRFillHoleNicelyParams mrFillHoleNicelyParamsNew( void );

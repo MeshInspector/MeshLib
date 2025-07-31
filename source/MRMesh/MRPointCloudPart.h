0526ada25a215df180bf1a0efac45894e445f1e4
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-07-31 18:19:00 +0300

    make PointCloudPart assignable

--- source/MRMesh/MRPointCloudPart.h
+++ source/MRMesh/MRPointCloudPart.h
@@ -12,6 +12,19 @@ struct PointCloudPart
     const VertBitSet* region = nullptr; // nullptr here means all valid points of point cloud
 
     PointCloudPart( const PointCloud& c, const VertBitSet* bs = nullptr ) noexcept : cloud( c ), region( bs )  {}
+
+    // Make this assignable. A better idea would be to rewrite the class to not use references, but doing this instead preserves API compatibility.
+    PointCloudPart( const PointCloudPart& other ) noexcept = default;
+    PointCloudPart& operator=( const PointCloudPart& other ) noexcept
+    {
+        if ( this != &other )
+        {
+            // In modern C++ the result doesn't need to be `std::launder`ed, right?
+            this->~PointCloudPart();
+            ::new( ( void* )this ) PointCloudPart( other );
+        }
+        return *this;
+    }
 };
 
 } // namespace MR

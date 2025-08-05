0318058ff5ec4507b8b56996018d28998ad5d1c1
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-08-05 11:27:44 -0500

    Add the remaining examples.

--- examples/c-examples/NoiseDenoise.dox.c
+++ examples/c-examples/NoiseDenoise.dox.c
@@ -1,35 +1,72 @@
-#include <MRMeshC/MRAddNoise.h>
-#include <MRMeshC/MRMesh.h>
-#include <MRMeshC/MRMeshLoad.h>
-#include <MRMeshC/MRMeshSave.h>
-#include <MRMeshC/MRNormalDenoising.h>
+#include <MRCMesh/MRAddNoise.h>
+#include <MRCMesh/MRMesh.h>
+#include <MRCMesh/MRMeshLoad.h>
+#include <MRCMesh/MRMeshSave.h>
+#include <MRCMesh/MRNormalDenoising.h>
+#include <MRCMisc/expected_MR_Mesh_std_string.h>
+#include <MRCMisc/expected_void_std_string.h>
+#include <MRCMisc/std_string.h>
 
+#include <stdio.h>
 #include <stdlib.h>
 
-int main( int argc, char* argv[] )
+int main( void )
 {
+    int rc = EXIT_FAILURE;
+
     // Load mesh
-    MRMesh* mesh = mrMeshLoadFromAnySupportedFormat( "mesh.stl", NULL );
+    MR_expected_MR_Mesh_std_string* meshEx = MR_MeshLoad_fromAnySupportedFormat_2( "mesh.stl", NULL, NULL );
+    MR_Mesh* mesh = MR_expected_MR_Mesh_std_string_GetMutableValue( meshEx );
+    if ( !mesh )
+    {
+        fprintf( stderr, "Failed to load mesh: %s\n", MR_std_string_Data( MR_expected_MR_Mesh_std_string_GetError( meshEx ) ) );
+        goto fail_load;
+    }
 
     // Add noise to the mesh
-    MRBox3f box = mrMeshComputeBoundingBox( mesh, NULL );
-    MRNoiseSettings noiseSettings = mrNoiseSettingsNew();
-    noiseSettings.sigma = mrBox3fDiagonal( &box ) * 0.0001f;
-    mrAddNoiseToMesh( mesh, NULL, &noiseSettings, NULL );
+    MR_Box3f box = MR_Mesh_computeBoundingBox_1( mesh, NULL );
+    MR_NoiseSettings* noiseSettings = MR_NoiseSettings_DefaultConstruct();
+    MR_NoiseSettings_Set_sigma( noiseSettings, MR_Box3f_diagonal( &box ) * 0.0001f );
+    MR_expected_void_std_string* noiseEx = MR_addNoise_MR_Mesh( mesh, NULL, noiseSettings );
+    MR_NoiseSettings_Destroy( noiseSettings );
+
+    if ( !noiseEx )
+    {
+        fprintf( stderr, "Failed to add noise: %s\n", MR_std_string_Data( MR_expected_void_std_string_GetError( noiseEx ) ) );
+        goto fail_noise;
+    }
 
     // Invalidate the mesh because of the external vertex changes
-    mrMeshInvalidateCaches( mesh, true );
+    MR_Mesh_invalidateCaches( mesh, NULL );
 
     // Save the noised mesh
-    mrMeshSaveToAnySupportedFormat( mesh, "noised_mesh.stl", NULL, NULL );
+    MR_expected_void_std_string* saveEx = MR_MeshSave_toAnySupportedFormat_3( mesh, "mesh_noised.stl", NULL, NULL);
+    if ( MR_expected_void_std_string_GetError( saveEx ) )
+    {
+        fprintf( stderr, "Failed to save mesh: %s\n", MR_std_string_Data( MR_expected_void_std_string_GetError( saveEx ) ) );
+        goto fail_save;
+    }
 
     // Denoise the mesh with sharpening for sharp edges
     // see the article "Mesh Denoising via a Novel Mumford-Shah Framework"
-    mrMeshDenoiseViaNormals( mesh, NULL, NULL );
+    MR_meshDenoiseViaNormals( mesh, NULL );
 
     // Save the denoised mesh
-    mrMeshSaveToAnySupportedFormat( mesh, "denoised_mesh.stl", NULL, NULL );
+    MR_expected_void_std_string* saveEx2 = MR_MeshSave_toAnySupportedFormat_3( mesh, "mesh_denoised.stl", NULL, NULL);
+    if ( MR_expected_void_std_string_GetError( saveEx2 ) )
+    {
+        fprintf( stderr, "Failed to save mesh: %s\n", MR_std_string_Data( MR_expected_void_std_string_GetError( saveEx2 ) ) );
+        goto fail_save2;
+    }
 
-    mrMeshFree( mesh );
-    return EXIT_SUCCESS;
+    rc = EXIT_SUCCESS;
+fail_save2:
+    MR_expected_void_std_string_Destroy( saveEx2 );
+fail_save:
+    MR_expected_void_std_string_Destroy( saveEx );
+fail_noise:
+    MR_expected_void_std_string_Destroy( noiseEx );
+fail_load:
+    MR_expected_MR_Mesh_std_string_Destroy( meshEx );
+    return rc;
 }

a259da78cd980f5e8b5c2885d722e4724b54afce
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-08-08 13:08:13 +0200

    Fix VS project scripts for multi-dir structure

--- scripts/check_vcxproj.py
+++ scripts/check_vcxproj.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 import sys
 import xml.etree.ElementTree as ET
-from pathlib import Path
+from pathlib import Path, PureWindowsPath
 
 VCXPROJ_NAMESPACES = {
     'msbuild': "http://schemas.microsoft.com/developer/msbuild/2003",
+}
+
+KNOWN_ITEM_GROUPS = {
+    'ClCompile': {".c", ".cpp"},
+    'ClInclude': {".h", ".hpp", ".cuh"},
+    'CudaCompile': {".cu"},
 }
 
 IGNORED_FILENAMES = {
@@ -21,48 +27,31 @@ def find_missing_entries(vcxproj_path):
     vcxproj_dir = vcxproj_path.parent
 
     vcxproj = ET.parse(vcxproj_path)
-    project = vcxproj.getroot()
+    project = vcxproj.getroot() 
 
-    includes = {
-        item.attrib['Include'].lower()
-        for item in project.iterfind(
-            'msbuild:ItemGroup/msbuild:ClInclude',
-            VCXPROJ_NAMESPACES,
-        )
-    }
-    compiles = {
-        item.attrib['Include'].lower()
-        for item in project.iterfind(
-            'msbuild:ItemGroup/msbuild:ClCompile',
-            VCXPROJ_NAMESPACES,
-        )
-    }
-    cuda_compiles = {
-        item.attrib['Include'].lower()
-        for item in project.iterfind(
-            'msbuild:ItemGroup/msbuild:CudaCompile',
-            VCXPROJ_NAMESPACES,
-        )
-    }
-
-    result = {
-        'ClInclude': [],
-        'ClCompile': [],
-        'CudaCompile': [],
-    }
-    for path in vcxproj_dir.iterdir():
-        name, suffix = path.name.lower(), path.suffix.lower()
-        if name in IGNORED_FILENAMES:
-            continue
-        if suffix in {".cpp"}:
-            if name not in compiles:
-                result['ClCompile'].append(path)
-        elif suffix in {".h", ".hpp", ".cuh"}:
-            if name not in includes:
-                result['ClInclude'].append(path)
-        elif suffix in {".cu"}:
-            if name not in cuda_compiles:
-                result['CudaCompile'].append(path)
+    result = {}
+ 
+    for tag_name, file_suffixes in KNOWN_ITEM_GROUPS.items():
+        found_files = {
+            item.attrib['Include'].lower()
+            for item in project.iterfind(
+                f'msbuild:ItemGroup/msbuild:{tag_name}',
+                VCXPROJ_NAMESPACES,
+            )
+        }
+        results = []
+        for root, dirs, files in vcxproj_dir.walk():
+            dir = root.relative_to(vcxproj_dir)
+            for filename in files:
+                path = PureWindowsPath(dir / filename)
+                name, suffix = path.name.lower(), path.suffix.lower()
+                if name in IGNORED_FILENAMES:
+                    continue
+                if suffix not in file_suffixes:
+                    continue
+                if str(path).lower() not in found_files:
+                    results.append(path)
+        result[tag_name] = results
 
     return result
 
@@ -72,7 +61,7 @@ def process_file(vcxproj_path):
     ok = True
     for group_name, group in result.items():
         for path in group:
-            print(f"{vcxproj_path}: missing {group_name} item: {path.name}", file=sys.stderr)
+            print(f"{vcxproj_path}: missing {group_name} item: {path}", file=sys.stderr)
             ok = False
     return ok
 

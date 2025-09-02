09fd403981bbe0543710a19240450f6ad42660d3
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-09-02 20:42:31 +0300

    makeThickMesh (#5039)

--- source/MRMesh/MROffsetVerts.h
+++ source/MRMesh/MROffsetVerts.h
@@ -1,11 +1,18 @@
 #pragma once
+
 #include "MRMeshFwd.h"
+#include "MRProgressCallback.h"
 
 namespace MR
 {
 
-/// Modifies \p mesh shifting each vertex by the corresponding \p offset
+/// Modifies \p mesh shifting each vertex along its pseudonormal by the corresponding \p offset
 /// @return false if cancelled.
-MRMESH_API bool offsetVerts( Mesh& mesh, const VertMetric& offset, ProgressCallback cb );
+MRMESH_API bool offsetVerts( Mesh& mesh, const VertMetric& offset, const ProgressCallback& cb = {} );
 
-}
+/// given a mesh \p m, representing a surface,
+/// creates new closed mesh by cloning mirrored mesh, and shifting original part and cloned part in different direction on \p halfWidth each,
+/// if original mesh was open then stitches corresponding boundaries of two parts
+MRMESH_API Mesh makeThickMesh( const Mesh & m, float halfWidth );
+
+} //namespace MR

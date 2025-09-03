7132be39dc10eee1b27686fdb16728c197c799d4
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-09-03 23:13:10 +0300

    new thicken

--- source/MRMesh/MROffsetVerts.cpp
+++ source/MRMesh/MROffsetVerts.cpp
@@ -4,6 +4,8 @@
 #include "MRRegionBoundary.h"
 #include "MRMeshFillHole.h"
 #include "MRTimer.h"
+#include "MRPositionVertsSmoothly.h"
+#include "MRMapOrHashMap.h"
 
 namespace MR
 {
@@ -29,6 +31,18 @@ bool offsetVerts( Mesh& mesh, const VertMetric& offset, const ProgressCallback& 
 Mesh makeThickMesh( const Mesh & m, float halfWidth )
 {
     MR_TIMER;
+
+    VertNormals ns( m.topology.vertSize() );
+    BitSetParallelFor( m.topology.getValidVerts(), [&]( VertId v )
+    {
+        ns[v] = m.pseudonormal( v );
+    } );
+    positionVertsSmoothlySharpBd( m.topology, ns, { .stabilizer = 0.01f } );
+    BitSetParallelFor( m.topology.getValidVerts(), [&]( VertId v )
+    {
+        ns[v] = ns[v].normalized();
+    } );
+
     Mesh res = m;
     auto holesRepr = m.topology.findHoleRepresentiveEdges();
     EdgeLoops mHoles( holesRepr.size() );
@@ -39,10 +53,29 @@ Mesh makeThickMesh( const Mesh & m, float halfWidth )
         auto e = makeDegenerateBandAroundHole( res, holesRepr[i] );
         extHoles[i] = trackRightBoundaryLoop( res.topology, e );
     }
-    res.addMeshPart( m, true, extHoles, mHoles );
+    PartMapping map;
+    auto m2resVerts = VertMapOrHashMap::createMap();
+    map.src2tgtVerts = &m2resVerts;
+    res.addMeshPart( m, true, extHoles, mHoles, map );
+    ns.resize( res.topology.vertSize() );
+    BitSetParallelFor( m.topology.getValidVerts(), [&]( VertId v )
+    {
+        auto resV = getAt( m2resVerts, v );
+        if ( !resV )
+        {
+            assert( false );
+            return;
+        }
+        ns[resV] = -ns[v];
+    } );
+
+    BitSetParallelFor( res.topology.getValidVerts(), [&]( VertId v )
+    {
+        res.points[v] += halfWidth * ns[v];
+    } );
 
     // degenerate faces will be automatically ignored during pseudonormal computation
-    offsetVerts( res, [halfWidth]( VertId ) { return halfWidth; } );
+    //offsetVerts( res, [halfWidth]( VertId ) { return halfWidth; } );
 
     return res;
 }

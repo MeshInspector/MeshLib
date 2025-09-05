9eed1a99b497c3e64f6996cd626794d1da8a68f2
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-09-05 03:21:08 -0500

    Explicitly register em dash character.

--- source/MRViewer/ImGuiMenu.cpp
+++ source/MRViewer/ImGuiMenu.cpp
@@ -351,12 +351,13 @@ static std::pair<bool, bool> getRealValue( const std::vector<std::shared_ptr<MR:
 void ImGuiMenu::addMenuFontRanges_( ImFontGlyphRangesBuilder& builder ) const
 {
     builder.AddRanges( ImGui::GetIO().Fonts->GetGlyphRangesCyrillic() );
+    builder.AddChar( 0x2014 ); // EM DASH
     builder.AddChar( 0x2116 ); // NUMERO SIGN (shift+3 on cyrillic keyboards)
-    builder.AddChar( 0x2212 ); // MINUS SIGN
-    builder.AddChar( 0x222A ); // UNION
-    builder.AddChar( 0x2229 ); // INTERSECTION
     builder.AddChar( 0x2208 ); // INSIDE
     builder.AddChar( 0x2209 ); // OUTSIDE
+    builder.AddChar( 0x2212 ); // MINUS SIGN
+    builder.AddChar( 0x2229 ); // INTERSECTION
+    builder.AddChar( 0x222A ); // UNION
     // Characters not in the font, with custom glyphs added in `addCustomGlyphs_`:
     // 0x207B SUPERSCRIPT MINUS
 #ifndef __EMSCRIPTEN__

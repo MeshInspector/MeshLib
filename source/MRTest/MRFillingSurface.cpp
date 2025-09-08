1803229a95e4a25831e7622670483e30a2afda40
Andrey Aralov <andrey.aralov@meshinspector.com>
2025-09-08 18:03:11 +0200

    Add density test.

--- source/MRTest/MRFillingSurface.cpp
+++ source/MRTest/MRFillingSurface.cpp
@@ -4,6 +4,7 @@
 #include <MRMesh/MRMesh.h>
 #include <MRVoxels/MRMinimalSurface.h>
 #include "MRMesh/MRMeshFillHole.h"
+#include "MRMesh/MRBox.h"
 
 
 namespace MR
@@ -22,6 +23,30 @@ TEST( MRMesh, CellularFillingSurface )
                 auto res = FillingSurface::CellularSurface::build( size,
                                { .period = Vector3f::diagonal( T ), .width = Vector3f::diagonal( W ), .r = R } );
                 ASSERT_TRUE( res );
+            }
+        }
+    }
+}
+
+TEST( MRMesh, CellularFillingSurfaceDensity )
+{
+    Vector3f size{ 1.f, 1.f, 1.f };
+
+    for ( float T = 0.1f; T < 1.f; T += 0.1f )
+    {
+        for ( float R = 0.f; R < T / 2.f; R += 0.1f )
+        {
+            for ( float W = 0.1f; W < T; W += 0.1f )
+            {
+                auto res = FillingSurface::CellularSurface::build( size,
+                               { .period = Vector3f::diagonal( T ), .width = Vector3f::diagonal( W ), .r = R, .highRes = true } );
+                ASSERT_TRUE( res );
+                fillHoles( *res, res->topology.findHoleRepresentiveEdges() );
+                ASSERT_TRUE( res->topology.findNumHoles() == 0 );
+
+                const auto realDensity = res->volume() / res->getBoundingBox().volume();
+                const auto predictedDensity = FillingSurface::CellularSurface::estimateDensity( T, W, R );
+                ASSERT_NEAR( realDensity, predictedDensity, 0.01f );
             }
         }
     }

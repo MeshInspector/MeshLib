aff08e1740cb0bd038f3fd62363706e7c12dfe69
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-09-18 10:57:08 +0200

    WIP

--- source/MRViewer/MRRenderMeasurementObjects.cpp
+++ source/MRViewer/MRRenderMeasurementObjects.cpp
@@ -1,4 +1,5 @@
 #include "MRRenderMeasurementObjects.h"
+#include "MRUnits.h"
 
 #include "MRMesh/MRFeatureObject.h"
 #include "MRMesh/MRVisualObject.h"
@@ -109,22 +110,75 @@ RenderPointMeasurementObject::RenderPointMeasurementObject( const VisualObject& 
 
 ImGuiMeasurementIndicators::Text RenderPointMeasurementObject::getObjectNameText( const VisualObject& object, ViewportId ) const
 {
+    ImGuiMeasurementIndicators::Text result;
+
+    result.addText( object.name() );
+
     if ( const auto* pointObj = dynamic_cast<const PointMeasurementObject*>( &object ) )
     {
-        std::ostringstream oss;
-
-        const auto p = pointObj->worldXf().b;
-        oss << fmt::format( "{0:.{3}f}, {1:.{3}f}, {2:.{3}f}", p.x, p.y, p.z, 3 );
-
-        assert( pointObj->numComparableProperties() == 1 );
-        if ( const auto dev = pointObj->computeComparableProperty( 0 ) )
+        const auto refNormalValue = pointObj->getComparisonReferenceValue( 1 );
+        const auto* refNormal = std::get_if<Vector3f>( &refNormalValue.var );
+        const auto printValue = [&] ( const Vector3f& p )
         {
-            oss << fmt::format( "\nDeviation: {}", dev->value );
-        }
+            static const auto toString = [] ( float v )
+            {
+                return valueToString<NoUnit>( v, {
+                    .stripTrailingZeroes = false,
+                } );
+            };
+            if ( refNormalValue.isSet )
+            {
+                assert( refNormal );
+                if ( *refNormal == Vector3f::plusX() || *refNormal == Vector3f::minusX() )
+                    return fmt::format( "X {}", toString( p.x ) );
+                if ( *refNormal == Vector3f::plusY() || *refNormal == Vector3f::minusY() )
+                    return fmt::format( "Y {}", toString( p.y ) );
+                if ( *refNormal == Vector3f::plusZ() || *refNormal == Vector3f::minusZ() )
+                    return fmt::format( "Z {}", toString( p.z ) );
+            }
+            return fmt::format( "{} {} {}", toString( p.x ), toString( p.y ), toString( p.z ) );
+        };
+        const auto printTolerance = [&]
+        {
+            // `dir == 0` - symmetric, `dir > 0` - positive, `dir < 0` - negative.
+            static const auto toString = [] ( float value, int dir )
+            {
+                return valueToString<LengthUnit>( value, {
+                    .unitSuffix = false,
+                    .style = NumberStyle::normal,
+                    .plusSign = dir != 0,
+                    .zeroMode = dir >= 0 ? ZeroMode::alwaysPositive : ZeroMode::alwaysNegative,
+                    .stripTrailingZeroes = true,
+                } );
+            };
+            if ( const auto tol = pointObj->getComparisonTolerence( 0 ) )
+            {
+                if ( tol->positive == -tol->negative )
+                    return fmt::format( " \xC2\xB1{}", toString( tol->positive, 0 ) ); // U+00B1 PLUS-MINUS SIGN
+                else
+                    return fmt::format( " {}/{}", toString( tol->positive, 1 ), toString( tol->negative, -1 ) );
+            }
+            return std::string{};
+        };
 
-        return oss.str();
+        const auto p = pointObj->getPoint();
+        if ( const auto refPosValue = pointObj->getComparisonReferenceValue( 0 ); refPosValue.isSet )
+        {
+            const auto* refPos = std::get_if<Vector3f>( &refPosValue.var );
+            assert( refPos );
+            result.addLine();
+            result.addText( fmt::format( "Measured: {}", printValue( p ) ) );
+            result.addLine();
+            result.addText( fmt::format( "Nominal:  {}{}", printValue( *refPos ), printTolerance() ) );
+        }
+        else
+        {
+            result.addLine();
+            result.addText( printValue( p ) );
+        }
     }
-    return {};
+
+    return result;
 }
 
 }

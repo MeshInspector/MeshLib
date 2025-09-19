1a85674638def640fc9fcc7c751e71bce33358b9
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-09-19 22:45:50 +0300

    keep file names inside MRU not too long to avoid hitting limit in some OSes

--- source/MRMesh/MRObject.cpp
+++ source/MRMesh/MRObject.cpp
@@ -556,7 +556,8 @@ Expected<std::vector<std::future<Expected<void>>>> Object::serializeRecursive( c
     std::vector<std::future<Expected<void>>> res;
 
     // the key must be unique among all children of same parent
-    std::string key = std::to_string( childId ) + "_" + replaceProhibitedChars( name_ );
+    constexpr int maxFileNameLen = 12; /// keep file names not too long to avoid hitting limit in some OSes
+    std::string key = std::to_string( childId ) + "_" + replaceProhibitedChars( name_ ).substr( 0, maxFileNameLen );
 
     auto model = serializeModel_( path / pathFromUtf8( key ) );
     if ( !model.has_value() )

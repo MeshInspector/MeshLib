8ca5a24a4f7170822b02e17697902d3e579b4752
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-09-22 11:16:48 -0500

    Improve `inputTolerance()`.

--- source/MRViewer/MRUIQualityControl.h
+++ source/MRViewer/MRUIQualityControl.h
@@ -10,6 +10,6 @@ namespace MR::QualityControl
 
 // An input widget for the tolerance. Uses `inputPlusMinus()` internally.
 // NOTE: This doesn't include the "reset" button to remove the tolerance, must draw that manually.
-MRVIEWER_API bool inputTolerance( const char* label, std::optional<ObjectComparableWithReference::ComparisonTolerance>& toleranceOpt );
+MRVIEWER_API bool inputTolerance( const char* label, ObjectComparableWithReference& object, std::size_t index );
 
 }

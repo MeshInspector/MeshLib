0cd16ce183b038569a9bb425064a79808f74e15d
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-09-22 05:11:12 -0500

    Address comments.

--- source/MRViewer/MRItemEnabledPerViewport.h
+++ source/MRViewer/MRItemEnabledPerViewport.h
@@ -1,20 +1,22 @@
 #pragma once
 
 #include "MRMesh/MRViewportId.h"
-#include "MRViewer/MRRibbonMenuItem.h"
+#include "exports.h"
 
 namespace MR
 {
 
 // A base class for `RibbonMenuItem`s that can be toggled per viewport.
-class ItemEnabledPerViewport : public virtual RibbonMenuItem
+// The intent is that the derived classes should also be derived from `RibbonMenuItem` and be registered in the ribbon schema.
+// Doing this ensures that you can loop over all items in the schema to get a list of all items toggleable in this way.
+class ItemEnabledPerViewport
 {
 public:
     [[nodiscard]] MRVIEWER_API ViewportMask getEnabledViewports() const;
     MRVIEWER_API void setEnabledViewports( ViewportMask newMask );
 
 protected:
-    MRVIEWER_API ItemEnabledPerViewport();
+    ItemEnabledPerViewport() = default;
 
 private:
     ViewportMask enabledViewports_ = ViewportMask::all();

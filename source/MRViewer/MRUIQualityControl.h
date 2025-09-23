9682ebbf64464003ef4aa787f2683d38e2160d49
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-09-23 04:19:19 -0500

    Explain `index` in the comment.

--- source/MRViewer/MRUIQualityControl.h
+++ source/MRViewer/MRUIQualityControl.h
@@ -10,6 +10,7 @@ namespace MR::QualityControl
 
 // An input widget for the tolerance. Uses `inputPlusMinus()` internally.
 // NOTE: This doesn't include the "reset" button to remove the tolerance, must draw that manually.
+// `index` is the same index as used by `ObjectComparableWithReference::getComparisonTolerence()`, see that for more details.
 MRVIEWER_API bool inputTolerance( const char* label, ObjectComparableWithReference& object, std::size_t index );
 
 }

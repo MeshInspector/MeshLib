98c3f3131b5569710ffad2e9cfffe029cc57b06d
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-10-02 12:32:12 +0200

    Save distance maps to TIFF

--- source/MRIOExtras/MRTiff.h
+++ source/MRIOExtras/MRTiff.h
@@ -4,6 +4,7 @@
 #ifndef MRIOEXTRAS_NO_TIFF
 #include "exports.h"
 
+#include <MRMesh/MRDistanceMap.h>
 #include <MRMesh/MRExpected.h>
 #include <MRMesh/MRImage.h>
 
@@ -25,6 +26,14 @@ namespace ImageSave
 
 /// saves in .tiff format
 MRIOEXTRAS_API Expected<void> toTiff( const Image& image, const std::filesystem::path& path );
+
+} // namespace ImageSave
+
+namespace DistanceMapSave
+{
+
+/// saves in .tiff format
+MRIOEXTRAS_API Expected<void> toTiff( const DistanceMap& dmap, const std::filesystem::path& path, const DistanceMapSaveSettings& settings = {} );
 
 } // namespace ImageSave
 

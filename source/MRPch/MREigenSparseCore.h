f7273874fb6d325609389d4806199d17ee66bc1f
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-10-23 21:03:36 +0300

    MRPch/MREigenSparseCore.h: suppress msvc warning 4127

--- source/MRPch/MREigenSparseCore.h
+++ source/MRPch/MREigenSparseCore.h
@@ -2,6 +2,9 @@
 
 #include "MREigenCore.h"
 
+#pragma warning(push)
+#pragma warning(disable: 4127) // conditional expression is constant
+
 #if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunknown-warning-option" // for next one
@@ -13,3 +16,5 @@
 #if defined(__clang__)
 #pragma clang diagnostic pop
 #endif
+
+#pragma warning(pop)

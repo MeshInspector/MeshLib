d3dcff7ce5c830624951a5b218e362dc24b43319
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-10-26 11:44:28 +0300

    diff

--- source/MRMesh/MRMeshTopologyDiff.cpp
+++ source/MRMesh/MRMeshTopologyDiff.cpp
@@ -9,10 +9,10 @@ MeshTopologyDiff::MeshTopologyDiff( const MeshTopology & from, const MeshTopolog
 {
     MR_TIMER;
 
-    toEdgesSize_ = to.edges_.size();
+    toEdgesSize_ = to.edgeSize();
     for ( EdgeId e{0}; e < toEdgesSize_; ++e )
     {
-        if ( e >= from.edges_.size() || from.getHalfEdge_( e ) != to.getHalfEdge_( e ) )
+        if ( e >= from.edgeSize() || from.getHalfEdge_( e ) != to.getHalfEdge_( e ) )
             changedEdges_[e] = to.getHalfEdge_( e );
     }
 }
@@ -21,13 +21,16 @@ void MeshTopologyDiff::applyAndSwap( MeshTopology & m )
 {
     MR_TIMER;
 
-    auto mEdgesSize = m.edges_.size();
+    auto mEdgesSize = m.edgeSize();
     // remember edges_ being deleted from m
     for ( EdgeId e{toEdgesSize_}; e < mEdgesSize; ++e )
     {
         changedEdges_[e] = m.getHalfEdge_( e );
     }
-    m.edges_.resize( toEdgesSize_ );
+    m.next_.resize( toEdgesSize_ );
+    m.prev_.resize( toEdgesSize_ );
+    m.org_.resize( toEdgesSize_ );
+    m.left_.resize( toEdgesSize_ );
     // swap common edges_ and delete edges_ for vertices missing in original m (that will be next target)
     for ( auto it = changedEdges_.begin(); it != changedEdges_.end(); )
     {

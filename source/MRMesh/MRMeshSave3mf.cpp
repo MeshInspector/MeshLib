6c5232ab903ccd4d9da15a7c298cc27ac993a292
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-10-27 13:43:45 +0300

    save .model

--- source/MRMesh/MRMeshSave3mf.cpp
+++ source/MRMesh/MRMeshSave3mf.cpp
@@ -4,6 +4,7 @@
 #include "MRStringConvert.h"
 #include "MRMesh.h"
 #include "MRTimer.h"
+#include "MRPch/MRFmt.h"
 
 namespace MR
 {
@@ -17,35 +18,24 @@ Expected<void> toModel3mf( const Mesh & mesh, const std::filesystem::path & file
     if ( !out )
         return unexpected( std::string( "Cannot open file for writing " ) + utf8string( file ) );
 
-    return toPly( mesh, out, settings );
+    return toModel3mf( mesh, out, settings );
 }
 
 Expected<void> toModel3mf( const Mesh & mesh, std::ostream & out, const SaveSettings & settings )
 {
     MR_TIMER;
 
+    out <<
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        "<model unit=\"millimeter\" xml:lang=\"en-US\" xmlns=\"http://schemas.microsoft.com/3dmanufacturing/2013/01\">\n"
+        "  <resources>\n"
+        "    <object id=\"0\" type=\"model\">\n"
+        "      <mesh>\n"
+        "        <vertices>\n";
+
     const VertRenumber vertRenumber( mesh.topology.getValidVerts(), settings.onlyValidPoints );
     const int numPoints = vertRenumber.sizeVerts();
     const VertId lastVertId = mesh.topology.lastValidVert();
-    const bool saveColors = settings.colors && settings.colors->size() > lastVertId;
-
-    out << "ply\nformat binary_little_endian 1.0\ncomment MeshInspector.com\n"
-        "element vertex " << numPoints << "\nproperty float x\nproperty float y\nproperty float z\n";
-    if ( saveColors )
-        out << "property uchar red\nproperty uchar green\nproperty uchar blue\n";
-
-    const auto fLast = mesh.topology.lastValidFace();
-    const auto numSaveFaces = settings.packPrimitives ? mesh.topology.numValidFaces() : int( fLast + 1 );
-    out <<  "element face " << numSaveFaces << "\nproperty list uchar int vertex_indices\nend_header\n";
-
-    static_assert( sizeof( Vector3f ) == 12, "wrong size of Vector3f" );
-#pragma pack(push, 1)
-    struct PlyColor
-    {
-        unsigned char r = 0, g = 0, b = 0;
-    };
-#pragma pack(pop)
-    static_assert( sizeof( PlyColor ) == 3, "check your padding" );
 
     // write vertices
     int numSaved = 0;
@@ -53,52 +43,61 @@ Expected<void> toModel3mf( const Mesh & mesh, std::ostream & out, const SaveSett
     {
         if ( settings.onlyValidPoints && !mesh.topology.hasVert( i ) )
             continue;
-        const Vector3f p = applyFloat( settings.xf, mesh.points[i] );
-        out.write( ( const char* )&p, 12 );
-        if ( settings.colors )
-        {
-            const auto c = ( *settings.colors )[i];
-            PlyColor pc{ .r = c.r, .g = c.g, .b = c.b };
-            out.write( ( const char* )&pc, 3 );
-        }
+        const Vector3f& p = mesh.points[i];
+        out << fmt::format( "          <vertex x=\"{}\" y=\"{}\" z=\"{}\" />\n", p.x, p.y, p.z );
         ++numSaved;
         if ( settings.progress && !( numSaved & 0x3FF ) && !settings.progress( float( numSaved ) / numPoints * 0.5f ) )
             return unexpectedOperationCanceled();
     }
 
-    // write triangles
-    #pragma pack(push, 1)
-    struct PlyTriangle
-    {
-        char cnt = 3;
-        int v[3];
-    };
-    #pragma pack(pop)
-    static_assert( sizeof( PlyTriangle ) == 13, "check your padding" );
+    out <<
+        "        </vertices>\n"
+        "        <triangles>\n";
 
-    PlyTriangle tri;
+    const auto fLast = mesh.topology.lastValidFace();
+    const auto numSaveFaces = settings.packPrimitives ? mesh.topology.numValidFaces() : int( fLast + 1 );
+
+    // write triangles
     int savedFaces = 0;
     for ( FaceId f{0}; f <= fLast; ++f )
     {
+        int v[3] = { 0, 0, 0 };
         if ( mesh.topology.hasFace( f ) )
         {
             VertId vs[3];
             mesh.topology.getTriVerts( f, vs );
             for ( int i = 0; i < 3; ++i )
-                tri.v[i] = vertRenumber( vs[i] );
+                v[i] = vertRenumber( vs[i] );
         }
-        else if ( !settings.packPrimitives )
-            tri.v[0] = tri.v[1] = tri.v[2] = 0;
-        else
+        else if ( settings.packPrimitives )
             continue;
-        out.write( (const char *)&tri, sizeof( PlyTriangle ) );
+        out << fmt::format( "          <triangle v1=\"{}\" v2=\"{}\" v3=\"{}\" />\n", v[0], v[1], v[2] );
         ++savedFaces;
         if ( settings.progress && !( savedFaces & 0x3FF ) && !settings.progress( float( savedFaces ) / numSaveFaces * 0.5f + 0.5f ) )
             return unexpectedOperationCanceled();
     }
 
+    out <<
+        "        </triangles>\n"
+        "      </mesh>\n"
+        "    </object>\n"
+        "  </resources>\n";
+
+    AffineXf3d xf;
+    if ( settings.xf )
+        xf = *settings.xf;
+    const auto & A = xf.A;
+    const auto & b = xf.b;
+    out << fmt::format(
+        "  <build>\n"
+        "    <item objectid=\"0\" transform=\"{} {} {} {} {} {} {} {} {} {} {} {}\" />\n"
+        "  </build>\n", A.x.x, A.x.y, A.x.z, A.y.x, A.y.y, A.y.z, A.z.x, A.z.y, A.z.z, b.x, b.y, b.z );
+
+    out <<
+        "</model>\n";
+
     if ( !out )
-        return unexpected( std::string( "Error saving in PLY-format" ) );
+        return unexpected( std::string( "Error saving in 3MF model-format" ) );
 
     reportProgress( settings.progress, 1.f );
     return {};

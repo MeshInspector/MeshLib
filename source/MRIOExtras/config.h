c078e16848e85946cc2729d26d8f8bf19f49a38f
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-10-29 10:41:10 +0100

    Clarify comments

--- source/MRIOExtras/config.h
+++ source/MRIOExtras/config.h
@@ -1,7 +1,8 @@
 #pragma once
 
 // MRIOExtras/config.h
-// This comment is added to fix GCC builds.
+// This comment is added to make the config header files unique.
+// More info: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=52566
 
 #if defined( MR_USE_CMAKE_CONFIGURE_FILE ) && __has_include( "config_cmake.h" )
 #include "config_cmake.h"

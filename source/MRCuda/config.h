72f8ee31660b0625814b631273bcae397ec601ef
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-10-29 09:59:56 +0100

    Fix GCC build

--- source/MRCuda/config.h
+++ source/MRCuda/config.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// MRCuda/config.h
+// This comment is added to fix GCC builds.
+
 #if defined( MR_USE_CMAKE_CONFIGURE_FILE ) && __has_include( "config_cmake.h" )
 #include "config_cmake.h"
 #endif
--- source/MRViewer/config.h
+++ source/MRViewer/config.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// MRViewer/config.h
+// This comment is added to fix GCC builds.
+
 #if defined( MR_USE_CMAKE_CONFIGURE_FILE ) && __has_include( "config_cmake.h" )
 #include "config_cmake.h"
 #endif

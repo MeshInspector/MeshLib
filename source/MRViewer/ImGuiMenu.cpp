661907766c2026e70e225132730c1f405f871d52
Alexandr Burakov <alexandr.burakov@meshinspector.com>
2025-11-06 14:01:12 +0300

    fixed scrolling in imgui ui in emscripten (#5339)
    * fixed scrolling in imgui ui in emscripten
    
    * added comment

--- source/MRViewer/ImGuiMenu.cpp
+++ source/MRViewer/ImGuiMenu.cpp
@@ -207,6 +207,12 @@ void ImGuiMenu::initBackend()
     rescaleStyle_();
     ImGui_ImplGlfw_InitForOpenGL( viewer->window, false );
     ImGui_ImplOpenGL3_Init( glsl_version );
+
+    // init emscripten resize, fullscreen, mouse scroll callback
+    // may duplicate an existing resize callback (resizeEmsCanvas in MRViewer.cpp)
+#ifdef __EMSCRIPTEN__
+    ImGui_ImplGlfw_InstallEmscriptenCallbacks( viewer->window, "#canvas" );
+#endif
 }
 
 void reserveKeyEvent( ImGuiKey key )

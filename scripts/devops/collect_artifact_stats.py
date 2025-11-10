894d079037eaf928bd02e0030bf5511b1d0cb89c
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-11-10 09:12:59 +0100

    Fix script

--- scripts/devops/collect_artifact_stats.py
+++ scripts/devops/collect_artifact_stats.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if os.environ.get('CI'):
         wildcard = os.environ['ARTIFACT_PATH']
         results = {
-            filename: filename.stat().st_size
+            str(filename): filename.stat().st_size
             for filename in Path('.').glob(wildcard)
         }
         with open(os.environ['STATS_FILE'], 'w') as f:

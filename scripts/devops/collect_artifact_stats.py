b524da33b75261c8e1e3f8d86a0fbf718332896f
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-11-10 09:28:34 +0100

    Fix script

--- scripts/devops/collect_artifact_stats.py
+++ scripts/devops/collect_artifact_stats.py
@@ -4,10 +4,11 @@ from pathlib import Path
 
 if __name__ == "__main__":
     if os.environ.get('CI'):
-        wildcard = os.environ['ARTIFACT_PATH']
+        path = Path(os.environ.get('ARTIFACT_PATH', '.'))
+        glob = os.environ['ARTIFACT_GLOB']
         results = {
-            str(filename): filename.stat().st_size
-            for filename in Path('.').glob(wildcard)
+            str(filename.relative_to(path)): filename.stat().st_size
+            for filename in path.glob(glob)
         }
         with open(os.environ['STATS_FILE'], 'w') as f:
             json.dump(results, f)

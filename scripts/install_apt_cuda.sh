62386eeba18736f34115c3ae982f5ba4c4579ff0
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-11-17 11:47:09 +0100

    CI: Use minimal CUDA package set in Ubuntu images

---
+++ scripts/install_apt_cuda.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eo pipefail
+
+source /etc/os-release
+case "$NAME" in 
+    "Ubuntu")
+        DISTRO="ubuntu${VERSION_ID/./}" ;;
+    *)
+        echo "Unsupported distro: $NAME"
+        exit 1 ;;
+esac
+
+case "$(uname -m)" in
+    "x86_64")
+        ARCH="x86_64" ;;
+    "aarch64")
+        ARCH="arm64" ;;
+    *)
+        echo "Unsupported arch: $(uname -m)"
+        exit 1 ;;
+esac
+
+pushd $(mktemp -d)
+wget "https://developer.download.nvidia.com/compute/cuda/repos/$DISTRO/$ARCH/cuda-keyring_1.1-1_all.deb"
+dpkg -i cuda-keyring_1.1-1_all.deb
+rm cuda-keyring_1.1-1_all.deb
+popd
+
+apt update
+apt install -y cuda-minimal-build-12-6

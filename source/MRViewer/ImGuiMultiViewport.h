19992b703d3e283902da18c6f2c880332df84c8b
Alexandr Burakov <alexandr.burakov@meshinspector.com>
2025-11-19 17:29:12 +0300

    fix comments

--- source/MRViewer/ImGuiMultiViewport.h
+++ source/MRViewer/ImGuiMultiViewport.h
@@ -7,10 +7,13 @@
 namespace ImGuiMV
 {
 
+// attaches the next window to the viewport of the main window and sets the position relative to it
 MRVIEWER_API void SetNextWindowPosMainViewport( const ImVec2& pos, ImGuiCond cond = 0, const ImVec2& pivot = ImVec2( 0, 0 ) );
 
+// returns the coordinates of the mouse in the main viewport space
 MRVIEWER_API ImVec2 GetLocalMousePos();
 
+// returns the shift of the main viewport relative to global coordinates
 MRVIEWER_API ImVec2 GetMainViewportShift();
 
 }

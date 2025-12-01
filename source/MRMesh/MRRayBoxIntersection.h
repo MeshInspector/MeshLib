3da52196d41e5f3a2547a44b2f5fe2f9f5f6c3b4
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2025-12-01 06:21:17 -0500

    Try again.

--- source/MRMesh/MRRayBoxIntersection.h
+++ source/MRMesh/MRRayBoxIntersection.h
@@ -24,68 +24,65 @@ struct RayOrigin
 template<>
 struct RayOrigin<float>
 {
-    __m128 p;
+    MR_BIND_IGNORE __m128 p;
     RayOrigin( const Vector3f & ro ) { p = _mm_set_ps( ro.x, ro.y, ro.z, 0 ); }
 };
+#endif
 
 /// finds intersection between the Ray and the Box.
 /// Precomputed values could be useful for several calls with the same direction,
 /// see "An Efficient and Robust Ray-Box Intersection Algorithm" at https://people.csail.mit.edu/amy/papers/box-jgt.pdf
-#if MR_HAS_REQUIRES
-template <MR_SAME_TYPE_TEMPLATE_PARAM(float, T)> // Making this a template is needed for cross-platform C bindings.
-#else
-inline
-#endif
-bool rayBoxIntersect( const Box3f& box, const RayOrigin<float> & rayOrigin, float & t0, float & t1, const IntersectionPrecomputes<float>& prec )
-{
-    __m128 l = _mm_set_ps( box.min.x, box.min.y, box.min.z, t0 );
-    __m128 r = _mm_set_ps( box.max.x, box.max.y, box.max.z, t1 );
-    l = _mm_sub_ps( l, rayOrigin.p );
-    r = _mm_sub_ps( r, rayOrigin.p );
-    l = _mm_mul_ps( l, prec.invDir );
-    r = _mm_mul_ps( r, prec.invDir );
-
-    __m128 a = _mm_min_ps( l, r );
-    __m128 b = _mm_max_ps( l, r );
-
-    __m128 aa = _mm_movehl_ps( a, a );
-    aa = _mm_max_ps( aa, a );
-    __m128 aaa = _mm_shuffle_ps( aa, aa, 1 );
-    aaa = _mm_max_ss( aaa, aa );
-    t0 = _mm_cvtss_f32( aaa );
-
-    __m128 bb = _mm_movehl_ps( b, b );
-    bb = _mm_min_ps( bb, b );
-    __m128 bbb = _mm_shuffle_ps( bb, bb, 1 );
-    bbb = _mm_min_ss( bbb, bb );
-    t1 = _mm_cvtss_f32( bbb );
-
-    return t0 <= t1;
-}
-#else
-    #pragma message("rayBoxIntersect: no hardware optimized instructions")
-#endif
-
-template<typename T>
+template <typename T = float>
 bool rayBoxIntersect( const Box3<T>& box, const RayOrigin<T> & rayOrigin, T & t0, T & t1, const IntersectionPrecomputes<T>& prec )
 {
-    const Vector3i& sign = prec.sign;
+    #if defined(__x86_64__) || defined(_M_X64)
+    if constexpr (std::is_same_v<T, float>)
+    {
+        __m128 l = _mm_set_ps( box.min.x, box.min.y, box.min.z, t0 );
+        __m128 r = _mm_set_ps( box.max.x, box.max.y, box.max.z, t1 );
+        l = _mm_sub_ps( l, rayOrigin.p );
+        r = _mm_sub_ps( r, rayOrigin.p );
+        l = _mm_mul_ps( l, prec.invDir );
+        r = _mm_mul_ps( r, prec.invDir );
 
-    // compare and update x-dimension with t0-t1
-    t1 = std::min( (box[sign.x].x - rayOrigin.p.x) * prec.invDir.x, t1 );
-    t0 = std::max( (box[1 - sign.x].x - rayOrigin.p.x) * prec.invDir.x, t0 );
+        __m128 a = _mm_min_ps( l, r );
+        __m128 b = _mm_max_ps( l, r );
 
-    // compare and update y-dimension with t0-t1
-    t1 = std::min( (box[sign.y].y - rayOrigin.p.y) * prec.invDir.y, t1 );
-    t0 = std::max( (box[1 - sign.y].y - rayOrigin.p.y) * prec.invDir.y, t0 );
+        __m128 aa = _mm_movehl_ps( a, a );
+        aa = _mm_max_ps( aa, a );
+        __m128 aaa = _mm_shuffle_ps( aa, aa, 1 );
+        aaa = _mm_max_ss( aaa, aa );
+        t0 = _mm_cvtss_f32( aaa );
 
-    // compare and update z-dimension with t0-t1
-    t1 = std::min( (box[sign.z].z - rayOrigin.p.z) * prec.invDir.z, t1 );
-    t0 = std::max( (box[1 - sign.z].z - rayOrigin.p.z) * prec.invDir.z, t0 );
-    return t0 <= t1;
+        __m128 bb = _mm_movehl_ps( b, b );
+        bb = _mm_min_ps( bb, b );
+        __m128 bbb = _mm_shuffle_ps( bb, bb, 1 );
+        bbb = _mm_min_ss( bbb, bb );
+        t1 = _mm_cvtss_f32( bbb );
+
+        return t0 <= t1;
+    }
+    else
+    #else
+    #pragma message("rayBoxIntersect: no hardware optimized instructions")
+    #endif
+    {
+        const Vector3i& sign = prec.sign;
+
+        // compare and update x-dimension with t0-t1
+        t1 = std::min( (box[sign.x].x - rayOrigin.p.x) * prec.invDir.x, t1 );
+        t0 = std::max( (box[1 - sign.x].x - rayOrigin.p.x) * prec.invDir.x, t0 );
+
+        // compare and update y-dimension with t0-t1
+        t1 = std::min( (box[sign.y].y - rayOrigin.p.y) * prec.invDir.y, t1 );
+        t0 = std::max( (box[1 - sign.y].y - rayOrigin.p.y) * prec.invDir.y, t0 );
+
+        // compare and update z-dimension with t0-t1
+        t1 = std::min( (box[sign.z].z - rayOrigin.p.z) * prec.invDir.z, t1 );
+        t0 = std::max( (box[1 - sign.z].z - rayOrigin.p.z) * prec.invDir.z, t0 );
+        return t0 <= t1;
+    }
 }
-
-MR_BIND_TEMPLATE( bool rayBoxIntersect( const Box3f& box, const RayOrigin<float> & rayOrigin, float & t0, float & t1, const IntersectionPrecomputes<float>& prec ) );
 
 template <typename T = float>
 bool rayBoxIntersect( const Box3<T>& box, const Line3<T>& line, T t0, T t1 )

749744cc1bbf314660cef0c8bc5e115290244d18
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-12-04 16:21:01 +0300

    several improvements

--- source/MRTest/MRMultiwayICPTests.cpp
+++ source/MRTest/MRMultiwayICPTests.cpp
@@ -10,8 +10,8 @@ namespace MR
 
 TEST( MRMesh, MultiwayICPTorus )
 {
-    auto torusRef = makeTorus( 2.5f, 0.7f, 48, 48 );
-    auto torusMove = torusRef;
+    // all objects have same shape but different transformations
+    const auto torus = makeTorus( 2.5f, 0.7f, 48, 48 );
 
     auto axis = Vector3f( 1, 0, 0 );
     auto trans = Vector3f( 0, 0.2f, 0.105f );
@@ -21,23 +21,28 @@ TEST( MRMesh, MultiwayICPTorus )
     auto run = [&] ( ICPMethod method, int maxGroupSize, float eps )
     {
         ICPObjects objs;
-        objs.push_back( { torusMove, xf } );
-        objs.push_back( { torusRef, AffineXf3f{} } );
+        objs.push_back( { torus, xf } );
+        objs.push_back( { torus, xf.inverse() } );
+        objs.push_back( { torus, AffineXf3f{} } );
         MultiwayICP icp( objs, MultiwayICPSamplingParameters{ .maxGroupSize = maxGroupSize } );
 
         ICPProperties props
         {
             .method = method,
-            .iterLimit = 20
+            .iterLimit = 30
         };
         icp.setParams( props );
         auto newXfs = icp.calculateTransformations();
-        EXPECT_EQ( newXfs[ObjId( 1 )], AffineXf3f{} );
+        EXPECT_EQ( newXfs[ObjId( 2 )], AffineXf3f{} );
         std::cout << icp.getStatusInfo() << '\n';
 
-        auto newXf = newXfs[ObjId( 0 )];
-        EXPECT_LT( ( newXf.A - Matrix3f::identity() ).norm(), eps );
-        EXPECT_LT( newXf.b.length(), eps );
+        auto newXf0 = newXfs[ObjId( 0 )];
+        EXPECT_LT( ( newXf0.A - Matrix3f::identity() ).norm(), eps );
+        EXPECT_LT( newXf0.b.length(), eps );
+
+        auto newXf1 = newXfs[ObjId( 1 )];
+        EXPECT_LT( ( newXf1.A - Matrix3f::identity() ).norm(), eps );
+        EXPECT_LT( newXf1.b.length(), eps );
     };
 
     for ( int maxGroupSize : { 0, 1 } )

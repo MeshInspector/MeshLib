db6a58ea26b374c7ced673b19b3a04a829848200
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2025-12-11 22:19:13 +0300

    sampleVertexColors function (#5487)

---
+++ source/MRMesh/MRTextureColors.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "MRMeshFwd.h"
+
+namespace MR
+{
+
+/// computes the color in the given vertex of mesh textured per-triangle's-corner;
+/// if the vertex has different colors in different triangles, then angle-weight average is computed
+[[nodiscard]] MRMESH_API Color sampleVertexColor( const Mesh& mesh, VertId v, const MeshTexture& tex, const TriCornerUVCoords& triCornerUvCoords );
+
+/// computes the colors in the vertices of mesh textured per-triangle's-corner;
+/// if one vertex has different colors in different triangles, then angle-weight average is computed
+[[nodiscard]] MRMESH_API VertColors sampleVertexColors( const Mesh& mesh, const MeshTexture& tex, const TriCornerUVCoords& triCornerUvCoords );
+
+} //namespace MR

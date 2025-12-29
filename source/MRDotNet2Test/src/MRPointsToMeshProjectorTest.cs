1457de31df1288adf0d9558d4e4914efbf50bba3
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-12-29 14:32:07 +0100

    WIP: Update tests

--- source/MRDotNet2Test/src/MRPointsToMeshProjectorTest.cs
+++ source/MRDotNet2Test/src/MRPointsToMeshProjectorTest.cs
@@ -6,24 +6,27 @@ namespace MRTest
     [TestFixture]
     internal class PointsToMeshProjectorTest
     {
-        /*
-         * TODO: fix struct field assignment
         [Test]
         public void TestPointsToMeshProjector()
         {
-            var meshA = Mesh.MakeCube(Vector3f.Diagonal(1), Vector3f.Diagonal(0));
-            var meshB = Mesh.MakeCube(Vector3f.Diagonal(1), Vector3f.Diagonal(0));
+            var meshA = MakeCube(Vector3f.Diagonal(1), Vector3f.Diagonal(0));
+            var meshB = MakeCube(Vector3f.Diagonal(1), Vector3f.Diagonal(0));
             
             var parameters = new MeshProjectionParameters();
             var shift = new Vector3f(1, 2, 3);
 
-            parameters.xf = new AffineXf3f(shift);
+            parameters.Xf = AffineXf3f.Translation(shift);
 
             var res = FindSignedDistances(meshA, meshB, parameters);
-            Assert.That(res.Count,Is.EqualTo(meshB.ValidPoints.Count()));
-            Assert.That(res.Max(),Is.InRange(shift.Length()-1e-6, shift.Length() + 1e-6));
-        }
+            Assert.That(res.Size(), Is.EqualTo(meshB.Topology.GetValidVerts().Count()));
 
-        */
+            // TODO: iteration
+            float resMax = 0f;
+            for (ulong i = 0; i < res.Size(); i++)
+            {
+                resMax = Math.Max(resMax, res.Index(new VertId(i)));
+            }
+            Assert.That(resMax, Is.InRange(shift.Length()-1e-6, shift.Length() + 1e-6));
+        }
     }
 }

8ed834670862c2126fa983a24544251e5535c37e
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2026-01-15 17:48:20 +0300

    filter

--- source/MRMesh/MRTelemetryStlHead.cpp
+++ source/MRMesh/MRTelemetryStlHead.cpp
@@ -3,6 +3,7 @@
 namespace MR
 {
 
+/// removes white spaces, meaningless or case-specific information from a comment line, then calls telemetry signal
 void telemetryStlHead( std::string s )
 {
     while ( !s.empty() && ( s.back() == ' ' || s.back() == '\t' || s.back() == '\r' || s.back() == '\n' ) )
@@ -30,6 +31,56 @@ void telemetryStlHead( std::string s )
         for ( int i = 0; i < 12; ++i, ++n )
             s[n] = '_';
     }
+
+    // e.g. "solid objname"
+    const char SOLID[] = "solid ";
+    if ( s.starts_with( SOLID ) )
+        s.resize( sizeof( SOLID ) - 2 );
+
+    // e.g. "stlbn objname"
+    const char STLBN[] = "stlbn ";
+    if ( s.starts_with( STLBN ) )
+        s.resize( sizeof( STLBN ) - 2 );
+
+    // e.g. 'SketchUp STL tmpHEPDHM'
+    const char SKETCHUP[] = "SketchUp STL ";
+    if ( s.starts_with( SKETCHUP ) )
+        s.resize( sizeof( SKETCHUP ) - 2 );
+
+    // e.g. '3Design CAD STL : part0'
+    const char THREEDESIGN[] = "3Design CAD STL :";
+    if ( s.starts_with( THREEDESIGN ) )
+        s.resize( sizeof( THREEDESIGN ) - 3 );
+
+    // e.g. 'MW 1.0 1012069 US'
+    const char MW10[] = "MW 1.0 ";
+    if ( s.starts_with( MW10 ) )
+        s.resize( sizeof( MW10 ) - 2 );
+
+    // e.g. 'ML US IM3Dv2 176841983643314567'
+    const char MLUS[] = "ML US ";
+    if ( s.starts_with( MLUS ) )
+        s.resize( sizeof( MLUS ) - 2 );
+
+    // e.g. 'flashforge stl export: %120260113 04:27:48'
+    const char FLASHFORGE[] = "flashforge stl export: ";
+    if ( s.starts_with( FLASHFORGE ) )
+        s.resize( sizeof( FLASHFORGE ) - 2 );
+
+    // e.g. 'Uranium STLWriter Wed 07 Jan 2026 22:23:13'
+    const char URANIUM[] = "Uranium STLWriter ";
+    if ( s.starts_with( URANIUM ) )
+        s.resize( sizeof( URANIUM ) - 2 );
+
+    // e.g. CURA BINARY STL EXPORT. Mon 05 Jan 2026 22:22:20
+    const char CURA[] = "CURA BINARY STL EXPORT. ";
+    if ( s.starts_with( CURA ) )
+        s.resize( sizeof( CURA ) - 2 );
+
+    // e.g. "numpy-stl (3.0.0) 2026-01-05 14:46:07.404027 tmphpyx9npt.stl"
+    const char NUMPY[] = "numpy-stl (";
+    if ( s.starts_with( NUMPY ) )
+        s = s.substr( 0, s.find_first_of( ' ', sizeof( NUMPY ) ) ); // till the space after version
 
     TelemetrySignal( "STL head " + s );
 }

0c35bb9eb133477ccd53e64a768aca132da07658
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2026-01-20 11:47:17 +0100

    Windows: Add separate window message handler

---
+++ source/MRViewer/MRWin32MessageHandler.cpp
@@ -0,0 +1,72 @@
+#ifdef _WIN32
+#include "MRWin32MessageHandler.h"
+
+#include "MRMesh/MRMeshFwd.h"
+#include "MRPch/MRSpdlog.h"
+
+namespace
+{
+
+MR::HashMap<HWND, MR::Win32MessageHandler*> gRegistry;
+
+}
+
+namespace MR
+{
+
+Win32MessageHandler::~Win32MessageHandler()
+{
+    if ( parentProc_ != 0 )
+        SetWindowLongPtr( window_, GWLP_WNDPROC, parentProc_ );
+
+    assert( gRegistry[window_] == this );
+    gRegistry[window_] = nullptr;
+}
+
+std::shared_ptr<Win32MessageHandler> Win32MessageHandler::getHandler( HWND window )
+{
+    if ( auto* handler = gRegistry[window] )
+        return handler->shared_from_this();
+
+    auto handler = std::make_shared<Win32MessageHandler>( Private{}, window );
+    if ( !handler->isValid() )
+        return {};
+    return handler;
+}
+
+bool Win32MessageHandler::isValid() const
+{
+    return parentProc_ != 0;
+}
+
+LRESULT Win32MessageHandler::WindowSubclassProc( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
+{
+    auto* handler = gRegistry[hwnd];
+    assert( handler );
+    handler->onMessage( hwnd, uMsg, wParam, lParam );
+#pragma warning( push )
+#pragma warning( disable: 4312 )
+    return CallWindowProc( (WNDPROC)handler->parentProc_, hwnd, uMsg, wParam, lParam );
+#pragma warning( pop )
+}
+
+Win32MessageHandler::Win32MessageHandler( Private, HWND window )
+    : window_( window )
+{
+    gRegistry.emplace( window_, this );
+
+#pragma warning( push )
+#pragma warning( disable: 4302 )
+#pragma warning( disable: 4311 )
+    parentProc_ = SetWindowLongPtr( window_, GWLP_WNDPROC, ( LONG_PTR )&Win32MessageHandler::WindowSubclassProc );
+#pragma warning( pop )
+    if ( parentProc_ == 0 )
+    {
+        spdlog::warn( "Failed to set the window procedure (code {:08x})", GetLastError() );
+        return;
+    }
+}
+
+} // namespace MR
+
+#endif
---
+++ source/MRViewer/MRWin32MessageHandler.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifdef _WIN32
+
+#include "MRMesh/MRSignal.h"
+
+namespace MR
+{
+
+class Win32MessageHandler : public std::enable_shared_from_this<Win32MessageHandler>
+{
+    struct Private {};
+
+public:
+    Win32MessageHandler( Private, HWND window );
+    ~Win32MessageHandler();
+    static std::shared_ptr<Win32MessageHandler> getHandler( HWND window );
+
+    bool isValid() const;
+
+    Signal<void ( HWND window, UINT message, WPARAM wParam, LPARAM lParam )> onMessage;
+
+    static LRESULT WINAPI WindowSubclassProc( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
+
+private:
+    HWND window_;
+    LONG_PTR parentProc_;
+};
+
+} // namespace MR
+
+#endif

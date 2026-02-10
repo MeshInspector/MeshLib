2acccd1e0fc94418d038977d14f6deb38982bb38
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2026-02-10 15:04:05 +0300

    Update polyline components functions (#5691)
    - add `getNLarge...` function
     - add optional output to `getLargestComponent`

--- source/MRMesh/MRPolylineComponents.h
+++ source/MRMesh/MRPolylineComponents.h
@@ -21,12 +21,33 @@ MRMESH_API UndirectedEdgeBitSet getComponent( const PolylineTopology& topology, 
 /// \ingroup ComponentsGroup
 /// \note be careful, if mesh is large enough and has many components, the memory overflow will occur
 MRMESH_API std::vector<UndirectedEdgeBitSet> getAllComponents( const PolylineTopology& topology );
+
 /// gets all connected components of polyline topology
 /// \ingroup ComponentsGroup
 /// \detail if components  number more than the maxComponentCount, they will be combined into groups of the same size 
 /// \param maxComponentCount should be more then 1
 /// \return pair components bitsets vector and number components in one group if components number more than maxComponentCount
 MRMESH_API std::pair<std::vector<UndirectedEdgeBitSet>, int> getAllComponents( const PolylineTopology& topology, int maxComponentCount );
+
+struct LargeByLengthComponentsSettings
+{
+    /// return at most given number of largest by length connected components
+    int maxLargeComponents = 2;
+
+    /// optional output: the number of components in addition to returned ones
+    int* numSmallerComponents = nullptr;
+
+    /// do not consider a component large if its length is below this value
+    float minLength = 0;
+};
+
+/// returns requested number of largest by length connected components in descending by length order
+/// \ingroup ComponentsGroup
+template<typename V>
+std::vector<UndirectedEdgeBitSet> getNLargeByLengthComponents( const Polyline<V>& polyline, const LargeByLengthComponentsSettings& settings );
+
+MR_BIND_TEMPLATE( std::vector<UndirectedEdgeBitSet> getNLargeByLengthComponents( const Polyline2& polyline, const LargeByLengthComponentsSettings& settings ) )
+MR_BIND_TEMPLATE( std::vector<UndirectedEdgeBitSet> getNLargeByLengthComponents( const Polyline3& polyline, const LargeByLengthComponentsSettings& settings ) )
 
 /// gets union-find structure for given polyline
 /// \ingroup ComponentsGroup
@@ -35,7 +56,10 @@ MRMESH_API UnionFind<UndirectedEdgeId> getUnionFindStructure( const PolylineTopo
 /// returns largest by length component
 /// \ingroup ComponentsGroup
 template <typename V>
-UndirectedEdgeBitSet getLargestComponent( const Polyline<V>& polyline );
+UndirectedEdgeBitSet getLargestComponent( const Polyline<V>& polyline, float minLength = 0, int* numSmallerComponents = nullptr );
+
+MR_BIND_TEMPLATE( UndirectedEdgeBitSet getLargestComponent( const Polyline2& polyline, float minLength = 0, int* numSmallerComponents = nullptr ) )
+MR_BIND_TEMPLATE( UndirectedEdgeBitSet getLargestComponent( const Polyline3& polyline, float minLength = 0, int* numSmallerComponents = nullptr ) )
 
 }
 

ea2dca0d3cfe7443bec88293f2b843cab19cce01
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2026-02-13 14:18:43 +0100

    Fix macro redefinition when using precompiled headers (#5702)

--- source/MRPython/MRPybind11.h
+++ source/MRPython/MRPybind11.h
@@ -15,6 +15,11 @@ MR_SUPPRESS_WARNING_PUSH
 #pragma warning( disable: 4548 ) // expression before comma has no effect; expected expression with side-effect
 #endif
 
+// avoid macro redefinitions
+#undef _POSIX_C_SOURCE
+#undef _XOPEN_SOURCE
+#include <Python.h>
+
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
 #include <pybind11/stl_bind.h>

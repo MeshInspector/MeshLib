d113beab40ceef6f36b76fbd9438a81223c99c94
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2026-02-17 20:23:07 +0300

    open points telemetry

--- source/MRMesh/MRPointsLoad.h
+++ source/MRMesh/MRPointsLoad.h
@@ -38,6 +38,9 @@ MRMESH_API Expected<PointCloud> fromAnySupportedFormat( const std::filesystem::p
 /// extension in `*.ext` format
 MRMESH_API Expected<PointCloud> fromAnySupportedFormat( std::istream& in, const std::string& extension, const PointsLoadSettings& settings = {} );
 
+/// emits telemetry signal with the integer logarithm of the number of points
+MR_BIND_IGNORE MRMESH_API void telemetryOpenPoints( const std::string& ext, const PointCloud& cloud, const PointsLoadSettings& settings );
+
 /// \}
 
 } // namespace MR::PointsLoad

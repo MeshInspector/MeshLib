5aec726a1a14dba6a27ff1ee98f3dddb9c6cc409
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2026-02-18 19:24:36 +0300

    update self-inters c# example (#5729)

--- examples/c-sharp-examples/CollisionSelf.dox.cs
+++ examples/c-sharp-examples/CollisionSelf.dox.cs
@@ -1,32 +1,28 @@
 ﻿using static MR;
 
-public class CollisionSelf {
-
-    public static void Run(string[] args) {
+public class CollisionSelf
+{
+    public static void Run(string[] args)
+    {
         var mesh = new MeshPart(MR.makeTorusWithSelfIntersections());
 
         Console.WriteLine(" --- Beginning Colliding Self Test! --- ");
-        var selfCollidingPairs = MR.findSelfCollidingTriangles(mesh);
-        if (selfCollidingPairs.empty()) {
-            Console.WriteLine("Error with MR.FindSelfCollidingTriangles");
-            return;
-        }
+        // find self-intersecting faces pairs
+        var selfCollidingPairs = findSelfCollidingTriangles(mesh);
 
         FaceFace pair; // more efficient to declare outside loop
-        for (ulong i = 0; i < selfCollidingPairs.size(); i++) {
+        for (ulong i = 0; i < selfCollidingPairs.size(); i++)
+        {
             pair = selfCollidingPairs[i];
             Console.WriteLine($"FaceA: {pair.aFace.id} FaceB: {pair.bFace.id}"); // print each pair
         }
 
+        // find bitset of self-intersecting faces
         var selfCollidingBitSet = MR.findSelfCollidingTrianglesBS(mesh);
-        if (!selfCollidingBitSet.any()) {
-            Console.WriteLine("Error with MR.FindSelfCollidingTrianglesBS");
-            return;
-        }
-
         Console.WriteLine($"{selfCollidingBitSet.count()} faces self-intersecting");
 
-        var isSelfColliding = !MR.findSelfCollidingTriangles(mesh).empty();
+        // fast check if mesh has self-intersections
+        var isSelfColliding = MR.findSelfCollidingTriangles(mesh, outCollidingPairs: null);
         Console.WriteLine($"Is self colliding: {isSelfColliding}");
     }
 

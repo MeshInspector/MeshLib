256a14e937b0ad60f299be9e7843b37f5911c835
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2026-02-20 15:52:26 +0300

    Separate FillHoleNicely setting blocks
     - it allows to separate fillHoleNicely to smaller functions

--- source/MRMesh/MRFillHoleNicely.cpp
+++ source/MRMesh/MRFillHoleNicely.cpp
@@ -13,6 +13,127 @@
 namespace MR
 {
 
+static void smoothFillingNicely( Mesh& mesh, const VertBitSet& newVerts, const FaceBitSet& newFaces, bool smoothBd, const SmoothFillingSettings& settings )
+{
+    // exclude boundary vertices from positionVertsSmoothly(), since it tends to move them inside the mesh
+    auto vertsForSmoothing = newVerts - mesh.topology.findBdVerts( nullptr, &newVerts );
+    positionVertsSmoothlySharpBd( mesh, { .region = &vertsForSmoothing } );
+    if ( !smoothBd )
+        return;
+    positionVertsSmoothly( mesh, vertsForSmoothing, settings.edgeWeights, settings.vmass );
+    if ( settings.naturalSmooth )
+    {
+        auto undirectedEdgeBitSet = findRegionBoundaryUndirectedEdgesInsideMesh( mesh.topology, newFaces );
+        auto incidentVerts = getIncidentVerts( mesh.topology, undirectedEdgeBitSet );
+        expand( mesh.topology, incidentVerts, 5 );
+        shrink( mesh.topology, incidentVerts, 2 );
+        MeshComponents::excludeFullySelectedComponents( mesh, incidentVerts );
+        if ( incidentVerts.any() )
+        {
+            vertsForSmoothing = incidentVerts - mesh.topology.findBdVerts( nullptr, &incidentVerts );
+            positionVertsSmoothlySharpBd( mesh, { .region = &vertsForSmoothing } );
+            positionVertsSmoothly( mesh, vertsForSmoothing, settings.edgeWeights, settings.vmass );
+        }
+    }
+}
+
+static std::pair<FaceColors*, Color> prepareFillingFaceColors( const MeshTopology& tp, EdgeId h0, EdgeId h1, FaceColors* inFaceColors )
+{
+    FaceColors* faceColors = inFaceColors && tp.lastValidFace() < inFaceColors->size() ?
+        inFaceColors : nullptr;
+
+    Color newFaceColor;
+    if ( faceColors )
+    {
+        //compute average color of faces around the hole
+        Vector4i sumColors;
+        int num = 0;
+        if ( h0 )
+        {
+            for ( auto e : leftRing( tp, h0) )
+            {
+                auto r = tp.right( e );
+                if ( !r )
+                    continue;
+                sumColors += Vector4i( ( *faceColors )[r] );
+                ++num;
+            }
+        }
+        if ( h1 )
+        {
+            for ( auto e : leftRing( tp, h1 ) )
+            {
+                auto r = tp.right( e );
+                if ( !r )
+                    continue;
+                sumColors += Vector4i( ( *faceColors )[r] );
+                ++num;
+            }
+        }
+        if ( num > 0 )
+            newFaceColor = Color( sumColors / num );
+    }
+    return std::make_pair( faceColors, newFaceColor );
+}
+
+static VertBitSet subdivideFillingNicely( Mesh& mesh, FaceBitSet& newFaces, 
+    const SubdivideFillingSettings& settings, const OutAttributesFillingSettings& outAttribs )
+{
+    VertBitSet newVerts;
+    SubdivideSettings subset
+    {
+        .maxEdgeLen = settings.maxEdgeLen,
+        .maxEdgeSplits = settings.maxEdgeSplits,
+        .maxAngleChangeAfterFlip = settings.maxAngleChangeAfterFlip,
+        .region = &newFaces,
+        .notFlippable = settings.notFlippable,
+        .newVerts = &newVerts,
+        .beforeEdgeSplit = settings.beforeEdgeSplit,
+        .onEdgeSplit = settings.onEdgeSplit
+    };
+
+    const auto lastVert = mesh.topology.lastValidVert();
+    VertUVCoords* uvCoords = outAttribs.uvCoords && lastVert < outAttribs.uvCoords->size() ?
+        outAttribs.uvCoords : nullptr;
+    VertColors* colorMap = outAttribs.colorMap && lastVert < outAttribs.colorMap->size() ?
+        outAttribs.colorMap : nullptr;
+    if ( uvCoords || colorMap || outAttribs.faceColors )
+    {
+        subset.onEdgeSplit = [&mesh, uvCoords, colorMap, faceColors = outAttribs.faceColors] ( EdgeId e1, EdgeId e )
+        {
+            const auto org = mesh.topology.org( e1 );
+            const auto dest = mesh.topology.dest( e );
+            const auto newV = mesh.topology.org( e );
+            assert( newV == mesh.topology.dest( e1 ) );
+
+            if ( uvCoords )
+                uvCoords->autoResizeSet( newV, ( ( *uvCoords )[org] + ( *uvCoords )[dest] ) * 0.5f );
+
+            if ( colorMap )
+                colorMap->autoResizeSet( newV, ( *colorMap )[org] + ( ( *colorMap )[dest] - ( *colorMap )[org] ) * 0.5f );
+
+            if ( faceColors )
+            {
+                if ( auto l = mesh.topology.left( e ) )
+                {
+                    auto l1 = mesh.topology.left( e1 );
+                    assert( l1 && l < l1 );
+                    faceColors->autoResizeSet( l1, ( *faceColors )[l] );
+                }
+                if ( auto r = mesh.topology.right( e ) )
+                {
+                    auto r1 = mesh.topology.right( e1 );
+                    assert( r1 && r < r1 );
+                    faceColors->autoResizeSet( r1, ( *faceColors )[r] );
+                }
+            }
+        };
+    }
+
+    subdivideMesh( mesh, subset );
+    return newVerts;
+}
+
 FaceBitSet fillHoleNicely( Mesh & mesh,
     EdgeId holeEdge,
     const FillHoleNicelySettings & settings )
@@ -24,25 +145,7 @@ FaceBitSet fillHoleNicely( Mesh & mesh,
     if ( mesh.topology.left( holeEdge ) )
         return newFaces; //no hole exists
 
-    FaceColors * const faceColors = settings.faceColors && mesh.topology.lastValidFace() < settings.faceColors->size() ? settings.faceColors : nullptr;
-
-    Color newFaceColor;
-    if ( faceColors )
-    {
-        //compute average color of faces around the hole
-        Vector4i sumColors;
-        int num = 0;
-        for ( auto e : leftRing( mesh.topology, holeEdge ) )
-        {
-            auto r = mesh.topology.right( e );
-            if ( !r )
-                continue;
-            sumColors += Vector4i( (*faceColors)[r] );
-            ++num;
-        }
-        if ( num > 0 )
-            newFaceColor = Color( sumColors / num );
-    }
+    auto [faceColors, newFaceColor] = prepareFillingFaceColors( mesh.topology, holeEdge, EdgeId(), settings.outAttributes.faceColors );
 
     const auto fsz0 = mesh.topology.faceSize();
     fillHole( mesh, holeEdge, settings.triangulateParams );
@@ -55,82 +158,14 @@ FaceBitSet fillHoleNicely( Mesh & mesh,
 
     if ( !settings.triangulateOnly )
     {
-        VertBitSet newVerts;
-        SubdivideSettings subset
-        {
-            .maxEdgeLen = settings.maxEdgeLen,
-            .maxEdgeSplits = settings.maxEdgeSplits,
-            .maxAngleChangeAfterFlip = settings.maxAngleChangeAfterFlip,
-            .region = &newFaces,
-            .notFlippable = settings.notFlippable,
-            .newVerts = &newVerts,
-            .beforeEdgeSplit = settings.beforeEdgeSplit,
-            .onEdgeSplit = settings.onEdgeSplit
-        };
-
-        const auto lastVert = mesh.topology.lastValidVert();
-        VertUVCoords * uvCoords = settings.uvCoords && lastVert < settings.uvCoords->size() ? settings.uvCoords : nullptr;
-        VertColors * colorMap = settings.colorMap && lastVert < settings.colorMap->size() ? settings.colorMap : nullptr;
-        if ( uvCoords || colorMap || faceColors )
-        {
-            subset.onEdgeSplit = [&mesh, uvCoords, colorMap, faceColors] ( EdgeId e1, EdgeId e )
-            {
-                const auto org = mesh.topology.org( e1 );
-                const auto dest = mesh.topology.dest( e );
-                const auto newV = mesh.topology.org( e );
-                assert( newV == mesh.topology.dest( e1 ) );
-
-                if ( uvCoords )
-                    uvCoords->autoResizeSet( newV, ( (*uvCoords)[org] + (*uvCoords)[dest] ) * 0.5f );
-
-                if ( colorMap )
-                    colorMap->autoResizeSet( newV, (*colorMap)[org] + ( (*colorMap)[dest] - (*colorMap)[org] ) * 0.5f );
-
-                if ( faceColors )
-                {
-                    if ( auto l = mesh.topology.left( e ) )
-                    {
-                        auto l1 = mesh.topology.left( e1 );
-                        assert( l1 && l < l1 );
-                        faceColors->autoResizeSet( l1, (*faceColors)[l] );
-                    }
-                    if ( auto r = mesh.topology.right( e ) )
-                    {
-                        auto r1 = mesh.topology.right( e1 );
-                        assert( r1 && r < r1 );
-                        faceColors->autoResizeSet( r1, (*faceColors)[r] );
-                    }
-                }
-            };
-        }
-
-        subdivideMesh( mesh, subset );
+        auto outAttribs = settings.outAttributes;
+        outAttribs.faceColors = faceColors;
+        VertBitSet newVerts = subdivideFillingNicely( mesh, newFaces, settings.subdivideSettings, outAttribs );
 
         if ( settings.smoothCurvature )
         {
-            // exclude boundary vertices from positionVertsSmoothly(), since it tends to move them inside the mesh
-            auto vertsForSmoothing = newVerts - mesh.topology.findBdVerts( nullptr, &newVerts );
-            positionVertsSmoothlySharpBd( mesh, { .region = &vertsForSmoothing } );
-            if ( settings.triangulateParams.smoothBd )
-            {
-                positionVertsSmoothly( mesh, vertsForSmoothing, settings.edgeWeights, settings.vmass );
-                if ( settings.naturalSmooth )
-                {
-                    auto undirectedEdgeBitSet = findRegionBoundaryUndirectedEdgesInsideMesh( mesh.topology, newFaces );
-                    auto incidentVerts = getIncidentVerts( mesh.topology, undirectedEdgeBitSet );
-                    expand( mesh.topology, incidentVerts, 5 );
-                    shrink( mesh.topology, incidentVerts, 2 );
-                    MeshComponents::excludeFullySelectedComponents( mesh, incidentVerts );
-                    if ( incidentVerts.any() )
-                    {
-                        vertsForSmoothing = incidentVerts - mesh.topology.findBdVerts( nullptr, &incidentVerts );
-                        positionVertsSmoothlySharpBd( mesh, { .region = &vertsForSmoothing } );
-                        positionVertsSmoothly( mesh, vertsForSmoothing, settings.edgeWeights, settings.vmass );
-                    }
-                }
-            }
+            smoothFillingNicely( mesh, newVerts, newFaces, settings.triangulateParams.smoothBd, settings.smoothSeettings );
         }
-
     }
 
     return newFaces;
--- source/MRMesh/MRFillHoleNicely.h
+++ source/MRMesh/MRFillHoleNicely.h
@@ -7,14 +7,8 @@
 namespace MR
 {
 
-struct FillHoleNicelySettings
+struct SubdivideFillingSettings
 {
-    /// how to triangulate the hole, must be specified by the user
-    FillHoleParams triangulateParams;
-
-    /// If false then additional vertices are created inside the patch for best mesh quality
-    bool triangulateOnly = false;
-
     /// in triangulateOnly = false mode, edges specified by this bit-set will never be flipped, but they can be split so it is updated during the operation
     UndirectedEdgeBitSet* notFlippable = nullptr;
 
@@ -27,32 +21,56 @@ struct FillHoleNicelySettings
     /// Improves local mesh triangulation by doing edge flips if it does not change dihedral angle more than on this value (in radians)
     float maxAngleChangeAfterFlip = 30 * PI_F / 180.0f;
 
-    /// Whether to make patch over the hole smooth both inside and on its boundary with existed surface
-    bool smoothCurvature = true;
-
-    /// Additionally smooth 3 layers of vertices near hole boundary both inside and outside of the hole
-    bool naturalSmooth = false;
-
     /// (If this is set) this function is called in subdivision each time edge (e) is going to split, if it returns false then this split will be skipped
     std::function<bool( EdgeId e )> beforeEdgeSplit;
 
     /// (If this is set) this function is called in subdivision each time edge (e) is split into (e1->e), but before the ring is made Delone
     std::function<void( EdgeId e1, EdgeId e )> onEdgeSplit;
+};
+
+struct SmoothFillingSettings
+{
+    /// Additionally smooth 3 layers of vertices near hole boundary both inside and outside of the hole
+    bool naturalSmooth = false;
 
     /// edge weighting scheme for smoothCurvature mode
     EdgeWeights edgeWeights = EdgeWeights::Cotan;
 
     /// vertex mass scheme for smoothCurvature mode
     VertexMass vmass = VertexMass::Unit;
+};
 
+struct OutAttributesFillingSettings
+{
     /// optional uv-coordinates of vertices; if provided then elements corresponding to new vertices will be added there
-    VertUVCoords * uvCoords = {};
+    VertUVCoords* uvCoords = {};
 
     /// optional colors of vertices; if provided then elements corresponding to new vertices will be added there
-    VertColors * colorMap = {};
+    VertColors* colorMap = {};
 
     /// optional colors of faces; if provided then elements corresponding to new faces will be added there
-    FaceColors * faceColors = {};
+    FaceColors* faceColors = {};
+};
+
+struct FillHoleNicelySettings
+{
+    /// how to triangulate the hole, must be specified by the user
+    FillHoleParams triangulateParams;
+
+    /// If false then additional vertices are created inside the patch for best mesh quality
+    bool triangulateOnly = false;
+
+    /// if `triangulateOnly` is false - this settings are used to subdivide new filling
+    SubdivideFillingSettings subdivideSettings;
+
+    /// Whether to make patch over the hole smooth both inside and on its boundary with existed surface
+    bool smoothCurvature = true;
+
+    /// if `smoothCurvature` is true and `triangulateOnly is false - these settings are used to smooth new filling
+    SmoothFillingSettings smoothSeettings;
+
+    /// structure with optional output attributes
+    OutAttributesFillingSettings outAttributes;
 };
 
 /// fills a hole in mesh specified by one of its edge,

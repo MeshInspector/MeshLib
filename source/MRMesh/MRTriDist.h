f11e0d980151c9ce8c9d203e383ef3697b252822
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2026-02-24 18:19:15 +0300

    findTriTriDistance: early exit if the distance is more than a limit (#5747)

--- source/MRMesh/MRTriDist.h
+++ source/MRMesh/MRTriDist.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "MRVector3.h"
+#include <limits>
 
 namespace MR
 {
@@ -27,9 +28,38 @@ struct TriTriDistanceResult
 using TriTriDistanceResultf = TriTriDistanceResult<float>;
 using TriTriDistanceResultd = TriTriDistanceResult<double>;
 
+enum class UpLimitCheck : bool
+{
+    GreaterOrEqual, ///< findTriTriDistance exits earlier if (distSqLowerBound >= upDistLimitSq)
+    Greater         ///< findTriTriDistance exits earlier if (distSqLowerBound >  upDistLimitSq)
+};
+
+template<class T>
+struct TriTriDistanceParams
+{
+    /// upper limit on the distance in question, if the real distance is larger (or equal depending on upLimitCheck)
+    /// then findTriTriDistance exits earlier returning lower bound on distSq >= upDistLimitSq
+    /// and the points a and b can be arbitrary
+    T upDistLimitSq = std::numeric_limits<T>::max();
+
+    UpLimitCheck upLimitCheck = UpLimitCheck::Greater;
+
+    bool canExitEarlier() const // with the current parameters
+    {
+        return upDistLimitSq < std::numeric_limits<T>::max();
+    }
+
+    bool canExitEarlier( T distSqLowerBound ) const // with this particular lower bound
+    {
+        return distSqLowerBound > upDistLimitSq || ( upLimitCheck == UpLimitCheck::GreaterOrEqual && distSqLowerBound == upDistLimitSq );
+    }
+};
+using TriTriDistanceParamsf = TriTriDistanceParams<float>;
+using TriTriDistanceParamsd = TriTriDistanceParams<double>;
+
 /// computes the closest points on two triangles
-[[nodiscard]] MRMESH_API TriTriDistanceResultf findTriTriDistance( const Triangle3f& a, const Triangle3f& b );
-[[nodiscard]] MRMESH_API TriTriDistanceResultd findTriTriDistance( const Triangle3d& a, const Triangle3d& b );
+[[nodiscard]] MRMESH_API TriTriDistanceResultf findTriTriDistance( const Triangle3f& a, const Triangle3f& b, const TriTriDistanceParamsf& params = {} );
+[[nodiscard]] MRMESH_API TriTriDistanceResultd findTriTriDistance( const Triangle3d& a, const Triangle3d& b, const TriTriDistanceParamsd& params = {} );
 
 // This version is not in the bindings, because the pointer parameters are assumed to point to single objects, which is wrong here.
 [[deprecated( "Use findDistance() instead" )]] MRMESH_API MR_BIND_IGNORE float triDist( Vector3f & p, Vector3f & q, const Vector3f s[3], const Vector3f t[3] );

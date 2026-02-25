81aa1a5403a423d010ad519b397f0d314c4df769
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2026-02-25 20:19:36 +0300

    Introduce divideMeshWithPlane
    add single function that can cut mesh in two parts and fill/subdivide filling with respect to attributes

---
+++ source/MRMesh/MRMeshDivideWithPlane.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "MRMeshFwd.h"
+#include "MRPlane3.h"
+#include "MRObjectMeshData.h"
+
+namespace MR
+{
+
+struct DivideMeshWithPlaneParams
+{
+    /// plane used for division
+    Plane3f plane;
+
+    /// if existing vertex is within eps distance from the plane, then move the vertex not introducing new ones
+    float eps{ 0.0f };
+
+    /// if set - function tries to fill cut after dividing (this operation might fail leading to "params.errors")
+    bool fillCut{ true };
+
+    /// if set and filled - function subdivides filling after cut
+    bool subdivideFilling{ false };
+
+    /// optional output other part of dividing (expected to be empty)
+    ObjectMeshData* otherPart{ nullptr };
+
+    /// optional output list of errors that could possibly happen during 'divideMeshWithPlane' function call
+    std::vector<std::string>* errors{ nullptr };
+};
+
+/// divide mesh data (with attributes) on two parts by given plane
+/// optionally fills and subdivides cut area
+MRMESH_API void divideMeshWithPlane( ObjectMeshData& mesh, const DivideMeshWithPlaneParams& divideParams );
+
+}

6eab952fabf595557767e2cf5b60b1c2da947180
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2026-03-02 06:14:41 -0500

    Try again.

--- source/MRMesh/MRDirectory.h
+++ source/MRMesh/MRDirectory.h
@@ -6,6 +6,15 @@
 namespace MR
 {
 
+/// iterator of directory items that will save any errors in (ec) instead of throwing exceptions
+struct DirectoryIterator
+{
+    std::filesystem::directory_iterator it;
+    std::error_code & ec;
+    DirectoryIterator & operator ++() { it.increment( ec ); return * this; }
+    auto operator *() const { return *it; }
+};
+
 /// object of this struct can be passed to range-based-for to enumerate all entries in a filesystem directory
 /// _without_ throwing exceptions, see https://stackoverflow.com/q/67776830/7325599
 /// e.g.
@@ -16,27 +25,6 @@ struct Directory
 {
     std::filesystem::path dir;
     std::error_code & ec;
-};
-
-/// object of this struct can be passed to range-based-for to enumerate all entries in a filesystem directory
-/// _including_ all subdirectories and _without_ throwing exceptions
-/// e.g.
-/// std::error_code ec;
-/// for ( auto dirEntry : DirectoryRecursive{ std::filesystem::current_path(), ec } )
-///   { ... }
-struct DirectoryRecursive
-{
-    std::filesystem::path dir;
-    std::error_code & ec;
-};
-
-/// iterator of directory items that will save any errors in (ec) instead of throwing exceptions
-struct DirectoryIterator
-{
-    std::filesystem::directory_iterator it;
-    std::error_code & ec;
-    DirectoryIterator & operator ++() { it.increment( ec ); return * this; }
-    auto operator *() const { return *it; }
 
     [[nodiscard]] friend DirectoryIterator begin( const Directory & sd )
     {
@@ -49,9 +37,6 @@ struct DirectoryIterator
     }
 };
 
-/// given file name without final extension, finds in the same folder an existing file with same stem and any extension
-[[nodiscard]] MRMESH_API std::filesystem::path findPathWithExtension( const std::filesystem::path & pathWithoutExtension );
-
 /// recursive iterator of directory items that will save any errors in (ec) instead of throwing exceptions
 struct DirectoryRecursiveIterator
 {
@@ -59,6 +44,18 @@ struct DirectoryRecursiveIterator
     std::error_code & ec;
     DirectoryRecursiveIterator & operator ++() { it.increment( ec ); return * this; }
     auto operator *() const { return *it; }
+};
+
+/// object of this struct can be passed to range-based-for to enumerate all entries in a filesystem directory
+/// _including_ all subdirectories and _without_ throwing exceptions
+/// e.g.
+/// std::error_code ec;
+/// for ( auto dirEntry : DirectoryRecursive{ std::filesystem::current_path(), ec } )
+///   { ... }
+struct DirectoryRecursive
+{
+    std::filesystem::path dir;
+    std::error_code & ec;
 
     [[nodiscard]] friend DirectoryRecursiveIterator begin( const DirectoryRecursive & sd )
     {
@@ -70,6 +67,9 @@ struct DirectoryRecursiveIterator
         return {};
     }
 };
+
+/// given file name without final extension, finds in the same folder an existing file with same stem and any extension
+[[nodiscard]] MRMESH_API std::filesystem::path findPathWithExtension( const std::filesystem::path & pathWithoutExtension );
 
 [[nodiscard]] inline bool operator !=( const DirectoryIterator & a, const std::filesystem::directory_iterator & b )
 {

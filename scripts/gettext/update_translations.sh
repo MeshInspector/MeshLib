7d8118471b1276d84362d4e741ec01538ab603f9
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2026-03-03 18:02:57 +0100

    Store xgettext options in a separate file

--- scripts/gettext/update_translations.sh
+++ scripts/gettext/update_translations.sh
@@ -5,30 +5,9 @@ shift
 
 LOCALE_DIR=$PWD/locale
 
+XGETTEXT_OPTIONS=$(cat $LOCALE_DIR/xgettext_options.txt | tr '\n' ' ')
 xgettext \
-    -ktranslate:1,1t \
-    -ktranslate:1c,2,2t \
-    -ktranslate:1,2,3t \
-    -ktranslate:1c,2,3,4t \
-    -ktranslate_noop:1,1t \
-    -ktranslate_noop:1c,2,2t \
-    -ktranslate_noop:1,2,3t \
-    -ktranslate_noop:1c,2,3,4t \
-    -k_t:1,1t \
-    -k_t:1c,2,2t \
-    -k_t:1,2,3t \
-    -k_t:1c,2,3,4t \
-    -k_tr:1,1t \
-    -k_tr:1c,2,2t \
-    -k_tr:1,2,3t \
-    -k_tr:1c,2,3,4t \
-    -kf_tr:1,1t \
-    -kf_tr:1c,2,2t \
-    -kf_tr:1,2,3t \
-    -kf_tr:1c,2,3,4t \
-    --language=C++ \
-    --from-code=UTF-8 \
-    --add-comments="TRANSLATORS:" \
+    $XGETTEXT_OPTIONS \
     --default-domain=$APP_NAME \
     --output=$LOCALE_DIR/$APP_NAME.pot \
     "$@"

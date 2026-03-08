dd70dfaeea82059643d5ec03f6e87a9a62f51d9d
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2026-03-08 13:10:31 +0300

    SurfaceManipulationWidget: Subdivide Ridges/Grooves (#5789)

--- source/MRViewer/MRSurfaceManipulationWidget.cpp
+++ source/MRViewer/MRSurfaceManipulationWidget.cpp
@@ -34,6 +34,7 @@
 #include "MRMesh/MRChangeMeshDataAction.h"
 #include "MRMesh/MRMeshPatch.h"
 #include "MRMesh/MRTimer.h"
+#include "MRMesh/MRMeshSubdivide.h"
 
 namespace MR
 {
@@ -126,7 +127,7 @@ void SurfaceManipulationWidget::init( const std::shared_ptr<ObjectMesh>& objectM
     mousePressed_ = false;
     mousePos_ = { -1, -1 };
 
-    sameValidVerticesAsInOriginMesh_ = true;
+    sameOriginalMeshTopology_ = true;
 }
 
 void SurfaceManipulationWidget::reset()
@@ -214,7 +215,7 @@ void SurfaceManipulationWidget::enableDeviationVisualization( bool enable )
 
 void SurfaceManipulationWidget::setDeviationCalculationMethod( DeviationCalculationMethod method )
 {
-    if ( sameValidVerticesAsInOriginMesh_ )
+    if ( sameOriginalMeshTopology_ )
         deviationCalculationMethod_ = method;
     else
         deviationCalculationMethod_ = DeviationCalculationMethod::ExactDistance;
@@ -308,10 +309,35 @@ bool SurfaceManipulationWidget::onMouseDown_( MouseButton button, int modifiers 
 
 void SurfaceManipulationWidget::compressChangePointsAction_()
 {
-    if ( historyAction_ )
-    {
+    // appendHistoryAction_ == false if historyAction_ was already appended
+    if ( historyAction_ && !appendHistoryAction_ )
         historyAction_->compress();
-        historyAction_.reset();
+
+    historyAction_.reset();
+}
+
+void SurfaceManipulationWidget::subdivideAfterAddRemove_()
+{
+    MR_TIMER;
+    auto subdivData = obj_->data().clone();
+    auto fs = getIncidentFaces( obj_->mesh()->topology, generalEditingRegion_ );
+    if ( subdivideMesh( subdivData, SubdivideSettings
+        {
+            .maxEdgeLen = settings_.radius,
+            .curvaturePriority = 100,
+            .maxEdgeSplits = 1000,
+            .maxDeviationAfterFlip = FLT_MAX,
+            .region = &fs,
+            .smoothMode = true,
+            .minSharpDihedralAngle = FLT_MAX
+        } ) )
+    {
+        ownMeshChangedSignal_ = true;
+        AppendHistory<PartialChangeMeshDataAction>( "Subdivide Ridges/Grooves", obj_, std::move( subdivData ) );
+        reallocData_( obj_->mesh()->topology.lastValidVert() + 1 );
+        sameOriginalMeshTopology_ = false;
+        setDeviationCalculationMethod( deviationCalculationMethod_ );
+        obj_->setDirtyFlags( DIRTY_ALL );
     }
 }
 
@@ -325,13 +351,12 @@ bool SurfaceManipulationWidget::onMouseUp_( Viewer::MouseButton button, int /*mo
     if ( button != MouseButton::Left || !mousePressed_ )
         return false;
 
-    MR_FINALLY{ compressChangePointsAction_(); };
-
     mousePressed_ = false;
     if ( settings_.workMode == WorkMode::Laplacian )
     {
         removeLastStableObjMesh_();
         invalidateMetricsCache_();
+        compressChangePointsAction_();
         return true;
     }
 
@@ -409,7 +434,7 @@ bool SurfaceManipulationWidget::onMouseUp_( Viewer::MouseButton button, int /*mo
             }
 
             reallocData_( obj_->mesh()->topology.lastValidVert() + 1 );
-            sameValidVerticesAsInOriginMesh_ = originalMesh_->topology.getValidVerts() == obj_->mesh()->topology.getValidVerts();
+            sameOriginalMeshTopology_ = false;
             setDeviationCalculationMethod( deviationCalculationMethod_ );
             obj_->setDirtyFlags( DIRTY_ALL );
 
@@ -433,10 +458,13 @@ bool SurfaceManipulationWidget::onMouseUp_( Viewer::MouseButton button, int /*mo
     else
         invalidateMetricsCache_();
 
-    generalEditingRegion_.clear();
-    generalEditingRegion_.resize( numV, false );
-
     removeLastStableObjMesh_();
+    compressChangePointsAction_();
+
+    if ( settings_.subdivideGrooves && ( settings_.workMode == WorkMode::Add || settings_.workMode == WorkMode::Remove ) && generalEditingRegion_.any() )
+        subdivideAfterAddRemove_();
+
+    generalEditingRegion_.clear();
 
     return true;
 }
@@ -486,7 +514,7 @@ void SurfaceManipulationWidget::reallocData_( size_t size )
 {
     singleEditingRegion_.resize( size, false );
     visualizationRegion_.resize( size, false );
-    generalEditingRegion_.resize( size, false );
+    generalEditingRegion_.clear(); // there is no setting of bits by index in the code
     pointsShift_.resize( size, 0.f );
     editingDistanceMap_.resize( size, 0.f );
     visualizationDistanceMap_.resize( size, FLT_MAX );
@@ -521,7 +549,7 @@ void SurfaceManipulationWidget::initConnections_()
         reallocData_( obj_->mesh()->topology.lastValidVert() + 1 );
         if ( settings_.workMode == WorkMode::Patch )
             updateUVmap_( false, true );
-        sameValidVerticesAsInOriginMesh_ = originalMesh_->topology.getValidVerts() == obj_->mesh()->topology.getValidVerts();
+        sameOriginalMeshTopology_ = originalMesh_->topology == obj_->mesh()->topology;
         setDeviationCalculationMethod( deviationCalculationMethod_ );
         updateRegion_( Vector2f( getViewerInstance().mouseController().getMousePos() ) );
     } );
--- source/MRViewer/MRSurfaceManipulationWidget.h
+++ source/MRViewer/MRSurfaceManipulationWidget.h
@@ -53,19 +53,23 @@ public:
         EdgeWeights edgeWeights = EdgeWeights::Cotan; ///< edge weights for Laplacian and Patch
         VertexMass vmass = VertexMass::NeiArea; ///< vertex weights for Laplacian and Patch
         bool idealGrooves = false; ///< if true in Add/Remove modes, the closest vertices will be moved under mouse cursor to form ideal ridges or grooves
+        bool subdivideGrooves = false; ///< if true in Add/Remove modes, changed parts of mesh will be subdivided on mouse up
     };
 
     /// initialize widget according ObjectMesh
     MRVIEWER_API void init( const std::shared_ptr<ObjectMesh>& objectMesh );
+
     /// reset widget state
     MRVIEWER_API void reset();
+
     /// lock the mesh region (vertices in this region cannot be moved, added or deleted)
     /// @note boundary edges can be split to improve quality of the patch
     MRVIEWER_API void setFixedRegion( const FaceBitSet& region );
 
     /// set widget settings (mesh change settings)
     MRVIEWER_API void setSettings( const Settings& settings );
-    /// get widget settings 
+
+    /// get widget settings
     MRVIEWER_API const Settings& getSettings() { return settings_; }
 
     /// minimum radius of editing area.
@@ -73,19 +77,26 @@ public:
 
     /// get palette used for visualization point shifts
     Palette& palette() { return *palette_; }
+
     /// update texture used for colorize surface (use after change colorMap in palette)
     MRVIEWER_API void updateTexture();
+
     /// update texture uv coords used for colorize surface (use after change ranges in palette)
     MRVIEWER_API void updateUVs();
+
     /// enable visualization of mesh deviations
     MRVIEWER_API void enableDeviationVisualization( bool enable );
+
     /// set method for calculating mesh changes
     MRVIEWER_API void setDeviationCalculationMethod( DeviationCalculationMethod method );
+
     /// get method for calculating mesh changes
     MRVIEWER_API DeviationCalculationMethod deviationCalculationMethod() const { return deviationCalculationMethod_; }
-    /// checks for a one-to-one correspondence between the vertices of the original grid and the modified one
-    MRVIEWER_API bool sameValidVerticesAsInOriginMesh() const { return sameValidVerticesAsInOriginMesh_; }
-    /// get min / max point shifts for (usefull for setup palette)
+
+    /// returns true if the current object's mesh has the same topology as original input mesh (and vertices with same IDs can be compared)
+    MRVIEWER_API bool sameOriginalMeshTopology() const { return sameOriginalMeshTopology_; }
+
+    /// get min / max point shifts for (useful for setup palette)
     MRVIEWER_API Vector2f getMinMax();
 
     /// allow the user to edit parts of object that are hidden in the current view by other objects
@@ -94,15 +105,20 @@ public:
 
     /// restricts editable area to vertices whose normals look into the same half-space as normal under cursor
     void setEditOnlyCodirectedSurface( bool edit ) { editOnlyCodirectedSurface_ = edit; }
+
     /// get state of an editable region restriction 
     bool isEditOnlyCodirectedSurface() const { return editOnlyCodirectedSurface_; }
+
 protected:
     /// start modifying mesh surface
     MRVIEWER_API bool onMouseDown_( MouseButton button, int modifiers ) override;
+
     /// stop modifying mesh surface, generate history action
     MRVIEWER_API bool onMouseUp_( MouseButton button, int modifiers ) override;
+
     /// update
     MRVIEWER_API bool onMouseMove_( int mouse_x, int mouse_y ) override;
+
     /// need to visualize bad region (draw grey circle)
     MRVIEWER_API void postDraw_() override;
 
@@ -125,6 +141,7 @@ protected:
     void updateRegion_( const Vector2f& mousePos );
     void invalidateMetricsCache_();
     void abortEdit_();
+
     /// Laplacian
     void initLaplacian_( RememberShape rs ); // for singleEditingRegion_
     void laplacianPickVert_( const PointOnFace& pick );
@@ -141,8 +158,10 @@ protected:
     void removeLastStableObjMesh_();
 
     /// this function is called after all modifications are finished;
-    /// if we previously appended SmartChangeMeshPointsAction, then switch it from uncompressed to compressed format to occupy less amount of memory
+    /// if we previously appended VersatileChangeMeshPointsAction, then switch it from uncompressed to compressed format to occupy less amount of memory
     void compressChangePointsAction_();
+
+    void subdivideAfterAddRemove_();
 
     void updateDistancesAndRegion_( const Mesh& mesh, const std::vector<MeshTriPoint>& start, VertScalars& distances, VertBitSet& region, const VertBitSet* untouchable );
 
@@ -202,7 +221,7 @@ protected:
     std::shared_ptr<Palette> palette_;
     bool enableDeviationTexture_ = false;
     DeviationCalculationMethod deviationCalculationMethod_ = DeviationCalculationMethod::ExactDistance;
-    bool sameValidVerticesAsInOriginMesh_ = true;
+    bool sameOriginalMeshTopology_ = true;
 
     /// allow the user to edit parts of object that are hidden in the current view by other objects
     bool ignoreOcclusion_ = false;

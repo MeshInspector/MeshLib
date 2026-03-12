7137b672206593cf8ec1055033d5b94f0b0e1898
Alexandr Burakov <alexandr.burakov@meshinspector.com>
2026-03-12 18:10:22 +0300

    meshconv: added conversion for Polylines and PointClouds

--- source/meshconv/meshconv.cpp
+++ source/meshconv/meshconv.cpp
@@ -1,6 +1,12 @@
 #include "MRMesh/MRMesh.h"
 #include "MRMesh/MRMeshLoad.h"
 #include "MRMesh/MRMeshSave.h"
+#include "MRMesh/MRObjectMesh.h"
+#include "MRMesh/MRObjectLoad.h"
+#include "MRMesh/MRLinesSave.h"
+#include "MRMesh/MRObjectLines.h"
+#include "MRMesh/MRPointsSave.h"
+#include "MRMesh/MRObjectPoints.h"
 #include "MRMesh/MRMeshDecimate.h"
 #include "MRMesh/MRMeshBoolean.h"
 #include "MRMesh/MRConvexHull.h"
@@ -18,6 +24,8 @@
 #pragma warning(pop)
 #include <boost/exception/diagnostic_information.hpp>
 #include <iostream>
+
+#define MC_EXIT( exitCode ) if ( waitOnExit ) system( "pause" ); return exitCode;
 
 // Fix parsing std::filesystem::path with spaces (see https://github.com/boostorg/program_options/issues/69)
 namespace boost
@@ -99,12 +107,12 @@ static int mainInternal( int argc, char **argv )
     generalOptions.add_options()
         ("help", "produce help message")
         ("timings", "print performance timings in the end")
-        ("input-file", po::value<std::filesystem::path>( &inFilePath ), "filename of input mesh")
-        ("output-file", po::value<std::filesystem::path>( &outFilePath ), "filename of output mesh")
-        ("output-ext", po::value<std::string>( &outFormat ), "extension of output mesh \".ext\"")
+        ("input-file", po::value<std::filesystem::path>( &inFilePath ), "filename of input file")
+        ("output-file", po::value<std::filesystem::path>( &outFilePath ), "filename of output file")
+        ("output-ext", po::value<std::string>( &outFormat ), "extension of output file \".ext\"")
         ;
 
-    po::options_description commands( "Commands" );
+    po::options_description commands( "Mesh Commands" );
     commands.add_options()
         ( "remesh", po::value<float>()->implicit_value( 0 ), "optional argument if positive is target edge length after remeshing" )
         ( "unite", po::value<std::filesystem::path>(), "unite mesh from input file and given mesh" )
@@ -126,11 +134,17 @@ static int mainInternal( int argc, char **argv )
         .allow_unregistered()
         .run();
 
+    bool waitOnExit = false;
     std::vector<std::string> unregisteredOptions;
     for ( const auto& o : parsedGeneral.options )
     {
         if ( o.unregistered )
-            unregisteredOptions.insert( unregisteredOptions.end(), o.original_tokens.begin(), o.original_tokens.end() );
+        {
+            if ( o.string_key == "wait-on-exit" )
+                waitOnExit = true;
+            else
+                unregisteredOptions.insert( unregisteredOptions.end(), o.original_tokens.begin(), o.original_tokens.end() );
+        }
     }
     
     po::variables_map vm;
@@ -148,7 +162,7 @@ static int mainInternal( int argc, char **argv )
             "meshconv is mesh file conversion utility based on MeshInspector/MeshLib\n"
             "Usage: meshconv input-file [output-file] [options]\n"
             << allCommands << "\n";
-        return 0;
+        MC_EXIT( 0 );
     }
 
     if ( vm.count("timings") )
@@ -161,52 +175,91 @@ static int mainInternal( int argc, char **argv )
     }
 
     std::cout << "Loading " << inFilePath << "..." << std::endl;
-    MR::Timer t( "LoadMesh" );
-    auto loadRes = MR::MeshLoad::fromAnySupportedFormat( inFilePath );
-    if ( !loadRes.has_value() )
+    MR::Timer t( "Load file" );
+    auto objLoadRes = MR::makeObjectFromMeshFile( inFilePath );
+    if ( !objLoadRes.has_value() )
     {
-        std::cerr << "Mesh load error: " << loadRes.error() << "\n";
-        return 1;
+        std::cerr << "File load error: " << objLoadRes.error() << "\n";
+        MC_EXIT( 1 );
     }
-    auto mesh = std::move( loadRes.value() );
-    std::cout << "loaded successfully in " << t.secondsPassed().count() << "s" << std::endl;
+    std::cout << "Loaded successfully in " << t.secondsPassed().count() << "s" << std::endl;
     t.finish();
 
-    t.restart( "MeshInfo" );
-    std::cout
-        << "num vertices: " << mesh.topology.numValidVerts() << "\n"
-        << "num edges:    " << mesh.topology.computeNotLoneUndirectedEdges() << "\n"
-        << "num faces:    " << mesh.topology.numValidFaces() << std::endl;
-    t.finish();
-
-    std::vector<std::vector<std::string>> lists;
-    for ( const po::option& o : parsedCommands.options )
+    MR::ObjectPtr objPtr = objLoadRes->obj;
+    std::shared_ptr<MR::ObjectMesh> objMeshPtr;
+    std::shared_ptr<MR::ObjectLines> objLinesPtr;
+    std::shared_ptr<MR::ObjectPoints> objPointsPtr;
+    if ( objMeshPtr = std::dynamic_pointer_cast< MR::ObjectMesh >( objPtr ) )
     {
-        if ( !doCommand( o, mesh ) )
+        if ( !objMeshPtr->varMesh() )
         {
-            std::cerr << "Error in command : \""<< o.string_key << " " << o.value[0] << "\"\nBreak\n";
-            return 1;
+            std::cerr << "Error: mesh not found!\n";
+            MC_EXIT( 1 );
         }
+        auto& mesh = *objMeshPtr->varMesh();
+
+        t.restart( "MeshInfo" );
+        std::cout
+            << "num vertices: " << mesh.topology.numValidVerts() << "\n"
+            << "num edges:    " << mesh.topology.computeNotLoneUndirectedEdges() << "\n"
+            << "num faces:    " << mesh.topology.numValidFaces() << std::endl;
+        t.finish();
+
+        std::vector<std::vector<std::string>> lists;
+        for ( const po::option& o : parsedCommands.options )
+        {
+            if ( !doCommand( o, mesh ) )
+            {
+                std::cerr << "Error in command : \"" << o.string_key << " " << o.value[0] << "\"\nBreak\n";
+                MC_EXIT( 1 );
+            }
+        }
+    }
+    else if ( objLinesPtr = std::dynamic_pointer_cast<MR::ObjectLines>( objPtr ) )
+    {
+        if ( !objLinesPtr->polyline() )
+        {
+            std::cerr << "Error: polyline not found!\n";
+            MC_EXIT( 1 );
+        }
+    }
+    else if ( objPointsPtr = std::dynamic_pointer_cast<MR::ObjectPoints>( objPtr ) )
+    {
+        if ( !objPointsPtr->pointCloud() )
+        {
+            std::cerr << "Error: point cloud not found!\n";
+            MC_EXIT( 1 );
+        }
+    }
+    else
+    {
+        std::cerr << "Error: conversion is not supported for this file type!\n";
+        MC_EXIT( 1 );
     }
 
     if ( outFilePath.empty() && !outFormat.empty() )
     {
         outFilePath = inFilePath;
         outFilePath.replace_extension( outFormat );
-        std::cout << "outFilePath = " << outFilePath << "\n";
     }
 
     if ( !outFilePath.empty() )
     {
         std::cout << "Saving " << outFilePath << "..." << std::endl;
-        t.restart( "SaveMesh" );
-        auto saveRes = MR::MeshSave::toAnySupportedFormat( mesh, outFilePath );
+        t.restart( "SaveFile" );
+        MR::Expected<void> saveRes;
+        if ( objMeshPtr )
+            saveRes = MR::MeshSave::toAnySupportedFormat( *objMeshPtr->mesh(), outFilePath);
+        else if ( objLinesPtr )
+            saveRes = MR::LinesSave::toAnySupportedFormat( *objLinesPtr->polyline(), outFilePath );
+        else if ( objPointsPtr )
+            saveRes = MR::PointsSave::toAnySupportedFormat( *objPointsPtr->pointCloud(), outFilePath );
         if ( !saveRes.has_value() )
         {
-            std::cerr << "Mesh save error: " << saveRes.error() << "\n";
-            return 1;
+            std::cerr << "File save error: " << saveRes.error() << "\n";
+            MC_EXIT( 1 );
         }
-        std::cout << "saved successfully in " << t.secondsPassed().count() << "s" << std::endl;
+        std::cout << "Saved successfully in " << t.secondsPassed().count() << "s" << std::endl;
         t.finish();
     }
 
@@ -214,7 +267,7 @@ static int mainInternal( int argc, char **argv )
     std::cout << "Peak virtual memory usage: " << MR::bytesString( MR::getProccessMemoryInfo().maxVirtual ) << std::endl;
 #endif
 
-    return 0;
+    MC_EXIT( 0 );
 }
 
 int main( int argc, char **argv )

f8b74d126e03fab7aa994414b82d4ac8eec5ea74
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2026-03-17 16:06:36 +0300

    comment

--- source/MRMesh/MRFillContour.h
+++ source/MRMesh/MRFillContour.h
@@ -7,7 +7,7 @@ namespace MR
 
 /// returns the region located to the left from the given edges;
 /// if the contour(s) are not separating and there is a tunnel connecting left and right side of the contour(s),
-/// the all faces from contour's connected components are returned;
+/// then all faces from contour's connected components are returned;
 /// please use \ref fillContourLeftByGraphCut for not-separating contours instead
 MRMESH_API FaceBitSet fillContourLeft( const MeshTopology & topology, const EdgePath & contour );
 MRMESH_API FaceBitSet fillContourLeft( const MeshTopology & topology, const std::vector<EdgePath> & contours );

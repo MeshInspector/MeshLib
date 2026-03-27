23a3a3f82b74fe21c1ac33bd5820c9f7125dfb27
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2026-03-27 18:11:33 +0300

    Remove Old C and C# bindings

--- source/MRMeshC/detail/Concat.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-/// concat tokens
-#define MR_CONCAT( a, b ) MR_CONCAT_( a, b )
-#define MR_CONCAT_( a, b ) a##b
--- source/MRTestC/MRBox.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void testBoxf( void );
-void testBoxfInvalid( void );
-void testBoxi( void );
-void testBoxiInvalid( void );

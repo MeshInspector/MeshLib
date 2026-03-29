2549ace901e7375c0a1346c4fb21f676b01e39ec
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2026-03-29 23:08:23 +0300

    v1

--- source/MRMesh/MRMeshTotalAngle.cpp
+++ source/MRMesh/MRMeshTotalAngle.cpp
@@ -31,7 +31,8 @@ float totalAngleIncreaseOnFlip( const MeshTopology & topology, const VertCoords 
 
     VertId a, b, c, d;
     topology.getLeftTriVerts( edge, a, c, d );
-    b = topology.dest( topology.prev( edge ) );
+    auto prevEdge = topology.prev( edge );
+    b = topology.dest( prevEdge );
 
     auto ap = points[a];
     auto bp = points[b];
@@ -39,6 +40,27 @@ float totalAngleIncreaseOnFlip( const MeshTopology & topology, const VertCoords 
     auto dp = points[d];
     
     float res = calcAngleLength( bp, cp, dp, ap ) - calcAngleLength( ap, bp, cp, dp );
+    if ( topology.right( prevEdge ) )
+    {
+        auto ep = points[topology.dest( topology.prev( prevEdge ) )];
+        res += calcAngleLength( ap, ep, bp, dp ) - calcAngleLength( ap, ep, bp, cp );
+    }
+    if ( auto e1 = topology.next( edge.sym() ); topology.left( e1 ) )
+    {
+        auto fp = points[topology.dest( topology.next( e1 ) )];
+        res += calcAngleLength( bp, fp, cp, dp ) - calcAngleLength( bp, fp, cp, ap );
+    }
+    if ( auto e2 = topology.prev( edge.sym() ); topology.right( e2 ) )
+    {
+        auto gp = points[topology.dest( topology.prev( e2 ) )];
+        res += calcAngleLength( cp, gp, dp, bp ) - calcAngleLength( cp, gp, dp, ap );
+    }
+    if ( auto e3 = topology.next( edge ); topology.left( e3 ) )
+    {
+        auto hp = points[topology.dest( topology.next( e3 ) )];
+        res += calcAngleLength( dp, hp, ap, bp ) - calcAngleLength( dp, hp, ap, cp );
+    }
+
     return res;
 }
 

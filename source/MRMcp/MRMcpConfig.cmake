c86744ed7b45fd7a4dacede8e8294050d0d30d30
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2026-04-16 10:48:30 -0500

    Wrong file.

--- source/MRMcp/MRMcpConfig.cmake
+++ source/MRMcp/MRMcpConfig.cmake
@@ -1,15 +1,5 @@
 include(CMakeFindDependencyMacro)
 
-if(NOT EMSCRIPTEN)
-  find_dependency(glfw3)
+IF(NOT MESHLIB_USE_VCPKG AND NOT APPLE)
+  find_dependency(fastmcpp)
 endif()
-
-# static builds require to find private dependencies
-if(EMSCRIPTEN)
-  find_dependency(Boost COMPONENTS locale)
-  find_dependency(glad)
-endif()
-
-include("${CMAKE_CURRENT_LIST_DIR}/MRViewerTargets.cmake")
-
-check_required_components(MRViewer)
--- source/MRViewer/MRViewerConfig.cmake
+++ source/MRViewer/MRViewerConfig.cmake
@@ -1,5 +1,15 @@
 include(CMakeFindDependencyMacro)
 
-IF(NOT MESHLIB_USE_VCPKG AND NOT APPLE)
-  find_dependency(fastmcpp)
+if(NOT EMSCRIPTEN)
+  find_dependency(glfw3)
 endif()
+
+# static builds require to find private dependencies
+if(EMSCRIPTEN)
+  find_dependency(Boost COMPONENTS locale)
+  find_dependency(glad)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/MRViewerTargets.cmake")
+
+check_required_components(MRViewer)

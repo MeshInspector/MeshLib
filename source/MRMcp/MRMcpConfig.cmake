8dc86d26f11b2a113077420cddc8e3380bb50920
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2026-04-16 10:29:01 -0500

    Again?

---
+++ source/MRMcp/MRMcpConfig.cmake
@@ -0,0 +1,15 @@
+include(CMakeFindDependencyMacro)
+
+if(NOT EMSCRIPTEN)
+  find_dependency(glfw3)
+endif()
+
+# static builds require to find private dependencies
+if(EMSCRIPTEN)
+  find_dependency(Boost COMPONENTS locale)
+  find_dependency(glad)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/MRViewerTargets.cmake")
+
+check_required_components(MRViewer)

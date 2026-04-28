d90e2c1a505372f4aa0044c2928d6c40319107a9
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2026-04-28 06:15:30 -0500

    Try to fix emscripten builds.

--- source/MRViewer/MRMcpSettings.cpp
+++ source/MRViewer/MRMcpSettings.cpp
@@ -13,29 +13,43 @@ static const std::string cEnableByDefault = "mcp.enableByDefault";
 
 int getPort()
 {
+    #ifndef MESHLIB_NO_MCP
     return Config::instance().getInt( cPort, 7887 );
+    #else
+    return -1;
+    #endif
 }
 
 void setPort( int port )
 {
+    #ifndef MESHLIB_NO_MCP
     Config::instance().setInt( cPort, port );
+    #endif
 }
 
 bool getEnableByDefault()
 {
+    #ifndef MESHLIB_NO_MCP
     return Config::instance().getBool( cEnableByDefault );
+    #else
+    return false;
+    #endif
 }
 
 void setEnableByDefault( bool enable )
 {
+    #ifndef MESHLIB_NO_MCP
     Config::instance().setBool( cEnableByDefault, enable );
+    #endif
 }
 
 void applyToServer()
 {
+    #ifndef MESHLIB_NO_MCP
     Mcp::Server::Params params = Mcp::getDefaultServer().getParams();
     params.port = getPort();
     Mcp::getDefaultServer().setParams( std::move( params ) );
+    #endif
 }
 
 } // namespace MR::McpSettings

a26e5e12195ce5740d15a42effd91b80e2761e88
Evgeny Gorbovskoy <evgeny.gorbovskoy@smiledirectclub.com>
2026-05-05 11:17:47 +0300

    update for support good text message from code == 403

--- source/MRViewer/MRWebRequest.cpp
+++ source/MRViewer/MRWebRequest.cpp
@@ -581,14 +581,16 @@ Expected<Json::Value> parseResponse( const Json::Value& response )
     if ( response["error"].isString() )
         mayBeError = response["error"].asString();
 
+    if ( code == 403 )
+        return unexpected( "Connection to " + response["url"].asString() + " is forbidden." );
+
     if ( ( code < 200 || code > 399 ) && !mayBeError.empty() )
         return MR::unexpected( mayBeError );
 
     if ( code == 0 )
         return MR::unexpected( "Bad internet connection." );
 
-    if ( response["code"].asInt() == 403 )
-        return unexpected( "Connection to " + response["url"].asString() + " is forbidden." );
+
     std::string text;
     if ( !response["text"].isString() )
         return unexpected( "Unknown error." );

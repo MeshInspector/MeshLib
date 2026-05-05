e5bb550c267ee08b15fbb40722b1b39d37dce447
gorbovskoy <gorbovskoy@gmail.com>
2026-05-05 16:55:39 +0300

    update web_request.js for use error field in case of error only

--- wasm/web_request.js
+++ wasm/web_request.js
@@ -139,7 +139,7 @@ var web_req_sync_download = function (url, outputPath, ctxId) {
         url: urlCpy,
         code: req.status,
         text: "",
-        error: req.statusText
+        error: (req.status < 200 || req.status > 399) ? req.statusText : null
     };
     Module.ccall('emsCallResponseCallback', 'number', ['string', 'bool', 'number'], [JSON.stringify(res), false, ctxId]);
 }

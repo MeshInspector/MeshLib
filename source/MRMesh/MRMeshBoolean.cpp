22d8974fbc8ee5c94db73da94019ae00248e92f0
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2026-05-21 12:04:17 +0300

    Move MRMeshBoolean gtests to MRTest project (#6132)
    * Move MRMeshBoolean gtests to MRTest project
    
    Move the two gtest cases from source/MRMesh/MRMeshBoolean.cpp
    (MeshBoolean, BooleanMultipleEdgePropogationSort) into a new file
    source/MRTest/MRMeshBooleanTests.cpp.
    
    Also port testBooleanMapper from source/MRTestC2/MRMeshBoolean.c
    (the only non-duplicate among the C tests) as a new C++ gtest
    "MRMesh, BooleanResultMapper" in the same file.
    
    Drop now-unused includes from MRMeshBoolean.cpp (MRGTest, MRTorus,
    MRCube, MRMatrix3).
    
    * Add MRMeshBooleanTests.cpp to MRTest.vcxproj

--- source/MRMesh/MRMeshBoolean.cpp
+++ source/MRMesh/MRMeshBoolean.cpp
@@ -5,17 +5,13 @@
 #include "MRIntersectionContour.h"
 #include "MRContoursCut.h"
 #include "MRTimer.h"
-#include "MRTorus.h"
-#include "MRMatrix3.h"
 #include "MRAffineXf3.h"
 #include "MRLog.h"
-#include "MRGTest.h"
 #include "MRFillContour.h"
 #include "MRPrecisePredicates3.h"
 #include "MRRegionBoundary.h"
 #include "MRMeshComponents.h"
 #include "MRMeshCollide.h"
-#include "MRCube.h"
 #include "MRMeshBuilder.h"
 #include "MRParallelFor.h"
 #include "MRBitSetParallelFor.h"
@@ -802,86 +798,6 @@ Expected<BooleanResultPoints> getBooleanPoints( const Mesh& meshA, const Mesh& m
     }
 
     return result;
-}
-
-TEST( MRMesh, MeshBoolean )
-{
-    Mesh meshA = makeTorus( 1.1f, 0.5f, 8, 8 );
-    Mesh meshB = makeTorus( 1.0f, 0.2f, 8, 8 );
-    meshB.transform( AffineXf3f::linear( Matrix3f::rotation( Vector3f::plusZ(), Vector3f::plusY() ) ) );
-
-    const float shiftStep = 0.2f;
-    const float angleStep = PI_F;/* *1.0f / 3.0f*/;
-    const std::array<Vector3f, 3> baseAxis{Vector3f::plusX(),Vector3f::plusY(),Vector3f::plusZ()};
-    for ( int maskTrans = 0; maskTrans < 8; ++maskTrans )
-    {
-        for ( int maskRot = 0; maskRot < 8; ++maskRot )
-        {
-            for ( float shift = 0.01f; shift < 0.2f; shift += shiftStep )
-            {
-                Vector3f shiftVec;
-                for ( int i = 0; i < 3; ++i )
-                    if ( maskTrans & ( 1 << i ) )
-                        shiftVec += shift * baseAxis[i];
-                for ( float angle = PI_F * 0.01f; angle < PI_F * 7.0f / 18.0f; angle += angleStep )
-                {
-                    Matrix3f rotation;
-                    for ( int i = 0; i < 3; ++i )
-                        if ( maskRot & ( 1 << i ) )
-                            rotation = Matrix3f::rotation( baseAxis[i], angle ) * rotation;
-
-                    AffineXf3f xf;
-                    xf = AffineXf3f::translation( shiftVec ) * AffineXf3f::linear( rotation );
-
-                    EXPECT_TRUE( boolean( meshA, meshB, BooleanOperation::Union, &xf ).valid() );
-                    EXPECT_TRUE( boolean( meshB, meshA, BooleanOperation::Intersection, &xf ).valid() );
-                }
-            }
-        }
-    }
-}
-
-
-TEST( MRMesh, BooleanMultipleEdgePropogationSort )
-{
-    Mesh meshA;
-    meshA.points = std::vector<Vector3f>
-    {
-        {0.0f,0.0f,0.0f},
-        {-0.5f,1.0f,0.0f},
-        {0.5f,1.0f,0.0f},
-        {0.0f,1.5f,0.5f},
-        {-1.0f,1.5f,0.0f},
-        {1.0f,1.5f,0.0f}
-    };
-    Triangulation tA =
-    {
-        { 0_v, 2_v, 1_v },
-        { 1_v, 2_v, 3_v },
-        { 3_v, 4_v, 1_v },
-        { 2_v, 5_v, 3_v },
-        { 3_v, 5_v, 4_v }
-    };
-    meshA.topology = MeshBuilder::fromTriangles( tA );
-    {
-        Mesh meshASup = meshA;
-        meshASup.points[3_v] = { 0.0f,1.5f,-0.5f };
-
-
-        auto border = trackRightBoundaryLoop( meshA.topology, meshA.topology.findHoleRepresentiveEdges()[0] );
-
-        meshA.addMeshPart( meshASup, true, { border }, { border } );
-    }
-
-    auto meshB = makeCube( Vector3f::diagonal( 2.0f ) );
-    meshB.transform( AffineXf3f::translation( Vector3f( -1.5f, -0.2f, -0.5f ) ) );
-
-
-    for ( int i = 0; i<int( BooleanOperation::Count ); ++i )
-    {
-        EXPECT_TRUE( boolean( meshA, meshB, BooleanOperation( i ) ).valid() );
-        EXPECT_TRUE( boolean( meshB, meshA, BooleanOperation( i ) ).valid() );
-    }
 }
 
 } //namespace MR

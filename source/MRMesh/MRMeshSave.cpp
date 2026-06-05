65b4261dfb934812a007a0381c62df1bc269258b
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2026-06-05 19:24:20 +0400

    Add `debugSaveMesh_` for diagnostics
    also fix issue in when bad holes was incorrectly filled with `fillContours2DPlan` in force boolean

--- source/MRMesh/MRMeshSave.cpp
+++ source/MRMesh/MRMeshSave.cpp
@@ -9,6 +9,7 @@
 #include "MRPch/MRFmt.h"
 #include "MRMeshTexture.h"
 #include "MRImageSave.h"
+#include "MRSerializer.h"
 
 namespace MR
 {
@@ -544,6 +545,24 @@ Expected<void> toAnySupportedFormat( const Mesh& mesh, const std::string& extens
         return unexpected( std::string( "unsupported stream extension" ) );
 
     return saver.streamSave( mesh, out, settings );
+}
+
+void debugSaveMesh_( void* mesh, void* faces, const char* path )
+{
+    using namespace MR;
+    auto pp = pathFromUtf8( path );
+    if ( utf8string( pp.extension() ) == ".mru" || faces )
+    {
+        // isolation is needed so it does not steal actuall work during debug saving
+        tbb::this_task_arena::isolate( [&] ()
+        {
+            ( void )serializeMesh( *( const Mesh* )mesh, pp, ( const FaceBitSet* )faces );
+        } );
+    }
+    else
+    {
+        ( void )toAnySupportedFormat( *( const Mesh* )mesh, pp );
+    }
 }
 
 MR_ADD_MESH_SAVER_WITH_PRIORITY( IOFilter( "MrMesh (.mrmesh)", "*.mrmesh" ), toMrmesh, {}, -1 )

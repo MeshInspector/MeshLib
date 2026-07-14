f8396a7695a50061f341566add02fc2664d00f85
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2026-07-14 05:14:30 -0500

    Some refactoring.

--- scripts/devops/docker_image_source_checksum.sh
+++ scripts/devops/docker_image_source_checksum.sh
@@ -33,7 +33,7 @@ generate=( scripts/mrbind/install_deps_ubuntu.sh scripts/mrbind/clang_version.tx
 case "${distro}" in
   ubuntu22|ubuntu24)
     files=( "docker/${distro}Dockerfile" "${common[@]}" "${ubuntu[@]}" ) ;;
-  emscripten|emscripten-build-c-bindings-*)
+  emscripten|emscripten-build-c-bindings)
     files=( "docker/${distro}Dockerfile" "${common[@]}" "${emscripten[@]}" ) ;;
   emscripten-generate-c-bindings)
     files=( "docker/${distro}Dockerfile" "${generate[@]}" ) ;;

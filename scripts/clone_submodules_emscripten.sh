b22bc11c491f2c89f2cf3852ccd45c2ee95ed861
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2026-07-27 05:19:26 -0500

    Again?

--- scripts/clone_submodules_emscripten.sh
+++ scripts/clone_submodules_emscripten.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 git -C "$(dirname "$BASH_SOURCE")"/.. submodule update --init --depth 1 \
-    thirdparty/eigen \
-    thirdparty/expected \
     thirdparty/imgui \
-    thirdparty/mrbind \
-    thirdparty/mrbind-pybind11 \
     thirdparty/parallel-hashmap \
+    thirdparty/mrbind-pybind11 \
+    thirdparty/mrbind \
 
 git -C "$(dirname "$BASH_SOURCE")"/../thirdparty/mrbind submodule update --init --depth 1 deps/cppdecl

e0f964e13ec907ba92f660d44195133b2c7ccb4a
Fedor Chelnokov <fedor.chelnokov@meshinspector.com>
2026-08-24 14:11:21 +0300

    Review: install_deps_ubuntu.sh follows the suggested order
    Common packages first, then the LLVM_PREFIX exit, then the apt.llvm.org
    repo check and the LLVM packages. Verified that the `&&`-list exit does
    not trip `set -e` when LLVM_PREFIX is unset.

--- scripts/mrbind/install_deps_ubuntu.sh
+++ scripts/mrbind/install_deps_ubuntu.sh
@@ -15,10 +15,18 @@ SCRIPT_DIR="$(realpath "$(dirname "$BASH_SOURCE")")"
 CLANG_VER="$(cat $SCRIPT_DIR/clang_version.txt | xargs)"
 [[ $CLANG_VER ]] || (echo "Not sure what version of Clang to use." && false)
 
+# Install the packages.
+# Could also add `sudo` here for `install_mrbind_ubuntu.sh`, but I think the user can do that themselves.
+# `procps` is for the `free` utility, to measure how much RAM we have.
+apt install -y make cmake ninja-build gawk procps
+
+# LLVM_PREFIX points at a Clang installation to use instead of the apt one.
+[[ -n "${LLVM_PREFIX:-}" ]] && exit 0
+
 # Add LLVM repositories if the required package is not accessible right now.
 # If it's accessible, either we have already added the same repos, or the version of Ubuntu is new enough to have it in the official repos.
 # `-s` means dry run (check if it's installable or not).
-if [[ -z "${LLVM_PREFIX:-}" ]] && ! apt-get install -s clang-$CLANG_VER >/dev/null 2>/dev/null; then
+if ! apt-get install -s clang-$CLANG_VER >/dev/null 2>/dev/null; then
     # This is what `llvm.sh` needs (search `apt install` in it and see for yourself).
     apt install -y lsb-release wget software-properties-common gnupg
     # Download `llvm.sh`.
@@ -30,13 +38,5 @@ if [[ -z "${LLVM_PREFIX:-}" ]] && ! apt-get install -s clang-$CLANG_VER >/dev/nu
     popd
     rm -rf "$DIR"
 fi
-
-# Install the packages.
-# Could also add `sudo` here for `install_mrbind_ubuntu.sh`, but I think the user can do that themselves.
-# `procps` is for the `free` utility, to measure how much RAM we have.
-apt install -y make cmake ninja-build gawk procps
-
-# LLVM_PREFIX points at a Clang installation to use instead of the apt one.
-[[ -z "${LLVM_PREFIX:-}" ]] || exit 0
 
 apt install -y clang-$CLANG_VER lld-$CLANG_VER clang-tools-$CLANG_VER libclang-$CLANG_VER-dev llvm-$CLANG_VER-dev

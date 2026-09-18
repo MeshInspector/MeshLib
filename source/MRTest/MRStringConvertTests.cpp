68d9ce0da8f74d4e5fc996a358233959e96b11e0
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2026-09-18 11:38:33 +0200

    Expand unit tests

--- source/MRTest/MRStringConvertTests.cpp
+++ source/MRTest/MRStringConvertTests.cpp
@@ -21,6 +21,9 @@ TEST( MRMesh, Utf8Conversions )
     EXPECT_TRUE( utf8ToWide( "" ).empty() );
     EXPECT_TRUE( wideToUtf8( nullptr ).empty() );
     EXPECT_TRUE( wideToUtf8( L"" ).empty() );
+
+    EXPECT_EQ( utf32ToUtf8( std::u32string{ U'\\xD800' } ), "\\xEF\\xBF\\xBD" );
+    EXPECT_EQ( utf32ToUtf8( std::u32string{ char32_t( 0x110000 ) } ), "\\xEF\\xBF\\xBD" );
 }
 
 TEST( MRMesh, Utf8Substr )

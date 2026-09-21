b783fbf1a4f1ecea9412a88e3cab13ad081cb6a0
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2026-09-21 11:20:49 +0200

    Rewrite regexps

--- scripts/build_thirdparty.sh
+++ scripts/build_thirdparty.sh
@@ -110,8 +110,8 @@ if [ "${MR_EMSCRIPTEN}" == "ON" ]; then
     CXXFLAGS="${CFLAGS} -pthread"
   fi
   if [[ ${MR_EMSCRIPTEN_WASM64} == 1 ]] ; then
-    EM_VERSION=$("${EMSCRIPTEN_ROOT}/emcc" -v 2>&1 | sed -n "s/^emcc (.*) \([0-9][0-9.]*\).*/\1/p" | head -1)
-    if [ "$(printf '%s\n5.0.7\n' "${EM_VERSION:-0}" | sort -V | head -1)" = "5.0.7" ] ; then
+    EMSCRIPTEN_VERSION=$("${EMSCRIPTEN_ROOT}/emcc" -v 2>&1 | sed -nE 's/^emcc \(.*\) ([0-9.]+).*/\1/p' | head -1)
+    if [ "$(printf '%s\n5.0.7\n' "${EMSCRIPTEN_VERSION:-0}" | sort -V | head -1)" = "5.0.7" ] ; then
       WASM64_FLAG="-m64"
     else
       WASM64_FLAG="-s MEMORY64=1"

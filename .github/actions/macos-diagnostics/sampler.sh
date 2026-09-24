9e11d507e7a8058f2a9167a16cc991d4ee7f92a8
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2026-09-24 09:04:35 +0200

    Revert "WIP: Add more diagnostics"
    This reverts commit 7f0152ce81166b6266e6629ca37d6d002e26735a.

--- .github/actions/macos-diagnostics/sampler.sh
+++ .github/actions/macos-diagnostics/sampler.sh
@@ -8,6 +8,6 @@ while :; do
   echo "-- load averages --"
   uptime
   echo "-- top processes by CPU --"
-  top -l 2 -o cpu -n 8 -stats pid,cpu,command | awk '/^Processes:/ { n++ } n == 2'
+  ps -Ao %cpu,comm -r | head -8
   sleep 10
 done
